3526195065f822de712bff3735bbd2d87b866b78
camilo tapia <camilo.tapia@gmail.com>
2012-01-03 23:20:15 +0100

    Renamed "drag" to "dragAndDrop"
    Former-commit-id: 6929a45aed68a3f091830c6ff55a18ef5b300def

--- develop/develop.draganddrop.js
+++ develop/develop.draganddrop.js
@@ -3,7 +3,7 @@ var client = require("webdriverjs").remote();
 client
     .init()
     .url("http://wdjstest.local/")
-    .drag("#item1", "#dropContainer", function(result)
+    .dragAndDrop("#item1", "#dropContainer", function(result)
         {
             console.log("DROPPED");
         })
