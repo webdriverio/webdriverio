b51ff9a7322a90cea694cb06974bdb0aeae49412
camilo tapia <camilo.tapia@gmail.com>
2012-01-03 23:20:15 +0100

    Renamed "drag" to "dragAndDrop"

--- develop/tests/test.dragAndDrop.js
+++ develop/tests/test.dragAndDrop.js
@@ -4,7 +4,7 @@ var client = require("webdriverjs").remote();
 client
     .init()
     .url("http://localhost/projects/webdriverjs-testsite")
-    .drag("#item1", "#dropContainer")
+    .dragAndDrop("#item1", "#dropContainer")
     .getText("#dropContainer #item1", function(result) {
         assert.equal(result.value, "item 1");
     })
