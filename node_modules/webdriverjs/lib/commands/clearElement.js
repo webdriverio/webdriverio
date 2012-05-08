cccc648b86040f839be16185d83c997b84441f3e
Dan Jenkins <dan.jenkins88@gmail.com>
2012-05-08 09:32:36 +0100

    added new protocol method for clearing down an input so you can then type into it as fresh data, added a command clearElement to be able to use it

---
+++ node_modules/webdriverjs/lib/commands/clearElement.js
@@ -0,0 +1,28 @@
+exports.command = function(cssSelector, callback) 
+{
+	var self = this;
+	self.element("css selector", cssSelector,
+		function(result)
+		{
+			if (result.status == 0)
+			{
+				self.elementIdClear(result.value.ELEMENT,
+					function(result)
+					{
+						if (typeof callback === "function")
+						{
+							callback(result);
+						}
+					}
+				);
+			}
+			else
+			{
+				if (typeof callback === "function")
+				{
+					callback(result);
+				}
+			}
+		}
+	);
+};
