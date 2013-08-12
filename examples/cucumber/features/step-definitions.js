f3ce7756d0225af09d097b25f1cf507944c83109
christian-bromann <contact@christian-bromann.com>
2013-08-12 22:43:00 +0200

    added BDD cucumber test example
    Former-commit-id: 7b26aa369f297165aad827f4369e42d5633d2057

---
+++ examples/cucumber/features/step-definitions.js
@@ -0,0 +1,53 @@
+var webdriverjs = require('../../../index'),
+    assert      = require('assert');
+
+var sharedSteps = module.exports = function(){
+
+    var client    = webdriverjs.remote({ desiredCapabilities: {browserName: 'phantomjs'}, logLevel: 'silent' }),
+        tmpResult = null;
+    client.init();
+
+    this.Given(/^I go on the website "([^"]*)"$/, function(url, next) {
+        client
+            .url(url)
+            .call(next);
+    });
+
+    this.When(/^I use getElementSize\(\) on the element "([^"]*)"$/, function(className, next) {
+        client
+            .getElementSize(className, function(err, result) {
+                assert(err === null, 'command getElementSize() returns with an error');
+                tmpResult = result;
+                next();
+            });
+    });
+
+    this.When(/^I use getTitle\(\) to get the title of this website$/, function(next) {
+        client
+            .getTitle(function(err, title) {
+                assert(err === null, 'command getTitle() returns with an error');
+                tmpResult = title;
+                next();
+            });
+    });
+
+    this.When(/^I use getElementCssProperty\(\) to get the "([^"]*)" attribute of an element with "([^"]*)" "([^"]*)"$/, function(attribute, findBy, cssSelector, next) {
+        client
+            .getElementCssProperty(findBy, cssSelector, attribute, function(err, result) {
+                assert(err === null, 'command getElementCssProperty() returns with an error');
+                tmpResult = result;
+                next();
+            });
+    });
+
+    this.Then(/^I should get a width of "([^"]*)" and height of "([^"]*)"$/, function(width, height, next) {
+        assert(tmpResult.width  == width , 'width of element is ' + tmpResult.width + ' but should be ' + width);
+        assert(tmpResult.height == height, 'height of element is ' + tmpResult.width + ' but should be ' + height);
+        next();
+    });
+
+    this.Then(/^the command should return "([^"]*)"$/, function(result, next) {
+        assert(tmpResult == result , ' result of command is "'+ tmpResult + '" but should be "'+ result);
+        next();
+    });
+};
