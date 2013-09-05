3cb03d380f09e8044229e32ac2ad9f7da2a9a0c5
christian-bromann <contact@christian-bromann.com>
2013-09-06 00:43:36 +0200

    added more tests using modifier keys - refs issue #84
    Former-commit-id: 0698115473c94ba579551a3832e7a79661f5f90f

--- test/commands-test.js
+++ test/commands-test.js
@@ -754,6 +754,116 @@ describe('webdriverjs API test', function(){
                     })
                     .call(done);
             });
+
+            it('input should contains contain special characters created by modifier keys (Shift|Alt)', function(done) {
+                client
+                    .url(testpageURL)
+                    // to release the modifier a 'NULL' has to encounter, if not it should be held down
+                    .setValue('input.searchinput',['Shift','1','NULL','Shift','2','3','4','NULL','Shift','5','6','NULL','Shift','7','NULL','Shift','8','9','0'], function(err){
+                        expect(err).to.be.null;
+                    })
+                    .getValue('input.searchinput', function(err,res) {
+                        expect(err).to.be.null;
+                        // expect sepcial character according to american keyboard layout
+                        assert.strictEqual(res,'!@#$%^&*()');
+                    })
+                    .call(done);
+            });
+
+            it('should add number characters via numpad', function(done) {
+                client
+                    .url(testpageURL)
+                    .setValue('.searchinput',['Numpad 0','Numpad 1','Numpad 2','Numpad 3','Numpad 4','Numpad 5','Numpad 6','Numpad 7','Numpad 8','Numpad 9'], function(err) {
+                        expect(err).to.be.null;
+                    })
+                    .getValue('.searchinput', function(err,res) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(res,'0123456789');
+                    })
+                    .call(done);
+            });
+
+            it('it should cut&paste a text via Control + x and Control + v', function(done) {
+                var text = 'test';
+
+                client
+                    .url(testpageURL)
+                    // first set some text
+                    .setValue('.searchinput',text,function(err) {
+                        expect(err).to.be.null;
+                    })
+                    // mark text via shift + left arrow
+                    .addValue('.searchinput',['Shift','Left arrow','Left arrow','Left arrow','Left arrow','NULL'], function(err) {
+                        expect(err).to.be.null;
+                    })
+                    // cut text
+                    .addValue('.searchinput',['Control','x','NULL'],function(err) {
+                        expect(err).to.be.null;
+                    })
+                    // test: input field should be empty
+                    .getValue('.searchinput',function(err,res) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(res,'');
+                    })
+                    // paste value from clipboard
+                    .addValue('.searchinput',['Control','v'],function(err) {
+                        expect(err).to.be.null;
+                    })
+                    .getValue('.searchinput',function(err,res) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(res,text);
+                    })
+                    .call(done);
+            });
+
+            it('it should copy&paste a text via Control + c and Control + v', function(done) {
+                var text = 'test';
+
+                client
+                    .url(testpageURL)
+                    // first set some text
+                    .setValue('.searchinput',text,function(err) {
+                        expect(err).to.be.null;
+                    })
+                    // mark text via shift + left arrow
+                    .addValue('.searchinput',['Shift','Left arrow','Left arrow','Left arrow','Left arrow','NULL'], function(err) {
+                        expect(err).to.be.null;
+                    })
+                    // copy text and move cursor to the end of the input field
+                    .addValue('.searchinput',['Control','c','NULL','Right arrow'],function(err) {
+                        expect(err).to.be.null;
+                    })
+                    // test: input field should contain test value
+                    .getValue('.searchinput',function(err,res) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(res,text);
+                    })
+                    // paste value from clipboard
+                    .addValue('.searchinput',['Control','v'],function(err) {
+                        expect(err).to.be.null;
+                    })
+                    .getValue('.searchinput',function(err,res) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(res,text + text);
+                    })
+                    .call(done);
+            });
+
+            it('a new addValue command should release the modifier key', function(done) {
+                client
+                    .url(testpageURL)
+                    .addValue('.searchinput',['Shift','1'],function(err) {
+                        expect(err).to.be.null;
+                    })
+                    .addValue('.searchinput',['1'],function(err) {
+                        expect(err).to.be.null;
+                    })
+                    .getValue('.searchinput',function(err,res) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(res,'!1');
+                    })
+                    .call(done);
+            });
         });
 
     });
