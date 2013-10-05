b00584c2aac4b7e72181d9faf94fb93fa1b1d310
christian-bromann <contact@christian-bromann.com>
2013-10-05 16:48:54 +0200

    added tests for tab handling

--- test/commands-test.js
+++ test/commands-test.js
@@ -990,6 +990,80 @@ describe('webdriverjs API test', function(){
 
         });
 
+        describe('should work with window commands proberly', function() {
+
+            var tabs = null;
+
+            it('should open two tabs', function(done) {
+
+                var self = client;
+
+                client
+                    .url(testpageURL)
+                    .click('#newWindow', function(err){
+                        expect(err).to.be.null;
+                    })
+                    .url('http://google.com')
+                    .getTabIds(function(err,res) {
+                        expect(err).to.be.null;
+                        res.should.have.length(2);
+                        tabs = res;
+                    })
+                    .call(done);
+            });
+
+            it('should return title ("Google") of the current tab', function(done) {
+                client
+                    .getTitle(function(err,title) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(title,'Google');
+                    })
+                    .getCurrentTabId(function(err,res) {
+                        expect(err).to.be.null;
+                        assert(res,tabs[0]);
+                    })
+                    .call(done);
+            });
+
+            it('should return title of testpage after switching tab', function(done) {
+                client
+                    .switchTab(tabs[1], function(err,res) {
+                        expect(err).to.be.null;
+                    })
+                    .getTitle(function(err,title) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(title,testpageTitle);
+                    })
+                    .getCurrentTabId(function(err,res) {
+                        expect(err).to.be.null;
+                        assert(res,tabs[1]);
+                    })
+                    .call(done);
+            });
+
+            it('should change the tab after closing one by passing a tab id to the command', function(done) {
+                client
+                    .pause(1) // problem with command chain here
+                    .close(tabs[0],function(err,res) {
+                        expect(err).to.be.null;
+                    })
+                    .getTabIds(function(err,res) {
+                        expect(err).to.be.null;
+                        res.should.have.length(1);
+                    })
+                    .getCurrentTabId(function(err,res) {
+                        expect(err).to.be.null;
+                        assert(res,tabs[0]);
+                    })
+                    .getTitle(function(err,title) {
+                        expect(err).to.be.null;
+                        assert.strictEqual(title,'Google');
+                    })
+                    .call(done);
+            });
+
+        });
+
     });
 
     after(function(done) {
