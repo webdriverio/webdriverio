3c3356f1039052e0022cdda475a9f030fbef3256
vvo <vincent.voyer@gmail.com>
2013-12-05 20:16:09 +0100

    update travis yml
    update travis key
    
    travis tryfix
    
    travis fix
    
    fix travis
    
    fix travis
    
    fix travis
    
    
    Former-commit-id: 35d49c4cedbbaba2616a8aee77c4ac0630d66e38

--- test/conf/travis-ci.js
+++ test/conf/travis-ci.js
@@ -3,8 +3,9 @@ module.exports = {
         "url": 'http://127.0.0.1:8080/site/index.html',
         "url2": 'http://127.0.0.1:8080/site/two.html',
     },
-    "host": "localhost",
-    "port": 4445,
+    "host": "ondemand.saucelabs.com",
+    "port": 80,
+    "logLevel": "silent",
     "desiredCapabilities" : {
         "browserName": "chrome",
         "version": "31",
