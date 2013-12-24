3939a4cbb5a659d138da5e6d4123867eb034a427
christian-bromann <contact@christian-bromann.com>
2013-12-24 14:48:21 +0100

    improved test automation
    Former-commit-id: 2818689f3822781e8cf1d712c90d546efbcdca91

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -11,10 +11,20 @@ h = {
         return function(err, result) {
             h.noError(err);
             assert.strictEqual(result, expected);
-        }
+        };
     },
     setup: (function() {
         var client;
+
+        /**
+         * Only execute cross browser tests on changes within master branch.
+         * For tests caused by pull requests PhantomJS tests are sufficient.
+         */
+        if((process.env.TRAVIS_BRANCH === 'master' && process.env._BROWSER === 'phantomjs') ||
+           (process.env.TRAVIS_BRANCH !== 'master' && process.env._BROWSER !== 'phantomjs')) {
+            console.log('This test was skipped');
+            process.exit(0);
+        }
 
         return function(done) {
             var wdjs = require('../index.js');
@@ -26,6 +36,6 @@ h = {
             }
 
             this.client.url(conf.testPage.url, done);
-        }
+        };
     })()
-}
+};
--- test/conf/travis-ci.js
+++ test/conf/travis-ci.js
@@ -1,7 +1,7 @@
 module.exports = {
     "testPage": {
-        "url": 'http://127.0.0.1:8080/site/index.html',
-        "url2": 'http://127.0.0.1:8080/site/two.html',
+        "url": 'http://127.0.0.1:8080/test/site/index.html',
+        "url2": 'http://127.0.0.1:8080/test/site/two.html',
     },
     "host": "ondemand.saucelabs.com",
     "port": 80,
