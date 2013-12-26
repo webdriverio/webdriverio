d8392f3564c3950e59b26412c54c48faf47cfe97
ysono <yoshiaki.sono@gmail.com>
2013-12-26 17:00:38 -0500

    now the protocols are buttonDown, buttonUp, and buttonPress; and the single command is click. buttonClick alias is kept in for backward compatibility.

--- README.md
+++ README.md
@@ -152,8 +152,7 @@ can assert values or have more logic when the callback is called.
 - **addValue(`String` css selector, `String|String[]` value, `Function` callback)**<br>adds a value to an object found by a css selector. You can also use unicode characters like `Left arrow` or `Back space`. You'll find all supported characters [here](https://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/element/:id/value). To do that, the value has to correspond to a key from the table.
 - **call(callback)**<br>call given function in async order of current command queue
 - **clearElement(`String` css selector, `Function` callback)**<br>clear an element of text
-- **click(`String` css selector, `Function` callback)**<br>Clicks on an element based on a css selector
-- **clickMouseButton(`String` css selector, `String` button, `Function` callback)**<br>Clicks a mouse button on a location determined by the css selector. button is optional and is one of [left, middle, right].
+- **click(`String` css selector, `String` button, `Function` callback)**<br>Clicks on an element based on a css selector. `String` button is optional and is one of [left, middle, right].
 - **close([`String` tab ID to focus on,] `Function` callback)**<br>Close the current window (optional: and switch focus to opended tab)
 - **deleteCookie(`String` name, `Function` callback)**<br>Delete a cookie for current page.
 - **doubleClick(`String` css selector, `Function` callback)**<br>Clicks on an element based on a css selector
@@ -195,7 +194,7 @@ Here are the implemented bindings (and links to the official json protocol bindi
 - [alertDismiss](https://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/dismiss_alert)
 - [alertText](https://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/alert_text)
 - [back](http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/back)
-- [buttonClick](http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/click)
+- [buttonPress](http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/click)
 - [buttonDown](http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/buttondown)
 - [buttonUp](http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/buttonup)
 - [cookie](http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/cookie)
--- test/spec/click.js
+++ test/spec/click.js
@@ -1,73 +1,143 @@
 describe('click command test',function() {
-    before(h.setup);
 
-    it('text should be visible after click on .btn1', function(done){
-        this.client
-            .isVisible('.btn1',function(err,result) {
-                assert.equal(null, err);
-                assert.ok(result);
-            })
-            .click('.btn1',function(err,result) {
-                assert.equal(null, err);
-                assert.equal(0, result.status);
-            })
-            .isVisible('.btn1_clicked',function(err,result){
-                assert.equal(null, err)
-                assert(result, '.btn1 was clicked');
-            })
-            .call(done);
+    var testClickMethodAlias = function(clickMethodName) {
+
+        var clickAndConfirm = function(params) {
+            var checkClickResult = function(err, result) {
+                assert.equal(err, null);
+                assert.equal(result.status, 0);
+            };
+
+            params.client
+                .isVisible(params.btnClass, function(err,result) {
+                    assert.equal(err, null);
+                    assert.ok(result);
+                })
+                .isVisible(params.confirmationClass, function(err, result) {
+                    assert.equal(err, null);
+                    assert.ok(!result, 'confirmation must be invisible as a precondition');
+                });
+
+            if(params.buttonSpecified) {
+                params.client[clickMethodName](params.btnClass, params.button, checkClickResult);
+            } else {
+                params.client[clickMethodName](params.btnClass, checkClickResult);
+            }
+
+            params.client
+                .isVisible(params.confirmationClass, function(err, result) {
+                    assert.equal(err, null);
+                    assert.equal(result, params.expectConfirmationVisible,
+                        'expectation failed on ' + params.btnClass + ' being clicked');
+                });
+        };
+
+        beforeEach(h.setup);
+
+        it('text should be visible after click on .btn1', function(done){
+            clickAndConfirm({
+                client: this.client,
+                btnClass: '.btn1',
+                confirmationClass: '.btn1_clicked',
+                expectConfirmationVisible: true,
+            });
+            this.client.call(done);
+        });
+
+        it('text should NOT be visible after click on .btn2 because button is disabled', function(done){
+            clickAndConfirm({
+                client: this.client,
+                btnClass: '.btn2',
+                confirmationClass: '.btn2_clicked',
+                expectConfirmationVisible: false,
+            });
+            this.client.call(done);
+        });
+
+        it('text should be visible after click on .btn3 altought it is behind an overlay', function(done){
+            clickAndConfirm({
+                client: this.client,
+                btnClass: '.btn3',
+                confirmationClass: '.btn3_clicked',
+                expectConfirmationVisible: true,
+            });
+            this.client.call(done);
+        });
+
+
+        it('text should be visible after clicking on .btn4 1px/1px width/height', function(done){
+            clickAndConfirm({
+                client: this.client,
+                btnClass: '.btn4',
+                confirmationClass: '.btn4_clicked',
+                expectConfirmationVisible: true,
+            });
+            this.client.call(done);
+        });
+
+        describe('/session/:sessionId/click protocol', function() {
+
+            it('text should be visible after click on .btn1 with left button', function(done) {
+                clickAndConfirm({
+                    client: this.client,
+                    btnClass: '.btn1',
+                    confirmationClass: '.btn1_clicked',
+                    buttonSpecified: true,
+                    button: 'left',
+                    expectConfirmationVisible: true,
+                });
+                this.client.call(done);
+            });
+
+            it('text should be visible after click on .btn1 with undefined button', function(done) {
+                clickAndConfirm({
+                    client: this.client,
+                    btnClass: '.btn1',
+                    confirmationClass: '.btn1_clicked',
+                    buttonSpecified: true,
+                    button: undefined,
+                    expectConfirmationVisible: true,
+                });
+                this.client.call(done);
+            });
+
+            // note this fails with most browsers: middle click becomes left click.
+            it.skip('text should be visible after middle-clicking on .btn1', function(done) {
+                clickAndConfirm({
+                    client: this.client,
+                    btnClass: '.btn1',
+                    confirmationClass: '.btn1_middle_clicked',
+                    buttonSpecified: true,
+                    button: 'middle',
+                    expectConfirmationVisible: true,
+                });
+                this.client.call(done);
+            });
+
+            it('text should be visible after right-clicking on .btn1', function(done) {
+                clickAndConfirm({
+                    client: this.client,
+                    btnClass: '.btn1',
+                    confirmationClass: '.btn1_right_clicked',
+                    buttonSpecified: true,
+                    button: 'right',
+                    expectConfirmationVisible: true,
+                });
+                this.client
+                    // For some reason, in Chrome, right clicking again makes the context menu disappear.
+                    // This is only for cosmetics; letting the menu remain doesn't affect the rest of the tests.
+                    .buttonPress('right')
+                    .call(done);
+            });
+        });
+    };
+
+    describe('command `click`', function() {
+        testClickMethodAlias.call(this, 'click');
     });
 
-    it('text should NOT be visible after click on .btn2 because button is disabled', function(done){
-        this.client
-            .isVisible('.btn2',function(err,result) {
-                assert.equal(null, err);
-                assert.ok(result);
-            })
-            .click('.btn2',function(err,result) {
-                assert.equal(null, err);
-                assert.equal(0, result.status);
-            })
-            .isVisible('.btn2_clicked',function(err,result){
-                assert.equal(null, err)
-                assert(!result, '.btn2 was not clicked');
-            })
-            .call(done);
-    });
-
-    it('text should be visible after click on .btn3 altought it is behind an overlay', function(done){
-        this.client
-            .isVisible('.btn3',function(err,result) {
-                assert.equal(null, err);
-                assert.ok(result);
-            })
-            .click('.btn3',function(err,result) {
-                assert.equal(null, err);
-                assert.equal(0, result.status);
-            })
-            .isVisible('.btn3_clicked',function(err,result){
-                assert.equal(null, err);
-                assert(result, '.btn3 was clicked');
-            })
-            .call(done);
-    });
-
-
-    it('text should be visible after clicking on .btn4 1px/1px width/height', function(done){
-        this.client
-            .isVisible('.btn4',function(err,result) {
-                assert.equal(null, err);
-                assert.ok(result);
-            })
-            .click('.btn4',function(err,result) {
-                assert.equal(null, err);
-                assert.equal(0, result.status);
-            })
-            .isVisible('.btn4_clicked',function(err,result){
-                assert.equal(null, err);
-                assert(result, '.btn4 was clicked');
-            })
-            .call(done);
+    describe('legacy command `buttonClick`', function() {
+        testClickMethodAlias.call(this, 'buttonClick');
     });
 
 });
