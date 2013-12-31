312e75b7c46b3fffcc27e5292f382075cde82d85
ysono <yoshiaki.sono@gmail.com>
2013-12-31 16:06:53 -0500

    Reverted basically all changes to click command. Implemented leftClick, middleClick, and rightClick.
    Former-commit-id: bfa3fa5b7ca93c39f5225d004c00cfb06dbad549

--- test/spec/click.js
+++ test/spec/click.js
@@ -1,42 +1,33 @@
-describe('click command test',function() {
+var clickAndConfirm = function(params) {
+    params.client
+        .isVisible(params.btnClass, function(err,result) {
+            assert.equal(err, null);
+            assert.ok(result);
+        })
+        .isVisible(params.confirmationClass, function(err, result) {
+            assert.equal(err, null);
+            assert.ok(!result, 'confirmation must be invisible as a precondition');
+        })
+        [params.clickMethodName](params.btnClass, function(err, result) {
+            assert.equal(err, null);
+            assert.equal(result.status, 0);
+        })
+        .isVisible(params.confirmationClass, function(err, result) {
+            assert.equal(err, null);
+            assert.equal(result, params.expectConfirmationVisible,
+                'expectation failed on ' + params.btnClass + ' being clicked');
+        });
+};
 
-    var testClickMethodAlias = function(clickMethodName) {
 
-        var clickAndConfirm = function(params) {
-            var checkClickResult = function(err, result) {
-                assert.equal(err, null);
-                assert.equal(result.status, 0);
-            };
+describe('left click commands',function() {
 
-            params.client
-                .isVisible(params.btnClass, function(err,result) {
-                    assert.equal(err, null);
-                    assert.ok(result);
-                })
-                .isVisible(params.confirmationClass, function(err, result) {
-                    assert.equal(err, null);
-                    assert.ok(!result, 'confirmation must be invisible as a precondition');
-                });
-
-            if(params.buttonSpecified) {
-                params.client[clickMethodName](params.btnClass, params.button, checkClickResult);
-            } else {
-                params.client[clickMethodName](params.btnClass, checkClickResult);
-            }
-
-            params.client
-                .isVisible(params.confirmationClass, function(err, result) {
-                    assert.equal(err, null);
-                    assert.equal(result, params.expectConfirmationVisible,
-                        'expectation failed on ' + params.btnClass + ' being clicked');
-                });
-        };
-
-        beforeEach(h.setup);
+    var testMouseClick = function(clickMethodName) {
 
         it('text should be visible after click on .btn1', function(done){
             clickAndConfirm({
                 client: this.client,
+                clickMethodName: clickMethodName,
                 btnClass: '.btn1',
                 confirmationClass: '.btn1_clicked',
                 expectConfirmationVisible: true,
@@ -47,19 +38,10 @@ describe('click command test',function() {
         it('text should NOT be visible after click on .btn2 because button is disabled', function(done){
             clickAndConfirm({
                 client: this.client,
+                clickMethodName: clickMethodName,
                 btnClass: '.btn2',
                 confirmationClass: '.btn2_clicked',
                 expectConfirmationVisible: false,
-            });
-            this.client.call(done);
-        });
-
-        it('text should be visible after click on .btn3 altought it is behind an overlay', function(done){
-            clickAndConfirm({
-                client: this.client,
-                btnClass: '.btn3',
-                confirmationClass: '.btn3_clicked',
-                expectConfirmationVisible: true,
             });
             this.client.call(done);
         });
@@ -68,6 +50,7 @@ describe('click command test',function() {
         it('text should be visible after clicking on .btn4 1px/1px width/height', function(done){
             clickAndConfirm({
                 client: this.client,
+                clickMethodName: clickMethodName,
                 btnClass: '.btn4',
                 confirmationClass: '.btn4_clicked',
                 expectConfirmationVisible: true,
@@ -75,65 +58,64 @@ describe('click command test',function() {
             this.client.call(done);
         });
 
-        describe('/session/:sessionId/click protocol', function() {
+    };
 
-            it('text should be visible after click on .btn1 with left button', function(done) {
-                clickAndConfirm({
-                    client: this.client,
-                    btnClass: '.btn1',
-                    confirmationClass: '.btn1_clicked',
-                    buttonSpecified: true,
-                    button: 'left',
-                    expectConfirmationVisible: true,
-                });
-                this.client.call(done);
+    var testEventClick = function(clickMethodName) {
+        it('text should be visible after click on .btn3 altought it is behind an overlay', function(done){
+            clickAndConfirm({
+                client: this.client,
+                clickMethodName: clickMethodName,
+                btnClass: '.btn3',
+                confirmationClass: '.btn3_clicked',
+                expectConfirmationVisible: true,
             });
-
-            it('text should be visible after click on .btn1 with undefined button', function(done) {
-                clickAndConfirm({
-                    client: this.client,
-                    btnClass: '.btn1',
-                    confirmationClass: '.btn1_clicked',
-                    buttonSpecified: true,
-                    button: undefined,
-                    expectConfirmationVisible: true,
-                });
-                this.client.call(done);
-            });
-
-            // note this fails with most browsers: middle click becomes left click.
-            it.skip('text should be visible after middle-clicking on .btn1', function(done) {
-                clickAndConfirm({
-                    client: this.client,
-                    btnClass: '.btn1',
-                    confirmationClass: '.btn1_middle_clicked',
-                    buttonSpecified: true,
-                    button: 'middle',
-                    expectConfirmationVisible: true,
-                });
-                this.client.call(done);
-            });
-
-            it('text should be visible after right-clicking on .btn1', function(done) {
-                clickAndConfirm({
-                    client: this.client,
-                    btnClass: '.btn1',
-                    confirmationClass: '.btn1_right_clicked',
-                    buttonSpecified: true,
-                    button: 'right',
-                    expectConfirmationVisible: true,
-                });
-                this.client.call(done);
-            });
+            this.client.call(done);
         });
     };
 
-    describe('command `click`', function() {
-        testClickMethodAlias.call(this, 'click');
+    beforeEach(h.setup);
+
+    describe('mouse button click', function() {
+        ['click', 'buttonClick', 'leftClick'].forEach(function(clickMethodName) {
+            describe('`' + clickMethodName + '`', function() {
+                testMouseClick(clickMethodName);
+            });
+        });
     });
 
-    describe('legacy command `buttonClick`', function() {
-        testClickMethodAlias.call(this, 'buttonClick');
+    describe('click command can directly invoke click event', function() {
+        ['click', 'buttonClick'].forEach(function(clickMethodName) {
+            describe('`' + clickMethodName + '`', function() {
+                testEventClick(clickMethodName);
+            });
+        });
     });
 
+});
+
+describe('non-left clicks', function() {
+    before(h.setup);
+
+    // note this fails with most browsers: middle click becomes left click.
+    it.skip('text should be visible after middle-clicking on .btn1', function(done) {
+        clickAndConfirm({
+            client: this.client,
+            clickMethodName: 'middleClick',
+            btnClass: '.btn1',
+            confirmationClass: '.btn1_middle_clicked',
+            expectConfirmationVisible: true,
+        });
+        this.client.call(done);
+    });
+
+    it('text should be visible after right-clicking on .btn1', function(done) {
+        clickAndConfirm({
+            client: this.client,
+            clickMethodName: 'rightClick',
+            btnClass: '.btn1',
+            confirmationClass: '.btn1_right_clicked',
+            expectConfirmationVisible: true,
+        });
+        this.client.call(done);
+    });
 });
