928141c30ae79a79f5e3293685c30e866e7ed34d
christian-bromann <contact@christian-bromann.com>
2013-12-31 16:00:52 +0100

    added scroll command to cover scrolling for browser and native mobile applications, added scroll test
    Former-commit-id: 32424af2b38156982c0261ce0c6bbec2c6b7a8d5

--- test/spec/mobile.js
+++ test/spec/mobile.js
@@ -1,13 +1,12 @@
 describe.only('mobile tests', function() {
 
-	before(h.setup);
+    before(h.setup);
 
-	it('should get current orientation', function(done) {
+    it('should get current orientation', function(done) {
 
-		this.client
+        this.client
             .getOrientation(function(err,res) {
                 assert.equal(null, err);
-                console.log(res);
                 assert.equal(res, 'PORTRAIT');
             })
             .setOrientation('landscape',function(err,res) {
@@ -19,6 +18,26 @@ describe.only('mobile tests', function() {
             })
             .call(done);
 
-	});
+    });
+
+    it('should scroll to specified X and Y position', function(done) {
+
+        this.client
+            .isVisible('//window[1]/scrollview[1]/webview[1]/text[1]', function(err,res) {
+                assert.equal(null, err);
+                // headline should be visible on app start
+                assert.equal(res, true);
+            })
+            .scroll('//window[1]/scrollview[1]/webview[1]/textfield[5]',10,100,function(err) {
+                assert.equal(null, err);
+            })
+            .isVisible('//window[1]/scrollview[1]/webview[1]/text[1]', function(err,res) {
+                assert.equal(null, err);
+                // headline shouldn't be visible when we scroll down
+                assert.equal(res, false);
+            })
+            .call(done);
+
+    });
 
 });
