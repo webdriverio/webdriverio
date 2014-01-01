5cfb3fe3dca951da680bc67799bd7bcdd7eae29d
christian-bromann <contact@christian-bromann.com>
2014-01-01 21:36:01 +0100

    implemented tap and flick actions with protocol commands, covered tap command with test

--- test/spec/mobile.js
+++ test/spec/mobile.js
@@ -2,16 +2,14 @@ describe.only('mobile tests', function() {
 
     before(h.setup);
 
-    it('should get current orientation', function(done) {
+    it.skip('should get current orientation', function(done) {
 
         this.client
             .getOrientation(function(err,res) {
                 assert.equal(null, err);
                 assert.equal(res, 'PORTRAIT');
             })
-            .setOrientation('landscape',function(err,res) {
-                assert.equal(null, err);
-            })
+            .setOrientation('landscape', h.noError)
             .getOrientation(function(err,res) {
                 assert.equal(null, err);
                 assert.equal(res, 'LANDSCAPE');
@@ -20,7 +18,7 @@ describe.only('mobile tests', function() {
 
     });
 
-    it('should scroll to specified X and Y position', function(done) {
+    it.skip('should scroll to specified X and Y position', function(done) {
 
         this.client
             .isVisible('//window[1]/scrollview[1]/webview[1]/text[1]', function(err,res) {
@@ -28,13 +26,27 @@ describe.only('mobile tests', function() {
                 // headline should be visible on app start
                 assert.equal(res, true);
             })
-            .scroll('//window[1]/scrollview[1]/webview[1]/textfield[5]',10,100,function(err) {
-                assert.equal(null, err);
-            })
+            .scroll('//window[1]/scrollview[1]/webview[1]/textfield[5]',10,100,h.noError)
             .isVisible('//window[1]/scrollview[1]/webview[1]/text[1]', function(err,res) {
                 assert.equal(null, err);
                 // headline shouldn't be visible when we scroll down
                 assert.equal(res, false);
+            })
+            .call(done);
+
+    });
+
+    it.only('should tap on an element', function(done) {
+
+        this.client
+            .getValue('//window[1]/scrollview[1]/webview[1]/textfield[1]',function(err,res) {
+                assert.equal(null, err);
+                assert.equal(res.toLowerCase(), '');
+            })
+            .tap('//window[1]/scrollview[1]/webview[1]/text[2]', h.noError)
+            .getValue('//window[1]/scrollview[1]/webview[1]/textfield[1]',function(err,res) {
+                assert.equal(null, err);
+                assert.equal(res.toLowerCase(), 'tap');
             })
             .call(done);
 
