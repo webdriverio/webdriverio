b40f5fdf9a4a4014bc3f5eaeeae03551fe690973
christian-bromann <contact@christian-bromann.com>
2014-01-06 00:53:51 +0100

    fixed teardown process

--- test/conf/mobile.js
+++ test/conf/mobile.js
@@ -2,6 +2,7 @@ module.exports = {
     desiredCapabilities: {
         browserName: process.env._BROWSER || '',
         platform: (process.env._PLATFORM || '').replace(/_/g,' '),
+        version: (process.env._VERSION || ''),
         device: (process.env._DEVICE || '').replace(/_/g,' '),
         app: process.env._APP || '',
         // app: 'sauce-storage:site.zip',
