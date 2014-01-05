5624e1cee805495d13e7801d22e2643914b0271e
christian-bromann <contact@christian-bromann.com>
2014-01-05 02:50:12 +0100

    added mobile matrices, install bower components before testing
    Former-commit-id: 344feac66fc07dca366e0ad85b3412a0c94c18bb

--- test/conf/mobile.js
+++ test/conf/mobile.js
@@ -1,11 +1,11 @@
 module.exports = {
     port: 4723,
     desiredCapabilities: {
-        browserName: '',
-        platform: 'Mac',
-        app: 'safari',
+        browserName: process.env._BROWSER || '',
+        platform: process.env._PLATFORM || '',
+        device: process.env._DEVICE || '',
+        app: process.env._APP || '',
         // app: 'sauce-storage:site.zip',
         // app: 'http://localhost:8080/test/site.zip',
-        device: 'iPhone Simulator'
     }
 };
