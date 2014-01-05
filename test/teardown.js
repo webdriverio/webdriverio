6230d4a35623c8bea1a05808778e16cb9c297332
vvo <vincent.voyer@gmail.com>
2014-01-05 16:34:44 +0100

    mark tests on saucelabs only on travis
    Former-commit-id: f4b4bb8ff6d35c6f9cbab28fd84a2bb02bc0e1f5

--- test/teardown.js
+++ test/teardown.js
@@ -2,19 +2,21 @@ var request = require('request');
 
 describe('teardown', function() {
 
-    it('it marks tests on saucelabs as passed/failed', function(done) {
-        // mark travis job as passed
-        var options = {
-            headers: { 'Content-Type': 'text/json' },
-            url: 'http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + this.client.requestHandler.sessionID,
-            method: 'PUT',
-            body: JSON.stringify({
-                passed: true
-            })
-        };
+    if (process.env.TRAVIS !== undefined) {
+        it('it marks tests on saucelabs as passed/failed', function(done) {
+            // mark travis job as passed
+            var options = {
+                headers: { 'Content-Type': 'text/json' },
+                url: 'http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + this.client.requestHandler.sessionID,
+                method: 'PUT',
+                body: JSON.stringify({
+                    passed: true
+                })
+            };
 
-        request(options, done);
-    });
+            request(options, done);
+        });
+    }
 
     it('stops the client', function(done) {
         this.client.endAll(done);
