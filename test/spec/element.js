bff2f092009a146245cf9c063ddc748da646d2e9
vvo <vincent.voyer@gmail.com>
2014-01-06 12:29:55 +0100

    fix element old syntax API
    HOTFIX to be merged in 1.0.x and 1.1.x
    
    we should test all element selectors in element.js and not using higher
    methods like .getValue or .isVisible (selectors.js).

---
+++ test/spec/element.js
@@ -0,0 +1,27 @@
+describe('dsalkjdslakjdlksadsa element', function() {
+    before(h.setup);
+
+    describe('using the old syntax', function() {
+        it('returns an element', function(done) {
+            this
+                .client
+                .element('css selector', '.box.red', function(err, res) {
+                    assert.equal(err, null);
+                    assert.ok(res.value.ELEMENT);
+                    done(err);
+                })
+        });
+    });
+
+    describe('using the new syntax', function () {
+        it('returns an element', function(done) {
+            this
+                .client
+                .element('.box.red', function(err, res) {
+                    assert.equal(err, null);
+                    assert.ok(res.value.ELEMENT);
+                    done(err);
+                })
+        });
+    });
+});
