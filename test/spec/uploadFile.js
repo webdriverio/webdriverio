5d9a8483a58c9fb5a487a4e82384a911b9f6ca2d
vvo <vincent.voyer@gmail.com>
2014-01-07 14:40:18 +0100

    add `.chooseFile` method
    .chooseFile(selector, localFile) will upload and select the file in the
    provided selector given it is a select input.

--- test/spec/uploadFile.js
+++ test/spec/uploadFile.js
@@ -5,18 +5,12 @@ describe('using the uploadFile helper', function() {
     var toUpload = path.join(__dirname, '..', 'fixtures', 'cat-to-upload.gif');
 
     it('uploads a file to the distant client', function(done) {
-        this.client.uploadFile(toUpload, function(err, res) {
-            assert.equal(null, err);
-            assert.ok(res.value);
-            assert.ok(/cat\-to\-upload\.gif$/.test(res.value));
-            done(err);
-        });
-    });
-
-    it('errors if file does not exists', function(done) {
-        this.client.uploadFile('#@$oi4209432', function(err) {
-            assert(err !== null);
-            done();
-        });
+        this.client
+            .uploadFile(toUpload, function(err, res) {
+                assert.equal(null, err);
+                assert.ok(res.value);
+                assert.ok(/cat\-to\-upload\.gif$/.test(res.value));
+                done(err);
+            });
     });
 });
