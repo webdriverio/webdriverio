977b9ce2e65c5e1122cfe3395a7747ef0d89b102
kulke <kulke@pe-international.com>
2014-05-08 10:54:19 +0200

    Fix: test independency, Added interval option.
    Former-commit-id: 422cbd6124aa67b14e8c948e5156508b7feccee0

--- test/spec/desktop/waitForInvisible.js
+++ test/spec/desktop/waitForInvisible.js
@@ -1,20 +1,32 @@
 describe('waitForInvisible command test', function() {
     before(h.setup);
 
+    beforeEach(function(done) {
+        this.client
+            .execute(function() {
+                $('.visible-text').show();
+                setTimeout(function() {
+                    $('.visible-text').hide();
+                }, 2000)
+            })
+            .call(done);
+    });
+
+
     it('should wait for the element to disappear within the specified time', function(done) {
-        this.timeout(2100);
+        this.timeout(2500);
         this.client
             .isVisible('.visible-text',function(err,result) {
                 assert.equal(null, err);
                 assert(result);
             })
-            .waitForInvisible('.visible-text', 2100, function(err) {
+            .waitForInvisible('.visible-text', 2000, function(err) {
                 assert.equal(null, err);
             })
             .call(done);
     });
 
-    it.only('should fail when the element does not disappear within the specified time', function(done) {
+    it('should fail when the element does not disappear within the specified time', function(done) {
         this.client
             .isVisible('.visible-text',function(err,result) {
                 assert.equal(null, err);
