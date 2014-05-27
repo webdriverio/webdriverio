5443ba0709757a101347f91435547bd47b99c021
christian-bromann <contact@christian-bromann.com>
2014-05-28 01:26:34 +0200

    test runner improvements, added android test devices

--- test/runner.js
+++ test/runner.js
@@ -1,15 +1,24 @@
-var Mocha     = require('mocha'),
+var Mocha = require('mocha'),
+    should = require('should'),
     SauceLabs = require('saucelabs'),
-    glob      = require('glob'),
-    env       = process.env._ENV || 'desktop',
-    client;
+    glob = require('glob'),
+    env = process.env._ENV,
+    client, specFiles;
 
 var mocha = new Mocha({
     timeout: 1000000,
     reporter: 'spec'
 });
 
-glob('{test/spec/' + env + '/*.js,test/spec/*.js}', function (er, files) {
+if(env === 'functional') {
+    // only test functional test spec if required
+    specFiles = 'test/spec/functional/*.js';
+} else {
+    // otherwise test global + device specific test specs
+    specFiles = '{test/spec/' + env + '/*.js,test/spec/*.js}';
+}
+
+glob(specFiles, function(er, files) {
 
     files.forEach(function(file) {
         mocha.addFile(file);
@@ -22,7 +31,7 @@ glob('{test/spec/' + env + '/*.js,test/spec/*.js}', function (er, files) {
 
         client.end(function() {
 
-            if(process.env.TRAVIS_BUILD_NUMBER) {
+            if (process.env.TRAVIS_BUILD_NUMBER) {
                 var sauceAccount = new SauceLabs({
                     username: process.env.SAUCE_USERNAME,
                     password: process.env.SAUCE_ACCESS_KEY
@@ -31,7 +40,7 @@ glob('{test/spec/' + env + '/*.js,test/spec/*.js}', function (er, files) {
                 sauceAccount.updateJob(client.requestHandler.sessionID, {
                     passed: failures === 0,
                     public: true
-                },function(err,res){
+                }, function(err, res) {
                     console.log(err || res);
                     process.exit(failures);
                 });
@@ -43,12 +52,12 @@ glob('{test/spec/' + env + '/*.js,test/spec/*.js}', function (er, files) {
 });
 
 // globals for tests
-conf   = require('./conf/index.js');
+conf = require('./conf/index.js');
 assert = require('assert');
 
 h = {
     noError: function(err) {
-        assert(err === null);
+        assert(err === undefined);
     },
     checkResult: function(expected) {
         return function(err, result) {
@@ -56,17 +65,30 @@ h = {
             assert.strictEqual(result, expected);
         };
     },
-    setup: (function() {
+    setup: function(newSession, url) {
         return function(done) {
             var wdjs = require('../index.js');
 
-            if (client) {
+            /**
+             * if instance already exists and no new session was requested return existing instance
+             */
+            if (client && !newSession) {
                 this.client = client;
+
+            /**
+             * if new session was requested create temporary instance
+             */
+            } else if(newSession) {
+                this.client = wdjs.remote(conf).init();
+
+            /**
+             * otherwise store created intance for other specs
+             */
             } else {
                 this.client = client = wdjs.remote(conf).init();
             }
 
-            this.client.url(conf.testPage.start, done);
+            this.client.url(url || conf.testPage.start, done);
         };
-    })()
+    }
 };
