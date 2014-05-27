f9443a90faa6900179cc0826f6fa104dc74102d4
christian-bromann <contact@christian-bromann.com>
2014-05-28 01:26:34 +0200

    test runner improvements, added android test devices
    Former-commit-id: 5443ba0709757a101347f91435547bd47b99c021

--- test/conf/mobile.js
+++ test/conf/mobile.js
@@ -3,7 +3,7 @@ module.exports = {
         browserName: process.env._BROWSER || '',
         platform: (process.env._PLATFORM || '').replace(/_/g,' '),
         version: (process.env._VERSION || ''),
-        device: (process.env._DEVICE || '').replace(/_/g,' '),
+        deviceName: (process.env._DEVICENAME || '').replace(/_/g,' '),
         app: process.env._APP || '',
         // app: 'sauce-storage:site.zip',
         // app: 'http://localhost:8080/test/site.zip',
