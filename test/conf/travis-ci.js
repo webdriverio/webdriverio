582befa42528f288ae732d36bf6a23a7c4128f7f
christian-bromann <contact@christian-bromann.com>
2014-08-10 17:45:22 -0700

    set default env tag
    Former-commit-id: 8151278605edd99461099d7d0dd6220367d047d4

--- test/conf/travis-ci.js
+++ test/conf/travis-ci.js
@@ -12,7 +12,7 @@ module.exports = {
         version: process.env._VERSION,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'idle-timeout': 900,
-        tags: ['webdriverio', process.env._ENV, process.env._BROWSER, process.env._PLATFORM, process.env._VERSION],
+        tags: ['webdriverio', process.env._ENV || 'desktop', process.env._BROWSER, process.env._PLATFORM, process.env._VERSION],
         name: 'webdriverio test',
         build: process.env.TRAVIS_BUILD_NUMBER,
         username: process.env.SAUCE_USERNAME,
