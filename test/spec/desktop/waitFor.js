9e104f4558adb6aba82eaf3184849fb44b4cb517
christian-bromann <contact@christian-bromann.com>
2014-08-30 16:34:24 -0700

    use selectorExecuteAsync to improve waitFor elements be able to support any selector strategy
    Former-commit-id: 70a8c7e1cbcc87c1c4626c25026c49272731f20d

--- test/spec/desktop/waitFor.js
+++ test/spec/desktop/waitFor.js
@@ -40,7 +40,7 @@ describe('waitFor',function() {
 
         it('should return w/o err after element was checked', function(done) {
             var currentTime = Date.now();
-            this.client.waitForChecked('.radio_waitForSelected', duration, checkTime(currentTime, done));
+            this.client.waitForChecked('//html/body/section/input[6]', duration, checkTime(currentTime, done));
         });
 
         it('(reverse) should return w/o err after element was unchecked', function(done) {
@@ -56,7 +56,7 @@ describe('waitFor',function() {
 
         it('should return w/o err after element was enabled', function(done) {
             var currentTime = Date.now();
-            this.client.waitForEnabled('.waitForValueEnabled', duration, checkTime(currentTime, done));
+            this.client.waitForEnabled('//html/body/section/input[8]', duration, checkTime(currentTime, done));
         });
 
         it('(reverse) should return w/o err after element was disabled', function(done) {
@@ -72,7 +72,7 @@ describe('waitFor',function() {
 
         it('should return w/o err after element was appended to the DOM', function(done) {
             var currentTime = Date.now();
-            this.client.waitForExist('.lateElem', duration, checkTime(currentTime, done));
+            this.client.waitForExist('//div[text()="Sorry, I\'m late!"]', duration, checkTime(currentTime, done));
         });
 
         it('(reverse) should return w/o err after element was removed from the DOM', function(done) {
@@ -88,7 +88,7 @@ describe('waitFor',function() {
 
         it('should return w/o err after element was selected', function(done) {
             var currentTime = Date.now();
-            this.client.waitForSelected('.option3', duration, checkTime(currentTime, done));
+            this.client.waitForSelected('//*[@id="selectbox"]/option[3]', duration, checkTime(currentTime, done));
         });
 
         it('(reverse) should return w/o err after element was unselected', function(done) {
@@ -104,7 +104,7 @@ describe('waitFor',function() {
 
         it('should return w/o err after element got a text/content', function(done) {
             var currentTime = Date.now();
-            this.client.waitForText('.sometextlater', duration, checkTime(currentTime, done));
+            this.client.waitForText('//*[contains(@class, "sometextlater")]', duration, checkTime(currentTime, done));
         });
 
         it('(reverse) should return w/o err after text/content element was removed', function(done) {
@@ -125,7 +125,7 @@ describe('waitFor',function() {
 
         it('(reverse) should return w/o err after element lost its value', function(done) {
             var currentTime = Date.now();
-            this.client.waitForEnabled('.waitForValueEnabledReverse', duration, true, checkTime(currentTime, done));
+            this.client.waitForEnabled('//*[contains(@class, "waitForValueEnabledReverse")]', duration, true, checkTime(currentTime, done));
         });
 
     });
@@ -136,7 +136,7 @@ describe('waitFor',function() {
 
         it('should return w/o err after element moved into document bounderies', function(done) {
             var currentTime = Date.now();
-            this.client.waitForVisible('.notInViewport', duration, checkTime(currentTime, done));
+            this.client.waitForVisible('//*[contains(@class, "notInViewport")]', duration, checkTime(currentTime, done));
         });
 
         it('(reverse) should return w/o err after element left document bounderies', function(done) {
