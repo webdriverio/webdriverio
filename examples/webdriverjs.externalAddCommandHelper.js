149fc9c01df6dbe90cc836ce5e803ee94cfc1d37
Johannes Nickel <johannes.nickel@googlemail.com>
2014-10-13 14:36:21 +0200

    updated callback var
    Former-commit-id: fd3e180290de43a8ec095eedd81b593d87df2d2d

--- examples/webdriverjs.externalAddCommandHelper.js
+++ examples/webdriverjs.externalAddCommandHelper.js
@@ -1,7 +1,7 @@
 module.exports = {
   searchGoogle: function () {
         var searchString = arguments[0],
-        callback = arguments[1];
+        callback = arguments[arguments.length - 1]
     this
         .url('http://www.google.com')
         .click('input[name="q"]')
