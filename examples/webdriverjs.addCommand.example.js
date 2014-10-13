cd543be8a5648af04ecf47901578592ce59b33af
Johannes Nickel <johannes.nickel@googlemail.com>
2014-10-13 13:14:55 +0200

    Updated regarding to the comments

--- examples/webdriverjs.addCommand.example.js
+++ examples/webdriverjs.addCommand.example.js
@@ -13,9 +13,8 @@ var SearchString = 'webdriver.io';
 
 client
     .init()
-    .searchGoogle(SearchString, function(err){}) //external helper function
+    .searchGoogle(SearchString) //external helper function
     .title(function(err, res) {
         console.log('Title was: ' + res.value);
     })
     .end();
-    
