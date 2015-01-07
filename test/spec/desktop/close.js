9a8c11bcddb813968bc2b22cf330d0e76bf72b67
christian-bromann <contact@christian-bromann.com>
2015-01-07 14:54:21 +0100

    removed obsolete close test
    Former-commit-id: 2b791516f538e1fe12b7fb88da98b31260f5ba69

--- test/spec/desktop/close.js
+++ test/spec/desktop/close.js
@@ -26,44 +26,6 @@ describe('close', function() {
             // open new tab
             .newWindow(conf.testPage.subPage)
 
-            // command needs to be executed within new function context
-            // to have access to the windowHandle assigned in L23
-            .call(function() {
-                that.client.close(openTab);
-            })
-
-            // test if there is only one tab open
-            .windowHandle(function(err, windowHandle) {
-                assert.ifError(err);
-                windowHandle.value.should.be.exactly(openTab);
-            })
-            .call(done);
-
-    });
-
-    it('should close the current window to switch back to another tab', function(done) {
-
-        /**
-         * safari doenst support `newWindow`
-         */
-        if(this.client.desiredCapabilities.browserName === 'safari') {
-            done();
-        }
-
-        var that = this,
-            openTab;
-
-        this.client
-
-            // get current tab id
-            .getTabIds(function(err, tabs) {
-                assert.ifError(err);
-                openTab = tabs[0];
-            })
-
-            // open new tab
-            .newWindow(conf.testPage.subPage)
-
             // ensure that there are two tabs open
             .getTabIds(function(err, tabs) {
                 assert.ifError(err);
@@ -71,7 +33,7 @@ describe('close', function() {
             })
 
             // command needs to be executed within new function context
-            // to have access to the windowHandle assigned in L61
+            // to have access to the windowHandle assigned in L23
             .call(function() {
                 that.client.close(openTab);
             })
