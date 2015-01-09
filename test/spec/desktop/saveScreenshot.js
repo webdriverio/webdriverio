3efd0207cbf19e58a9ef99c19dad5960223bffd2
Vadim <moshasmail@gmail.com>
2015-01-09 23:35:57 +0200

    add ability to get screenshot without writing on disk
    Former-commit-id: e3b18ab957a6092bf1931f8dfb6cfcef2e7f29e1

--- test/spec/desktop/saveScreenshot.js
+++ test/spec/desktop/saveScreenshot.js
@@ -5,23 +5,30 @@ describe('saveScreenshot', function() {
 
     before(h.setup());
 
-    var imageSize = {};
-
     it('should take a screenshot and output it on a desired location', function(done) {
 
         var screenshotPath = path.join(__dirname, '..', '..', '..', 'test.png');
 
         this.client
-            .saveScreenshot(screenshotPath, true)
+            .saveScreenshot(screenshotPath)
             .call(function() {
-
                 fs.exists(screenshotPath, function(fileExists) {
-                    fileExists.should.be.true;
+                    assert.ok(fileExists);
                     done();
                 })
-
             });
 
+    });
+
+    it('should take a screenshot and return it as a PNG image in Buffer', function(done) {
+        this.client
+            .saveScreenshot(function(err, image) {
+                assert.ifError(err);
+
+                // Check for PNG header
+                assert.equal(image.toString('hex', 0, 4), '89504e47');
+            })
+            .call(done);
     });
 
 });
