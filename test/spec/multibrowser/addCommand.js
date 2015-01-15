5755d935d1674bcb67a3971a2d30138b234e8cdb
christian-bromann <contact@christian-bromann.com>
2015-01-15 16:50:27 +0100

    added more tests for multibrowser
    Former-commit-id: 197f66a9b00c4c028547ef06e110922eff483349

---
+++ test/spec/multibrowser/addCommand.js
@@ -0,0 +1,51 @@
+// var async = require('async');
+
+// describe('addCommand', function() {
+
+//     before(h.setupMultibrowser());
+
+//     before(function(done) {
+//         var self = this;
+
+//         this.matrix.addCommand('getUrlAndTitle', function(callback) {
+
+//             var result = {},
+//                 error;
+
+//             this.url(function(err, url) {
+//                     error = err;
+//                     result.url = url.browserA.value;
+//                 })
+//                 .getTitle(function(err, title) {
+//                     error = err;
+//                     result.title = title.browserB;
+//                 })
+//                 .pause(1000)
+//                 .call(callback.bind(this, error, result));
+
+//         });
+
+//         async.waterfall([
+//             function(cb) {
+//                 self.browserA.url(conf.testPage.subPage, cb);
+//             },
+//             function(res, cb) {
+//                 self.browserB.url(conf.testPage.start, cb);
+//             }
+//         ], done);
+
+//     });
+
+//     it('added a `getUrlAndTitle` command', function(done) {
+
+//         this.matrix
+//             .getUrlAndTitle(function(err, result) {
+//                 assert.ifError(err);
+//                 assert.strictEqual(result.url, conf.testPage.subPage);
+//                 assert.strictEqual(result.title, conf.testPage.title);
+//             })
+//             .call(done);
+
+//     });
+
+// });
