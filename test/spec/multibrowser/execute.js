5fb6d3b5cbcf3da2db83e625e8e7f63061e62317
christian-bromann <contact@christian-bromann.com>
2015-01-15 16:53:56 +0100

    don't commit commented test
    Former-commit-id: 51fc2f651393f4356891c2fe83bba062e1d1ad71

--- test/spec/multibrowser/execute.js
+++ test/spec/multibrowser/execute.js
@@ -1,52 +1,52 @@
-// /* global document */
-// var async = require('async');
+/* global document */
+var async = require('async');
 
-// describe('execute', function() {
-//     before(h.setupMultibrowser());
+describe('execute', function() {
+    before(h.setupMultibrowser());
 
-//     before(function(done) {
-//         var self = this;
+    before(function(done) {
+        var self = this;
 
-//         async.waterfall([
-//             function(cb) {
-//                 self.browserA.url(conf.testPage.subPage, cb);
-//             },
-//             function(res, cb) {
-//                 self.browserB.url(conf.testPage.start, cb);
-//             }
-//         ], done);
-//     });
+        async.waterfall([
+            function(cb) {
+                self.browserA.url(conf.testPage.subPage, cb);
+            },
+            function(res, cb) {
+                self.browserB.url(conf.testPage.start, cb);
+            }
+        ], done);
+    });
 
-//     it('should be able to execute some js', function(done) {
-//         this.matrix
-//             .execute('return document.title', [], function(err, res) {
-//                 assert.ifError(err);
-//                 res.browserA.value.should.be.equal('two');
-//                 res.browserB.value.should.be.equal(conf.testPage.title);
-//             })
-//             .call(done);
-//     });
+    it('should be able to execute some js', function(done) {
+        this.matrix
+            .execute('return document.title', [], function(err, res) {
+                assert.ifError(err);
+                res.browserA.value.should.be.equal('two');
+                res.browserB.value.should.be.equal(conf.testPage.title);
+            })
+            .call(done);
+    });
 
-//     it('should be forgiving on giving an `args` parameter', function(done) {
-//         this.matrix
-//             .execute('return document.title', function(err, res) {
-//                 assert.ifError(err);
-//                 res.browserA.value.should.be.equal('two');
-//                 res.browserB.value.should.be.equal(conf.testPage.title);
-//             })
-//             .call(done);
-//     });
+    it('should be forgiving on giving an `args` parameter', function(done) {
+        this.matrix
+            .execute('return document.title', function(err, res) {
+                assert.ifError(err);
+                res.browserA.value.should.be.equal('two');
+                res.browserB.value.should.be.equal(conf.testPage.title);
+            })
+            .call(done);
+    });
 
-//     it('should be able to execute a pure function', function(done) {
-//         this.matrix
-//             .execute(function() {
-//                 return document.title;
-//             }, function(err, res) {
-//                 assert.ifError(err);
-//                 res.browserA.value.should.be.equal('two');
-//                 res.browserB.value.should.be.equal(conf.testPage.title);
-//             })
-//             .call(done);
-//     });
+    it('should be able to execute a pure function', function(done) {
+        this.matrix
+            .execute(function() {
+                return document.title;
+            }, function(err, res) {
+                assert.ifError(err);
+                res.browserA.value.should.be.equal('two');
+                res.browserB.value.should.be.equal(conf.testPage.title);
+            })
+            .call(done);
+    });
 
-// });
+});
--- test/spec/multibrowser/executeAsync.js
+++ test/spec/multibrowser/executeAsync.js
@@ -1,36 +1,36 @@
-// /* global document */
-// var async = require('async');
+/* global document */
+var async = require('async');
 
-// describe('executeAsync', function() {
-//     before(h.setupMultibrowser());
+describe('executeAsync', function() {
+    before(h.setupMultibrowser());
 
-//     before(function(done) {
-//         var self = this;
+    before(function(done) {
+        var self = this;
 
-//         async.waterfall([
-//             function(cb) {
-//                 self.browserA.url(conf.testPage.subPage, cb);
-//             },
-//             function(res, cb) {
-//                 self.browserB.url(conf.testPage.start, cb);
-//             }
-//         ], done);
-//     });
+        async.waterfall([
+            function(cb) {
+                self.browserA.url(conf.testPage.subPage, cb);
+            },
+            function(res, cb) {
+                self.browserB.url(conf.testPage.start, cb);
+            }
+        ], done);
+    });
 
-//     it('should execute an async function', function(done) {
-//         this.matrix
-//             .timeouts('script', 5000)
-//             .executeAsync(function() {
-//                 var cb = arguments[arguments.length - 1];
-//                 setTimeout(function() {
-//                     cb(document.title + '-async');
-//                 }, 500);
-//             }, function(err, res) {
-//                 assert.ifError(err);
-//                 res.browserA.value.should.be.equal('two-async');
-//                 res.browserB.value.should.be.equal(conf.testPage.title + '-async');
-//             })
-//             .call(done);
-//     });
+    it('should execute an async function', function(done) {
+        this.matrix
+            .timeouts('script', 5000)
+            .executeAsync(function() {
+                var cb = arguments[arguments.length - 1];
+                setTimeout(function() {
+                    cb(document.title + '-async');
+                }, 500);
+            }, function(err, res) {
+                assert.ifError(err);
+                res.browserA.value.should.be.equal('two-async');
+                res.browserB.value.should.be.equal(conf.testPage.title + '-async');
+            })
+            .call(done);
+    });
 
-// });
+});
