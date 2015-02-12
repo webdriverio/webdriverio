cb124223ac6ff4a606b6b5ea13fa6e780be965ff
christian-bromann <contact@christian-bromann.com>
2015-02-12 12:38:11 +0100

    upgraded appium version in test config
    Former-commit-id: 0c1af03231a1d50020d43ffb7ea5d3abb8c625c1

--- test/conf/mobile.js
+++ test/conf/mobile.js
@@ -3,7 +3,7 @@ module.exports = {
         browserName: process.env._BROWSER || '',
         platformName: (process.env._PLATFORM || '').replace(/_/g,' '),
         platformVersion: (process.env._VERSION || ''),
-        'appium-version': '1.0',
+        'appium-version': '1.3.4',
         deviceName: (process.env._DEVICENAME || '').replace(/_/g,' ')
     }
 };
