ce96ebc4ebac13041782700b6a07a5dd88e0c4f0
christian-bromann <contact@christian-bromann.com>
2015-02-13 16:02:46 +0100

    simplified multibrowser example

--- examples/webdriverio.multiremote.chat.js
+++ examples/webdriverio.multiremote.chat.js
@@ -1,57 +1,45 @@
-var async = require('async'),
-    WebdriverIO = require('../'),
+var WebdriverIO = require('../'),
     matrix = WebdriverIO.multiremote({
         browserA: { desiredCapabilities: { browserName: 'firefox' } },
         browserB: { desiredCapabilities: { browserName: 'firefox' } }
     }),
     browserA = matrix.select('browserA'),
-    browserB = matrix.select('browserB');
+    browserB = matrix.select('browserB'),
+    name;
 
-async.waterfall([
-    /**
-     * open chat application
-     */
-    function(done) {
-        matrix
-            .init()
-            .url('http://chat.socket.io/')
-            .call(done);
-    },
-    /**
-     * login
-     */
-    function(done) {
-        browserA.setValue('.usernameInput', 'Browser A');
-        browserB.setValue('.usernameInput', 'Browser B').call(done);
-    },
-    /**
-     * submit login form
-     */
-    function(done) {
-        matrix.keys('Enter').call(done);
-    },
-    /**
-     * post message with browser A and check if message arrives and answer properly
-     */
-    function(done) {
-        browserA
-            .setValue('.inputMessage', 'Hey Whats up!').keys('Enter')
-            .setValue('.inputMessage', 'My name is Browser A').keys('Enter');
+/**
+ * open chat application
+ */
+matrix.init().url('http://chat.socket.io/');
 
-        browserB
-            .waitForExist('.messages .message')
-            .getText('.messages .message', function(err, messages) {
-                var message = messages[messages.length - 1],
-                    name = message.slice(20);
+/**
+ * login
+ */
+browserA.setValue('.usernameInput', 'Browser A');
+browserB.setValue('.usernameInput', 'Browser B');
 
-                browserB.setValue('.inputMessage', 'Hello ' + name + '! How are you today?').keys('Enter').call(done);
-            });
-    }
-], function() {
+/**
+ * submit login form
+ */
+matrix.keys('Enter');
 
-    /**
-     * end session
-     */
-    matrix.pause(2000).end();
+/**
+ * post message with browser A and check if message arrives and answer properly
+ */
+browserA
+    .setValue('.inputMessage', 'Hey Whats up!').pause(1000).keys('Enter').pause(100)
+    .setValue('.inputMessage', 'My name is LULULULULU').keys('Enter');
 
-});
+browserB
+    .waitForExist('.messages .message')
+    .getText('.messages .message', function(err, messages) {
+        var message = messages[messages.length - 1],
+            name = message.slice(20);
+
+        browserB.setValue('.inputMessage', 'Hello ' + name + '! How are you today?').keys('Enter');
+    });
+
+/**
+ * end session
+ */
+matrix.pause(2000).end();
