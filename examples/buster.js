916fd0056266e234beb38ba46b03a4bb4ca3b449
christian-bromann <contact@christian-bromann.com>
2015-02-13 16:02:46 +0100

    simplified multibrowser example
    Former-commit-id: ce96ebc4ebac13041782700b6a07a5dd88e0c4f0

---
+++ examples/buster.js
@@ -0,0 +1,9 @@
+var config = module.exports;
+
+config['My tests'] = {
+    rootPath: '../',
+    environment: 'node', // or 'browser'
+    tests: [
+        'examples/webdriverjs.with.buster.js'
+    ]
+}
