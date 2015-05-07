7ebc4bb891b033fba9d187d63145c71f8d1aea67
christian-bromann <contact@christian-bromann.com>
2015-05-07 14:48:26 +0200

    fixed chooseFile command

--- test/spec/desktop/chooseFile.js
+++ test/spec/desktop/chooseFile.js
@@ -4,23 +4,17 @@ describe('choosing a file in an <input type=file>', function() {
     var path = require('path');
     var toUpload = path.join(__dirname, '..', '..', 'fixtures', 'cat-to-upload.gif');
 
-    it('uploads a file and fills the form with it', function(done) {
-        this.client
-            .chooseFile('#upload-test', toUpload, function(err) {
-                assert.ifError(err);
-            })
-            .getValue('#upload-test', function(err, val) {
-                assert.ifError(err);
-                assert.ok(/cat\-to\-upload\.gif$/.test(val));
-            })
-            .call(done);
+    it('uploads a file and fills the form with it', function() {
+        return this.client.chooseFile('#upload-test', toUpload).catch(function(err) {
+            assert.ifError(err);
+        }).getValue('#upload-test').then(function(val) {
+            assert.ok(/cat\-to\-upload\.gif$/.test(val));
+        });
     });
 
-    it('errors if file does not exists', function(done) {
-        this.client
-            .chooseFile('#upload-test', '$#$#940358435', function(err) {
-                assert.notEqual(err, null);
-                done();
-            });
+    it('errors if file does not exists', function() {
+        return this.client.chooseFile('#upload-test', '$#$#940358435').catch(function(err) {
+            assert.notEqual(err, null);
+        });
     });
 });
