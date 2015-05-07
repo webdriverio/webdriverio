d9e5c477f4942ed842b19d60eb4147192c335eed
christian-bromann <contact@christian-bromann.com>
2015-05-07 12:45:41 +0200

    fixed functional tests

--- test/spec/functional/executeAsync.js
+++ test/spec/functional/executeAsync.js
@@ -3,19 +3,17 @@ describe('executeAsync', function() {
 
     before(h.setup());
 
-    it('should execute an async function', function(done) {
-        this.client
+    it('should execute an async function', function() {
+        return this.client
             .timeouts('script', 5000)
             .executeAsync(function() {
                 var cb = arguments[arguments.length - 1];
                 setTimeout(function() {
                     cb(document.title + '-async');
                 }, 500);
-            }, function(err, res) {
-                assert.ifError(err);
+            }).then(function(res) {
                 res.value.should.be.exactly(conf.testPage.title + '-async');
-            })
-            .call(done);
+            });
     });
 
 });
