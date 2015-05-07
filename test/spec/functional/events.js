751780b66621e9ec8cd35c3a3758ff99e0141cde
christian-bromann <contact@christian-bromann.com>
2015-05-07 12:01:20 +0200

    fixed eventhandling

--- test/spec/functional/events.js
+++ test/spec/functional/events.js
@@ -6,8 +6,8 @@ var webdriverjs = require('../../../index.js'),
         }
     };
 
-/* global beforeEach */
 describe('event handling', function() {
+
     describe('is able to emit and listen to driver specific events and', function() {
 
         var isCommandHandlerEmitted = false,
@@ -43,55 +43,36 @@ describe('event handling', function() {
             });
         });
 
-        it('should emit an init event after calling the init command', function(done) {
-            client
+        it('should emit an init event after calling the init command', function() {
+            return client
                 .init()
                 .call(function() {
                     assert.ok(isInitHandlerEmitted, 'init handler wasn\'t called');
                     assert.strictEqual(uri.host, '127.0.0.1:4444');
                     assert.strictEqual(desiredCapabilties, 'phantomjs');
-                })
-                .call(done);
+                });
         });
 
-        it('should emit an error event after querying a non existing element', function(done) {
-
-            client
+        it('should emit an error event after querying a non existing element', function() {
+            return client
                 .url(conf.testPage.start)
                 // click on non existing element to cause an error
-                .click('#notExistentant', function(err) {
+                .click('#notExistentant').catch(function(err) {
                     assert(err.message.match(/Problem: Unable to find element with id 'notExistentant'/));
                 })
                 .call(function() {
                     assert.ok(isErrorHandlerEmitted, 'error handler wasn\'t called');
-                })
-                .call(done);
-
+                });
         });
 
-        it('should not throw an error if an error handler is registered', function(done) {
-            isErrorHandlerEmitted = false;
-
-            client
-                .url(conf.testPage.start)
-                // click on non existing element should not cause an error because an event listener is registered
-                .click('#notExistentant')
-                .call(function() {
-                    assert.ok(isErrorHandlerEmitted, 'error handler was called');
-                })
-                .call(done);
-
+        it('should emit an end event after calling the end command', function() {
+            return client.end().call(function() {
+                assert.ok(isEndHandlerEmitted, 'end handler wasn\'t called');
+            });
         });
 
-        it('should emit an end event after calling the end command', function(done) {
-
-            client
-                .end()
-                .call(function() {
-                    assert.ok(isEndHandlerEmitted, 'end handler wasn\'t called');
-                })
-                .call(done);
-
+        after(function() {
+            return client.end();
         });
     });
 
@@ -107,9 +88,8 @@ describe('event handling', function() {
             this.client.removeAllListeners('testme');
         });
 
-        it('should register and fire events with on/emit', function(done) {
-
-            this.client
+        it('should register and fire events with on/emit', function() {
+            return this.client
                 .emit('testme')
                 .on('testme', function() {
                     assert.ok(iShouldBeGetTriggered, 'event was triggered unexpected');
@@ -120,18 +100,13 @@ describe('event handling', function() {
                 })
                 .emit('testme')
                 .call(function() {
-                    if (eventWasTriggeredAtLeastOnce) {
-                        done();
-                    } else {
-                        done(new Error('event wasn\'t thrown'));
-                    }
+                    eventWasTriggeredAtLeastOnce.should.be.ok;
                 });
 
         });
 
-        it('should register and fire events with once/emit', function(done) {
-
-            this.client
+        it('should register and fire events with once/emit', function() {
+            return this.client
                 .once('testme', function() {
                     assert.ok(iShouldBeGetTriggered, 'event was triggered unexpected');
                     assert.ok(!eventWasTriggeredAtLeastOnce, 'once event got triggered twice');
@@ -141,9 +116,7 @@ describe('event handling', function() {
                     iShouldBeGetTriggered = true;
                 })
                 .emit('testme')
-                .emit('testme')
-                .call(done);
-
+                .emit('testme');
         });
 
         it('two instances should have different event handlers', function() {
@@ -159,6 +132,10 @@ describe('event handling', function() {
 
             clientA.emit('testme', 'A');
             clientB.emit('testme', 'B');
+
+            return clientA.end().then(function() {
+                return clientB.end();
+            });
         });
     });
 });
