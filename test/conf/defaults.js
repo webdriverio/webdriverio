2d8062336a004d7dc750da6514e79f5cbec57863
christian-bromann <contact@christian-bromann.com>
2015-05-19 01:35:40 +0200

    added support baseUrl for - closes #352
    Former-commit-id: d090174d2964fb5ccd83369e38e8c19ed35d4bad

--- test/conf/defaults.js
+++ test/conf/defaults.js
@@ -1,4 +1,5 @@
 module.exports = {
+    baseUrl: 'http://127.0.0.1:8080',
     testPage: {
         start: 'http://127.0.0.1:8080/test/site/www/index.html',
         subPage: 'http://127.0.0.1:8080/test/site/www/two.html',
