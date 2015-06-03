3645058d5149927ee50cdae884565398862366c6
christian-bromann <contact@christian-bromann.com>
2015-06-04 01:57:52 +0200

    fixed multibrowser tests

--- test/spec/multibrowser/pause.js
+++ test/spec/multibrowser/pause.js
@@ -1,22 +1,16 @@
-/**
- * sufficient to get tested with phantomjs
- */
-describe.skip('pause', function() {
+describe('pause', function() {
 
     before(h.setupMultibrowser());
 
-    it('should pause command queue', function(done) {
+    it('should pause command queue', function() {
 
         var time = new Date().getTime();
 
-        this.matrix
-            .pause(1000)
-            .call(function() {
-                var newTime = new Date().getTime();
-                (newTime - time).should.be.greaterThan(999);
-                (newTime - time).should.be.lessThan(1010);
-            })
-            .call(done);
+        return this.matrix.pause(1000).call(function() {
+            var newTime = new Date().getTime();
+            (newTime - time).should.be.greaterThan(999);
+            (newTime - time).should.be.lessThan(1010);
+        });
 
     });
 
