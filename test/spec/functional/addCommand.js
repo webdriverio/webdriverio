faa9380900f97c8eb66c4a9f7002293c4cce7e67
christian-bromann <contact@christian-bromann.com>
2015-06-24 02:57:47 +0200

    prevent user from overwriting existing commands - closes #580

--- test/spec/functional/addCommand.js
+++ test/spec/functional/addCommand.js
@@ -2,34 +2,62 @@ describe('addCommand', function() {
 
     before(h.setup());
 
-    before(function() {
-        this.client.addCommand('getUrlAndTitle', function() {
+    describe('add regular command', function() {
 
-            var result = {};
+        before(function() {
+            this.client.addCommand('getUrlAndTitle', function() {
 
-            return this.url().then(function(url) {
-                result.url = url.value;
-            }).getTitle().then(function(title) {
-                result.title = title;
-            }).then(function() {
-                return result;
+                var result = {};
+
+                return this.url().then(function(url) {
+                    result.url = url.value;
+                }).getTitle().then(function(title) {
+                    result.title = title;
+                }).then(function() {
+                    return result;
+                });
+
             });
-
         });
+
+        it('added a `getUrlAndTitle` command', function() {
+            return this.client.getUrlAndTitle().then(function(result) {
+                assert.strictEqual(result.url, conf.testPage.start);
+                assert.strictEqual(result.title, conf.testPage.title);
+            });
+        });
+
+        it('should promisify added command', function() {
+            return this.client.getUrlAndTitle().then(function(result) {
+                assert.strictEqual(result.url, conf.testPage.start);
+                assert.strictEqual(result.title, conf.testPage.title);
+            });
+        });
+
     });
 
-    it('added a `getUrlAndTitle` command', function() {
-        return this.client.getUrlAndTitle().then(function(result) {
-            assert.strictEqual(result.url, conf.testPage.start);
-            assert.strictEqual(result.title, conf.testPage.title);
-        });
-    });
+    describe('try to add an existing command', function() {
 
-    it('should promisify added command', function() {
-        return this.client.getUrlAndTitle().then(function(result) {
-            assert.strictEqual(result.url, conf.testPage.start);
-            assert.strictEqual(result.title, conf.testPage.title);
+        it('should throw an error', function() {
+            var self = this;
+
+            (function(){
+                self.client.addCommand('shake', function() {
+                    return 'should fail';
+                });
+            }).should.throw();
         });
+
+        it('should overwrite existing method if overwrite flag is given', function() {
+            this.client.addCommand('shake', function() {
+                return 'should not fail';
+            }, true);
+
+            return this.client.shake().then(function(res) {
+                expect(res).to.be.equal('should not fail');
+            });
+        });
+
     });
 
 });
