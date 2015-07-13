7f62af82d685c1a621f4efde24d53af1bcf35173
André de Jager <andrethehunter@gmail.com>
2015-07-13 21:44:57 +0100

    added grunt-env, simplified config with 'config' task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var selenium = {
         base: 'https://selenium-release.storage.googleapis.com',
         version: '2.45.0',
@@ -12,126 +11,166 @@ var selenium = {
         get url () {
             return [this.base, this.minorVersion, this.filename].join('/');
         }
+    },
+    gruntOptions = {
+        env: '_ENV',
+        browser: '_BROWSER',
+        spec: '_SPEC'
     };
 
-process.env.PATH = 'node_modules/.bin/:' + process.env.PATH;
-
 module.exports = function (grunt) {
+
+    function addEnv(envs) {
+        grunt.config.merge({
+            env: {
+                env : {
+                    add: envs
+                }
+            }
+        });
+    }
+
     require('load-grunt-tasks')(grunt);
-    require('./test/tasks/appium')(grunt);
+    require('./test/tasks/grunt-appium')(grunt);
+
+    // TODO move this to a separate grunt task
+    (function gruntOptionsToEnvVars() {
+        var option, value, envName;
+        for (option in gruntOptions) {
+            value = grunt.option(option);
+            if (value) {
+                envName = gruntOptions[option];
+                process.env[envName] = value;
+            }
+        }
+    }());
 
     if (grunt.option('selenium')) {
         selenium.version = grunt.option('selenium');
     }
 
-    function setBrowser(browserName) {
-        if (grunt.option('browser')) {
-            browserName = grunt.option('browser');
-        }
-        process.env._BROWSER = browserName;
-        grunt.config.set('start-selenium-server.dev.options.serverOptions.browser', browserName);
-    }
-
-    function setEnv(env) {
-        var testSrc = 'mochaTest.test.src',
-            specDir = env.match(/^(desktop|functional|mobile|multibrowser)$/);
-
-        process.env._ENV = env;
-
-        if (!grunt.config.get(testSrc)) {
-            if (specDir) {
-                // only test functional test spec if required
-                grunt.config.set(testSrc, ['test/spec/' + specDir[0] + '/**/*.js']);
-            } else {
-                // otherwise test global + device specific test specs
-                grunt.config.set(testSrc, ['test/spec/' + env + '/**/*.js', 'test/spec/*.js']);
-            }
-        }
-    }
-
-    grunt.initConfig({
-        'connect': {
-            server: {
-                options: {
-                    hostname: '127.0.0.1',
-                    port: 8080,
-                    base: 'test/site/www'
-                }
-            }
-        },
-        'start-selenium-server': {
-            dev: {
-                options: {
-                    autostop: true,
-                    downloadUrl: selenium.url,
-                    serverOptions: {
-                        browser: 'chrome'
-                    }
-                }
-            }
-        },
-        'appium': {
-            options: {
-                args: []
-            }
-        },
-        'mochaTest': {
-            options: {
-                force: true
+    grunt.config('env', {
+        env: {
+            add: {
+                _MOCHATIMEOUT: 5 * 1000
             },
-            test: {
-                src: grunt.option('spec') || process.env._SPEC,
-                options: {
-                    reporter: 'spec',
-                    timeout: 5 * 1000,
-                    require: 'test/setup.js'
-                }
-            }
-        },
-        'passfail': {
-            options: {
-                force: true
-            },
-            'mochaTest': {
-                success: function() {
-                    require('./test/helper').success(this.async());
-                },
-                fail: function (warncount, errorcount) {
-                    require('./test/helper').failure(this.async());
-                }
+            unshift: {
+                PATH: 'node_modules/.bin/:'
             }
         }
     });
 
-    grunt.registerTask('run-test', ['connect', 'start-selenium-server', 'mochaTest', 'passfail']);
+    grunt.registerTask('config', function() {
+        grunt.initConfig({
+            'connect': {
+                server: {
+                    options: {
+                        hostname: '127.0.0.1',
+                        port: 8080,
+                        base: 'test/site/www'
+                    }
+                }
+            },
+            'start-selenium-server': {
+                dev: {
+                    options: {
+                        autostop: true,
+                        downloadUrl: selenium.url,
+                        serverOptions: {
+                            browser: process.env._BROWSER
+                        }
+                    }
+                }
+            },
+            'appium': {
+            },
+            'mochaTest': {
+                options: {
+                    force: true
+                },
+                test: {
+                    src: (function (){
+                        var env, src, includeSpec;
+                        if (process.env._SPEC) {
+                            return [process.env._SPEC];
+                        }
+                        env = process.env._ENV;
+                        src = ['test/spec/' + env + '/**/*.js'];
+                        includeSpec = env.match(/^(mobile|desktop)$/);
+                        if (includeSpec) {
+                            src.push('test/spec/*.js');
+                        }
+                        return src;
+                    }()),
+                    options: {
+                        reporter: 'spec',
+                        require: 'test/setup.js',
+                        timeout: process.env._MOCHATIMEOUT
+                    }
+                }
+            },
+            'passfail': {
+                options: {
+                    force: true
+                },
+                'mochaTest': {
+                    success: function() {
+                        require('./test/helper').success(this.async());
+                    },
+                    fail: function (warncount, errorcount) {
+                        require('./test/helper').failure(this.async());
+                    }
+                }
+            }
+        });
+    });
+
+    grunt.registerTask('run-test', function() {
+        grunt.task.run('env', 'config', 'connect', 'start-selenium-server', 'mochaTest', 'passfail');
+    });
+
+    grunt.registerTask('test', function () {
+        if (process.env._ENV === undefined) {
+            throw new Error('_ENV environment variable must be defined');
+        }
+        grunt.task.run('test-' + process.env._ENV.toLowerCase());
+    });
 
     grunt.registerTask('test-functional', function () {
-        setEnv('functional');
-        setBrowser('phantomjs');
+        addEnv({
+            _ENV: 'functional',
+            _BROWSER: 'phantomjs'
+        });
         grunt.task.run('run-test');
     });
 
     grunt.registerTask('test-desktop', function () {
-        setEnv('desktop');
-        setBrowser('chrome');
+        addEnv({
+            _ENV: 'desktop',
+            _BROWSER: 'chrome'
+        });
         grunt.task.run('run-test');
     });
 
     grunt.registerTask('test-multibrowser', function () {
-        setEnv('multibrowser');
+        addEnv({
+            _ENV: 'multibrowser'
+        });
         grunt.task.run('run-test');
     });
 
     grunt.registerTask('test-mobile', function () {
-        setEnv('mobile');
-        setBrowser('safari');
-        process.env._DEVICENAME = 'iPhone 6 Plus';
-        process.env._PLATFORMNAME = 'iOS';
-        process.env._PLATFORMVERSION = 8.4;
-        process.env._APP = 'safari';
-        process.env._PORT = 4723;
-        process.env._APPIUMVERSION = '1.3.4';
-        grunt.config.set('mochaTest.test.options.timeout', 30 * 1000);
-        grunt.task.run('connect', 'appium', 'mochaTest', 'passfail');
+        addEnv({
+            _ENV: 'mobile',
+            _BROWSER: 'safari',
+            _DEVICENAME: 'iPhone 6 Plus',
+            _PLATFORMNAME: 'iOS',
+            _PLATFORMVERSION: 8.4,
+            _APP: 'safari',
+            _PORT: 4723,
+            _APPIUMVERSION: '1.3.4',
+            _MOCHATIMEOUT: 30 * 1000
+        });
+        grunt.task.run('env', 'config', 'connect', 'appium', 'mochaTest', 'passfail');
     });
 }
