c22fbe8d4346255b3cef6476c98f965f8e26389f
christian-bromann <contact@christian-bromann.com>
2015-08-22 10:18:11 -0700

    increased width and height in viewportSize test to make it work with sauce
    Former-commit-id: a73434ac992a901a7f9e97e1d8151d0eb80ceaa1

--- test/spec/desktop/viewportSize.js
+++ test/spec/desktop/viewportSize.js
@@ -3,43 +3,44 @@ describe('setViewportSize/getViewportSize', function() {
 
     beforeEach(function() {
         return this.client.windowHandleSize({
-            width: 300,
-            height: 300
+            width: 500,
+            height: 500
         });
     })
 
     it('should change viewport size of current window and should return the exact value', function() {
         return this.client.setViewportSize({
-            width: 500,
-            height: 500
+            width: 700,
+            height: 700
         }, true).getViewportSize().then(function(size) {
-            size.width.should.be.exactly(500);
-            size.height.should.be.exactly(500);
+            size.width.should.be.exactly(700);
+            size.height.should.be.exactly(700);
         });
     });
 
     it('should set window size exactly when parameter \'type\' is true by default', function() {
         return this.client.setViewportSize({
-            width: 500,
-            height: 500
+            width: 700,
+            height: 700
         }).getViewportSize().then(function(size) {
-            size.width.should.be.exactly(500);
-            size.height.should.be.exactly(500);
+            size.width.should.be.exactly(700);
+            size.height.should.be.exactly(700);
         }).windowHandleSize().then(function(res) {
-            res.value.width.should.be.exactly(500);
-            res.value.height.should.be.greaterThan(500);
+            res.value.width.should.be.exactly(700);
+            res.value.height.should.be.greaterThan(700);
         });
     });
 
     it('should let windowHandleSize return bigger values since it includes menu and status bar heights', function() {
         return this.client.setViewportSize({
-            width: 500,
-            height: 500
+            width: 700,
+            height: 700
         }, false).getViewportSize().then(function(size) {
-            size.width.should.be.exactly(500);
-            size.height.should.be.lessThan(500);
+            size.width.should.be.exactly(700);
+            size.height.should.be.lessThan(700);
         }).windowHandleSize().then(function(res) {
-            res.value.height.should.be.exactly(500);
+            res.value.height.should.be.exactly(700);
         });
     });
+
 });
