b45636b1117ddbe09fe345335fa4da7fbc837809
christian-bromann <contact@christian-bromann.com>
2015-08-27 01:13:51 -0700

    updated wdio examples to work with new sync runner

--- examples/wdio/runner-specs/mocha.test.js
+++ examples/wdio/runner-specs/mocha.test.js
@@ -2,36 +2,12 @@ var assert = require('assert');
 
 describe('webdriver.io page', function() {
 
-    it('should have the right title - the good old callback way', function(done) {
-
-        browser
-            .url('/')
-            .getTitle(function(err, title) {
-                assert.equal(err, undefined);
-                assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
-            })
-            .call(done);
-
-    });
-
-    it('should have the right title - the promise way', function() {
-
-        return browser
-            .url('/')
-            .getTitle().then(function(title) {
-                assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
-            });
-
-    });
-
     it('should be a pending test');
 
-    it('should have the right title - the fancy generator way', function* () {
-
-        yield browser.url('/');
-        var title = yield browser.getTitle();
+    it('should have the right title - the fancy generator way', function () {
+        browser.url('/');
+        var title = browser.getTitle();
         assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
-
     });
 
 });
