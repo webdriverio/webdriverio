f4839c088ff0bfb3be812615fe06110342a0ab74
christian-bromann <contact@christian-bromann.com>
2015-09-08 23:55:07 -0700

    implemented ability to use custom reporter - closes #758
    Former-commit-id: 0dfd216bf93c8583abcbef3f7a4d67541a541b2c

---
+++ examples/wdio/wdio.customreporter.conf.js
@@ -0,0 +1,58 @@
+var CustomReporter = require('./reporter/my.custom.reporter');
+
+exports.config = {
+
+    /**
+     * server configurations
+     */
+    host: '0.0.0.0',
+    port: 4444,
+
+    /**
+     * specify test files
+     */
+    specs: [
+        './examples/wdio/runner-specs/mocha.customreporter.js'
+    ],
+    exclude: [
+        'test/spec/multibrowser/**',
+        'test/spec/mobile/**'
+    ],
+
+    /**
+     * capabilities
+     */
+    capabilities: [{
+        browserName: 'phantomjs'
+    }],
+
+    /**
+     * test configurations
+     */
+    logLevel: 'silent',
+    coloredLogs: true,
+    screenshotPath: 'shots',
+    baseUrl: 'http://webdriver.io',
+    waitforTimeout: 10000,
+    framework: 'mocha',
+
+    reporter: CustomReporter,
+    reporterOptions: {
+        outputDir: './'
+    },
+
+    mochaOpts: {
+        ui: 'bdd'
+    },
+
+    /**
+     * hooks
+     */
+    onPrepare: function() {
+        console.log('let\'s go');
+    },
+    onComplete: function() {
+        console.log('that\'s it');
+    }
+
+};
