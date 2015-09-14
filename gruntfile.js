108843a7e4d219ca7c2df073fad339085f74b139
christian-bromann <contact@christian-bromann.com>
2015-09-13 20:11:13 -0700

    prepare package for es2016 development
    Former-commit-id: 5e9984ce73457818e6432783a70ccb7e768204a2

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkgFile: 'package.json',
+        clean: ['build'],
+        babel: {
+            options: {
+                sourceMap: false
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: ['./**/*', '!test','!node_modules', '!examples', '!docs'],
+                    dest: 'build',
+                    ext: '.js'
+                }]
+            }
+        },
+        eslint: {
+            options: {
+                parser: 'babel-eslint'
+            },
+            target: ['index.js']
+        },
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            src: ['index.js']
+        },
+        contributors: {
+            options: {
+                commitMessage: 'update contributors'
+            }
+        },
+        bump: {
+            options: {
+                commitMessage: 'v%VERSION%',
+                pushTo: 'upstream'
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', 'Build wdio-mocha', function() {
+        grunt.task.run([
+            'jshint',
+            'eslint',
+            'clean',
+            'babel'
+        ]);
+    });
+    grunt.registerTask('release', 'Bump and tag version', function(type) {
+        grunt.task.run([
+            'build',
+            'contributors',
+            'bump:' + (type || 'patch')
+        ]);
+    });
+};
