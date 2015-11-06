f1311bef71f58f814e239f7d1381687c39a60ebe
George Crawford <george.crawford@ft.com>
2015-11-06 12:07:30 +0000

    Basic namespaced addCommmand. Needs further work for wdio and multiremote
    Former-commit-id: 55b50cf25361c3fdc512df1a980f9dd5b41c37c6

--- test/spec/functional/addCommand.js
+++ test/spec/functional/addCommand.js
@@ -2,22 +2,24 @@ describe('addCommand', function() {
 
     before(h.setup());
 
+    function getUrlAndTitle() {
+
+        var result = {};
+
+        return this.url().then(function(url) {
+            result.url = url.value;
+        }).getTitle().then(function(title) {
+            result.title = title;
+        }).then(function() {
+            return result;
+        });
+
+    }
+
     describe('add regular command', function() {
 
         before(function() {
-            this.client.addCommand('getUrlAndTitle', function() {
-
-                var result = {};
-
-                return this.url().then(function(url) {
-                    result.url = url.value;
-                }).getTitle().then(function(title) {
-                    result.title = title;
-                }).then(function() {
-                    return result;
-                });
-
-            });
+            this.client.addCommand('getUrlAndTitle', getUrlAndTitle);
         });
 
         it('added a `getUrlAndTitle` command', function() {
@@ -54,6 +56,52 @@ describe('addCommand', function() {
             }, true);
 
             return this.client.shake().then(function(res) {
+                expect(res).to.be.equal('should not fail');
+            });
+        });
+
+    });
+
+    describe.only('add a namespaced command', function() {
+
+        it('should add a namespaced getUrlAndTitle', function() {
+            this.client.addCommand('mynamespace', 'getUrlAndTitle', getUrlAndTitle);
+            return this.client.mynamespace.getUrlAndTitle().then(function(result) {
+                assert.strictEqual(result.url, conf.testPage.start);
+                assert.strictEqual(result.title, conf.testPage.title);
+            });
+        });
+
+        it('should not allow overwriting internal functions', function() {
+            var self = this;
+
+            (function(){
+                self.client.addCommand('shake', 'mycommand', function() {
+                    return 'should fail';
+                }, true);
+            }).should.throw();
+        });
+
+        it('should not overwrite commands by default', function() {
+            this.client.addCommand('mynamespace', 'mycommand', getUrlAndTitle);
+
+            var self = this;
+
+            (function(){
+                self.client.addCommand('mynamespace', 'mycommand', function() {
+                    return 'should fail';
+                });
+            }).should.throw();
+        });
+
+        it('should overwrite existing namespaced method if overwrite flag is given', function() {
+            this.client.addCommand('mynamespace', 'mysecondcommand', getUrlAndTitle);
+
+            this.client.addCommand('mynamespace', 'mysecondcommand', function() {
+                return 'should not fail';
+            }, true);
+
+            return this.client.mynamespace.mysecondcommand().then(function(res) {
                 expect(res).to.be.equal('should not fail');
             });
         });
