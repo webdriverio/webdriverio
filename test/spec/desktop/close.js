8bc943b9a2cc3bf66f13349154174f5dcf5ad3b0
W.van Kuipers <w.vankuipers@lukkien.com>
2015-11-24 08:18:06 +0100

    Added tests for the switchTab bugfix
    Former-commit-id: 62e8cf0c5ebaee91242f9ea25ec6d2c9942dc130

--- test/spec/desktop/close.js
+++ test/spec/desktop/close.js
@@ -42,4 +42,43 @@ describe('close', function() {
 
     });
 
+    it('should activate the next window when closing the current window', function() {
+
+        /**
+         * safari doenst support `newWindow`
+         */
+        if(this.client.desiredCapabilities.browserName === 'safari') {
+            return;
+        }
+
+        var openTab;
+
+        return this.client
+
+            // get current tab id
+            .getTabIds().then(function(tabs) {
+                openTab = tabs[0];
+            })
+
+            // open new tab
+            .newWindow(conf.testPage.subPage)
+
+            // ensure that there are two tabs open
+            .getTabIds().then(function(tabs) {
+                tabs.should.have.length(2);
+            })
+
+            // command needs to be executed within new function context
+            // to have access to the windowHandle assigned in L23
+            .call(function() {
+                return this.close();
+            })
+
+            // test if there is only one tab open
+            .windowHandle().then(function(windowHandle) {
+                windowHandle.value.should.be.exactly(openTab);
+            });
+
+    });
+
 });
