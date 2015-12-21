0e382d19cd42fb3fbe8655fc2ccc97d874ab50f5
christian-bromann <contact@christian-bromann.com>
2015-12-21 04:22:19 +0100

    rewritten conf files

--- test/conf/mobile.js
+++ test/conf/mobile.js
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
     desiredCapabilities: {
         browserName: process.env._BROWSER || '',
-        platformName: (process.env._PLATFORM || '').replace(/_/g,' '),
+        platformName: (process.env._PLATFORM || '').replace(/_/g, ' '),
         platformVersion: (process.env._VERSION || ''),
         'appium-version': '1.3.4',
-        deviceName: (process.env._DEVICENAME || '').replace(/_/g,' ')
+        deviceName: (process.env._DEVICENAME || '').replace(/_/g, ' ')
     }
-};
+}
