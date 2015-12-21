01a4265d125821bbf5b08cb57e0493d550037f49
christian-bromann <contact@christian-bromann.com>
2015-12-21 21:07:16 +0100

    rewritten multibrowser tests

--- test/spec/multibrowser/execute.js
+++ test/spec/multibrowser/execute.js
@@ -1,33 +1,29 @@
-describe('execute', function() {
-    before(h.setupMultibrowser());
+import conf from '../../conf/index'
 
-    before(function() {
-        this.browserA.url(conf.testPage.subPage);
-        this.browserB.url(conf.testPage.start);
-        return this.matrix.sync();
-    });
+describe('execute', () => {
+    beforeEach(async function () {
+        this.browserA.url(conf.testPage.subPage)
+        this.browserB.url(conf.testPage.start)
+        await this.client.sync()
+    })
 
-    it('should be able to execute some js', function() {
-        return this.matrix.execute('return document.title').then(function(browserA, browserB) {
-            browserA.value.should.be.equal('two');
-            browserB.value.should.be.equal(conf.testPage.title);
-        });
-    });
+    it('should be able to execute some js', async function () {
+        const { browserA, browserB } = await this.client.execute('return document.title')
+        browserA.value.should.be.equal('two')
+        browserB.value.should.be.equal(conf.testPage.title)
+    })
 
-    it('should be forgiving on giving an `args` parameter', function() {
-        return this.matrix.execute('return document.title').then(function(browserA, browserB) {
-            browserA.value.should.be.equal('two');
-            browserB.value.should.be.equal(conf.testPage.title);
-        });
-    });
+    it('should be forgiving on giving an `args` parameter', async function () {
+        const { browserA, browserB } = await this.client.execute('return document.title')
+        browserA.value.should.be.equal('two')
+        browserB.value.should.be.equal(conf.testPage.title)
+    })
 
-    it('should be able to execute a pure function', function() {
-        return this.matrix.execute(function() {
-            return document.title;
-        }).then(function(browserA, browserB) {
-            browserA.value.should.be.equal('two');
-            browserB.value.should.be.equal(conf.testPage.title);
-        });
-    });
-
-});
+    it('should be able to execute a pure function', async function () {
+        const { browserA, browserB } = await this.client.execute(function () {
+            return document.title
+        })
+        browserA.value.should.be.equal('two')
+        browserB.value.should.be.equal(conf.testPage.title)
+    })
+})
