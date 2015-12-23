92db14123f6f0e93821733fb86f0d4d8a585a45b
Dmitry Sorin <dsorin@yandex-team.ru>
2015-12-23 10:28:45 +0300

    add functional tests for connectionRetryCount and connectionRetryTimeout options

---
+++ test/spec/functional/connectionRetries.js
@@ -0,0 +1,78 @@
+var merge = require('deepmerge');
+var nock = require('nock');
+var conf = require('../../conf/index.js');
+var WebdriverIO = require('../../../index.js');
+
+var FAKE_SUCCESS_RESPONSE = {
+    sessionId: '31571f3a-4824-4378-b352-65de24952903',
+    status: 0
+};
+
+describe('connection retries', function() {
+    afterEach(function () {
+        nock.cleanAll();
+    });
+
+    it('should retry connection 3 times if network error occurs', function () {
+        // mock 2 request errors and third successful
+        nock('http://localhost:4444')
+            .post('/wd/hub/session')
+                .twice()
+                .replyWithError('some error')
+            .post('/wd/hub/session')
+                .reply(200, FAKE_SUCCESS_RESPONSE);
+
+        return WebdriverIO.remote(conf).init();
+    });
+
+    it('should fail with ECONNREFUSED if all connection attempts failed', function () {
+        // mock 3 request errors
+        nock('http://localhost:4444')
+            .post('/wd/hub/session')
+            .thrice()
+            .replyWithError('some error');
+
+        return WebdriverIO.remote(conf).init().catch(function (err) {
+            expect(err).not.to.be.undefined;
+            expect(err.message).to.equal('Couldn\'t connect to selenium server');
+            expect(err.seleniumStack.type).to.equal('ECONNREFUSED');
+        });
+    });
+
+    it('should use connectionRetryTimeout option in requests retrying', function () {
+        // mock 1 slow request and 1 successful one
+        nock('http://localhost:4444')
+            .post('/wd/hub/session')
+                .delayConnection(5000)
+                .reply(200, 'some response')
+            .post('/wd/hub/session')
+                .reply(200, FAKE_SUCCESS_RESPONSE);
+
+        var start = Date.now();
+
+        var localConf = merge({}, conf);
+        localConf.connectionRetryTimeout = 3000;
+
+        return WebdriverIO.remote(localConf).init().then(function () {
+            var connectionTime = Date.now() - start;
+
+            expect(connectionTime).to.be.above(3000);
+            expect(connectionTime).to.be.below(4000);
+        });
+    });
+
+    it('should use connectionRetryCount option in requests retrying', function () {
+        // mock 12 request errors
+        nock('http://localhost:4444')
+            .post('/wd/hub/session')
+                .times(12)
+                .replyWithError('some error')
+            .post('/wd/hub/session')
+                .reply(200, FAKE_SUCCESS_RESPONSE);
+
+        var localConf = merge({}, conf);
+        localConf.connectionRetryCount = 15;
+
+        return WebdriverIO.remote(localConf).init();
+    });
+});
