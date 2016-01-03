30ad69282d7329c1919cde57dc314ad086c17bc7
christian-bromann <contact@christian-bromann.com>
2016-01-03 13:49:05 +0100

    make sure exclude/specs properties work for each capability - closes #924
    Former-commit-id: fb508a2d27f00ee97c9824212a7825d19485b885

---
+++ test/spec/unit/configparser.js
@@ -0,0 +1,27 @@
+import path from 'path'
+import ConfigParser from '../../../lib/utils/ConfigParser'
+
+const FIXTURES_PATH = __dirname + '/../../fixtures'
+
+describe('ConfigParser', () => {
+    it('should exclude files', () => {
+        let configParser = new ConfigParser()
+        configParser.addConfigFile(FIXTURES_PATH + '/exclude.conf.js')
+        let specs = configParser.getSpecs()
+        specs.should.not.include(__filename)
+        specs.should.include(__dirname + '/call.js')
+    })
+
+    it('should exclude/include capability excludes', () => {
+        let configParser = new ConfigParser()
+        configParser.addConfigFile(FIXTURES_PATH + '/exclude2.conf.js')
+        let specs = configParser.getSpecs(
+            [FIXTURES_PATH + '/exclude*'],
+            [__dirname + '/call.js']
+        )
+        specs.should.not.include(__filename)
+        specs.should.not.include(__dirname + '/call.js')
+        specs.should.include(path.resolve(FIXTURES_PATH + '/exclude.conf.js'))
+        specs.should.include(path.resolve(FIXTURES_PATH + '/exclude2.conf.js'))
+    })
+})
