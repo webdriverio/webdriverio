c2f3662d0174ee48b8a2e757219edf7c8c85937f
George Crawford <george.crawford@ft.com>
2016-01-07 12:59:08 +0000

    Add (failing, broken) test for custom command
    Former-commit-id: a41159dbb4eb81a8948c352e8046bb29718d7365

--- test/spec/functional/staleElementRetry.js
+++ test/spec/functional/staleElementRetry.js
@@ -7,4 +7,25 @@ describe('staleElementRetry', () => {
             }
         })
     })
+
+    describe('custom command', () => {
+        it('does not throw staleElementReference exception when waiting for element to become invisible but which is removed from DOM in a custom command', async function () {
+            this.client.addCommand('waitForVisibleInParallel', function async () {
+                const promises = []
+                for (let i = 1; i <= 8; i++) {
+                    promises.push(this.client.waitForVisible('.staleElementContainer2 .stale-element-container-row-' + i, 10000, true))
+                }
+
+                return Promise.all(promises)
+                .then(function (results) {
+                    results.map(function (result) {
+                        result.should.be.true
+                    })
+                })
+            })
+
+            this.client.waitForVisibleInParallel()
+            throw Error("not currently working, as the Promise.all() doesn't seem to wait for the commands to execute")
+        })
+    })
 })
