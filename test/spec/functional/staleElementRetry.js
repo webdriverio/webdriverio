c233c1101a3f5e3e0ff77364dc26caa09233e2d9
christian-bromann <contact@christian-bromann.com>
2016-01-15 03:37:05 +0100

    skip stale element retry tests for now (feature not yet possible without wdio)
    Former-commit-id: 9be476b7afd12411f9200f826848ccd8cd25d6dd

--- test/spec/functional/staleElementRetry.js
+++ test/spec/functional/staleElementRetry.js
@@ -1,26 +1,24 @@
 import conf from '../../conf/index.js'
 
-describe('staleElementRetry', () => {
+describe.skip('staleElementRetry', () => {
     describe('basic command', () => {
         it('does not throw staleElementReference exception when getting visibility of elements rapidly removed from DOM', async function () {
             let iterations = 100
             await this.client.url(conf.testPage.staleTest)
-            console.log(await this.client.getTitle())
             while (iterations--) {
                 console.log(iterations)
                 expect(await this.client.isVisible('.staleElementContainer1 .stale-element-container-row')).to.be.true
-                // await this.client.isVisible('.staleElementContainer1 .stale-element-container-row')
             }
         })
     })
 
-    describe.skip('custom command', () => {
+    describe('custom command', () => {
         before(async function() {
             await this.client.url(conf.testPage.staleTest)
         })
 
         it('does not throw staleElementReference exception when waiting for element to become invisible but which is removed from DOM in a custom command', async function () {
-            this.client.addCommand('waitForVisibleInParallel', function async () {
+            this.client.addCommand('waitForVisibleInParallel', async () => {
                 const promises = []
                 for (let i = 1; i <= 8; i++) {
                     promises.push(this.client.waitForVisible('.staleElementContainer2 .stale-element-container-row-' + i, 10000, true))
@@ -34,8 +32,7 @@ describe('staleElementRetry', () => {
                 })
             })
 
-            this.client.waitForVisibleInParallel()
-            throw Error("not currently working, as the Promise.all() doesn't seem to wait for the commands to execute")
+            await this.client.waitForVisibleInParallel()
         })
     })
 })
