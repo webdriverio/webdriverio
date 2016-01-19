21cd1c700c78360f04317bda53b76fbe10d49d91
christian-bromann <contact@christian-bromann.com>
2016-01-19 03:23:18 +0100

    added wdio integration test setup with error handler tests
    Former-commit-id: 41926ae8ce1799dcbd72234e993e94f5292780a5

--- test/conf/index.js
+++ test/conf/index.js
@@ -4,7 +4,7 @@ import defaults from './defaults.js'
 const ENV = process.env.TRAVIS && process.env._BROWSER !== 'phantomjs' && process.env._ENV !== 'multibrowser' ? 'travis-ci' : 'local'
 let asked = require(`./${ENV}.js`)
 
-if (process.env._ENV.match(/(android|ios)/)) {
+if (process.env._ENV && process.env._ENV.match(/(android|ios)/)) {
     const mobile = require('./mobile')
     asked = merge(asked, mobile)
 }
