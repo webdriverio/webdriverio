e516c198510e3755c6f6fabcbd9a47cde6968ec8
christian-bromann <contact@christian-bromann.com>
2016-01-20 18:03:38 +0100

    rewrote ported PR #877 into es2016
    Former-commit-id: c44ed4f0718a0051d939f61cc0bc2dada13c5658

--- test/spec/getUrl.js
+++ test/spec/getUrl.js
@@ -1,11 +1,7 @@
-describe('getUrl', function() {
+import conf from '../conf/index'
 
-    before(h.setup());
-
-    it('should return the url of the current webpage', function() {
-        return this.client.getUrl().then(function (url) {
-            url.should.be.exactly(conf.testPage.start);
-        });
-    });
-
-});
+describe('getTitle', () => {
+    it('should return the url of the current webpage', async function () {
+        (await this.client.getUrl()).should.be.equal(conf.testPage.start)
+    })
+})
