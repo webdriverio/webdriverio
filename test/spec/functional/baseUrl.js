ee993ce97927b9b3b98a1f8f9f31d13c615aa591
christian-bromann <contact@christian-bromann.com>
2016-01-22 03:57:02 +0100

    use grunt-contrib-server to run server
    Former-commit-id: a2796cf669aaf930ed26e52a111e7c2c83c96588

--- test/spec/functional/baseUrl.js
+++ test/spec/functional/baseUrl.js
@@ -1,6 +1,6 @@
 describe('baseUrl', () => {
     it('should get prepended if url starts with /', async function () {
-        await this.client.url('/test/site/www/two.html');
+        await this.client.url('/two.html');
         (await this.client.getTitle()).should.be.equal('two')
     })
 })
