1a06625852ca2aea12809a38a552c5b29edf87d2
christian-bromann <contact@christian-bromann.com>
2016-01-24 22:46:14 +0100

    minor build tweaks

--- test/conf/mobile.js
+++ test/conf/mobile.js
@@ -4,10 +4,9 @@ export default {
         app: process.env._APP,
         appPackage: 'io.webdriverjs.example',
         appActivity: '.MainActivity',
-        browserName: '',
         platformName: (process.env._PLATFORM || '').replace(/_/g, ' '),
         platformVersion: (process.env._VERSION || ''),
         deviceName: (process.env._DEVICENAME || '').replace(/_/g, ' '),
-        'appium-version': '1.4.13'
+        'appium-version': '1.4.16'
     }
 }
