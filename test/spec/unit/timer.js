a53618d2f5209ce8831052243c7d4163d6f2d3e9
christian-bromann <contact@christian-bromann.com>
2016-04-16 16:24:42 +0200

    fixed timer tests for node v5 - closes #1241
    Former-commit-id: 826b9f915dced061811fa1c84b8dad2e726225d5

--- test/spec/unit/timer.js
+++ test/spec/unit/timer.js
@@ -1,81 +1,42 @@
 import Timer from '../../../lib/utils/Timer.js'
 import sinon from 'sinon'
+import assert from 'assert'
 
-describe('timer', function() {
-
-    describe('ticks', function() {
-
-        let timer
-        let spy
-        let clock
-
-        beforeEach(function() {
-            sinon.stub(process, 'nextTick').yields()
-            clock = sinon.useFakeTimers(Date.now())
-
-            spy = sinon.spy(() => Promise.resolve())
-            timer = new Timer(20, 100, spy, false)
-        })
-
-        afterEach(function() {
-            process.nextTick.restore()
-            clock.restore()
-        })
-
-        it('should tick once', function() {
-            clock.tick(20)
-            expect(spy.calledOnce).to.be.true
-        })
-
-        it('should tick many times', function() {
-            clock.tick(80)
-            expect(spy.callCount).to.be.equal(5)
-        })
-
-        it('should not tick after timeout', function() {
-            clock.tick(300)
-            expect(spy.callCount).to.be.equal(5)
-        })
-
-    })
-
-    describe('promise', function() {
-
-        it('should be rejected by timeout', function() {
+describe('timer', () => {
+    describe('promise', () => {
+        it('should be rejected by timeout', () => {
             let timer = new Timer(20, 30, () => Promise.resolve(false))
 
-            return timer.then(assert.fail, function(msg) {
+            return timer.then(assert.fail, (msg) => {
                 expect(msg).to.be.equal('timeout')
             })
         })
 
-        it('should be fulfilled when resolved with true value', function() {
+        it('should be fulfilled when resolved with true value', () => {
             let timer = new Timer(20, 30, () => Promise.resolve(true))
 
             return timer.then(assert.isTrue, assert.fail)
         })
 
-        it('should not be fulfilled when resolved with false value', function() {
+        it('should not be fulfilled when resolved with false value', () => {
             let timer = new Timer(20, 30, () => Promise.resolve(false))
 
             return timer.then(assert.fail, assert.ok)
         })
 
-        it('should be rejected', function() {
+        it('should be rejected', () => {
             let timer = new Timer(20, 30, () => Promise.reject('err'))
 
-            return timer.then(assert.fail).catch(function(msg) {
+            return timer.then(assert.fail).catch((msg) => {
                 expect(msg).to.be.equal('err')
             })
         })
-
     })
 
-    it('should have leading fn call', function() {
+    it('should have leading fn call', () => {
         let spy = sinon.spy(() => Promise.resolve(true))
         let timer = new Timer(100, 100, spy, true)
 
         return timer.then(assert.isTrue)
     })
-
 })
