ceaccf75dfa86746389713c9709ded9bb5e1f861
Minkyu Cho <mrnoname@naver.com>
2016-04-27 17:09:42 +0900

    Add missing commas

--- examples/wdio.conf.js
+++ examples/wdio.conf.js
@@ -252,7 +252,7 @@ exports.config = {
     // Gets executed after all workers got shut down and the process is about to exit. It is not
     // possible to defer the end of the process using a promise.
     onComplete: function (exitCode) {
-    }
+    },
     //
     // Cucumber specific hooks
     beforeFeature: function (feature) {
