5c63ef6e19021ab9d50e0cd4e2d0922755a1349a
christian-bromann <contact@christian-bromann.com>
2016-05-05 10:45:11 -0700

    fixed remote settings - closes #1280
    Former-commit-id: 690b21759ed945d9104074e0ecd075d401d06716

--- test/fixtures/exclude2.conf.js
+++ test/fixtures/exclude2.conf.js
@@ -1,6 +1,8 @@
 var ROOT = __dirname + '/../..'
 
 exports.config = {
+    host: '172.168.0.1',
+    port: 4445,
     specs: [ROOT + '/test/spec/unit/*'],
     exclude: [ROOT + '/test/spec/unit/configparser.js'],
     capabilities: {
