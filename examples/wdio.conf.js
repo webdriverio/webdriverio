371563712ea9ce7e4ea7428cd9a7511ed4a8a07d
Sébastien David <sdavid@alkemics.com>
2016-05-18 18:40:21 +0200

    add beforeSession hook

--- examples/wdio.conf.js
+++ examples/wdio.conf.js
@@ -205,6 +205,10 @@ exports.config = {
     onPrepare: function (config, capabilities) {
     },
     //
+    // Gets executed just before initialising webdriver session
+    beforeSession: function (config, capabilities, specs) {
+    },
+    //
     // Gets executed before test execution begins. At this point you can access to all global
     // variables, such as `browser`. It is the perfect place to define custom commands.
     before: function (capabilities, specs) {
