bc080ede6bb49c9ff62f88904d368fc57372c036
christian-bromann <contact@christian-bromann.com>
2016-06-17 19:42:05 -0700

    waitUntil improvements - run wdio commands in condition synchronously or asynchronously if function name is async or sync options in config is false - allow to set a custom error message for waitUntil command - fixed raise condition in Timer.js where _reject is not defined when condition failed/timed out - let user know that only promises are valid as return value - added tests for sync/async usage of waitUntil - adapted command docs
    closes #1393, #1377, #1359

---
+++ test/fixtures/specs/waitUntil.async.spec.js
@@ -0,0 +1,42 @@
+import q from 'q'
+import conf from '../../conf/index.js'
+
+describe('waitUntil async', () => {
+    beforeEach(() => {
+        return browser.url(conf.testPage.start)
+    })
+
+    /**
+     * just to make sure we actually running async mode
+     */
+    it('command result should be a promise', () => {
+        q.isPromiseAlike(browser.status()).should.be.equal(true)
+    })
+
+    it('should bexecute wdio commands in condition synchronously', () => {
+        const rand = Math.floor(Math.random() * 10e10)
+        return browser.execute((_rand) => {
+            setTimeout(function () {
+                window._waitUntilTest = _rand
+            }, 2000)
+        }, rand).waitUntil(() => {
+            return browser.execute(() => window._waitUntilTest).then((res) => {
+                return res.value === rand
+            })
+        }, 3000)
+    })
+
+    it('should allow to define own error message', () => {
+        const errorMsg = 'my own error message'
+        return browser.waitUntil(() => false, 100, errorMsg).catch((e) => {
+            expect(e.message).to.be.equal(errorMsg)
+
+        /**
+         * second catch required here to not let the test fail
+         * ToDo fix promise bug
+         */
+        }).catch((e) => {
+            expect(e.message).to.be.equal(errorMsg)
+        })
+    })
+})
---
+++ test/spec/wdio/waitUntil.js
@@ -0,0 +1,21 @@
+import path from 'path'
+import Launcher from '../../../build/lib/launcher'
+
+const FIXTURE_ROOT = path.join(__dirname, '..', '..', 'fixtures')
+
+describe('waitUntil', () => {
+    // it('should to execute commands in condition synchronously', async function () {
+    //     let launcher = new Launcher(path.join(FIXTURE_ROOT, 'waitUntil.conf'), {
+    //         suite: 'sync'
+    //     })
+    //     expect(await launcher.run()).to.be.equal(0, 'wdio command failed unexpected')
+    // })
+
+    it('should to execute commands in condition asynchronously', async function () {
+        let launcher = new Launcher(path.join(FIXTURE_ROOT, 'waitUntil.conf'), {
+            suite: 'async',
+            sync: false
+        })
+        expect(await launcher.run()).to.be.equal(0, 'wdio command failed unexpected')
+    })
+})
