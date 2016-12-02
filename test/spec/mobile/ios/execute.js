d04506875d8423299a3d02495e94b2c7d637eb1c
Dierkens, Christopher <CDierkens@contractor.lb.com>
2016-12-02 13:55:39 -0500

    Added javasctipt string tests for native and webview contexts

--- test/spec/mobile/ios/execute.js
+++ test/spec/mobile/ios/execute.js
@@ -1,11 +1,30 @@
+import conf from '../../../conf/index'
 import labels from '../../../fixtures/labels'
 
-describe('execute', () => {
+describe('native context execute', () => {
     before(async function () {
         await this.client.context(labels.NATIVE_APP_CONTEXT)
     })
 
-    it('should be able to execute native function objects', async function () {
+    it('should be able to execute function objects', async function () {
         (await this.client.execute((a) => a + 2, 3)).value.should.be.equal(5)
     })
+
+    it('should be able to execute strings', async function () {
+        (await this.client.execute('return 3 + 2')).value.should.be.equal(5)
+	})
+})
+
+describe('webview context execute', () => {
+    before(async function () {
+        await this.client.context(labels.WEBVIEW_CONTEXT)
+    })
+
+    it('should be able to execute function objects', async function () {
+        (await this.client.execute((a) => a + 2, 3)).value.should.be.equal(5)
+    })
+
+    it('should be able to execute strings', async function () {
+        (await this.client.execute('return 3 + 2')).value.should.be.equal(5)
+	})
 })
