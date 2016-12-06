322754d52409543809183d5dfe418cbe2e1e7cd0
christian-bromann <mail@christian-bromann.com>
2016-12-06 15:34:30 +0100

    enhanced execute tests for ios - closes #1707
    Former-commit-id: 0212ac198407387f70b16c1677704392d3728364

--- test/spec/mobile/ios/execute.js
+++ test/spec/mobile/ios/execute.js
@@ -1,11 +1,39 @@
-import labels from '../../../fixtures/labels'
+describe.only('execute', () => {
+    let contexts
 
-describe('execute', () => {
     before(async function () {
-        await this.client.context(labels.NATIVE_APP_CONTEXT)
+        contexts = (await this.client.contexts()).value
     })
 
-    it('should be able to execute native function objects', async function () {
-        (await this.client.execute((a) => a + 2, 3)).value.should.be.equal(5)
+    describe('NATIVE context', () => {
+        before(async function () {
+            await this.client.context(contexts[0])
+        })
+
+        /**
+         * can't be executed in native context due to wrong ios driver behavior
+         */
+        it.skip('should be able to execute native function objects', async function () {
+            (await this.client.execute((a) => a + 2, 3)).value.should.be.equal(5)
+        })
+
+        it('should be able to execute strings', async function () {
+            (await this.client.execute('3 + 2')).value.should.be.equal(5) // no correct according protocol
+            // (await this.client.execute('return 3 + 2')).value.should.be.equal(5)
+        })
+    })
+
+    describe('WEBVIEW context', () => {
+        before(async function () {
+            await this.client.context(contexts[1])
+        })
+
+        it('should be able to execute native function objects', async function () {
+            (await this.client.execute((a) => a + 2, 3)).value.should.be.equal(5)
+        })
+
+        it('should be able to execute strings', async function () {
+            (await this.client.execute('return 3 + 2')).value.should.be.equal(5)
+        })
     })
 })
