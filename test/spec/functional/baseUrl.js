f59fa5550fe92aa624160f0adb35bc254b7e47fc
tormozz48 <tormozz48@gmail.com>
2017-01-20 20:20:25 +0300

    Add ability to navigate by relative urls without pathname parts
    Former-commit-id: 75aecae435e5d94ec03e82ca70c8bd630e2387c2

--- test/spec/functional/baseUrl.js
+++ test/spec/functional/baseUrl.js
@@ -3,4 +3,14 @@ describe('baseUrl', () => {
         await this.client.url('/two.html');
         (await this.client.getTitle()).should.be.equal('two')
     })
+
+    it('should get prepended if url starts with ?', async function () {
+        await this.client.url('?foo=bar')
+
+        const title = await this.client.getTitle()
+        title.should.be.equal('WebdriverIO Testpage')
+
+        const url = await this.client.getUrl()
+        url.should.contain('?foo=bar')
+    })
 })
