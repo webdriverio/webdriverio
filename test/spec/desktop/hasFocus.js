398ac2faefc4e6ee0bc95edab591abe7f6423bae
marekj <marekj@marekj.com>
2017-02-01 17:29:06 -0600

    fix element.hasFocus()
    Issue webdriverio/webdriverio#1853
    
    Thanks @mattc41190 for pairing
    
    
    Former-commit-id: eb33fec4d298271fbbcc16464930663b1ed0c5c7

--- test/spec/desktop/hasFocus.js
+++ test/spec/desktop/hasFocus.js
@@ -1,9 +1,29 @@
 describe('hasFocus', () => {
+    it('should return true when one of the element type is active.', async function () {
+        (await this.client.hasFocus('input')).should.be.true
+    })
+
     it('should return true if element is active', async function () {
         (await this.client.hasFocus('[name="login"]')).should.be.true
     })
 
     it('should return false on inactive elements', async function () {
         (await this.client.hasFocus('body')).should.be.false
+    })
+
+    it('should return true when selector is used as context and has focus', async function () {
+        (await this.client.element('[name="login"]').hasFocus()).should.be.true
+    })
+
+    it('should return false when selector is used as context and does not have focus', async function () {
+        (await this.client.element('body').hasFocus()).should.be.false
+    })
+
+    it('should return true when one of the elements collection is active and is using `elements()`', async function () {
+        (await this.client.elements('input').hasFocus()).should.be.true
+    })
+
+    it('should return true when one of the elements collection is active and is using `element()`', async function () {
+        (await this.client.element('input').hasFocus()).should.be.true
     })
 })
