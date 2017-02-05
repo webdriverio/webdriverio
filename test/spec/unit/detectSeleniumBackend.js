488df9571c7b1d4c691c70a3d93653b3b1325c7b
christian-bromann <mail@christian-bromann.com>
2017-02-05 17:59:37 +0100

    don't detect Sauce Labs as Selenium backend if host is given - closes #1857
    Former-commit-id: 06b6aced46c86d8975f339032c36315fb6cfe8ff

---
+++ test/spec/unit/detectSeleniumBackend.js
@@ -0,0 +1,45 @@
+import detectSeleniumBackend from '../../../lib/helpers/detectSeleniumBackend'
+
+describe('WebdriverIO detects Selenium backend', () => {
+    it('should not set anything if host is set in caps', () => {
+        const caps = {
+            host: '0.0.0.0',
+            port: 1234
+        }
+        detectSeleniumBackend(caps).should.be.deep.equal({})
+    })
+
+    it('should default to local selenium server', () => {
+        const caps = detectSeleniumBackend({})
+        caps.host.should.be.deep.equal('127.0.0.1')
+        caps.port.should.be.deep.equal(4444)
+    })
+
+    it('should detect browserstack user', () => {
+        const caps = detectSeleniumBackend({
+            user: 'foobar',
+            key: 'zHcv9sZ39ip8ZPsxBVJ2'
+        })
+        caps.host.should.be.deep.equal('hub.browserstack.com')
+        caps.port.should.be.deep.equal(80)
+    })
+
+    it('should detect testingbot user', () => {
+        const caps = detectSeleniumBackend({
+            user: 'foobar',
+            key: 'ec337d7b677720a4dde7bd72be0bfc67'
+        })
+        caps.host.should.be.deep.equal('hub.testingbot.com')
+        caps.port.should.be.deep.equal(80)
+    })
+
+    it('should detect saucelabs user', () => {
+        const caps = detectSeleniumBackend({
+            user: 'foobar',
+            key: '50aa152c-1932-B2f0-9707-18z46q2n1mb0'
+        })
+        caps.host.should.be.deep.equal('ondemand.saucelabs.com')
+        caps.port.should.be.deep.equal(443)
+        caps.protocol.should.be.deep.equal('https')
+    })
+})
