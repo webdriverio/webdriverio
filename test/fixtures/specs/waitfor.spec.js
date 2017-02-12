5ec6d5b6f3be4e7db79d05fbd52e6922270decec
christian-bromann <mail@christian-bromann.com>
2017-02-12 18:26:29 +0100

    fix scoping of waitFor calls - closes #1869

--- test/fixtures/specs/waitfor.spec.js
+++ test/fixtures/specs/waitfor.spec.js
@@ -20,56 +20,99 @@ const PageObject = Object.create({}, {
     visible: { get: () => browser.element(PageObject.visibleSelector) }
 })
 
-describe('waitUntil', () => {
+let errorWasThrown = false
+
+describe('waitFor', () => {
     beforeEach(() => {
         browser.url(conf.testPage.start)
+        errorWasThrown = false
     })
 
     it('should have selector in error message executing waitForEnabled', () => {
         try {
             PageObject.enabled.waitForEnabled(1)
         } catch (e) {
+            errorWasThrown = true
             expect(e.message).to.contain(PageObject.enabledSelector)
         }
+
+        expect(errorWasThrown).to.be.ok
     })
 
     it('should have selector in error message executing waitForExist', () => {
         try {
             PageObject.exist.waitForExist(1)
         } catch (e) {
+            errorWasThrown = true
             expect(e.message).to.contain(PageObject.existSelector)
         }
+
+        expect(errorWasThrown).to.be.ok
+    })
+
+    it('should be able to wait if only a timeout is given', () => {
+        try {
+            PageObject.exist.waitForExist(5500)
+        } catch (e) {
+            errorWasThrown = true
+            expect(e.message).to.contain(PageObject.existSelector)
+        }
+
+        expect(errorWasThrown).to.be.not.ok
+    })
+
+    it('should not find elements that are not within child tree', () => {
+        const elem = $('=3')
+        try {
+            elem.waitForExist('.page')
+        } catch (e) {
+            errorWasThrown = true
+        }
+
+        expect(errorWasThrown).to.be.ok
     })
 
     it('should have selector in error message executing waitForSelected', () => {
         try {
             PageObject.selected.waitForSelected(1)
         } catch (e) {
+            errorWasThrown = true
             expect(e.message).to.contain(PageObject.selectedSelector)
         }
+
+        expect(errorWasThrown).to.be.ok
     })
 
     it('should have selector in error message executing waitForText', () => {
         try {
             PageObject.text.waitForText(1)
         } catch (e) {
+            errorWasThrown = true
             expect(e.message).to.contain(PageObject.textSelector)
         }
+
+        expect(errorWasThrown).to.be.ok
     })
 
     it('should have selector in error message executing waitForValue', () => {
         try {
             PageObject.value.waitForValue(1)
         } catch (e) {
+            errorWasThrown = true
             expect(e.message).to.contain(PageObject.valueSelector)
         }
+
+        expect(errorWasThrown).to.be.ok
     })
 
     it('should have selector in error message executing waitForVisible', () => {
         try {
             PageObject.visible.waitForVisible(1)
         } catch (e) {
+            errorWasThrown = true
             expect(e.message).to.contain(PageObject.visibleSelector)
         }
+
+        expect(errorWasThrown).to.be.ok
     })
 })
