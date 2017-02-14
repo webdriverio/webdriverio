49b5cc6dbc66608a6a772c09024e5705eab9bf9b
Ruslan Login <log1n@yandex-team.ru>
2017-02-14 16:31:44 +0300

    Add timestamp field to commandList.
    getCommandHistory now returns command history with timestamps of their execution.
    
    
    Former-commit-id: 11b6deb8a853ba17614538ca1a91586a72efcc21

--- test/spec/functional/getCommandHistory.js
+++ test/spec/functional/getCommandHistory.js
@@ -1,17 +1,27 @@
+const sinon = require('sinon')
+
 describe('getCommandHistory', () => {
     it('text should be visible after clicking on .btn1', async function () {
-        await this.client.getTitle()
+        const clock = sinon.useFakeTimers()
+
+        await this.client.getTitle().then(() => clock.tick(100))
         await this.client.click('body')
 
         const commandList = (await this.client.getCommandHistory()).slice(-5)
         commandList[0].name.should.be.equal('getTitle')
         commandList[0].args.should.have.length(0)
+        commandList[0].timestamp.should.be.equal(0)
         commandList[1].name.should.be.equal('title')
-        commandList[0].args.should.have.length(0)
+        commandList[1].args.should.have.length(0)
+        commandList[1].timestamp.should.be.equal(0)
         commandList[2].name.should.be.equal('click')
         commandList[2].args[0].should.be.equal('body')
         commandList[3].name.should.be.equal('element')
         commandList[3].args[0].should.be.equal('body')
+        commandList[3].timestamp.should.be.equal(100)
         commandList[4].name.should.be.equal('elementIdClick')
+        commandList[4].timestamp.should.be.equal(100)
+
+        clock.restore()
     })
 })
