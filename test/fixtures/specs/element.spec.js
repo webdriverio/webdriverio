e6cc55e259966adcfdecc2e37f6dc83fc9097d2c
christian-bromann <mail@christian-bromann.com>
2017-04-19 17:23:21 +0200

    fix changes that never passed build

--- test/fixtures/specs/element.spec.js
+++ test/fixtures/specs/element.spec.js
@@ -74,32 +74,46 @@ describe('element as first class citizen', () => {
         }, 'was', 'the', 'element')).to.be.equal('body was the element')
     })
 
-    it('should add selector when returning error with errorCode 7', () => {
+    describe('error code 7 messages always contain the selector', () => {
         const Page = Object.create({}, {
             elem: { get: () => browser.element('span=notExisting') },
             elems: { get: () => browser.elements('span=notExisting') }
         })
 
-        // normal usage
-        try {
-            browser.getValue('span=notExisting')
-        } catch (e) {
-            expect(e.message).to.be.contain('given search parameters ("span=notExisting").')
-        }
+        const expectedError = 'An element could not be located on the page using the given search parameters ("span=notExisting").'
+        let givenError = null
 
-        // po usage
-        try {
-            Page.elem.getValue()
-        } catch (e) {
-            expect(e.message).to.be.contain('given search parameters ("span=notExisting").')
-        }
+        beforeEach(() => (givenError = null))
 
-        // pos usage with multiple elements
-        try {
-            Page.elems.getValue()
-        } catch (e) {
-            expect(e.message).to.be.contain('given search parameters ("span=notExisting").')
-        }
+        it('fetching elements the standard way', () => {
+            try {
+                browser.getValue('span=notExisting')
+            } catch (e) {
+                givenError = e.message
+            }
+
+            expect(givenError).to.be.equal(expectedError)
+        })
+
+        it('fetching element using PO pattern', () => {
+            try {
+                Page.elem.getValue()
+            } catch (e) {
+                givenError = e.message
+            }
+
+            expect(givenError).to.be.equal(expectedError)
+        })
+
+        it('fetching element using PO pattern', () => {
+            try {
+                Page.elems.getValue()
+            } catch (e) {
+                givenError = e.message
+            }
+
+            expect(givenError).to.be.equal(expectedError)
+        })
     })
 
     it('element calls within wait commands should not influence following action calls', () => {
@@ -124,7 +138,7 @@ describe('element as first class citizen', () => {
     })
 
     it('throws an error if user attempts to call an action on a not found element', () => {
-        let result = 'not found'
+        let result
         try {
             result = $('.nested').$('h1').getText()
         } catch (e) {
@@ -132,7 +146,7 @@ describe('element as first class citizen', () => {
         }
 
         expect(result).to.be.equal(
-            'An element ("h1") could not be located on the page using the given search parameters.'
+            'An element could not be located on the page using the given search parameters ("h1").'
         )
     })
 
@@ -165,9 +179,9 @@ describe('element as first class citizen', () => {
             }
 
             expect(error).to.be.not.equal(undefined)
-            expect(error.message).to.be.equal('Promise was rejected with the following reason: Error: ' +
-                'An element could not be located on the page using the given search parameters ' +
-                '("#notExisting").')
+            expect(error.message).to.be.equal(
+                'An element could not be located on the page using the given search parameters ("#notExisting").'
+            )
         })
     })
 
