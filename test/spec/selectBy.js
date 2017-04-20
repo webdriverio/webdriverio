eadcb72ee1c979af8c7f397ad25cb8a10ff219e6
christian-bromann <mail@christian-bromann.com>
2017-04-20 17:46:05 +0200

    missed space in selectBy test

--- test/spec/selectBy.js
+++ test/spec/selectBy.js
@@ -65,7 +65,7 @@ describe('selectBy', () => {
 
         it('should find element with spaces before and after the value', async function () {
             await this.client.selectByValue('#selectTest', 'someValue3');
-            (await this.client.getValue('#selectTest')).should.be.equal('  someValue3  ')
+            (await this.client.getValue('#selectTest')).should.be.equal('   someValue3  ')
         })
 
         it('should find element with spaces before and after the value parameter', async function () {
@@ -83,7 +83,7 @@ describe('selectBy', () => {
 
             it('should find element with spaces before and after the value', async function () {
                 await this.client.selectByAttribute('#selectTest', 'value', 'someValue3');
-                (await this.client.getValue('#selectTest')).should.be.equal('  someValue3  ')
+                (await this.client.getValue('#selectTest')).should.be.equal('   someValue3  ')
             })
 
             it('should find element with spaces before and after the value parameter', async function () {
