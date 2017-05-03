3dbc68324723cc4a6f22831621b6465b692c52b6
Christian Bromann <github@christian-bromann.com>
2017-05-03 13:54:29 +0200

    use short syntax to fetch elements
    ci skip
    
    Former-commit-id: 71c38ef70ba14499f3846b9a022ae6572f78fe58

--- examples/pageobject/pageobjects/form.page.js
+++ examples/pageobject/pageobjects/form.page.js
@@ -4,10 +4,10 @@ var formPage = Object.create(page, {
     /**
      * define elements
      */
-    username: { get: function () { return browser.element('#username'); } },
-    password: { get: function () { return browser.element('#password'); } },
-    form:     { get: function () { return browser.element('#login'); } },
-    flash:    { get: function () { return browser.element('#flash'); } },
+    username: { get: function () { return $('#username'); } },
+    password: { get: function () { return $('#password'); } },
+    form:     { get: function () { return $('#login'); } },
+    flash:    { get: function () { return $('#flash'); } },
 
     /**
      * define or overwrite page methods
