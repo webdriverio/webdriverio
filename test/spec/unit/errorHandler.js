7de4851aa47935095ff3441b1326e742627e18bb
Boris Serdiuk <just-boris@hotmail.com>
2017-08-07 15:44:22 +0200

    handle a case when error is created with details

--- test/spec/unit/errorHandler.js
+++ test/spec/unit/errorHandler.js
@@ -1,4 +1,5 @@
 import { ErrorHandler } from '../../../index'
+import { CommandError, RuntimeError } from '../../../lib/utils/ErrorHandler'
 
 describe('ErrorHandler', () => {
     it('should accessible through module object', () => {
@@ -7,7 +8,21 @@ describe('ErrorHandler', () => {
     })
 
     it('should throw selenium error when passing specific error ID', () => {
-        const error = new ErrorHandler(17)
+        const error = new CommandError(17)
         error.name.should.be.equal('Error')
+        error.message.should.be.equal('An error occurred while executing user supplied JavaScript.')
+    })
+
+    it('should report command error', () => {
+        const error = new CommandError('Some error in command', 'extra info')
+        error.name.should.be.equal('Error')
+        error.message.should.be.equal('Some error in command')
+        error.details.should.be.equal('extra info')
+    })
+
+    it('should report runtime error', () => {
+        const error = new RuntimeError('Some error in runtime')
+        error.name.should.be.equal('Error')
+        error.message.should.be.equal('Some error in runtime')
     })
 })
