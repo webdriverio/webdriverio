10d456a122aeec816d1e65f38db1a2493b2c2f0d
christian-bromann <mail@christian-bromann.com>
2017-11-08 10:32:50 +0100

    better response compatibility to w3c spec - closes #2291
    Former-commit-id: 752bb60b09cc3aeba526eae1d679a5a0fcca4e19

---
+++ test/spec/unit/utilities.js
@@ -0,0 +1,48 @@
+import { isSuccessfulResponse } from '../../../lib/helpers/utilities'
+
+describe('utilities', () => {
+    describe('isSuccessfulResponse', () => {
+        it('should fail when there is no error', () => {
+            expect(isSuccessfulResponse()).to.be.equal(false)
+        })
+
+        it('should fail when status is not 0', () => {
+            expect(isSuccessfulResponse({
+                status: 7,
+                value: 'foobar'
+            })).to.be.equal(false)
+        })
+
+        it('should fail when there is no value', () => {
+            expect(isSuccessfulResponse({
+                error: true
+            })).to.be.equal(false)
+        })
+
+        it('should fail if value has an error', () => {
+            expect(isSuccessfulResponse({
+                value: {
+                    error: 'foobar'
+                }
+            })).to.be.equal(false)
+
+            expect(isSuccessfulResponse({
+                value: {
+                    stacktrace: 'foobar'
+                }
+            })).to.be.equal(false)
+
+            expect(isSuccessfulResponse({
+                value: {
+                    stackTrace: 'foobar'
+                }
+            })).to.be.equal(false)
+        })
+
+        it('should pass otherwise', () => {
+            expect(isSuccessfulResponse({
+                value: false
+            })).to.be.equal(true)
+        })
+    })
+})
