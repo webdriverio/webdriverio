webdriverio/webdriverio

commit 4ec6516058fdbe821762b5bfc7e45ab36951b147

Create http.Agent with keep-alive enabled, fix #2321

lib/utils/RequestHandler.js
@@ -1,10 +1,13 @@
 import url from 'url'
+import http from 'http'
 import request from 'request'
 import merge from 'deepmerge'
 
 import { ERROR_CODES } from '../helpers/constants'
 import { RuntimeError } from './ErrorHandler'
 import pkg from '../../package.json'
+
+const agent = new http.Agent({ keepAlive: true })
 
 /**
  * RequestHandler
@@ -86,6 +89,7 @@ class RequestHandler {
         newOptions.json = true
         newOptions.followAllRedirects = true
 
+        newOptions.agent = agent
         newOptions.headers = {
             'Connection': 'keep-alive',
             'Accept': 'application/json',

test/spec/unit/remote.js
@@ -1,3 +1,4 @@
+import http from 'http'
 import { remote } from '../../../index.js'
 import RequestHandler from '../../../lib/utils/RequestHandler'
 import q from 'q'
@@ -37,6 +38,12 @@ describe('remote method', () => {
     it('should not force firefox when app is undefined but appPackage is not', () => {
         var client = remote({ desiredCapabilities: { browserName: '', appPackage: 'com.example' } })
         client.desiredCapabilities.browserName.should.be.equal('')
+    })
+
+    it('should create http.Agent with keep-alive enabled', () => {
+        var options = remote().requestHandler.createOptions({ path: startPath }, {})
+        options.agent.should.be.an.instanceof(http.Agent)
+        options.agent.keepAlive.should.be.equal(true)
     })
 
     it('should append query parameters to remote calls', () => {

commit 7cbf4a0c3e6b906fb155d1aee1f0bf656dd2d5da

Fix #2417 Use https Agent when protocol is https
Regression was introduced in 4ec65160 with addition of http agent

lib/utils/RequestHandler.js
@@ -1,5 +1,6 @@
 import url from 'url'
 import http from 'http'
+import https from 'https'
 import request from 'request'
 import merge from 'deepmerge'
 
@@ -8,7 +9,8 @@ import { isSuccessfulResponse } from '../helpers/utilities'
 import { RuntimeError } from './ErrorHandler'
 import pkg from '../../package.json'
 
-const agent = new http.Agent({ keepAlive: true })
+const httpAgent = new http.Agent({ keepAlive: true })
+const httpsAgent = new https.Agent({ keepAlive: true })
 
 /**
  * RequestHandler
@@ -90,7 +92,15 @@ class RequestHandler {
         newOptions.json = true
         newOptions.followAllRedirects = true
 
-        newOptions.agent = agent
+        if (this.defaultOptions.protocol === 'http') {
+            newOptions.agent = httpAgent
+        } else if (this.defaultOptions.protocol === 'https') {
+            newOptions.agent = httpsAgent
+        } else {
+            throw new RuntimeError('Unsupported protocol, must be http or https: ' +
+                this.defaultOptions.protocol)
+        }
+
         newOptions.headers = {
             'Connection': 'keep-alive',
             'Accept': 'application/json',

test/spec/unit/remote.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import https from 'https'
 import { remote } from '../../../index.js'
 import RequestHandler from '../../../lib/utils/RequestHandler'
 import q from 'q'
@@ -44,6 +45,16 @@ describe('remote method', () => {
         var options = remote().requestHandler.createOptions({ path: startPath }, {})
         options.agent.should.be.an.instanceof(http.Agent)
         options.agent.keepAlive.should.be.equal(true)
+    })
+
+    it('should create https.Agent with keep-alive enabled if protocol is https', () => {
+        var options = remote({ protocol: 'https' }).requestHandler.createOptions({ path: startPath }, {})
+        options.agent.should.be.an.instanceof(https.Agent)
+        options.agent.keepAlive.should.be.equal(true)
+    })
+
+    it('should fail when trying to create request handler options with a bad protocol', () => {
+        expect(() => remote({ protocol: 'foo' }).requestHandler.createOptions({ path: startPath }, {})).to.throw()
     })
 
     it('should append query parameters to remote calls', () => {