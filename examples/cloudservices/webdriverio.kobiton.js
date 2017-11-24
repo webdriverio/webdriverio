e78fb83b7f6f673d6087b311f7b11b52a0d98d62
Khanh Do <khanhdo.pmp@gmail.com>
2017-11-24 12:19:41 +0700

    Support Kobiton cloud service

---
+++ examples/cloudservices/webdriverio.kobiton.js
@@ -0,0 +1,29 @@
+var webdriverio = require('../../build/index'),
+  client = webdriverio.remote({
+    desiredCapabilities: {
+      sessionName: 'Automation test session',
+      sessionDescription: 'This is an example for Automation Test on Android device',
+      deviceOrientation:  'portrait',
+      captureScreenshots: true,
+      browserName:        'chrome',
+      deviceGroup:        'KOBITON',
+      deviceName:         'Galaxy Note5',
+      platformVersion:    '6.0.1',
+      platformName:       'Android'
+    },
+    protocol: 'https',
+    host: 'api.kobiton.com',
+    user: process.env.KOBITON_USERNAME,
+    key: process.env.KOBITON_ACCESS_KEY,
+    logLevel: 'verbose'
+  }).init();
+
+client
+  .url('http://google.com')
+  .setValue('*[name="q"]','webdriverio')
+  .click('*[name="btnG"]')
+  .pause(1000)
+  .getTitle(function(err,title) {
+      console.log(title);
+  })
+  .end();
