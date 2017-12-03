19f44c9c2d2fe6390963b041c22a0b53226c86d3
christian-bromann <mail@christian-bromann.com>
2017-12-03 03:56:05 +0100

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  env: {
+    node: true,
+    es6: true
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: [2, 4],
+
+    "import/no-unresolved": [2, {commonjs: true, amd: true}],
+    "import/named": 2,
+    "import/namespace": 2,
+    "import/default": 2,
+    "import/export": 2
+  }
+}
