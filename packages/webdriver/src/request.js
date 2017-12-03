458deff4866716aee08e07d0bb2b153af2006d7b
christian-bromann <mail@christian-bromann.com>
2017-12-03 12:52:09 +0100

    webdriver: basic implementation

--- packages/webdriver/src/request.js
+++ packages/webdriver/src/request.js
@@ -1,3 +1,112 @@
-export default function request () {
+import url from 'url'
+import http from 'http'
+import https from 'https'
+import request from 'request'
 
+import { isSuccessfulResponse } from './utils'
+import pkg from '../package.json'
+
+const httpAgent = new http.Agent({ keepAlive: true })
+const httpsAgent = new https.Agent({ keepAlive: true })
+
+export default class WebDriverRequest {
+    constructor (method, endpoint, body) {
+        this.method = method
+        this.endpoint = endpoint
+        this.body = body
+        this.defaultOptions = {
+            method,
+            json: true,
+            followAllRedirects: true,
+            headers: {
+                'Connection': 'keep-alive',
+                'Accept': 'application/json',
+                'User-Agent': 'webdriver/' + pkg.version
+            }
+        }
+    }
+
+    makeRequest (options, sessionId) {
+        const fullRequestOptions = this._createOptions(options, sessionId)
+        return this._request(fullRequestOptions, options.connectionRetryCount).then(({ body }) => {
+            const sessionId = body.sessionId || body.value.sessionId
+            return { body, sessionId }
+        })
+    }
+
+    _createOptions (options, sessionId) {
+        const requestOptions = {}
+
+        /**
+         * if we don't have a session id we set it here, unless we call commands that don't require session ids, for
+         * example /sessions. The call to /sessions is not connected to a session itself and it therefore doesn't
+         * require it
+         */
+        if (this.endpoint.match(/:sessionId/) && !sessionId) {
+            throw new Error('A sessionId is required for this command')
+        }
+
+        requestOptions.uri = url.parse(
+            `${options.protocol}://` +
+            `${options.hostname}:${options.port}` +
+            `${options.path}${this.endpoint.replace(':sessionId', sessionId)}`
+        )
+
+        if (options.protocol === 'http') {
+            requestOptions.agent = httpAgent
+        } else if (options.protocol === 'https') {
+            requestOptions.agent = httpsAgent
+        } else {
+            throw new Error(`Unsupported protocol, must be http or https: ${options.protocol}`)
+        }
+
+        /**
+         * Check for custom authorization header
+         */
+        if (typeof options.headers === 'object') {
+            requestOptions.headers = Object.assign(requestOptions.headers, options.headers)
+        }
+
+        /**
+         * Add query parameters to request options if it is an object
+         */
+        if (typeof this.defaultOptions.queryParams === 'object') {
+            requestOptions.qs = options.queryParams
+        }
+
+        /**
+         * send authentication credentials only when creating new session
+         */
+        if (this.endpoint === '/session' && options.auth) {
+            requestOptions.auth = this.auth
+        }
+
+        if (this.method === 'POST') {
+            requestOptions.json = this.body
+        }
+
+        return requestOptions
+    }
+
+    _request (fullRequestOptions, totalRetryCount, retryCount = 0) {
+        return new Promise((resolve, reject) => {
+            request(fullRequestOptions, (err, response, body) => {
+                /**
+                 * Resolve only if successful response
+                 */
+                if (!err && isSuccessfulResponse(response)) {
+                    return resolve({body, response})
+                }
+
+                if (retryCount >= totalRetryCount) {
+                    // ToDo make proper request error
+                    return reject(new Error('request failed'))
+                }
+
+                this._request(fullRequestOptions, totalRetryCount, ++retryCount)
+                    .then(resolve)
+                    .catch(reject)
+            })
+        })
+    }
 }
---
+++ packages/webdriver/src/utils.js
@@ -0,0 +1,44 @@
+/**
+ * check if WebDriver requests was successful
+ * @param  {Object}  body  body payload of response
+ * @return {Boolean}       true if request was successful
+ */
+export function isSuccessfulResponse ({ body, statusCode } = {}) {
+    /**
+     * response contains a body
+     */
+    if (!body) {
+        return false
+    }
+
+    /**
+     * if it has a status property, it should be 0
+     * (just here to stay backwards compatible to the jsonwire protocol)
+     */
+    if (body.status && body.status !== 0) {
+        return false
+    }
+
+    /**
+     * the body contains an actual result
+     */
+    if (typeof body.value === 'undefined') {
+        return false
+    }
+
+    /**
+     * check status code
+     */
+    if (statusCode === 200) {
+        return true
+    }
+
+    /**
+     * that has no error property (Appium only)
+     */
+    if (body.value && (body.value.error || body.value.stackTrace || body.value.stacktrace)) {
+        return false
+    }
+
+    return true
+}
