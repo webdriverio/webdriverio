44dfb5a88c6b665ca0f6eaf536abc38d90545874
christian-bromann <mail@christian-bromann.com>
2017-12-03 02:23:02 +0100

    webdriver: build basic monad structure

--- packages/webdriver/src/index.js
+++ packages/webdriver/src/index.js
@@ -1,1 +1,18 @@
-export default {}
+import webdriverMonad from './monad'
+import command from './command'
+import WebDriverProtocol from '../protocol/webdriver.json'
+
+export default class WebDriver {
+    static newSession = function (options = {}, modifier) {
+        const monad = webdriverMonad(options, modifier)
+
+        for (const [endpoint, methods] of Object.entries(WebDriverProtocol)) {
+            for (const [method, commandData] of Object.entries(methods)) {
+                monad.lift(commandData.command, command(method, endpoint, commandData))
+            }
+        }
+
+        const prototype = monad()
+        return prototype
+    }
+}
