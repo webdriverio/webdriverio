f525b2e5507ab6b1b7faf0289b4f56d4d298c483
christian-bromann <mail@christian-bromann.com>
2017-12-04 13:12:30 +0100

    wdio-config: implement basic logic for validating configs

--- packages/wdio-config/src/index.js
+++ packages/wdio-config/src/index.js
@@ -1,3 +1,25 @@
-export default function parseConfig (options) {
-    return options
+export default function validateConfig (defaults, options) {
+    const params = {}
+
+    for (const [name, expectedOption] of Object.entries(defaults)) {
+        /**
+         * check if options is given
+         */
+        if (typeof options[name] === 'undefined' && !expectedOption.default && expectedOption.required) {
+            throw new Error(`Required option "${name}" is missing`)
+        }
+
+        if (typeof options[name] === 'undefined' && expectedOption.default) {
+            params[name] = expectedOption.default
+        }
+
+        const isValidType = typeof expectedOption.type === 'string'
+            ? typeof options[name] === expectedOption.type && (!expectedOption.match || options[name].match(expectedOption.match))
+            : expectedOption.type(options[name])
+        if (typeof options[name] !== 'undefined' && isValidType) {
+            params[name] = options[name]
+        }
+    }
+
+    return params
 }
