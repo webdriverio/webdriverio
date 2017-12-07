edcbfbea5c68b7e4cf4dfce4fa7cdd8c310782c4
christian-bromann <mail@christian-bromann.com>
2017-12-07 10:16:07 +0100

    webdriver: respect url variables

--- packages/webdriver/src/command.js
+++ packages/webdriver/src/command.js
@@ -4,19 +4,27 @@ import WebDriverRequest from './request'
 const log = logger('webdriver')
 
 export default function (method, endpoint, commandInfo) {
-    const { command, ref, parameters } = commandInfo
+    const { command, ref, parameters, variables = [] } = commandInfo
 
     return function (...args) {
-        const commandUsage = `${command}(${parameters.map((p) => p.name).join(', ')})`
+        const commandParams = [...variables.map((v) => Object.assign(v, {
+            /**
+             * url variables are:
+             */
+            required: true, // always required as they are part of the endpoint
+            type: 'string'  // have to be always type of string
+        })), ...parameters]
+
+        const commandUsage = `${command}(${commandParams.map((p) => p.name).join(', ')})`
         const moreInfo = `\n\nFor more info see ${ref}\n`
         const body = {}
 
         /**
          * parameter check
          */
-        if (args.length !== parameters.length) {
-            const parameterDescription = parameters.length
-                ? `\n\nProperty Description:\n${parameters.map((p) => `  "${p.name}" (${p.type}): ${p.description}`).join('\n')}`
+        if (args.length !== commandParams.length) {
+            const parameterDescription = commandParams.length
+                ? `\n\nProperty Description:\n${commandParams.map((p) => `  "${p.name}" (${p.type}): ${p.description}`).join('\n')}`
                 : ''
 
             throw new Error(
@@ -31,16 +39,27 @@ export default function (method, endpoint, commandInfo) {
          * parameter type check
          */
         for (const [i, arg] of Object.entries(args)) {
-            if (!(typeof arg).match(parameters[i].type)) {
+            if (!(typeof arg).match(commandParams[i].type)) {
                 throw new Error(
-                    `Malformed type for "${parameters[i].name}" parameter of command ${command}\n` +
-                    `Expected: ${parameters[i].type}\n` +
+                    `Malformed type for "${commandParams[i].name}" parameter of command ${command}\n` +
+                    `Expected: ${commandParams[i].type}\n` +
                     `Actual: ${typeof arg}` +
                     moreInfo
                 )
             }
 
-            body[parameters[i].name] = arg
+            /**
+             * inject url variables
+             */
+            if (i < variables.length) {
+                endpoint = endpoint.replace(`:${commandParams[i].name}`, arg)
+                continue
+            }
+
+            /**
+             * rest of args are part of body payload
+             */
+            body[commandParams[i].name] = arg
         }
 
         const request = new WebDriverRequest(method, endpoint, body)
