7e4ffd53d1ab6a9e26a3903fc23404f0c7665ede
christian-bromann <mail@christian-bromann.com>
2017-12-08 11:54:31 +0100

    wdio-conifg: give specific errors for type checks

--- packages/wdio-config/src/index.js
+++ packages/wdio-config/src/index.js
@@ -13,10 +13,19 @@ export default function validateConfig (defaults, options) {
             params[name] = expectedOption.default
         }
 
-        const isValidType = typeof expectedOption.type === 'string'
-            ? typeof options[name] === expectedOption.type && (!expectedOption.match || options[name].match(expectedOption.match))
-            : expectedOption.type(options[name])
-        if (typeof options[name] !== 'undefined' && isValidType) {
+        if (typeof options[name] !== 'undefined') {
+            if (typeof expectedOption.type === 'string' && typeof options[name] !== expectedOption.type) {
+                throw new Error(`Expected option "${name}" to be type of ${expectedOption.type} but was ${typeof options[name]}`)
+            }
+
+            if (typeof expectedOption.type === 'function' && !expectedOption.type(options[name])) {
+                throw new Error(`Option "${name}" failed type check ${expectedOption.type}`)
+            }
+
+            if (expectedOption.match && !options[name].match(expectedOption.match)) {
+                throw new Error(`Option "${name}" doesn't match expected values: ${expectedOption.match}`)
+            }
+
             params[name] = options[name]
         }
     }
