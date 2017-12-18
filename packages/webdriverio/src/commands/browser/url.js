a080bb2d57e780c2ed7b35214e84abd3e585bdd6
christian-bromann <mail@christian-bromann.com>
2017-12-18 13:34:04 +0100

    webdriverio: implement remote export

---
+++ packages/webdriverio/src/commands/browser/url.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * Protocol binding to load or get the URL of the browser. If a baseUrl is
+ * specified in the config, it will be prepended to the url parameter using
+ * node's url.resolve() method.
+ *
+ * <example>
+    :url.js
+    // navigate to a new URL
+    browser.url('http://webdriver.io');
+    // receive url
+    console.log(browser.getUrl()); // outputs: "http://webdriver.io"
+
+    :baseUrlResolutions.js
+    // With a base URL of http://example.com/site, the following url parameters resolve as such:
+    // When providing a scheme:
+    // http://webdriver.io
+    browser.url('http://webdriver.io');
+    // When not starting with a slash, the URL resolves relative to the baseUrl
+    // http://example.com/site/relative
+    browser.url('relative');
+    // When starting with a slash, the URL resolves relative to the root path of the baseUrl
+    // http://example.com/rootRelative
+    browser.url('/rootRelative');
+ * </example>
+ *
+ * @param {String=} url  the URL to navigate to
+ * @return {String}     the current URL
+ *
+ * @see  https://w3c.github.io/webdriver/webdriver-spec.html#dfn-get
+ * @see  https://nodejs.org/api/url.html#url_url_resolve_from_to
+ * @type protocol
+ *
+ */
+
+import nodeUrl from 'url'
+
+export default function url (path) {
+    if (typeof path !== 'string') {
+        throw new Error(`Parameter for 'url' command needs to be type of string`)
+    }
+
+    if (typeof this.options.baseUrl === 'string') {
+        path = nodeUrl.resolve(this.options.baseUrl, path)
+    }
+
+    return this.navigateTo({ url: path })
+}
--- packages/webdriverio/src/index.js
+++ packages/webdriverio/src/index.js
@@ -1,1 +1,44 @@
-export default {}
+import fs from 'fs'
+import path from 'path'
+
+import validateConfig from 'wdio-config'
+import { WebDriverProtocol, MJsonWProtocol, AppiumProtocol, webdriverMonad, command } from 'webdriver'
+
+import { WDIO_DEFAULTS } from './constants'
+
+const ProtocolCommands = [...WebDriverProtocol, ...MJsonWProtocol, ...AppiumProtocol]
+
+const remote = function (options = {}, modifier) {
+    const params = validateConfig(WDIO_DEFAULTS, options)
+    const monad = webdriverMonad(params, modifier)
+
+    /**
+     * register protcol commands
+     */
+    for (const [endpoint, methods] of Object.entries(ProtocolCommands)) {
+        for (const [method, commandData] of Object.entries(methods)) {
+            monad.lift(commandData.command, command(method, endpoint, commandData))
+        }
+    }
+
+    /**
+     * register action commands
+     */
+    const dir = path.resolve(__dirname, 'commands', 'browser')
+    const files = fs.readdirSync(dir)
+    for (let filename of files) {
+        const commandName = filename.slice(0, -3)
+        monad.lift(commandName, require(path.join(dir, commandName)))
+    }
+
+    const prototype = monad()
+    return prototype
+}
+
+const multiremote = function () {
+    /**
+     * ToDo implement multiremote here
+     */
+}
+
+export { remote, multiremote }
