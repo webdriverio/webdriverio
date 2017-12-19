9368c2b8851dec743e05638eb3774a2d72d3490a
christian-bromann <mail@christian-bromann.com>
2017-12-19 15:18:21 +0100

    webdriverio: implement element scope commands

---
+++ packages/webdriverio/src/commands/element/getAttribute.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * Get an attribute from a DOM-element based on the selector and attribute name.
+ * Returns a list of attribute values if selector matches multiple elements.
+ *
+ * <example>
+    :index.html
+    <form action="/submit" method="post" class="loginForm">
+        <input type="text" name="name" placeholder="username"></input>
+        <input type="text" name="password" placeholder="password"></input>
+        <input type="submit" name="submit" value="submit"></input>
+    </form>
+    :getAttribute.js
+    it('should demonstrate the getAttribute command', function () {
+        var form = $('form')
+        var attr = form.getAttribute('method')
+        console.log(attr) // outputs: "post"
+        // or
+        console.log(browser.getAttribute('form', 'method')) // outputs: "post"
+        // if your selector matches multiple elements it returns an array of results
+        var allInputs = $$('.loginForm input')
+        console.log(allInputs.map(function(el) { return el.getAttribute('name'); })) // outputs: ['name', 'password', 'submit']
+    })
+ * </example>
+ *
+ * @alias browser.getAttribute
+ * @param {String} selector      element with requested attribute
+ * @param {String} attributeName requested attribute
+ * @return {String|String[]|null} The value of the attribute(s), or null if it is not set on the element.
+ * @uses protocol/elements, protocol/elementIdAttribute
+ * @type property
+ *
+ */
+
+export default function getAttribute (attributeName) {
+    /*!
+     * parameter check
+     */
+    if (typeof attributeName !== 'string') {
+        throw new Error('number or type of arguments don\'t agree with getAttribute command')
+    }
+
+    return this.getElementAttribute(this.elementId, attributeName)
+}
