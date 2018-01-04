7c19420db42e96a11bd91e1cb72a36ceae28bf94
christian-bromann <mail@christian-bromann.com>
2018-01-04 12:52:29 +0100

    wdio-cli: port launcher code

---
+++ packages/wdio-cli/src/launcher.js
@@ -0,0 +1,516 @@
+import path from 'path'
+import child from 'child_process'
+import logger from 'wdio-logger'
+
+import ConfigParser from './lib/ConfigParser'
+import BaseReporter from './lib/BaseReporter'
+
+const log = logger('wdio-cli:Launcher')
+
+class Launcher {
+    constructor (configFile, argv) {
+        this.configParser = new ConfigParser()
+        this.configParser.addConfigFile(configFile)
+        this.configParser.merge(argv)
+
+        this.reporters = this.initReporters()
+
+        this.argv = argv
+        this.configFile = configFile
+
+        this.exitCode = 0
+        this.hasTriggeredExitRoutine = false
+        this.hasStartedAnyProcess = false
+        this.processes = []
+        this.schedule = []
+        this.rid = []
+        this.processesStarted = 0
+        this.runnerFailed = 0
+    }
+
+    /**
+     * check if multiremote or wdio test
+     */
+    isMultiremote () {
+        let caps = this.configParser.getCapabilities()
+        return !Array.isArray(caps)
+    }
+
+    /**
+     * initialise reporters
+     */
+    initReporters () {
+        let reporter = new BaseReporter()
+        let config = this.configParser.getConfig()
+
+        /**
+         * if no reporter is set or config property is in a wrong format
+         * just use the dot reporter
+         */
+        if (!config.reporters || !Array.isArray(config.reporters) || !config.reporters.length) {
+            config.reporters = ['dot']
+        }
+
+        const reporters = {}
+
+        for (let reporterName of config.reporters) {
+            let Reporter
+            if (typeof reporterName === 'function') {
+                Reporter = reporterName
+                if (!Reporter.reporterName) {
+                    throw new Error('Custom reporters must export a unique \'reporterName\' property')
+                }
+                reporters[Reporter.reporterName] = Reporter
+            } else if (typeof reporterName === 'string') {
+                try {
+                    const pkgName = reporterName.startsWith('@') ? reporterName : `wdio-${reporterName}-reporter`
+                    Reporter = require(pkgName)
+                } catch (e) {
+                    throw new Error(`reporter "wdio-${reporterName}-reporter" is not installed. Error: ${e.stack}`)
+                }
+                reporters[reporterName] = Reporter
+            }
+            if (!Reporter) {
+                throw new Error(`config.reporters must be an array of strings or functions, but got '${typeof reporterName}': ${reporterName}`)
+            }
+        }
+
+        /**
+         * if no reporter options are set or property is in a wrong format default to
+         * empty object
+         */
+        if (!config.reporterOptions || typeof config.reporterOptions !== 'object') {
+            config.reporterOptions = {}
+        }
+
+        for (let reporterName in reporters) {
+            const Reporter = reporters[reporterName]
+            let reporterOptions = {}
+            for (let option of Object.keys(config.reporterOptions)) {
+                if (option === reporterName && typeof config.reporterOptions[reporterName] === 'object') {
+                    // Copy over options specifically for this reporter type
+                    reporterOptions = Object.assign(reporterOptions, config.reporterOptions[reporterName])
+                } else if (reporters[option]) {
+                    // Don't copy options for other reporters
+                    continue
+                } else {
+                    // Copy over generic options
+                    reporterOptions[option] = config.reporterOptions[option]
+                }
+            }
+
+            reporter.add(new Reporter(reporter, config, reporterOptions))
+        }
+
+        return reporter
+    }
+
+    /**
+     * run sequence
+     * @return  {Promise} that only gets resolves with either an exitCode or an error
+     */
+    async run () {
+        let config = this.configParser.getConfig()
+        let caps = this.configParser.getCapabilities()
+        let launcher = this.getLauncher(config)
+
+        this.reporters.handleEvent('start', {
+            isMultiremote: this.isMultiremote(),
+            capabilities: caps,
+            config
+        })
+
+        /**
+         * run onPrepare hook
+         */
+        await config.onPrepare(config, caps)
+        await this.runServiceHook(launcher, 'onPrepare', config, caps)
+
+        /**
+         * if it is an object run multiremote test
+         */
+        if (this.isMultiremote()) {
+            let exitCode = await new Promise((resolve) => {
+                this.resolve = resolve
+                this.startInstance(this.configParser.getSpecs(), caps, 0)
+            })
+
+            /**
+             * run onComplete hook for multiremote
+             */
+            await this.runServiceHook(launcher, 'onComplete', exitCode, config, caps)
+            await config.onComplete(exitCode, config, caps)
+
+            return exitCode
+        }
+
+        /**
+         * schedule test runs
+         */
+        let cid = 0
+        for (let capabilities of caps) {
+            this.schedule.push({
+                cid: cid++,
+                caps: capabilities,
+                specs: this.configParser.getSpecs(capabilities.specs, capabilities.exclude),
+                availableInstances: capabilities.maxInstances || config.maxInstancesPerCapability,
+                runningInstances: 0,
+                seleniumServer: { host: config.host, port: config.port, protocol: config.protocol }
+            })
+        }
+
+        /**
+         * catches ctrl+c event
+         */
+        process.on('SIGINT', this.exitHandler.bind(this))
+
+        /**
+         * make sure the program will not close instantly
+         */
+        if (process.stdin.isPaused()) {
+            process.stdin.resume()
+        }
+
+        let exitCode = await new Promise((resolve) => {
+            this.resolve = resolve
+
+            /**
+             * return immediately if no spec was run
+             */
+            if (this.runSpecs()) {
+                resolve(0)
+            }
+        })
+
+        /**
+         * run onComplete hook
+         */
+        await this.runServiceHook(launcher, 'onComplete', exitCode, config, caps)
+        await config.onComplete(exitCode, config, caps)
+
+        return exitCode
+    }
+
+    /**
+     * run service launch sequences
+     */
+    async runServiceHook (launcher, hookName, ...args) {
+        try {
+            return await Promise.all(launcher.map((service) => {
+                if (typeof service[hookName] === 'function') {
+                    return service[hookName](...args)
+                }
+            }))
+        } catch (e) {
+            log.error(`A service failed in the '${hookName}' hook\n${e.stack}\n\nContinue...`)
+        }
+    }
+
+    /**
+     * run multiple single remote tests
+     * @return {Boolean} true if all specs have been run and all instances have finished
+     */
+    runSpecs () {
+        let config = this.configParser.getConfig()
+
+        /**
+         * stop spawning new processes when CTRL+C was triggered
+         */
+        if (this.hasTriggeredExitRoutine) {
+            return true
+        }
+
+        while (this.getNumberOfRunningInstances() < config.maxInstances) {
+            let schedulableCaps = this.schedule
+                /**
+                 * bail if number of errors exceeds allowed
+                 */
+                .filter(() => {
+                    const filter = typeof config.bail !== 'number' || config.bail < 1 ||
+                                   config.bail > this.runnerFailed
+
+                    /**
+                     * clear number of specs when filter is false
+                     */
+                    if (!filter) {
+                        this.schedule.forEach((t) => { t.specs = [] })
+                    }
+
+                    return filter
+                })
+                /**
+                 * make sure complete number of running instances is not higher than general maxInstances number
+                 */
+                .filter(() => this.getNumberOfRunningInstances() < config.maxInstances)
+                /**
+                 * make sure the capability has available capacities
+                 */
+                .filter((a) => a.availableInstances > 0)
+                /**
+                 * make sure capability has still caps to run
+                 */
+                .filter((a) => a.specs.length > 0)
+                /**
+                 * make sure we are running caps with less running instances first
+                 */
+                .sort((a, b) => a.runningInstances > b.runningInstances)
+
+            /**
+             * continue if no capability were schedulable
+             */
+            if (schedulableCaps.length === 0) {
+                break
+            }
+
+            this.startInstance(
+                [schedulableCaps[0].specs.shift()],
+                schedulableCaps[0].caps,
+                schedulableCaps[0].cid,
+                schedulableCaps[0].seleniumServer
+            )
+            schedulableCaps[0].availableInstances--
+            schedulableCaps[0].runningInstances++
+        }
+
+        return this.getNumberOfRunningInstances() === 0 && this.getNumberOfSpecsLeft() === 0
+    }
+
+    /**
+     * gets number of all running instances
+     * @return {number} number of running instances
+     */
+    getNumberOfRunningInstances () {
+        return this.schedule.map((a) => a.runningInstances).reduce((a, b) => a + b)
+    }
+
+    /**
+     * get number of total specs left to complete whole suites
+     * @return {number} specs left to complete suite
+     */
+    getNumberOfSpecsLeft () {
+        return this.schedule.map((a) => a.specs.length).reduce((a, b) => a + b)
+    }
+
+    /**
+     * Start instance in a child process.
+     * @param  {Array} specs  Specs to run
+     * @param  {Number} cid  Capabilities ID
+     */
+    startInstance (specs, caps, cid, server) {
+        let config = this.configParser.getConfig()
+        cid = this.getRunnerId(cid)
+        let processNumber = this.processesStarted + 1
+
+        // process.debugPort defaults to 5858 and is set even when process
+        // is not being debugged.
+        let debugArgs = []
+        let debugType
+        let debugHost = ''
+        let debugPort = process.debugPort
+        for (let i in process.execArgv) {
+            const debugArgs = process.execArgv[i].match('--(debug|inspect)(?:-brk)?(?:=(.*):)?')
+            if (debugArgs) {
+                let [, type, host] = debugArgs
+                if (type) {
+                    debugType = type
+                }
+                if (host) {
+                    debugHost = `${host}:`
+                }
+            }
+        }
+
+        if (debugType) {
+            debugArgs.push(`--${debugType}=${debugHost}${(debugPort + processNumber)}`)
+        }
+
+        // if you would like to add --debug-brk, use a different port, etc...
+        let capExecArgs = [
+            ...(config.execArgv || []),
+            ...(caps.execArgv || [])
+        ]
+
+        // The default value for child.fork execArgs is process.execArgs,
+        // so continue to use this unless another value is specified in config.
+        let defaultArgs = (capExecArgs.length) ? process.execArgv : []
+
+        // If an arg appears multiple times the last occurrence is used
+        let execArgv = [ ...defaultArgs, ...debugArgs, ...capExecArgs ]
+
+        let childProcess = child.fork(path.join(__dirname, '/runner.js'), process.argv.slice(2), {
+            cwd: process.cwd(),
+            execArgv
+        })
+
+        this.processes.push(childProcess)
+
+        childProcess
+            .on('message', this.messageHandler.bind(this, cid))
+            .on('exit', this.endHandler.bind(this, cid))
+
+        childProcess.send({
+            cid,
+            command: 'run',
+            configFile: this.configFile,
+            argv: this.argv,
+            caps,
+            processNumber,
+            specs,
+            server,
+            isMultiremote: this.isMultiremote()
+        })
+
+        this.processesStarted++
+    }
+
+    /**
+     * generates a runner id
+     * @param  {Number} cid capability id (unique identifier for a capability)
+     * @return {String}     runner id (combination of cid and test id e.g. 0a, 0b, 1a, 1b ...)
+     */
+    getRunnerId (cid) {
+        if (!this.rid[cid]) {
+            this.rid[cid] = 0
+        }
+        return `${cid}-${this.rid[cid]++}`
+    }
+
+    /**
+     * emit event from child process to reporter
+     * @param  {String} cid
+     * @param  {Object} m event object
+     */
+    messageHandler (cid, m) {
+        this.hasStartedAnyProcess = true
+
+        if (!m.cid) {
+            m.cid = cid
+        }
+
+        if (m.event === 'runner:error') {
+            this.reporters.handleEvent('error', m)
+        }
+
+        this.reporters.handleEvent(m.event, m)
+    }
+
+    /**
+     * Close test runner process once all child processes have exited
+     * @param  {Number} cid  Capabilities ID
+     * @param  {Number} childProcessExitCode  exit code of child process
+     */
+    endHandler (cid, childProcessExitCode) {
+        this.exitCode = this.exitCode || childProcessExitCode
+        this.runnerFailed += childProcessExitCode !== 0 ? 1 : 0
+
+        // Update schedule now this process has ended
+        if (!this.isMultiremote()) {
+            // get cid (capability id) from rid (runner id)
+            cid = parseInt(cid, 10)
+
+            this.schedule[cid].availableInstances++
+            this.schedule[cid].runningInstances--
+        }
+
+        if (!this.isMultiremote() && !this.runSpecs()) {
+            return
+        }
+
+        this.reporters.handleEvent('end', {
+            sigint: this.hasTriggeredExitRoutine,
+            exitCode: this.exitCode,
+            isMultiremote: this.isMultiremote(),
+            capabilities: this.configParser.getCapabilities(),
+            config: this.configParser.getConfig()
+        })
+
+        if (this.exitCode === 0) {
+            return this.resolve(this.exitCode)
+        }
+
+        /**
+         * finish with exit code 1
+         */
+        return this.resolve(1)
+    }
+
+    /**
+     * Make sure all started selenium sessions get closed properly and prevent
+     * having dead driver processes. To do so let the runner end its Selenium
+     * session first before killing
+     */
+    exitHandler () {
+        if (this.hasTriggeredExitRoutine || !this.hasStartedAnyProcess) {
+            log.log('\nKilling process, bye!')
+
+            // When spawned as a subprocess,
+            // SIGINT will not be forwarded to childs.
+            // Thus for the child to exit cleanly, we must force send SIGINT
+            if (!process.stdin.isTTY) {
+                this.processes.forEach(p => p.kill('SIGINT'))
+            }
+
+            /**
+             * finish with exit code 1
+             */
+            return this.resolve(1)
+        }
+
+        // When spawned as a subprocess,
+        // SIGINT will not be forwarded to childs.
+        // Thus for the child to exit cleanly, we must force send SIGINT
+        if (!process.stdin.isTTY) {
+            this.processes.forEach(p => p.kill('SIGINT'))
+        }
+
+        log.log(`
+
+End selenium sessions properly ...
+(press ctrl+c again to hard kill the runner)
+`)
+
+        this.hasTriggeredExitRoutine = true
+    }
+
+    /**
+     * loads launch services
+     */
+    getLauncher (config) {
+        let launchServices = []
+
+        if (!Array.isArray(config.services)) {
+            return launchServices
+        }
+
+        for (let serviceName of config.services) {
+            let service
+
+            /**
+             * allow custom services
+             */
+            if (typeof serviceName === 'object') {
+                launchServices.push(serviceName)
+                continue
+            }
+
+            try {
+                const pkgName = serviceName.startsWith('@')
+                    ? `${serviceName}/launcher`
+                    : `wdio-${serviceName}-service/launcher`
+                service = require(pkgName)
+            } catch (e) {
+                if (!e.message.match(`Cannot find module 'wdio-${serviceName}-service/launcher'`)) {
+                    throw new Error(`Couldn't initialise launcher from service "${serviceName}".\n${e.stack}`)
+                }
+            }
+
+            if (service && (typeof service.onPrepare === 'function' || typeof service.onComplete === 'function')) {
+                launchServices.push(service)
+            }
+        }
+
+        return launchServices
+    }
+}
+
+export default Launcher
---
+++ packages/wdio-cli/src/lib/ConfigParser.js
@@ -0,0 +1,306 @@
+import fs from 'fs'
+import path from 'path'
+import glob from 'glob'
+import merge from 'deepmerge'
+
+import logger from 'wdio-logger'
+import { detectBackend } from 'wdio-config'
+
+const log = logger('wdio-cli:ConfigParser')
+
+const HOOKS = [
+    'before', 'beforeSession', 'beforeSuite', 'beforeHook', 'beforeTest', 'beforeCommand',
+    'afterCommand', 'afterTest', 'afterHook', 'afterSuite', 'afterSession', 'after',
+    'beforeFeature', 'beforeScenario', 'beforeStep', 'afterFeature',
+    'afterScenario', 'afterStep', 'onError', 'onReload'
+]
+const MERGE_OPTIONS = { clone: false }
+const DEFAULT_TIMEOUT = 10000
+const NOOP = function () {}
+const DEFAULT_CONFIGS = {
+    sync: true,
+    specs: [],
+    suites: {},
+    exclude: [],
+    logLevel: 'silent',
+    coloredLogs: true,
+    deprecationWarnings: true,
+    baseUrl: null,
+    bail: 0,
+    waitforInterval: 500,
+    waitforTimeout: 1000,
+    framework: 'mocha',
+    reporters: [],
+    reporterOptions: {},
+    maxInstances: 100,
+    maxInstancesPerCapability: 100,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    debug: false,
+    execArgv: null,
+
+    /**
+     * framework defaults
+     */
+    mochaOpts: {
+        timeout: DEFAULT_TIMEOUT
+    },
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: DEFAULT_TIMEOUT
+    },
+
+    /**
+     * hooks
+     */
+    onPrepare: NOOP,
+    before: [],
+    beforeSession: [],
+    beforeSuite: [],
+    beforeHook: [],
+    beforeTest: [],
+    beforeCommand: [],
+    afterCommand: [],
+    afterTest: [],
+    afterHook: [],
+    afterSuite: [],
+    afterSession: [],
+    after: [],
+    onComplete: NOOP,
+    onError: [],
+    onReload: [],
+
+    /**
+     * cucumber specific hooks
+     */
+    beforeFeature: [],
+    beforeScenario: [],
+    beforeStep: [],
+    afterFeature: [],
+    afterScenario: [],
+    afterStep: []
+}
+
+export default class ConfigParser {
+    constructor () {
+        this._config = DEFAULT_CONFIGS
+        this._capabilities = []
+    }
+
+    /**
+     * merges config file with default values
+     * @param {String} filename path of file relative to current directory
+     */
+    addConfigFile (filename) {
+        if (typeof filename !== 'string') {
+            throw new Error('addConfigFile requires filepath')
+        }
+
+        var filePath = path.resolve(process.cwd(), filename)
+
+        try {
+            /**
+             * clone the original config
+             */
+            var fileConfig = merge(require(filePath).config, {}, MERGE_OPTIONS)
+
+            if (typeof fileConfig !== 'object') {
+                throw new Error('configuration file exports no config object')
+            }
+
+            /**
+             * merge capabilities
+             */
+            const defaultTo = Array.isArray(this._capabilities) ? [] : {}
+            this._capabilities = merge(this._capabilities, fileConfig.capabilities || defaultTo, MERGE_OPTIONS)
+            delete fileConfig.capabilities
+
+            /**
+             * add service hooks and remove them from config
+             */
+            this.addService(fileConfig)
+            for (let hookName of HOOKS) {
+                delete fileConfig[hookName]
+            }
+
+            this._config = merge(this._config, fileConfig, MERGE_OPTIONS)
+
+            /**
+             * detect Selenium backend
+             */
+            this._config = merge(detectBackend(this._config), this._config, MERGE_OPTIONS)
+        } catch (e) {
+            log.error(`Failed loading configuration file: ${filePath}`)
+            throw e
+        }
+    }
+
+    /**
+     * merge external object with config object
+     * @param  {Object} object  desired object to merge into the config object
+     */
+    merge (object = {}) {
+        this._config = merge(this._config, object, MERGE_OPTIONS)
+
+        /**
+         * overwrite config specs that got piped into the wdio command
+         */
+        if (object.specs && object.specs.length > 0) {
+            this._config.specs = object.specs
+        }
+
+        /**
+         * merge capabilities
+         */
+        const defaultTo = Array.isArray(this._capabilities) ? [] : {}
+        this._capabilities = merge(this._capabilities, this._config.capabilities || defaultTo, MERGE_OPTIONS)
+
+        /**
+         * run single spec file only, regardless of multiple-spec specification
+         */
+        if (typeof object.spec === 'string') {
+            const specs = []
+            const specList = object.spec.split(/,/g)
+
+            for (let spec of specList) {
+                if (fs.existsSync(spec)) {
+                    specs.push(path.resolve(process.cwd(), spec))
+                }
+            }
+
+            if (specs.length === 0) {
+                throw new Error(`spec file ${object.spec} not found`)
+            }
+
+            this._config.specs = specs
+        }
+
+        /**
+         * user and key could get added via cli arguments so we need to detect again
+         * Note: cli arguments are on the right and overwrite config
+         * if host and port are default, remove them to get new values
+         */
+        let defaultBackend = detectBackend({})
+        if (this._config.host === defaultBackend.host && this._config.port === defaultBackend.port) {
+            delete this._config.host
+            delete this._config.port
+        }
+
+        this._config = merge(detectBackend(this._config), this._config, MERGE_OPTIONS)
+    }
+
+    /**
+     * add hooks from services to runner config
+     * @param {Object} service  a service is basically an object that contains hook methods
+     */
+    addService (service) {
+        for (let hookName of HOOKS) {
+            if (!service[hookName]) {
+                continue
+            } else if (typeof service[hookName] === 'function') {
+                this._config[hookName].push(service[hookName].bind(service))
+            } else if (Array.isArray(service[hookName])) {
+                for (let hook of service[hookName]) {
+                    if (typeof hook === 'function') {
+                        this._config[hookName].push(hook.bind(service))
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * get excluded files from config pattern
+     */
+    getSpecs (capSpecs, capExclude) {
+        let specs = ConfigParser.getFilePaths(this._config.specs)
+        let exclude = ConfigParser.getFilePaths(this._config.exclude)
+
+        /**
+         * check if user has specified a specific suites to run
+         */
+        let suites = typeof this._config.suite === 'string' ? this._config.suite.split(',') : []
+        if (Array.isArray(suites) && suites.length > 0) {
+            let suiteSpecs = []
+            for (let suiteName of suites) {
+                // ToDo: log warning if suite was not found
+                let suite = this._config.suites[suiteName]
+                if (suite && Array.isArray(suite)) {
+                    suiteSpecs = suiteSpecs.concat(ConfigParser.getFilePaths(suite))
+                }
+            }
+
+            if (suiteSpecs.length === 0) {
+                throw new Error(`The suite(s) "${suites.join('", "')}" you specified don't exist ` +
+                                'in your config file or doesn\'t contain any files!')
+            }
+
+            specs = typeof this._config.spec === `string` ? [...specs, ...suiteSpecs] : suiteSpecs
+        }
+
+        if (Array.isArray(capSpecs)) {
+            specs = specs.concat(ConfigParser.getFilePaths(capSpecs))
+        }
+        if (Array.isArray(capExclude)) {
+            exclude = exclude.concat(ConfigParser.getFilePaths(capExclude))
+        }
+
+        return specs.filter(spec => exclude.indexOf(spec) < 0)
+    }
+
+    /**
+     * return configs
+     */
+    getConfig () {
+        return this._config
+    }
+
+    /**
+     * return capabilities
+     */
+    getCapabilities (i) {
+        if (typeof i === 'number' && this._capabilities[i]) {
+            return this._capabilities[i]
+        }
+
+        return this._capabilities
+    }
+
+    /**
+     * returns a flatten list of globed files
+     *
+     * @param  {String[]} filenames  list of files to glob
+     * @return {String[]} list of files
+     */
+    static getFilePaths (patterns, omitWarnings) {
+        let files = []
+
+        if (typeof patterns === 'string') {
+            patterns = [patterns]
+        }
+
+        if (!Array.isArray(patterns)) {
+            throw new Error('specs or exclude property should be an array of strings')
+        }
+
+        for (let pattern of patterns) {
+            let filenames = glob.sync(pattern)
+
+            filenames = filenames.filter(filename =>
+                filename.slice(-3) === '.js' ||
+                filename.slice(-3) === '.ts' ||
+                filename.slice(-8) === '.feature' ||
+                filename.slice(-7) === '.coffee')
+
+            filenames = filenames.map(filename =>
+                path.isAbsolute(filename) ? path.normalize(filename) : path.resolve(process.cwd(), filename))
+
+            if (filenames.length === 0 && !omitWarnings) {
+                log.warn('pattern', pattern, 'did not match any file')
+            }
+
+            files = merge(files, filenames, MERGE_OPTIONS)
+        }
+
+        return files
+    }
+}
