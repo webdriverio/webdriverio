0f99731bb5862510da7711e2cfa4562a780cf057
christian-bromann <mail@christian-bromann.com>
2018-01-05 16:39:20 +0100

    wdio-local-launcher: simple log poc

---
+++ packages/wdio-local-launcher/src/runner.js
@@ -0,0 +1,55 @@
+import logger from 'wdio-logger'
+
+const log = logger('wdio-local-launcher')
+
+export default class Runner {
+    constructor () {
+        this.haltSIGINT = false
+        this.sigintWasCalled = false
+        this.hasSessionID = false
+        this.failures = 0
+        this.forceKillingProcess = false
+        this.isRunning = false
+        this.fileTriggeredWhileRunning = null
+    }
+
+    async run (m) {
+        log.info('I am running', m)
+        process.exit(1)
+    }
+}
+
+const runner = new Runner()
+process.on('message', (m) => {
+    runner[m.command](m).catch((e) => {
+        /**
+         * custom exit code to propagate initialisation error
+         */
+        process.send({
+            event: 'runner:error',
+            error: {
+                message: e.message,
+                stack: e.stack
+            },
+            capabilities: runner.configParser.getCapabilities(runner.cid),
+            cid: runner.cid,
+            specs: runner.specs
+        })
+        process.exit(1)
+    })
+})
+
+/**
+ * catches ctrl+c event
+ */
+process.on('SIGINT', () => {
+    /**
+     * force killing process when 2nd SIGINT comes in
+     */
+    if (runner.forceKillingProcess) {
+        return process.exit(1)
+    }
+
+    runner.forceKillingProcess = true
+    runner.sigintHandler()
+})
