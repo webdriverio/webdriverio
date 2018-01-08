fe1e23ae92f17abcd4da6b72d7f6718a4a4a4a0c
christian-bromann <mail@christian-bromann.com>
2018-01-08 10:52:43 +0100

    wdio-config: add utility functions

--- packages/wdio-config/src/index.js
+++ packages/wdio-config/src/index.js
@@ -1,45 +1,12 @@
+import ConfigParser from './lib/ConfigParser'
+
 const DEFAULT_HOST = '127.0.0.1'
 const DEFAULT_PORT = 4444
-
-export function validateConfig (defaults, options) {
-    const params = {}
-
-    for (const [name, expectedOption] of Object.entries(defaults)) {
-        /**
-         * check if options is given
-         */
-        if (typeof options[name] === 'undefined' && !expectedOption.default && expectedOption.required) {
-            throw new Error(`Required option "${name}" is missing`)
-        }
-
-        if (typeof options[name] === 'undefined' && expectedOption.default) {
-            params[name] = expectedOption.default
-        }
-
-        if (typeof options[name] !== 'undefined') {
-            if (typeof expectedOption.type === 'string' && typeof options[name] !== expectedOption.type) {
-                throw new Error(`Expected option "${name}" to be type of ${expectedOption.type} but was ${typeof options[name]}`)
-            }
-
-            if (typeof expectedOption.type === 'function' && !expectedOption.type(options[name])) {
-                throw new Error(`Option "${name}" failed type check ${expectedOption.type}`)
-            }
-
-            if (expectedOption.match && !options[name].match(expectedOption.match)) {
-                throw new Error(`Option "${name}" doesn't match expected values: ${expectedOption.match}`)
-            }
-
-            params[name] = options[name]
-        }
-    }
-
-    return params
-}
 
 /**
  * helper to detect the Selenium backend according to given capabilities
  */
-export function detectBackend (options = {}) {
+function detectBackend (options = {}) {
     const { port, host, key } = options
 
     /**
@@ -84,4 +51,69 @@ export function detectBackend (options = {}) {
         host: 'ondemand.saucelabs.com',
         port: 443
     })
+}
+
+/**
+ * initialise WebdriverIO compliant plugins
+ */
+function initialisePlugin (name, type) {
+    /**
+     * don't populate scoped package names
+     */
+    const pkgName = name[0] === '@' ? name : `wdio-${name.toLowerCase()}-${type}`
+
+    try {
+        return require(pkgName)
+    } catch (e) {
+        if (!e.message.match(`Cannot find module '${pkgName}'`)) {
+            throw new Error(`Couldn't initialise ${type} "${name}".\n${e.stack}`)
+        }
+
+        throw new Error(
+            `Couldn't find plugin "${pkgName}". You need to install it ` +
+            `with \`$ npm install ${pkgName}\`!\n${e.stack}`
+        )
+    }
+}
+
+function validateConfig (defaults, options) {
+    const params = {}
+
+    for (const [name, expectedOption] of Object.entries(defaults)) {
+        /**
+         * check if options is given
+         */
+        if (typeof options[name] === 'undefined' && !expectedOption.default && expectedOption.required) {
+            throw new Error(`Required option "${name}" is missing`)
+        }
+
+        if (typeof options[name] === 'undefined' && expectedOption.default) {
+            params[name] = expectedOption.default
+        }
+
+        if (typeof options[name] !== 'undefined') {
+            if (typeof expectedOption.type === 'string' && typeof options[name] !== expectedOption.type) {
+                throw new Error(`Expected option "${name}" to be type of ${expectedOption.type} but was ${typeof options[name]}`)
+            }
+
+            if (typeof expectedOption.type === 'function' && !expectedOption.type(options[name])) {
+                throw new Error(`Option "${name}" failed type check ${expectedOption.type}`)
+            }
+
+            if (expectedOption.match && !options[name].match(expectedOption.match)) {
+                throw new Error(`Option "${name}" doesn't match expected values: ${expectedOption.match}`)
+            }
+
+            params[name] = options[name]
+        }
+    }
+
+    return params
+}
+
+export default {
+    validateConfig,
+    detectBackend,
+    initialisePlugin,
+    ConfigParser
 }
