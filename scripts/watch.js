e6c056de9a55a16c2db186ac3947b53597801d7c
christian-bromann <mail@christian-bromann.com>
2018-02-06 09:42:32 +0100

    add watch command

---
+++ scripts/watch.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const shell = require('shelljs')
+
+const packages = shell.ls(path.join(__dirname, '..', 'packages'))
+
+/**
+ * set proper size of max listener
+ */
+require('events').EventEmitter.defaultMaxListeners = packages.length + 3
+
+shell.cd(path.join(__dirname, '..'))
+packages.forEach(
+    (pkg) => shell.exec(`lerna exec 'npm run compile -- --watch' --scope ${pkg}`, { async: true })
+)
