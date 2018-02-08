b8336821610cca6aa08b0cb836f563ba1316f70b
christian-bromann <mail@christian-bromann.com>
2018-02-08 13:07:47 +0100

    wdio-dot-reporter: ported reported using new wdio reporter

--- packages/wdio-dot-reporter/src/index.js
+++ packages/wdio-dot-reporter/src/index.js
@@ -1,1 +1,37 @@
-export default {}
+import chalk from 'chalk'
+import WDIOReporter from 'wdio-reporter'
+
+/**
+ * Initialize a new `Dot` matrix test reporter.
+ */
+class DotReporter extends WDIOReporter {
+    /**
+     * print empty line at the beginning
+     */
+    start () {
+        process.stdout.write('\n')
+    }
+
+    /**
+     * pending tests
+     */
+    testPending () {
+        process.stdout.write(chalk.cyanBright('.'))
+    }
+
+    /**
+     * passing tests
+     */
+    testPass () {
+        process.stdout.write(chalk.greenBright('.'))
+    }
+
+    /**
+     * failing tests
+     */
+    testFail () {
+        process.stdout.write(chalk.redBright('.'))
+    }
+}
+
+export default DotReporter
