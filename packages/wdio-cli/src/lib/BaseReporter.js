f8676f59304247336f6141e59bae24b609a173b1
christian-bromann <mail@christian-bromann.com>
2018-02-08 13:57:00 +0100

    wdio-cli: remove reporter handling, now done in wdio-runner

--- packages/wdio-cli/src/lib/BaseReporter.js
+++
@@ -1,9 +0,0 @@
-import logger from 'wdio-logger'
-
-const log = logger('wdio-cli:BaseReporter')
-
-export default class BaseReporter {
-    handleEvent (...args) {
-        log.debug(args)
-    }
-}
