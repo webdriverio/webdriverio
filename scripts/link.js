7ead587244c40972ae4cad59f9cbdb32e6cf5bda
christian-bromann <mail@christian-bromann.com>
2018-03-08 10:42:04 +0100

    add link script

---
+++ scripts/link.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/**
+ * This script initialises the playground by linking all package into the /test directory
+ */
+
+const path = require('path')
+const shell = require('shelljs')
+
+const packagesDir = path.join(__dirname, '..', 'packages')
+const nodeModulesDir = path.join(__dirname, '..', 'test', 'node_modules')
+
+const packages = shell.ls(packagesDir)
+shell.mkdir(nodeModulesDir)
+packages.forEach(
+    (pkg) => shell.ln('-s', path.join(packagesDir, pkg), path.join(nodeModulesDir, pkg))
+)
