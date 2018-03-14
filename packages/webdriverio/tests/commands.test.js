3dfac31672ba2758e1dd89ba519a81647028ae2b
Jim Davis <fijijavis@gmail.com>
2018-03-13 21:59:37 -0400

    #33 | split out elements tests

--- packages/webdriverio/tests/commands.test.js
+++ packages/webdriverio/tests/commands.test.js
@@ -95,31 +95,6 @@ describe('commands test', () => {
         })
     })
 
-    describe('elements commands', () => {
-        let elems
-
-        beforeAll(async () => {
-            elems = await browser.$$('.foo')
-        })
-
-        it('should fetch an element', async () => {
-            expect(request.mock.calls[0][0].method).toBe('POST')
-            expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/elements')
-            expect(request.mock.calls[0][0].body).toEqual({ using: 'css selector', value: '.foo' })
-            expect(elems).toHaveLength(3)
-
-            expect(elems[0].elementId).toBe('some-elem-123')
-            expect(elems[0].selector).toBe('.foo')
-            expect(elems[0].index).toBe(0)
-            expect(elems[1].elementId).toBe('some-elem-456')
-            expect(elems[1].selector).toBe('.foo')
-            expect(elems[1].index).toBe(1)
-            expect(elems[2].elementId).toBe('some-elem-789')
-            expect(elems[2].selector).toBe('.foo')
-            expect(elems[2].index).toBe(2)
-        })
-    })
-
     afterEach(() => {
         request.mockClear()
     })
