0a9e16eae81ccd2b8f0dc8ebb22cfd4e66d37035
Jim Davis <fijijavis@gmail.com>
2018-03-13 22:27:04 -0400

    #33 | split out gitElementSize tests

--- packages/webdriverio/tests/commands.test.js
+++ packages/webdriverio/tests/commands.test.js
@@ -21,33 +21,15 @@ describe('commands test', () => {
     })
 
     describe('element commands', () => {
-        let elem
+        // let elem
 
-        beforeAll(async () => {
-            elem = await browser.$('#foo')
-        })
+        // beforeAll(async () => {
+        //     elem = await browser.$('#foo')
+        // })
 
        
 
-        it('should allow to get the width and height of an element', async () => {
-            const size = await elem.getElementSize()
-
-            expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-elem-123/rect');
-            expect(size.width).toBe(50)
-            expect(size.height).toBe(30)
-        })
-
-        it('should allow to get the width of an element', async () => {
-            const size = await elem.getElementSize('width')
-
-            expect(size).toBe(50)
-        })
-
-        it('should allow to get the height of an element', async () => {
-            const size = await elem.getElementSize('height')
-
-            expect(size).toBe(30)
-        })
+       
     })
 
     afterEach(() => {
