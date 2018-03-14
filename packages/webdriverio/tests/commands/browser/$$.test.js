575ed604504673e44d0fff97d17d5d71d05ed9a6
Jim Davis <fijijavis@gmail.com>
2018-03-13 21:59:37 -0400

    #33 | split out elements tests

---
+++ packages/webdriverio/tests/commands/browser/$$.test.js
@@ -0,0 +1,45 @@
+import request from 'request'
+import { remote } from '../../../src'
+
+describe('elements', () => {
+    let browser
+    let elems
+
+    beforeAll(async () => {
+        browser = await remote({
+            baseUrl: 'http://foobar.com',
+            capabilities: {
+                browserName: 'foobar'
+            }
+        })
+    })
+
+    it('should have a sessionId when instance was created', () => {
+        expect(browser.sessionId).toBe('foobar-123')
+        expect(request.mock.calls).toHaveLength(1)
+        expect(request.mock.calls[0][0].method).toBe('POST')
+        expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session')
+    })
+    
+    it('should fetch elements', async () => {
+        elems = await browser.$$('.foo')
+        expect(request.mock.calls[0][0].method).toBe('POST')
+        expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/elements')
+        expect(request.mock.calls[0][0].body).toEqual({ using: 'css selector', value: '.foo' })
+        expect(elems).toHaveLength(3)
+
+        expect(elems[0].elementId).toBe('some-elem-123')
+        expect(elems[0].selector).toBe('.foo')
+        expect(elems[0].index).toBe(0)
+        expect(elems[1].elementId).toBe('some-elem-456')
+        expect(elems[1].selector).toBe('.foo')
+        expect(elems[1].index).toBe(1)
+        expect(elems[2].elementId).toBe('some-elem-789')
+        expect(elems[2].selector).toBe('.foo')
+        expect(elems[2].index).toBe(2)
+    })
+
+    afterEach(() => {
+        request.mockClear()
+    })
+})
