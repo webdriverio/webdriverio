6484bb3f4eec0e4b7d021d06bf3ad721500159bf
Erwin Heitzman <erwin.heitzman@gmail.com>
2018-03-23 09:54:26 +0100

    webdriverio: ported cookie commands

---
+++ packages/webdriverio/src/commands/browser/getCookies.js
@@ -0,0 +1,15 @@
+export default async function getCookies(names) {
+    const namesList = typeof names !== 'undefined' && !Array.isArray(names) ? [names] : names
+
+    if (typeof namesList === 'undefined') {
+        return await this.getAllCookies()
+    }
+
+    if (namesList.every(obj => typeof obj !== 'string')) {
+        throw new Error('Invalid input (see http://webdriver.io/api/cookie/getCookies.html for documentation.')
+    }
+
+    const allCookies = await this.getAllCookies()
+
+    return allCookies.filter(cookie => namesList.includes(cookie.name));
+}
