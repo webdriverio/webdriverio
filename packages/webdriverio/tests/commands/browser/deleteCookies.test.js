ffea6130c23eff23d43489b6515797c769b3358b
Erwin Heitzman <erwin.heitzman@gmail.com>
2018-03-23 09:54:26 +0100

    webdriverio: ported cookie commands

---
+++ packages/webdriverio/tests/commands/browser/deleteCookies.test.js
@@ -0,0 +1,56 @@
+import request from 'request'
+import { remote } from '../../../src'
+
+describe('deleteCookies', () => {
+    let browser
+
+    beforeAll(async () => {
+        browser = await remote({
+            baseUrl: 'http://foobar.com',
+            capabilities: {
+                browserName: 'foobar'
+            }
+        })
+    })
+
+    it('should delete all cookies', async () => {
+        await browser.deleteCookies()
+
+        expect(request.mock.calls[1][0].method).toBe('DELETE')
+        expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie')
+    })
+
+    it('should support passing a string', async () => {
+        await browser.deleteCookies('cookie1')
+
+        expect(request.mock.calls[0][0].method).toBe('DELETE')
+        expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie/cookie1')
+    })
+
+    it('should support passing a array with a string', async () => {
+        await browser.deleteCookies(['cookie1'])
+
+        expect(request.mock.calls[0][0].method).toBe('DELETE')
+        expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie/cookie1')
+    })
+
+    it('should delete cookies that match by name', async () => {
+        const cookieNames = ['cookie1', 'cookie2', 'cookie3']
+        await browser.deleteCookies(cookieNames)
+
+        cookieNames.forEach((name, i) => {
+            expect(request.mock.calls[i][0].method).toBe('DELETE')
+            expect(request.mock.calls[i][0].uri.path).toBe(`/wd/hub/session/foobar-123/cookie/${name}`)
+        })
+    })
+
+    it('should throw error if invalid arguments are passed', async () => {
+        await expect(browser.deleteCookies([2]))
+            .rejects
+            .toEqual(new Error('Invalid input (see http://webdriver.io/api/cookie/deleteCookies.html for documentation.'))
+    })
+
+    afterEach(() => {
+        request.mockClear()
+    })
+})
---
+++ packages/webdriverio/tests/commands/browser/setCookies.test.js
@@ -0,0 +1,69 @@
+import request from 'request'
+import { remote } from '../../../src'
+
+describe('setCookies', () => {
+    let browser
+
+    beforeAll(async () => {
+        browser = await remote({
+            baseUrl: 'http://foobar.com',
+            capabilities: {
+                browserName: 'foobar'
+            }
+        })
+    })
+
+    it('should output the expected format', async () => {
+        await browser.setCookies([{ name: 'cookie1', value: 'dummy-value-1' }])
+        expect(request.mock.calls.length).toBe(2)
+        expect(request.mock.calls[1][0].method).toBe('POST')
+        expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie')
+        expect(request.mock.calls[1][0].body).toEqual({ 'cookie': { name: 'cookie1', value: 'dummy-value-1' } })
+    })
+
+    it('should support passing an object', async () => {
+        await browser.setCookies({ name: 'cookie1', value: 'dummy-value-1' })
+        expect(request.mock.calls.length).toBe(1)
+        expect(request.mock.calls[0][0].method).toBe('POST')
+        expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie')
+        expect(request.mock.calls[0][0].body).toEqual({ 'cookie': { name: 'cookie1', value: 'dummy-value-1' } })
+    })
+
+    it('can be called multiple times', async () => {
+        await browser.setCookies({ name: 'cookie1', value: 'dummy-value-1' })
+        await browser.setCookies({ name: 'cookie2', value: 'dummy-value-1' })
+        expect(request.mock.calls.length).toBe(2)
+        expect(request.mock.calls[0][0].method).toBe('POST')
+        expect(request.mock.calls[0][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie')
+        expect(request.mock.calls[0][0].body).toEqual({ 'cookie': { name: 'cookie1', value: 'dummy-value-1' } })
+        expect(request.mock.calls[1][0].method).toBe('POST')
+        expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie')
+        expect(request.mock.calls[1][0].body).toEqual({ 'cookie': { name: 'cookie2', value: 'dummy-value-1' } })
+    })
+
+    it('should work with multiple objects passed', async () => {
+        const cookies = [
+            { name: 'cookie1', value: 'dummy-value-1' },
+            { name: 'cookie2', value: 'dummy-value-2' },
+            { name: 'cookie3', value: 'dummy-value-3' }
+        ]
+
+        await browser.setCookies(cookies)
+
+        cookies.forEach((cookie, i) => {
+            expect(request.mock.calls[i][0].method).toBe('POST')
+            expect(request.mock.calls[i][0].uri.path).toBe('/wd/hub/session/foobar-123/cookie')
+            expect(request.mock.calls[i][0].body).toEqual({ 'cookie': cookie })
+        })
+    })
+
+    it('should throw error if invalid arguments are passed', async () => {
+        await expect(browser.setCookies([2]))
+            .rejects
+            .toEqual(new Error('Invalid input (see http://webdriver.io/api/cookie/setCookies.html for documentation.'))
+    })
+
+    afterEach(() => {
+        request.mockClear()
+    })
+})
