1166354954f9d1d827b0ea0f634c813cafc8eb67
christian-bromann <mail@christian-bromann.com>
2018-03-31 17:11:35 +0200

    wdio-interface: add unit tests

--- packages/wdio-interface/src/utils.js
+++
@@ -1,3 +0,0 @@
-import isCI from 'is-ci'
-
-export const isInteractive = process.stdout.isTTY && process.env.TERM !== 'dumb' && !isCI
---
+++ packages/wdio-interface/tests/__mocks__/ansi-escapes.js
@@ -0,0 +1,5 @@
+export default {
+    clearScreen: 'clearScreen',
+    eraseStartLine: 'eraseStartLine',
+    cursorLeft: 'cursorLeft'
+}
