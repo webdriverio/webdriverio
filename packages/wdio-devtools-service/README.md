bbf2926d1f9da56ead95782fcb492fa3cf79d755
christian-bromann <mail@christian-bromann.com>
2018-04-10 13:39:39 +0200

    wdio-devtools-service: move over code

--- packages/wdio-devtools-service/README.md
+++ packages/wdio-devtools-service/README.md
@@ -2,3 +2,99 @@ WebdriverIO DevTools Service
 ============================
 
 > A WebdriverIO service that allows you to run Chrome DevTools commands in your tests
+
+With Chrome v63 and up the browser [started to support](https://developers.google.com/web/updates/2017/10/devtools-release-notes#multi-client) multi clients allowing arbitrary clients to access the [Chrome DevTools Protocol](https://chromedevtools.github.io/devtools-protocol/). This provides interesting opportunities to automate Chrome beyond the [WebDriver protocol](https://www.w3.org/TR/webdriver/). With this service you can enhance the wdio browser object to leverage that access and call Chrome DevTools commands within your tests to e.g. intercept requests, throttle network capabilities or take CSS/JS coverage.
+
+__Note:__ this service currently only supports Chrome v63 and up!
+
+## Installation
+
+The easiest way is to keep `wdio-devtools-service` as a devDependency in your `package.json`.
+
+```json
+{
+  "devDependencies": {
+    "wdio-devtools-service": "^0.1.2"
+  }
+}
+```
+
+You can simple do it by:
+
+```bash
+npm install wdio-devtools-service --save-dev
+```
+
+Instructions on how to install `WebdriverIO` can be found [here.](http://webdriver.io/guide/getstarted/install.html)
+
+## Configuration
+
+In order to use the service you just need to add the service to your service list in your `wdio.conf.js` like:
+
+```js
+// wdio.conf.js
+export.config = {
+  // ...
+  services: ['devtools'],
+  // ...
+};
+```
+
+## Usage
+
+For now the service allows two different ways to access the Chrome DevTools Protocol:
+
+### Via `cdp` Command
+
+The `cdp` command is a custom command added to the browser scope that allows you to call directly commands to the protocol.
+
+```js
+browser.cdp(<domain>, <command>, <arguments>)
+```
+
+For example if you want to get the JavaScript coverage of your page you can do the following:
+
+```js
+it('should take JS coverage', () => {
+    /**
+     * enable necessary domains
+     */
+    browser.cdp('Profiler', 'enable')
+    browser.cdp('Debugger', 'enable')
+
+    /**
+     * start test coverage profiler
+     */
+    browser.cdp('Profiler', 'startPreciseCoverage', {
+        callCount: true,
+        detailed: true
+    })
+
+    browser.url('http://google.com')
+
+    /**
+     * capture test coverage
+     */
+    const { result } = browser.cdp('Profiler', 'takePreciseCoverage')
+    const coverage = result.filter((res) => res.url !== '')
+    console.log(coverage)
+})
+```
+
+### Via Event Listener
+
+In order to capture events in the browser you can register an event listener to a Chrome DevTools event like:
+
+```js
+it('should listen on network events', () => {
+    browser.cdp('Network', 'enable')
+    browser.on('Network.responseReceived', (params) => {
+        console.log(`Loaded ${params.response.url}`)
+    })
+    browser.url('https://www.google.com')
+})
+```
+
+----
+
+For more information on WebdriverIO see the [homepage](http://webdriver.io).
--- packages/wdio-devtools-service/src/index.js
+++ packages/wdio-devtools-service/src/index.js
@@ -1,1 +1,87 @@
-export default {}
+import logger from 'wdio-logger'
+import CDP from 'chrome-remote-interface'
+
+const log = logger('wdio-devtools-service')
+
+const UNSUPPORTED_ERROR_MESSAGE = 'The wdio-devtools-service currently only supports Chrome version 63 and up'
+
+export default class DevToolsService {
+    constructor () {
+        this.isSupported = false
+    }
+
+    beforeSession (_, caps) {
+        if (caps.browserName !== 'chrome' || (caps.version && caps.version < 63)) {
+            return log.error(UNSUPPORTED_ERROR_MESSAGE)
+        }
+
+        this.isSupported = true
+    }
+
+    async before () {
+        if (!this.isSupported) {
+            return global.browser.addCommand('cdp', () => {
+                throw new Error(UNSUPPORTED_ERROR_MESSAGE)
+            })
+        }
+
+        this.chromePort = await this._findChromePort()
+        this.client = await this._getCDPClient(this.chromePort)
+
+        /**
+         * allow to easily access the CDP from the browser object
+         */
+        global.browser.addCommand('cdp', (domain, command, args = {}) => {
+            if (!this.client[domain]) {
+                throw new Error(`Domain "${domain}" doesn't exist in the Chrome DevTools protocol`)
+            }
+
+            if (!this.client[domain][command]) {
+                throw new Error(`The "${domain}" domain doesn't have a method called "${command}"`)
+            }
+
+            return new Promise((resolve, reject) => this.client[domain][command](args, (err, result) => {
+                if (err) {
+                    return reject(new Error(`Chrome DevTools Error: ${result.message}`))
+                }
+
+                return resolve(result)
+            }))
+        })
+
+        /**
+         * helper method to receive Chrome remote debugging connection data to
+         * e.g. use external tools like lighthouse
+         */
+        const { host, port } = this.client
+        global.browser.addCommand('cdpConnection', () => ({ host, port }))
+
+        /**
+         * propagate CDP events to the browser event listener
+         */
+        this.client.on('event', (event) => {
+            const method = event.method || 'event'
+            log.debug(`cdp event: ${method} with params ${JSON.stringify(event.params)}`)
+            global.browser.emit(method, event.params)
+        })
+    }
+
+    async _findChromePort () {
+        try {
+            await global.browser.url('chrome://version')
+
+            const cmdLine = await global.browser.$('#command_line')
+            return cmdLine.getText().then((args) => parseInt(args.match(/--remote-debugging-port=(\d*)/)[1]))
+        } catch (err) {
+            log.error(`Couldn't connect to chrome: ${err.stack}`)
+        }
+    }
+
+    async _getCDPClient (port) {
+        return new Promise((resolve) => CDP({
+            port,
+            host: 'localhost',
+            target: (targets) => targets.findIndex((t) => t.type === 'page')
+        }, resolve))
+    }
+}
