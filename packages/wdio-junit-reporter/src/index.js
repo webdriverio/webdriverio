12587f44780f90702e73b0cf2ba304e0aff5cfc3
christian-bromann <mail@christian-bromann.com>
2018-04-13 13:23:45 +0200

    wdio-junit-reporter: more progress on reporter

--- packages/wdio-junit-reporter/src/index.js
+++ packages/wdio-junit-reporter/src/index.js
@@ -1,77 +1,19 @@
-import path from 'path'
-import fs from 'fs'
-import mkdirp from 'mkdirp'
-
 import junit from 'junit-report-builder'
-import get from 'lodash.get'
-
 import WDIOReporter from 'wdio-reporter'
-import logger from 'wdio-logger'
 
-const log = logger('wdio-junit-reporter')
+import { limit } from './utils'
 
 class JunitReporter extends WDIOReporter {
-    constructor (baseReporter, config, options = {}) {
-        super()
-
-        this.baseReporter = baseReporter
-        this.config = config
-        this.options = options
+    constructor (options) {
+        super(options)
         this.suiteNameRegEx = this.options.suiteNameFormat instanceof RegExp
             ? this.options.suiteNameFormat
             : /[^a-zA-Z0-9]+/
-
-        this.on('end', ::this.onEnd)
     }
 
-    onRunnerEnd () {
-        const outputIsFunction = typeof this.options.outputFileFormat === 'function'
-        if (outputIsFunction) {
-            // backward compatibility
-            this.options.outputFileFormat = {
-                multi: this.options.outputFileFormat
-            }
-        }
-        const noFileOutputDefined = !this.options.outputFileFormat
-        const hasSingleFileOutput = get(this.options, 'outputFileFormat.single')
-        const hasMultiFileOutput = get(this.options, 'outputFileFormat.multi')
-        if (noFileOutputDefined || hasMultiFileOutput) {
-            this.onMultiFileOutput()
-        }
-        if (hasSingleFileOutput) {
-            this.onSingleFileOutput()
-        }
-        const { epilogue } = this.baseReporter
-        epilogue.call(this.baseReporter)
-    }
-
-    onSingleFileOutput () {
-        const xml = this.prepareXml(this.baseReporter.stats.runners)
-        let filename = `WDIO.xunit.all.xml` // default
-        if (typeof get(this.options, 'outputFileFormat.single') === 'function') {
-            filename = this.options.outputFileFormat.single({
-                config: this.config
-            })
-        }
-        this.write(filename, xml)
-    }
-
-    onMultiFileOutput () {
-        for (let cid of Object.keys(this.baseReporter.stats.runners)) {
-            const capabilities = this.baseReporter.stats.runners[cid]
-            const singleRunner = {}
-            singleRunner[cid] = capabilities
-            const xml = this.prepareXml(singleRunner)
-            let filename = `WDIO.xunit.${capabilities.sanitizedCapabilities}.${cid}.xml` // default
-            if (typeof get(this.options, 'outputFileFormat.multi') === 'function') {
-                filename = this.options.outputFileFormat.multi({
-                    capabilities: capabilities.sanitizedCapabilities,
-                    cid: cid,
-                    config: this.config
-                })
-            }
-            this.write(filename, xml)
-        }
+    onRunnerEnd (runner) {
+        const xml = this.prepareXml(runner)
+        this.write(xml)
     }
 
     prepareName (name = 'Skipped test') {
@@ -80,66 +22,62 @@ class JunitReporter extends WDIOReporter {
         ).join('_')
     }
 
-    prepareXml (runners) {
+    prepareXml (runner) {
         const builder = junit.newBuilder()
-        for (const key of Object.keys(runners)) {
-            const capabilities = runners[key]
-            const packageName = this.options.packageName
-                ? `${capabilities.sanitizedCapabilities}-${this.options.packageName}`
-                : capabilities.sanitizedCapabilities
 
-            for (let specId of Object.keys(capabilities.specs)) {
-                const spec = capabilities.specs[specId]
+        const packageName = this.options.packageName
+            ? `${runner.sanitizedCapabilities}-${this.options.packageName}`
+            : runner.sanitizedCapabilities
 
-                for (let suiteKey of Object.keys(spec.suites)) {
-                    /**
-                     * ignore root before all
-                     */
-                    /* istanbul ignore if  */
-                    if (suiteKey.match(/^"before all"/)) {
-                        continue
-                    }
+        for (let specId of Object.keys(runner.specs)) {
+            for (let suiteKey of Object.keys(this.suites)) {
+                /**
+                 * ignore root before all
+                 */
+                /* istanbul ignore if  */
+                if (suiteKey.match(/^"before all"/)) {
+                    continue
+                }
 
-                    const suite = spec.suites[suiteKey]
-                    const suiteName = this.prepareName(suite.title)
-                    const testSuite = builder.testSuite()
-                        .name(suiteName)
-                        .timestamp(suite.start)
-                        .time(suite.duration / 1000)
-                        .property('specId', specId)
-                        .property('suiteName', suite.title)
-                        .property('capabilities', capabilities.sanitizedCapabilities)
-                        .property('file', spec.files[0].replace(process.cwd(), '.'))
+                const suite = this.suites[suiteKey]
+                const suiteName = this.prepareName(suite.title)
+                const testSuite = builder.testSuite()
+                    .name(suiteName)
+                    .timestamp(suite.start)
+                    .time(suite.duration / 1000)
+                    .property('specId', specId)
+                    .property('suiteName', suite.title)
+                    .property('capabilities', runner.sanitizedCapabilities)
+                    .property('file', runner.specs[0].replace(process.cwd(), '.'))
 
-                    for (let testKey of Object.keys(suite.tests)) {
-                        if (testKey !== 'undefined') { // fix cucumber hooks crashing reporter
-                            const test = suite.tests[testKey]
-                            const testName = this.prepareName(test.title)
-                            const testCase = testSuite.testCase()
-                                .className(`${packageName}.${suiteName}`)
-                                .name(testName)
-                                .time(test.duration / 1000)
+                for (let testKey of Object.keys(suite.tests)) {
+                    if (testKey !== 'undefined') { // fix cucumber hooks crashing reporter
+                        const test = suite.tests[testKey]
+                        const testName = this.prepareName(test.title)
+                        const testCase = testSuite.testCase()
+                            .className(`${packageName}.${suiteName}`)
+                            .name(testName)
+                            .time(test.duration / 1000)
 
-                            if (test.state === 'pending') {
-                                testCase.skipped()
-                            }
-
-                            if (test.error) {
-                                const errorOptions = this.options.errorOptions
-                                if (errorOptions) {
-                                    for (const key of Object.keys(errorOptions)) {
-                                        testCase[key](test.error[errorOptions[key]])
-                                    }
-                                } else {
-                                    // default
-                                    testCase.error(test.error.message)
-                                }
-                                testCase.standardError(`\n${test.error.stack}\n`)
-                            }
-
-                            const output = this.getStandardOutput(test)
-                            if (output) testCase.standardOutput(`\n${output}\n`)
+                        if (test.state === 'pending') {
+                            testCase.skipped()
                         }
+
+                        if (test.error) {
+                            const errorOptions = this.options.errorOptions
+                            if (errorOptions) {
+                                for (const key of Object.keys(errorOptions)) {
+                                    testCase[key](test.error[errorOptions[key]])
+                                }
+                            } else {
+                                // default
+                                testCase.error(test.error.message)
+                            }
+                            testCase.standardError(`\n${test.error.stack}\n`)
+                        }
+
+                        const output = this.getStandardOutput(test)
+                        if (output) testCase.standardOutput(`\n${output}\n`)
                     }
                 }
             }
@@ -148,48 +86,25 @@ class JunitReporter extends WDIOReporter {
     }
 
     getStandardOutput (test) {
-        /* istanbul ignore if  */
-        if (this.options.writeStandardOutput === false) {
-            return ''
-        }
         let standardOutput = []
         test.output.forEach((data) => {
             switch (data.type) {
             case 'command':
                 standardOutput.push(
-                    `COMMAND: ${data.payload.method.toUpperCase()} ` +
-                    `${data.payload.uri.href} - ${this.format(data.payload.data)}`
+                    `COMMAND: ${data.method.toUpperCase()} ` +
+                    `${data.endpoint.replace(':sessionId', data.sessionId)} - ${this.format(data.body)}`
                 )
                 break
             case 'result':
-                standardOutput.push(`RESULT: ${this.format(data.payload.body)}`)
+                standardOutput.push(`RESULT: ${this.format(data.body)}`)
                 break
             }
         })
         return standardOutput.length ? standardOutput.join('\n') : ''
     }
 
-    write (filename, xml) {
-        /* istanbul ignore if  */
-        if (!this.options || typeof this.options.outputDir !== 'string') {
-            return log.info(`Cannot write xunit report: empty or invalid 'outputDir'.`)
-        }
-
-        try {
-            const dir = path.resolve(this.options.outputDir)
-            const filepath = path.join(dir, filename)
-            mkdirp.sync(dir)
-            fs.writeFileSync(filepath, xml)
-            log.info(`Wrote xunit report "${filename}" to [${this.options.outputDir}].`)
-        } catch (e) {
-            /* istanbul ignore next */
-            log.info(`Failed to write xunit report "${filename}"
-             to [${this.options.outputDir}]. Error: ${e}`)
-        }
-    }
-
     format (val) {
-        return JSON.stringify(this.baseReporter.limit(val))
+        return JSON.stringify(limit(val))
     }
 }
 
