93aee10e16f71a984109c94cca1fc2e009287566
christian-bromann <mail@christian-bromann.com>
2018-04-17 13:45:46 +0200

    wdio-sumologic-reporter: port code

--- packages/wdio-sumologic-reporter/src/index.js
+++ packages/wdio-sumologic-reporter/src/index.js
@@ -1,1 +1,136 @@
-export default {}
+import request from 'request'
+import dateFormat from 'dateformat'
+import stringify from 'json-stringify-safe'
+
+import WDIOReporter from 'wdio-reporter'
+import logger from 'wdio-logger'
+
+const log = logger('wdio-sumologic-reporter')
+
+const MAX_LINES = 100
+const DATE_FORMAT = 'yyyy-mm-dd HH:mm:ss,l o'
+
+/**
+ * Initialize a new sumologic test reporter.
+ */
+export default class SumoLogicReporter extends WDIOReporter {
+    constructor (options) {
+        super(options)
+        this.options = Object.assign({
+            // define sync interval how often logs get pushed to Sumologic
+            syncInterval: 100,
+            // endpoint of collector source
+            sourceAddress: process.env.SUMO_SOURCE_ADDRESS
+        }, options)
+
+        if (typeof this.options.sourceAddress !== 'string') {
+            log.error('Sumo Logic requires "sourceAddress" paramater')
+        }
+
+        // Cache of entries we are yet to sync
+        this.unsynced = []
+        this.inSync = false
+
+        this.errorCount = 0
+        this.specs = {}
+        this.results = {}
+        this.interval = setInterval(::this.sync, this.options.syncInterval)
+    }
+
+    onRunnerStart (runner) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'runner:start',
+            data: runner
+        }))
+    }
+
+    onSuiteStart (suite) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'suite:start',
+            data: suite
+        }))
+    }
+
+    onTestStart (test) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'test:start',
+            data: test
+        }))
+    }
+
+    onTestSkip (test) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'test:skip',
+            data: test
+        }))
+    }
+
+    onTestPass (test) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'test:pass',
+            data: test
+        }))
+    }
+
+    onTestFail (test) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'test:fail',
+            data: test
+        }))
+    }
+
+    onTestEnd (test) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'test:end',
+            data: test
+        }))
+    }
+
+    onSuiteEnd (suite) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'suite:end',
+            data: suite
+        }))
+    }
+
+    onRunnerEnd (runner) {
+        this.unsynced.push(stringify({
+            time: dateFormat(new Date(), DATE_FORMAT),
+            event: 'runner:end',
+            data: runner
+        }))
+    }
+
+    sync () {
+        if (this.inSync || this.unsynced.length === 0 || typeof this.options.sourceAddress !== 'string') {
+            return
+        }
+
+        const logLines = this.unsynced.slice(0, MAX_LINES).join('\n')
+        this.inSync = true
+
+        request({
+            method: 'POST',
+            uri: this.options.sourceAddress,
+            body: logLines
+        }, (err, resp) => {
+            const failed = Boolean(err) || resp.status < 200 || resp.status >= 400
+
+            if (failed) {
+                log.error('failed send data to Sumo Logic:\n', err.stack ? err.stack : err)
+            } else {
+                this.unsynced.splice(0, MAX_LINES)
+            }
+
+            this.inSync = false
+        })
+    }
+}
---
+++ packages/wdio-sumologic-reporter/tests/reporter.test.js
@@ -0,0 +1,108 @@
+import request from 'request'
+import SumoLogicReporter from '../src'
+
+import logger from 'wdio-logger'
+
+jest.useFakeTimers()
+
+describe('wdio-sumologic-reporter', () => {
+    let reporter
+
+    beforeEach(() => {
+        logger().error.mockClear()
+        reporter = new SumoLogicReporter({ stdout: true })
+    })
+
+    it('it should start sync when reporter gets initiated', () => {
+        expect(setInterval).toHaveBeenCalledTimes(1);
+        expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 100)
+    })
+
+    it('should log error if sourceAddress is not defined', () => {
+        expect(logger().error.mock.calls).toHaveLength(1)
+    })
+
+    it('should push to event bucket for every event', () => {
+        expect(reporter.unsynced).toHaveLength(0)
+        reporter.onRunnerStart('onRunnerStart')
+        expect(reporter.unsynced).toHaveLength(1)
+        expect(reporter.unsynced[0]).toContain('"event":"runner:start"')
+        expect(reporter.unsynced[0]).toContain('"data":"onRunnerStart"')
+        reporter.onSuiteStart('onSuiteStart')
+        expect(reporter.unsynced).toHaveLength(2)
+        expect(reporter.unsynced[1]).toContain('"event":"suite:start"')
+        expect(reporter.unsynced[1]).toContain('"data":"onSuiteStart"')
+        reporter.onTestStart('onTestStart')
+        expect(reporter.unsynced).toHaveLength(3)
+        expect(reporter.unsynced[2]).toContain('"event":"test:start"')
+        expect(reporter.unsynced[2]).toContain('"data":"onTestStart"')
+        reporter.onTestSkip('onTestSkip')
+        expect(reporter.unsynced).toHaveLength(4)
+        expect(reporter.unsynced[3]).toContain('"event":"test:skip"')
+        expect(reporter.unsynced[3]).toContain('"data":"onTestSkip"')
+        reporter.onTestPass('onTestPass')
+        expect(reporter.unsynced).toHaveLength(5)
+        expect(reporter.unsynced[4]).toContain('"event":"test:pass"')
+        expect(reporter.unsynced[4]).toContain('"data":"onTestPass"')
+        reporter.onTestFail('onTestFail')
+        expect(reporter.unsynced).toHaveLength(6)
+        expect(reporter.unsynced[5]).toContain('"event":"test:fail"')
+        expect(reporter.unsynced[5]).toContain('"data":"onTestFail"')
+        reporter.onTestEnd('onTestEnd')
+        expect(reporter.unsynced).toHaveLength(7)
+        expect(reporter.unsynced[6]).toContain('"event":"test:end"')
+        expect(reporter.unsynced[6]).toContain('"data":"onTestEnd"')
+        reporter.onSuiteEnd('onSuiteEnd')
+        expect(reporter.unsynced).toHaveLength(8)
+        expect(reporter.unsynced[7]).toContain('"event":"suite:end"')
+        expect(reporter.unsynced[7]).toContain('"data":"onSuiteEnd"')
+        reporter.onRunnerEnd('onRunnerEnd')
+        expect(reporter.unsynced).toHaveLength(9)
+        expect(reporter.unsynced[8]).toContain('"event":"runner:end"')
+        expect(reporter.unsynced[8]).toContain('"data":"onRunnerEnd"')
+    })
+
+    describe('should not sync if it', () => {
+        it('is currently syncing data', () => {
+            reporter.inSync = true
+            reporter.sync()
+            expect(request.mock.calls).toHaveLength(0)
+        })
+
+        it('has no data to sync', () => {
+            reporter.sync()
+            expect(request.mock.calls).toHaveLength(0)
+        })
+
+        it('has no source address set up', () => {
+            reporter.onRunnerStart('onRunnerStart')
+            reporter.sync()
+            expect(request.mock.calls).toHaveLength(0)
+        })
+    })
+
+    it('should sync', () => {
+        reporter.options.sourceAddress = 'http://localhost:1234'
+        reporter.onRunnerStart('onRunnerStart')
+        reporter.sync()
+
+        expect(request.mock.calls).toHaveLength(1)
+        expect(request.mock.calls[0][0].method).toBe('POST')
+        expect(request.mock.calls[0][0].uri).toBe('http://localhost:1234')
+        expect(request.mock.calls[0][0].body).toContain('"event":"runner:start","data":"onRunnerStart"')
+
+        expect(reporter.unsynced).toHaveLength(0)
+    })
+
+    it('should log if it fails syncing', () => {
+        logger().error.mockClear()
+
+        reporter.options.sourceAddress = 'http://localhost:1234'
+        reporter.onRunnerStart('onRunnerStart')
+        reporter.sync()
+
+        request.mock.calls[0][1](new Error('ups'))
+        expect(logger().error.mock.calls).toHaveLength(1)
+        expect(logger().error.mock.calls[0][0]).toContain('failed send data to Sumo Logic')
+    })
+})
