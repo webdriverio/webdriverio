4c0c46f98f8928bf8cfb2434b4416f7a12fa444f
christian-bromann <mail@christian-bromann.com>
2018-05-17 12:00:22 +0200

    add port of wdio-jasmine-framework

---
+++ packages/wdio-jasmine-framework/README.md
@@ -0,0 +1,84 @@
+WDIO Jasmine Framework Adapter
+==============================
+
+> A WebdriverIO plugin. Adapter for Jasmine testing framework.
+
+## Installation
+
+The easiest way is to keep `wdio-jasmine-framework` as a devDependency in your `package.json`.
+
+```json
+{
+  "devDependencies": {
+    "wdio-jasmine-framework": "~1.0.0"
+  }
+}
+```
+
+You can simple do it by:
+
+```bash
+npm install wdio-jasmine-framework --save-dev
+```
+
+Instructions on how to install `WebdriverIO` can be found [here.](http://webdriver.io/guide/getstarted/install.html)
+
+## Configuration
+
+Following code shows the default wdio test runner configuration...
+
+```js
+// wdio.conf.js
+module.exports = {
+  // ...
+  framework: 'jasmine'
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 10000
+  }
+  // ...
+};
+```
+
+## `jasmineNodeOpts` Options
+
+### defaultTimeoutInterval
+Timeout until specs will be marked as failed.
+
+Type: `Number`<br>
+Default: 10000
+
+### expectationResultHandler
+The Jasmine framework allows it to intercept each assertion in order to log the state of the application
+or website depending on the result. For example it is pretty handy to take a screenshot every time
+an assertion fails.
+
+Type: `Function`<br>
+Default: null
+
+### grep
+Optional pattern to selectively select it/describe cases to run from spec files.
+
+Type: `RegExp | string`<br>
+Default: undefined
+
+## invertGrep
+Inverts 'grep' matches
+
+Type: `Boolean`<br>
+Default: false
+
+## cleanStack
+Cleans up stack trace and removes all traces of node module packages
+
+Type: `Boolean`<br>
+Default: true
+
+## random
+Run specs in semi-random order
+
+Type: `Boolean`<br>
+Default: `false`
+
+----
+
+For more information on WebdriverIO see the [homepage](http://webdriver.io).
---
+++ packages/wdio-jasmine-framework/src/reporter.js
@@ -0,0 +1,100 @@
+const STACKTRACE_FILTER = /(node_modules(\/|\\)(\w+)*|wdio-sync\/build|- - - - -)/g
+
+export default class JasmineReporter {
+    constructor (reporter, params) {
+        this.reporter = reporter
+
+        this.cid = params.cid
+        this.capabilities = params.capabilities
+        this.specs = params.specs
+        this.cleanStack = params.cleanStack
+        this.parent = []
+        this.failedCount = 0
+    }
+
+    suiteStarted (suite = {}) {
+        this._suiteStart = new Date()
+        suite.type = 'suite'
+
+        this.emit('suite:start', suite)
+        this.parent.push({
+            description: suite.description,
+            id: suite.id
+        })
+    }
+
+    specStarted (test = {}) {
+        this._testStart = new Date()
+        test.type = 'test'
+        this.emit('test:start', test)
+    }
+
+    specDone (test) {
+        /**
+         * jasmine can't set test pending if async (`pending()` got called)
+         * this is a workaround until https://github.com/jasmine/jasmine/issues/937 is resolved
+         */
+        if (Array.isArray(test.failedExpectations)) {
+            test.failedExpectations.forEach((e) => {
+                if (e.message.includes('Failed: => marked Pending')) {
+                    test.status = 'pending'
+                    test.failedExpectations = []
+                }
+            })
+        }
+
+        if (test.failedExpectations && this.cleanStack) {
+            test.failedExpectations = test.failedExpectations.map(::this.cleanStack)
+        }
+
+        var e = 'test:' + test.status.replace(/ed/, '')
+        test.type = 'test'
+        test.duration = new Date() - this._testStart
+        this.emit(e, test)
+        this.failedCount += test.status === 'failed' ? 1 : 0
+        this.emit('test:end', test)
+    }
+
+    suiteDone (suite = {}) {
+        this.parent.pop()
+        suite.type = 'suite'
+        suite.duration = new Date() - this._suiteStart
+        this.failedCount += suite.status === 'failed' ? 1 : 0
+        this.emit('suite:end', suite)
+    }
+
+    emit (event, payload) {
+        let message = {
+            cid: this.cid,
+            uid: this.getUniqueIdentifier(payload),
+            event: event,
+            title: payload.description,
+            pending: payload.status === 'pending',
+            parent: this.parent.length ? this.getUniqueIdentifier(this.parent[this.parent.length - 1]) : null,
+            type: payload.type,
+            file: '',
+            err: payload.failedExpectations && payload.failedExpectations.length ? payload.failedExpectations[0] : null,
+            duration: payload.duration,
+            runner: {},
+            specs: this.specs
+        }
+
+        message.runner[this.cid] = this.capabilities
+        this.reporter.emit(event, message)
+    }
+
+    getFailedCount () {
+        return this.failedCount
+    }
+
+    getUniqueIdentifier (target) {
+        return target.description + target.id
+    }
+
+    cleanStack (error) {
+        let stack = error.stack.split('\n')
+        stack = stack.filter((line) => !line.match(STACKTRACE_FILTER))
+        error.stack = stack.join('\n')
+        return error
+    }
+}
