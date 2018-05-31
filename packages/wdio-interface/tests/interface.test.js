61059154e1ff435f91b4c8cab5b64d6215c7613c
christian-bromann <mail@christian-bromann.com>
2018-05-31 14:04:50 +0200

    wdio-interface: use jest interface

--- packages/wdio-interface/tests/interface.test.js
+++ packages/wdio-interface/tests/interface.test.js
@@ -2,48 +2,33 @@ import WDIOInterface from '../src'
 
 process.env.WDIO_TEST = 1
 
-describe('WDIOInterface', () => {
-    let iface
+let iface
 
-    beforeEach(() => {
-        iface = new WDIOInterface()
-        iface.out = jest.fn()
-        iface.err = jest.fn()
-    })
-
-    it('can clear lines', () => {
-        iface.clearLine()
-        expect(iface.out.mock.calls).toEqual([['eraseStartLine'], ['cursorLeft']])
-    })
-
-    it('can log', () => {
-        iface.log('foo', 'bar')
-        expect(iface.out.mock.calls).toEqual([['foo bar\n']])
-    })
-
-    it('can write', () => {
-        iface.write('foobar')
-        expect(iface.out.mock.calls).toEqual([['foobar']])
-    })
-
-    it('can wrap stdout or stderr', () => {
-        const buffer = []
-        const stream = {}
-        iface.wrapStdio(stream, buffer)
-        stream.write('foobar')
-        expect(buffer).toHaveLength(1)
-    })
+beforeEach(() => {
+    iface = new WDIOInterface()
+    iface.out = jest.fn()
+    iface.err = jest.fn()
 })
 
-// clearLine () {
-//     this.out(ansiEscapes.eraseStartLine)
-//     this.out(ansiEscapes.cursorLeft)
-// }
-//
-// log(...messages) {
-//     this.out(util.format.apply(this, messages) + '\n')
-// }
-//
-// write (message) {
-//     this.out(message)
-// }
+test('can clear lines', () => {
+    iface.clearLine()
+    expect(iface.out.mock.calls).toEqual([['eraseStartLine'], ['cursorLeft']])
+})
+
+test('can log', () => {
+    iface.log('foo', 'bar')
+    expect(iface.out.mock.calls).toEqual([['foo bar\n']])
+})
+
+test('can write', () => {
+    iface.write('foobar')
+    expect(iface.out.mock.calls).toEqual([['foobar']])
+})
+
+test('can wrap stdout or stderr', () => {
+    const buffer = []
+    const stream = {}
+    iface.wrapStdio(stream, buffer)
+    stream.write('foobar')
+    expect(buffer).toHaveLength(1)
+})
