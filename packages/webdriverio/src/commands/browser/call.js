7eda35e81b406db995cb33609805c304183b0b43
christian-bromann <mail@christian-bromann.com>
2018-06-26 13:01:20 +0200

    webdriverio: add call command

---
+++ packages/webdriverio/src/commands/browser/call.js
@@ -0,0 +1,42 @@
+/**
+ * You can use `call` to execute any async action within your test spec. The command itself
+ * is treated like a synchronous function. It accepts promises and stops the execution until
+ * the promise has resolved.
+ *
+ * <example>
+    :call.js
+    it('some testing here', function() {
+        browser.url('http://google.com')
+        // make an asynchronous call using any 3rd party library supporting promises
+        // e.g. call to backend or db to inject fixture data
+        browser.call(function () {
+            return somePromiseLibrary.someMethod().then(function () {
+                // ...
+            })
+        })
+        // example for async call to 3rd party library that doesn't support promises
+        browser.call(function () {
+            return new Promise(function(resolve, reject) {
+                someOtherNodeLibrary.someMethod(param1, function(err, res) {
+                    if (err) {
+                        return reject(err)
+                    }
+                    resolve(res)
+                })
+            })
+        })
+        // continue synchronously
+        browser.click('#elemA')
+        browser.setValue('.firstname','webdriverbot')
+    });
+ * </example>
+ *
+ * @alias browser.call
+ * @param {Function} callback  function to be called
+ * @type utility
+ *
+ */
+
+export default function call (fn = () => {}) {
+    return fn()
+}
