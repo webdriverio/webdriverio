21ec37ecb74cb4b793a779d224995016aaf629bd
Andrii Lazebnyi <lazebniy.a@gmail.com>
2018-07-09 11:06:14 +0300

    Port wdio-concise-reporter (#83)
    * wdio-concise-reporter: initial commit
    
    * wdio-concise-reporter: code cleanup
    
    * wdio-concise-reporter: finish reporter
    
    * wdio-concise-reporter: add tests
    
    * wdio-concise-reporter: update readme

---
+++ packages/wdio-concise-reporter/README.md
@@ -0,0 +1,38 @@
+WDIO Concise Reporter
+=====================
+
+> A WebdriverIO plugin to report in concise style.
+
+## Installation
+
+The easiest way is to keep `wdio-concise-reporter` as a devDependency in your `package.json`.
+
+```json
+{
+  "devDependencies": {
+    "wdio-concise-reporter": "~0.0.1"
+  }
+}
+```
+
+You can simple do it by:
+
+```sh
+$ npm install wdio-concise-reporter --save-dev
+```
+
+Instructions on how to install `WebdriverIO` can be found [here](http://webdriver.io/guide/getstarted/install.html).
+
+## Configuration
+
+The following code shows the default wdio test runner configuration. Just add `'concise'` as a reporter
+to the array.
+
+```js
+// wdio.conf.js
+module.exports = {
+  // ...
+  reporters: ['dot', 'concise'],
+  // ...
+};
+```
---
+++ packages/wdio-concise-reporter/tests/fixtures.js
@@ -0,0 +1,85 @@
+export const RUNNER = {
+    cid : '0-0',
+    _duration : 1001,
+    capabilities : {
+        browserName : 'chrome',
+    },
+    specs : ['/foo/bar/baz.js'],
+}
+
+export const SUITE_UIDS = [
+    'Foo test1',
+    'Bar test2',
+    'Baz test3',
+]
+
+export const SUITES = [
+    {
+        uid : SUITE_UIDS[0],
+        title : SUITE_UIDS[0].slice(0, -1),
+        tests : [
+            {
+                uid : 'foo1',
+                title : 'foo',
+                state : 'passed',
+            },
+            {
+                uid : 'bar1',
+                title : 'bar',
+                state : 'passed',
+            }
+        ],
+    },
+    {
+        uid : SUITE_UIDS[1],
+        title : SUITE_UIDS[1].slice(0, -1),
+        tests : [
+            {
+                uid : 'some test1',
+                title : 'some test',
+                state : 'passed',
+            },
+            {
+                uid : 'a failed test2',
+                title : 'a failed test',
+                state : 'failed',
+                error : {
+                    message : `'Google' == 'Google2'`,
+                    stack : 'Failed test stack trace',
+                    type: 'AssertionError [ERR_ASSERTION]'
+                }
+            }
+        ],
+    },
+    {
+        uid : SUITE_UIDS[2],
+        title : SUITE_UIDS[2].slice(0, -1),
+        tests : [
+            {
+                uid : 'foo bar baz1',
+                title : 'foo bar baz',
+                state : 'passed',
+            },
+            {
+                uid : 'a skipped test2',
+                title : 'a skipped test',
+                state : 'skipped',
+            }],
+    }
+]
+
+export const SUITES_NO_TESTS = [
+    {
+        uid: SUITE_UIDS[0],
+        title: SUITE_UIDS[0].slice(0, -1),
+        tests: [],
+        suites: []
+    },
+]
+
+export const REPORT = `yellow ========= Your concise report ==========
+chrome
+Test failed (1):
+  Fail : red a failed test
+    AssertionError [ERR_ASSERTION] : yellow 'Google' == 'Google2'
+`
