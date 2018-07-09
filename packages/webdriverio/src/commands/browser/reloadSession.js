9b9f3eea51aa60887b48b020aed263364082b929
christian-bromann <mail@christian-bromann.com>
2018-07-10 01:22:31 +0200

    webdriverio: add unit test for reloadSession hook

--- packages/webdriverio/src/commands/browser/reloadSession.js
+++ packages/webdriverio/src/commands/browser/reloadSession.js
@@ -41,10 +41,11 @@ export default async function reloadSession () {
 
     const response = await sessionRequest.makeRequest(this.options)
     const newSessionId = response.sessionId
+    const config = this.sessionConfig || this.options
     this.sessionId = newSessionId
 
-    if (Array.isArray(this.sessionConfig.onReload) && this.sessionConfig.onReload.length) {
-        await Promise.all(this.sessionConfig.onReload.map((hook) => hook(oldSessionId, newSessionId)))
+    if (Array.isArray(config.onReload) && config.onReload.length) {
+        await Promise.all(config.onReload.map((hook) => hook(oldSessionId, newSessionId)))
     }
 
     return newSessionId
