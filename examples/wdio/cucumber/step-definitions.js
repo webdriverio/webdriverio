a49c5c6a74c412e92428612e59f56215938f7abb
christian-bromann <mail@christian-bromann.com>
2018-07-13 12:29:58 +0200

    (chore): lint examples

--- examples/wdio/cucumber/step-definitions.js
+++ examples/wdio/cucumber/step-definitions.js
@@ -9,24 +9,22 @@
  * $ cucumber.js
  */
 
-var assert = require('assert'),
-    tmpResult;
+var assert = require('assert')
 
-module.exports = function(){
+module.exports = () => {
+    this.Given(/^I go on the website "([^"]*)"$/, (url) => {
+        browser.url(url)
+    })
 
-    this.Given(/^I go on the website "([^"]*)"$/, function(url) {
-        browser.url(url);
-    });
+    this.Then(/^should the element "([^"]*)" be (\d+)px wide and (\d+)px high$/, (selector, width, height) => {
+        var elemSize = browser.getElementSize(selector)
+        assert.equal(elemSize.width, width, 'width of element is ' + elemSize.width + ' but should be ' + width)
+        assert.equal(elemSize.height, height, 'height of element is ' + elemSize.height + ' but should be ' + height)
+    })
 
-    this.Then(/^should the element "([^"]*)" be (\d+)px wide and (\d+)px high$/, function(selector, width, height) {
-        var elemSize = browser.getElementSize(selector);
-        assert.equal(elemSize.width, width, 'width of element is ' + elemSize.width + ' but should be ' + width);
-        assert.equal(elemSize.height, height, 'height of element is ' + elemSize.height + ' but should be ' + height);
-    });
+    this.Then(/^should the title of the page be "([^"]*)"$/, (expectedTitle) => {
+        var title = browser.getTitle()
+        assert.equal(title, expectedTitle, ' title is "'+ title + '" but should be "'+ expectedTitle)
+    })
 
-    this.Then(/^should the title of the page be "([^"]*)"$/, function(expectedTitle) {
-        var title = browser.getTitle();
-        assert.equal(title, expectedTitle, ' title is "'+ title + '" but should be "'+ expectedTitle);
-    });
-
-};
+}
