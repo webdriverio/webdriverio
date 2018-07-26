f64e88a9c46d3a396b9b8d428d2cd2643b698c24
Will Brock <wpbrock@gmail.com>
2018-07-26 10:38:47 -0400

    webdriverio: selectByVisibleText with spaces

--- packages/webdriverio/tests/commands/element/selectByVisibleText.test.js
+++ packages/webdriverio/tests/commands/element/selectByVisibleText.test.js
@@ -28,7 +28,19 @@ describe('selectByVisibleText test', () => {
 
         expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/element')
         expect(request.mock.calls[2][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-elem-123/element')
-        expect(request.mock.calls[2][0].body.value).toBe(`./option[normalize-space(translate(., ' ', '')) = "someValue1"]|./optgroup/option[normalize-space(translate(., ' ', '')) = "someValue1"]`)
+        expect(request.mock.calls[2][0].body.value).toBe(`./option[normalize-space(text()) = "someValue1"]|./optgroup/option[normalize-space(text()) = "someValue1"]`)
+        expect(request.mock.calls[3][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-sub-elem-321/click')
+        expect(getElementFromResponseSpy).toBeCalledWith({
+            [ELEMENT_KEY]: 'some-sub-elem-321'
+        })
+    })
+
+    it('should select value by visible text with spaces', async () => {
+        await elem.selectByVisibleText('some Value1')
+
+        expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/element')
+        expect(request.mock.calls[2][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-elem-123/element')
+        expect(request.mock.calls[2][0].body.value).toBe(`./option[normalize-space(text()) = "some Value1"]|./optgroup/option[normalize-space(text()) = "some Value1"]`)
         expect(request.mock.calls[3][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-sub-elem-321/click')
         expect(getElementFromResponseSpy).toBeCalledWith({
             [ELEMENT_KEY]: 'some-sub-elem-321'
@@ -40,7 +52,19 @@ describe('selectByVisibleText test', () => {
 
         expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/element')
         expect(request.mock.calls[2][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-elem-123/element')
-        expect(request.mock.calls[2][0].body.value).toBe(`./option[normalize-space(translate(., ' ', '')) = concat("", '"', "someValue1", '"', "", '"', "")]|./optgroup/option[normalize-space(translate(., ' ', '')) = concat("", '"', "someValue1", '"', "", '"', "")]`)
+        expect(request.mock.calls[2][0].body.value).toBe(`./option[normalize-space(text()) = concat("", '"', "someValue1", '"', "", '"', "")]|./optgroup/option[normalize-space(text()) = concat("", '"', "someValue1", '"', "", '"', "")]`)
+        expect(request.mock.calls[3][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-sub-elem-321/click')
+        expect(getElementFromResponseSpy).toBeCalledWith({
+            [ELEMENT_KEY]: 'some-sub-elem-321'
+        })
+    })
+
+    it('should convert number to string when selecting', async () => {
+        await elem.selectByVisibleText(123)
+
+        expect(request.mock.calls[1][0].uri.path).toBe('/wd/hub/session/foobar-123/element')
+        expect(request.mock.calls[2][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-elem-123/element')
+        expect(request.mock.calls[2][0].body.value).toBe(`./option[normalize-space(text()) = "123"]|./optgroup/option[normalize-space(text()) = "123"]`)
         expect(request.mock.calls[3][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-sub-elem-321/click')
         expect(getElementFromResponseSpy).toBeCalledWith({
             [ELEMENT_KEY]: 'some-sub-elem-321'
