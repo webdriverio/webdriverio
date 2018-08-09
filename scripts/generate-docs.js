d191078019637c70da60e31302e4e208a0cd796b
christian-bromann <mail@christian-bromann.com>
2018-08-09 14:49:49 +0200

    fix docs urls

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const ejs = require('../packages/wdio-cli/node_modules/ejs')
 
+const config = require('../website/siteConfig')
 const sidebars = require('../website/_sidebars.json')
 
 const PROTOCOLS = {
@@ -30,19 +31,24 @@ for (const [protocolName, definition] of Object.entries(PROTOCOLS)) {
                 return Object.assign(variable, { required: true, type: 'String' })
             }), ...description.parameters || []]
 
-            description.id = `${protocolName}_${description.command}`
             description.paramString = description.paramTags.map((param) => param.name).join(', ')
             description.examples = [] // tbd
             description.returnTags = [] // tbd
             description.throwsTags = [] // tbd
+            description.customEditUrl = `${config.repoUrl}/edit/master/packages/webdriver/protocol/${protocolName}.json`
 
             if (!description.description) {
                 description.description = `${protocol} command. More details can be found ` +
                     `in the [official protocol docs](${description.ref}).`
             }
 
+            const docDir = path.join(__dirname, '..', 'docs', 'api', protocolName)
+            if (!fs.existsSync(docDir)){
+                fs.mkdirSync(docDir);
+            }
+
             const markdown = ejs.render(template, { method: description }, {})
-            const docPath = path.join(__dirname, '..', 'docs', 'api', `${description.id}.md`)
+            const docPath = path.join(docDir, `${description.command}.md`)
             fs.writeFileSync(docPath, markdown, { encoding: 'utf-8' })
 
             /**
@@ -51,7 +57,7 @@ for (const [protocolName, definition] of Object.entries(PROTOCOLS)) {
             if (!sidebars.api[protocol]) {
                 sidebars.api[protocol] = []
             }
-            sidebars.api[protocol].push(`api/${description.id}`)
+            sidebars.api[protocol].push(`api/${protocolName}/${description.command}`)
 
             // eslint-disable-next-line no-console
             console.log(`Generated docs for ${method} ${endpoint} - ${docPath}`);
