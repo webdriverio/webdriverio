8c714882eec1921486a15cffb72c3238658589d0
amarnathk1547 <43011408+amarnathk1547@users.noreply.github.com>
2018-09-09 19:39:18 +0530

    Added parameters & type-hints

--- packages/webdriverio/src/scripts/getProperty.js
+++ packages/webdriverio/src/scripts/getProperty.js
@@ -1,5 +1,8 @@
 /**
  * get property from element
+ * @param  {String} element    element with requested property
+ * @param  {String} property   requested property
+ * @return {String}            the value of the property
  */
 export default function getProperty (element, property) {
     return element[property]
