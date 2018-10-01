b45c2885f5376c0f30b958f141dbfadfd2b2108a
uzzal2k5 <uzzal2k5@gmail.com>
2018-10-01 19:25:53 +0600

    user_script.sh: modification

--- user-script.sh
+++ user-script.sh
@@ -1,7 +1,7 @@
 # Indicate the test suit / Test case here to run the test
 cd examples/pageobject &&  npm run test 
-cd ../../examples/wdio &&  npm run test:mocha
-cd ../../examples/wdio &&  npm run test:jasmine
-cd ../../examples/wdio &&  npm run test:cucumber
-cd ../../examples/wdio &&  npm run test:multiremote
-cd ../../examples/wdio &&  npm run test:customReporter
+# cd ../../examples/wdio &&  npm run test:mocha
+# cd ../../examples/wdio &&  npm run test:jasmine
+# cd ../../examples/wdio &&  npm run test:cucumber
+# cd ../../examples/wdio &&  npm run test:multiremote
+# cd ../../examples/wdio &&  npm run test:customReporter
