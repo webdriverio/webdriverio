8a10a29b99982fc7c79688d6138a7b21a98591f6
christian-bromann <mail@christian-bromann.com>
2018-11-05 11:11:57 +0100

    wdio-cli: relaunch session when pressing save

--- packages/wdio-cli/src/watch.js
+++ packages/wdio-cli/src/watch.js
@@ -2,17 +2,16 @@ import chokidar from 'chokidar'
 import logger from 'wdio-logger'
 import { ConfigParser } from 'wdio-config'
 
-// import Launcher from './launcher.js'
+import Launcher from './launcher.js'
 
 const log = logger('wdio-cli:watch')
 
-// function launch (wdioConf, params) {
-//     log.debug('Run suite with config', wdioConf, 'and params', params)
-//     let launcher = new Launcher(wdioConf, params)
-//     launcher.run().then(
-//         (code) => process.exit(code),
-//         (e) => process.nextTick(() => { throw e }))
-// }
+function launch (wdioConf, params) {
+    log.debug('Run suite with config', wdioConf, 'and params', params)
+    let launcher = new Launcher(wdioConf, params)
+    launcher.run().then(
+        () => launcher.interface.log('\nWaiting for files to change...'))
+}
 
 export default function watch (configFile, argv) {
     log.info('Starting launcher in watch mode')
@@ -22,8 +21,7 @@ export default function watch (configFile, argv) {
     configParser.merge(argv)
 
     const specs = configParser.getSpecs()
-    const config = configParser.getConfig()
     chokidar.watch(specs)
-        .on('add', path => log.info(`File ${path} has been added`))
-        .on('change', path => log.info(`File ${path} has been changed`))
+        .on('add', path => launch(configFile, Object.assign(argv, { spec: path })))
+        .on('change', path => launch(configFile, Object.assign(argv, { spec: path })))
 }
