fb6cdd06c565c8ae011e57974220fbbe9b75236a
christian-bromann <mail@christian-bromann.com>
2018-11-05 15:04:26 +0100

    (chore): fix link script

--- scripts/link.js
+++ scripts/link.js
@@ -8,9 +8,17 @@ const shell = require('shelljs')
 
 const packagesDir = path.join(__dirname, '..', 'packages')
 const nodeModulesDir = path.join(__dirname, '..', 'packages', 'node_modules')
+const nodeModulesWDIODir = path.join(__dirname, '..', 'packages', 'node_modules', '@wdio')
 
 const packages = shell.ls(packagesDir)
 shell.mkdir(nodeModulesDir)
+shell.mkdir(nodeModulesWDIODir)
 packages.forEach(
-    (pkg) => shell.ln('-s', path.join(packagesDir, pkg), path.join(nodeModulesDir, pkg))
+    (pkg) => shell.ln(
+        '-s',
+        path.join(packagesDir, pkg),
+        pkg.startsWith('wdio-')
+            ? path.join(nodeModulesWDIODir, pkg.replace('wdio-', ''))
+            : path.join(nodeModulesDir, pkg)
+    )
 )
