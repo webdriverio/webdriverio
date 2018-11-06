bc0beef27941eb1c223499ded9e61728646689b6
christian-bromann <mail@christian-bromann.com>
2018-11-06 10:42:22 +0100

    ci: fixed unit tests

--- packages/wdio-logger/src/index.js
+++ packages/wdio-logger/src/index.js
@@ -2,5 +2,5 @@
  * environment check to allow to use this package in a web context
  */
 export default (typeof process !== 'undefined' && process.release.name === 'node')
-    ? require('./node')
-    : require('./web')
+    ? require('./node').default
+    : require('./web').default
