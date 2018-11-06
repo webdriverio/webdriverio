95a7d8ca9d29a861fb50cb1555abb01249879de5
christian-bromann <mail@christian-bromann.com>
2018-11-07 00:04:37 +0100

    fix sourcemap setup

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,11 @@ module.exports = {
         '@babel/plugin-proposal-function-bind',
         '@babel/plugin-proposal-export-default-from',
         '@babel/plugin-proposal-class-properties'
-    ]
+    ],
+    env: {
+        development: {
+            sourceMaps: 'inline',
+            plugins: ['source-map-support']
+        }
+    }
 }
