cbc04abbf42af6eb620ac4ea2a0855a21bf76218
christian-bromann <mail@christian-bromann.com>
2018-11-12 17:23:10 +0100

    ci: increase coverage

---
+++ packages/wdio-reporter/tests/stats/runner.test.js
@@ -0,0 +1,20 @@
+import RunnerStats from '../../src/stats/runner'
+
+test('should get initialised', () => {
+    const capabilities = { browserName: 'chrome' }
+    const config = { foo: 'bar' }
+    const specs = ['./foo/bar.js']
+    const runner = new RunnerStats({
+        cid: '0-0',
+        capabilities,
+        config,
+        specs,
+        isMultiremote: false
+    })
+
+    expect(runner.type).toBe('runner')
+    expect(runner.cid).toBe('0-0')
+    expect(runner.capabilities).toEqual(capabilities)
+    expect(runner.config).toEqual(config)
+    expect(runner.specs).toEqual(specs)
+})
