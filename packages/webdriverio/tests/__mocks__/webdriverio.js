49b7f37330021c4eb94d8549d694197ca5e3995d
christian-bromann <mail@christian-bromann.com>
2018-11-12 22:31:33 +0100

    ci: fix tests

--- packages/webdriverio/tests/__mocks__/webdriverio.js
+++ packages/webdriverio/tests/__mocks__/webdriverio.js
@@ -1,3 +1,10 @@
-export const attach = jest.fn()
-export const remote = jest.fn()
-export const multiremote = jest.fn().mockImplementation(() => ({}))
+const wdioMock = {
+    $: jest.fn(),
+    $$: jest.fn(),
+    debug: jest.fn(),
+    deleteSession: jest.fn()
+}
+
+export const attach = jest.fn().mockImplementation(() => wdioMock)
+export const remote = jest.fn().mockImplementation(() => wdioMock)
+export const multiremote = jest.fn().mockImplementation(() => wdioMock)
