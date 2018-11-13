148e6e44c414a093ce6fcd600bce1ea0ee8d3ba7
christian-bromann <mail@christian-bromann.com>
2018-11-13 11:17:28 +0100

    wdio-local-runner: set max listener dynamically for stdout/err

--- packages/wdio-local-runner/src/index.js
+++ packages/wdio-local-runner/src/index.js
@@ -1,4 +1,8 @@
+import logger from '@wdio/logger'
+
 import WorkerInstance from './worker'
+
+const log = logger('wdio-local-runner')
 
 export default class LocalRunner {
     constructor (configFile, config) {
@@ -12,10 +16,34 @@ export default class LocalRunner {
      */
     initialise () {}
 
+    getWorkerCount () {
+        return Object.keys(this.workerPool).length
+    }
+
     run ({ command, argv, ...options }) {
+        /**
+         * adjust max listeners on stdout/stderr when creating listeners
+         */
+        const workerCnt = this.getWorkerCount()
+        if (workerCnt >= process.stdout.getMaxListeners() - 2) {
+            process.stdout.setMaxListeners(workerCnt + 2)
+            process.stderr.setMaxListeners(workerCnt + 2)
+        }
+
         const worker = new WorkerInstance(this.config, options)
         this.workerPool[options.cid] = worker
         worker.postMessage(command, argv)
+
+        /**
+         * ensure to delete worker from pool once exited
+         */
+        worker.on('exit', ({ cid }) => {
+            log.debug(`Remove worker with cid ${cid} from pool`)
+            delete this.workerPool[cid]
+            const workerCnt = this.getWorkerCount()
+            process.stdout.setMaxListeners(workerCnt + 2)
+            process.stderr.setMaxListeners(workerCnt + 2)
+        })
 
         return worker
     }
