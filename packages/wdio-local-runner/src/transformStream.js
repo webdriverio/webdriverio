fc9e4559b28468769e794f5aa8b2ec98e9c6b406
christian-bromann <mail@christian-bromann.com>
2018-11-30 14:10:20 +0100

    webdriverio: register process as debugger target when debug is called

--- packages/wdio-local-runner/src/transformStream.js
+++ packages/wdio-local-runner/src/transformStream.js
@@ -1,13 +1,20 @@
 import { Transform } from 'stream'
 
 export default class RunnerTransformStream extends Transform {
-    constructor (cid) {
+    constructor (cid, emitter) {
         super()
         this.cid = cid
+        this.emitter = emitter
     }
 
     _transform(chunk, encoding, callback) {
-        this.push(`[${this.cid}] ${chunk.toString()}`)
+        const logMsg = chunk.toString()
+
+        if (logMsg.startsWith('Debugger listening on ws')) {
+            return callback()
+        }
+
+        this.push(`[${this.cid}] ${logMsg}`)
         callback()
     }
 }
