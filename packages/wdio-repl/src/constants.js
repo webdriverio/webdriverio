e3af1603a93d4e414f105a1a8072c62fecaabb1e
christian-bromann <mail@christian-bromann.com>
2018-11-30 13:13:47 +0100

    wdio-repl: add packages for handling repl mode

---
+++ packages/wdio-repl/src/constants.js
@@ -0,0 +1,12 @@
+export const STATIC_RETURNS = {
+    'driver': '[WebdriverIO REPL client]',
+    'browser': '[WebdriverIO REPL client]',
+    '$': '[Function: findElement]',
+    '$$': '[Function: findElements]'
+}
+
+export const INTRO_MESSAGE = `
+The execution has stopped!
+You can now go into the browser or use the command line as REPL
+(To exit, press ^C again or type .exit)
+`
