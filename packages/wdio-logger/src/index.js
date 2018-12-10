e8bb74631f21317ebd6963b5777a0e98f1d247d5
Jonathan Lipps <jlipps@gmail.com>
2018-12-09 22:10:49 -0800

    fix logging target determination we might wind up in a webpack environment where 'process' is defined but fake, in which case we can't rely on 'process.release' existing

--- packages/wdio-logger/src/index.js
+++ packages/wdio-logger/src/index.js
@@ -1,6 +1,6 @@
 /**
  * environment check to allow to use this package in a web context
  */
-export default (typeof process !== 'undefined' && process.release.name === 'node')
+export default (typeof process !== 'undefined' &&  && typeof process.release !== 'undefined' && process.release.name === 'node')
     ? require('./node').default
     : require('./web').default
