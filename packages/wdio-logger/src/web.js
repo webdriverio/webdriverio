fd0c30f35cb3caed1fa0e9c0ac95756b10a867d7
Jonathan Lipps <jlipps@gmail.com>
2018-12-10 09:32:07 -0800

    make setLevel a no-op for web

--- packages/wdio-logger/src/web.js
+++ packages/wdio-logger/src/web.js
@@ -1,7 +1,12 @@
 export default function getLogger (component) {
-    return ['error', 'warn', 'info', 'debug', 'trace'].reduce((acc, cur) => {
+    let logger = ['error', 'warn', 'info', 'debug', 'trace'].reduce((acc, cur) => {
         // eslint-disable-next-line no-console
         acc[cur] = console[cur].bind(console, `${component}:`)
         return acc
     }, {})
+
+    // logging interface expects a 'setLevel' method
+    logger.setLevel = () => {}
+
+    return logger
 }
