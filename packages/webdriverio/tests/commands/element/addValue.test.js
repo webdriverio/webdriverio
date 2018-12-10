7f60f13a59a0010efff7d6371749c17e8c766be3
christian-bromann <mail@christian-bromann.com>
2018-12-10 11:59:33 +0100

    webdriverio: adjust set/add value commands

--- packages/webdriverio/tests/commands/element/addValue.test.js
+++ packages/webdriverio/tests/commands/element/addValue.test.js
@@ -13,7 +13,7 @@ describe('addValue test', () => {
 
         await elem.addValue('foobar')
         expect(request.mock.calls[2][0].uri.path).toBe('/wd/hub/session/foobar-123/element/some-elem-123/value')
-        expect(request.mock.calls[2][0].body.value).toEqual(['f', 'o', 'o', 'b', 'a', 'r'])
+        expect(request.mock.calls[2][0].body.text).toEqual('foobar')
     })
 
     it('should allow to add value to an input element using jsonwp', async () => {
