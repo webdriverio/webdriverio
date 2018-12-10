e01238fa148bcf3304d5e82efe417bb2f53dbe3b
Jonathan Lipps <jlipps@gmail.com>
2018-12-10 09:40:58 -0800

    oops, put the method in the right place

--- packages/wdio-logger/src/web.js
+++ packages/wdio-logger/src/web.js
@@ -1,12 +1,10 @@
 export default function getLogger (component) {
-    let logger = ['error', 'warn', 'info', 'debug', 'trace'].reduce((acc, cur) => {
+    return ['error', 'warn', 'info', 'debug', 'trace'].reduce((acc, cur) => {
         // eslint-disable-next-line no-console
         acc[cur] = console[cur].bind(console, `${component}:`)
         return acc
     }, {})
-
-    // logging interface expects a 'setLevel' method
-    logger.setLevel = () => {}
-
-    return logger
 }
+
+// logging interface expects a 'setLevel' method
+getLogger.setLevel = () => {}
