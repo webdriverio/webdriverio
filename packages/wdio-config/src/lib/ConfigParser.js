f5b5722d0442ffac7e3285b5c66723b67e63a8d7
Christian Bromann <github@christian-bromann.com>
2018-12-28 17:07:00 +0100

    Add support for .es6 file types (#3197)
    ## Proposed changes
    
    Add support for `.es6` file types.
    
    ## Types of changes
    
    [//]: # (What types of changes does your code introduce to WebdriverIO?)
    [//]: # (_Put an `x` in the boxes that apply_)
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    [//]: # (_Put an `x` in the boxes that apply. You can also fill these out after creating the PR. If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code._)
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [x] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    closes #3193
    
    ### Reviewers: @webdriverio/technical-committee

--- packages/wdio-config/src/lib/ConfigParser.js
+++ packages/wdio-config/src/lib/ConfigParser.js
@@ -250,6 +250,7 @@ export default class ConfigParser {
 
             filenames = filenames.filter(filename =>
                 filename.slice(-3) === '.js' ||
+                filename.slice(-4) === '.es6' ||
                 filename.slice(-3) === '.ts' ||
                 filename.slice(-8) === '.feature' ||
                 filename.slice(-7) === '.coffee')
