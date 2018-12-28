613a33c8e989f6f766da67be3f61c4ff8ae0f282
christian-bromann <mail@christian-bromann.com>
2018-12-28 16:33:57 +0100

    add more unit tests

--- packages/webdriver/src/request.js
+++ packages/webdriver/src/request.js
@@ -52,7 +52,7 @@ export default class WebDriverRequest extends EventEmitter {
         /**
          * only apply body property if existing
          */
-        if (Object.keys(this.body).length) {
+        if (this.body && Object.keys(this.body).length) {
             requestOptions.body = this.body
         }
 
