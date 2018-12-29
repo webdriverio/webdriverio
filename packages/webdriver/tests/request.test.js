0a464fc329a8efccf9900a62ad89bac1160e73bd
Tobias von Klipstein <tobias@klpstn.com>
2018-12-29 21:21:51 +0100

    webdriver: add additional test for GET method

--- packages/webdriver/tests/request.test.js
+++ packages/webdriver/tests/request.test.js
@@ -59,8 +59,14 @@ describe('webdriver request', () => {
             expect(options.body).toEqual({ some: 'body' })
         })
 
-        it('should not attach a body if none is needed (e.g. DELETE)', () => {
+        it('sets request body to "undefined" when request object is empty and DELETE is used', () => {
             const req = new WebDriverRequest('DELETE', '/session', {})
+            const options = req._createOptions({})
+            expect(Boolean(options.body)).toEqual(false)
+        })
+
+        it('sets request body to "undefined" when request object is empty and GET is used', () => {
+            const req = new WebDriverRequest('GET', '/title', {})
             const options = req._createOptions({})
             expect(Boolean(options.body)).toEqual(false)
         })
