791fff24cd02a59f293604d744bdb8dc12e76083
Stephen Boyd <stephen.boyd823@outlook.com>
2019-01-02 10:43:43 -0600

    webdriverio: updated typing generation location

--- scripts/type-generation/webdriverio-generate-typings.js
+++ scripts/type-generation/webdriverio-generate-typings.js
@@ -2,125 +2,126 @@
 
 const fs = require('fs')
 const path = require('path')
-const dox = require('dox');
-const specifics = require('./specific-types.json');
+const dox = require('dox')
+const specifics = require('./specific-types.json')
 
-const elementDir = path.resolve(__dirname + '../../../packages/webdriverio/src/commands/element');
-const elementCommands = fs.readdirSync(elementDir);
+const elementDir = path.resolve(__dirname + '../../../packages/webdriverio/src/commands/element')
+const elementCommands = fs.readdirSync(elementDir)
 
-const browserDir = path.resolve(__dirname + '../../../packages/webdriverio/src/commands/browser');
-const browserCommands = fs.readdirSync(browserDir);
+const browserDir = path.resolve(__dirname + '../../../packages/webdriverio/src/commands/browser')
+const browserCommands = fs.readdirSync(browserDir)
 
-let allTypeLines = [];
+let allTypeLines = []
 
 const changeType = (text) => {
     if (text.indexOf('Array.') > -1) {
         const arrayText = 'Array.<'
-        text = text.substring(arrayText.length, text.length - 1) + '[]';
+        text = text.substring(arrayText.length, text.length - 1) + '[]'
     }
 
     switch (text) {
-        case 'Buffer':
-        case 'Function':
-        case 'RegExp': {
-            break;
-        }
-        case 'Element': {
-            text = 'Element<void>';
-            break;
-        }
-        default: {
-            text = text.toLowerCase();
-        }
+    case 'Buffer':
+    case 'Function':
+    case 'RegExp': {
+        break
+    }
+    case 'Element': {
+        text = 'Element<void>'
+        break
+    }
+    default: {
+        text = text.toLowerCase()
+    }
 
     }
-    return text;
+    return text
 }
 
 const getTypes = (types, alwaysType) => {
     types.forEach((type, index, array) => {
-        array[index] = changeType(type);
-    });
-    types = types.join(' | ');
+        array[index] = changeType(type)
+    })
+    types = types.join(' | ')
     if (types === '' && !alwaysType) {
         types = 'undefined'
     } else if (types === '*' || (types === '' && alwaysType)) {
         types = 'any'
     }
 
-    return types;
+    return types
 }
 
 const gatherCommands = (commandPath, commandFile) => {
-    const commandContents = fs.readFileSync(commandPath).toString();
-    const commandDocs = dox.parseComments(commandContents);
-    const commandTags = commandDocs[0].tags;
-    const commandName = commandFile.substr(0, commandFile.indexOf('.js'));
+    const commandContents = fs.readFileSync(commandPath).toString()
+    const commandDocs = dox.parseComments(commandContents)
+    const commandTags = commandDocs[0].tags
+    const commandName = commandFile.substr(0, commandFile.indexOf('.js'))
     
-    const allParameters = [];
-    let returnType = 'undefined';
+    const allParameters = []
+    let returnType = 'undefined'
 
     if(specifics[commandName]){
         const specificCommand = specifics[commandName]
         // console.log(commandName)
         specificCommand.forEach((cmd) => {
-            const params = [];
+            const params = []
             cmd.parameters.forEach((p) => {
                 params.push(`${p.name}: ${p.type}`)
-            });
-            const returns = cmd.return;
+            })
+            const returns = cmd.return
 
-            allTypeLines.push(`${commandName}(${params.length > 0 ? '\n            ' : ''}${params.join(',\n            ')}${params.length > 0 ? '\n        ' : ''}): ${returns}`);
-        });
+            allTypeLines.push(`${commandName}(${params.length > 0 ? '\n            ' : ''}${params.join(',\n            ')}${params.length > 0 ? '\n        ' : ''}): ${returns}`)
+        })
     } else {
-        for ({type, name, optional, types} of commandTags) {
+        for (const {type, name, optional, types} of commandTags) {
             if (type === 'param') {
-                let commandTypes = getTypes(types, true);
+                let commandTypes = getTypes(types, true)
     
                 // console.log(commandTag.name)
                 if (name.indexOf('.') < 0) {
-                    allParameters.push(`${name}${optional ? '?' : ''}: ${commandTypes}`);
+                    allParameters.push(`${name}${optional ? '?' : ''}: ${commandTypes}`)
                 }
             }
     
             if (type === 'return') {
-                returnType = getTypes(types, false);
+                returnType = getTypes(types, false)
             }
         }
     
         if (commandName !== '$' && commandName !== '$$' && commandName !== 'waitUntil') {
-            allTypeLines.push(`${commandName}(${allParameters.length > 0 ? '\n            ' : ''}${allParameters.join(',\n            ')}${allParameters.length > 0 ? '\n        ' : ''}): ${returnType}`);
+            allTypeLines.push(`${commandName}(${allParameters.length > 0 ? '\n            ' : ''}${allParameters.join(',\n            ')}${allParameters.length > 0 ? '\n        ' : ''}): ${returnType}`)
         }
     }
     
 
-    return allTypeLines;
+    return allTypeLines
 }
 
-let bCommands = [];
+let bCommands = []
 browserCommands.forEach((commandFile) => {
-    const commandPath = path.resolve(`${browserDir}/${commandFile}`);
-    bCommands = gatherCommands(commandPath, commandFile);
-});
-const allBrowserCommands = `${bCommands.join(';\n        ')};`;
+    const commandPath = path.resolve(`${browserDir}/${commandFile}`)
+    bCommands = gatherCommands(commandPath, commandFile)
+})
+const allBrowserCommands = `${bCommands.join(';\n        ')};`
 
-allTypeLines = [];
+allTypeLines = []
 
-let eCommands = [];
+let eCommands = []
 elementCommands.forEach((commandFile) => {
-    const commandPath = path.resolve(`${elementDir}/${commandFile}`);
-    eCommands = gatherCommands(commandPath, commandFile);
-});
+    const commandPath = path.resolve(`${elementDir}/${commandFile}`)
+    eCommands = gatherCommands(commandPath, commandFile)
+})
 
-const allElementCommands = `${eCommands.join(';\n        ')};`;
+const allElementCommands = `${eCommands.join(';\n        ')};`
 
 const templatePath = path.resolve(__dirname + '../../templates/webdriverio.tpl.d.ts')
-const templateContents = fs.readFileSync(templatePath, 'utf8');
+const templateContents = fs.readFileSync(templatePath, 'utf8')
 
 let typingsContents = templateContents.replace('// ... element commands ...', allElementCommands)
-typingsContents = typingsContents.replace('// ... browser commands ...', allBrowserCommands);
+typingsContents = typingsContents.replace('// ... browser commands ...', allBrowserCommands)
 
-const outputFile = path.join(__dirname, '..', '..', 'packages/webdriverio', 'webdriverio.d.ts');
+const outputFile = path.join(__dirname, '..', '..', 'packages/webdriverio', 'webdriverio.d.ts')
 fs.writeFileSync(outputFile, typingsContents, { encoding: 'utf-8' })
 
-console.log(`Generated typings file at ${outputFile}`);
+// eslint-disable-next-line no-console
+console.log(`Generated typings file at ${outputFile}`)
