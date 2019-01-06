038cce195f023289321634635f133fa4e03d9192
Stephen Boyd <stephen.boyd823@outlook.com>
2019-01-06 09:54:03 -0600

    wdio-reporter: added pending check for adding skipped tests

--- packages/wdio-reporter/src/index.js
+++ packages/wdio-reporter/src/index.js
@@ -111,13 +111,14 @@ export default class WDIOReporter extends EventEmitter {
             const currentSuite = this.currentSuites[this.currentSuites.length - 1]
             currentTest = new TestStats(test)
 
-            currentTest.uid = test.uid in this.tests ? 'skipped-' + this.counts.skipping : currentTest.uid
-
             /**
              * In Mocha: tests that are skipped don't have a start event but a test end.
              * In Jasmine: tests have a start event, therefor we need to replace the
              * test instance with the pending test here
              */
+            if (test.uid in this.tests && this.tests[test.uid].state !== 'pending') {
+                currentTest.uid = test.uid in this.tests ? 'skipped-' + this.counts.skipping : currentTest.uid
+            }
             const suiteTests = currentSuite.tests
             if (!suiteTests.length || currentTest.uid !== suiteTests[suiteTests.length - 1].uid) {
                 currentSuite.tests.push(currentTest)
@@ -125,7 +126,7 @@ export default class WDIOReporter extends EventEmitter {
                 suiteTests[suiteTests.length - 1] = currentTest
             }
 
-            this.tests[test.uid] = currentTest
+            this.tests[currentTest.uid] = currentTest
             currentTest.skip()
             this.counts.skipping++
             this.counts.tests++
