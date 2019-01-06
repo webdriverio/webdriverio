15e25451d32602a11d04fe615d9d3dad66542f79
StephenABoyd <31546234+StephenABoyd@users.noreply.github.com>
2019-01-06 14:11:33 -0600

    webdriverio: updating screenshot call to handle backslash (#3251)
    ## Proposed changes
    
    Updating the ```saveScreenshot``` method to be able to start with a backslash.
    
    ## Types of changes
    
    [//]: # (What types of changes does your code introduce to WebdriverIO?)
    [//]: # (_Put an `x` in the boxes that apply_)
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    [//]: # (_Put an `x` in the boxes that apply. You can also fill these out after creating the PR. If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code._)
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [ ] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
     I wanted to introduce this because when trying to save a screenshot to a network location the filepath starts with ```\\\\``` (Two backslashes and two escape characters). Before this, it would just append the server path to the current directory.
    
    ### Reviewers: @webdriverio/technical-committee

--- packages/webdriverio/src/commands/browser/saveScreenshot.js
+++ packages/webdriverio/src/commands/browser/saveScreenshot.js
@@ -32,7 +32,7 @@ export default async function saveScreenshot (filepath) {
         throw new Error('saveScreenshot expects a filepath of type string and ".png" file ending')
     }
 
-    const absoluteFilepath = filepath.startsWith('/')
+    const absoluteFilepath = filepath.startsWith('/') || filepath.startsWith('\\')
         ? filepath
         : path.join(process.cwd(), filepath)
 
