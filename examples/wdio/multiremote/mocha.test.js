02efbec25bf7e212352784360034c7e972cfbf33
Martijn Dijkhuizen <git@martomo.nl>
2019-01-09 18:45:48 +0100

    Update multiremote example to provide with use cases which actually executes.

--- examples/wdio/multiremote/mocha.test.js
+++ examples/wdio/multiremote/mocha.test.js
@@ -5,26 +5,32 @@ describe('multiremote example', () => {
 
     it('should login the browser', () => {
         const nameInput = $('.usernameInput')
-        nameInput.addValue('Browser A')
+        nameInput.addValue('Browser')
         browser.keys('Enter')
     })
 
     it('should post something in browserA', () => {
         const msgElemBrowserA = browserA.$('.inputMessage')
+
         msgElemBrowserA.addValue('Hey Whats up!')
         browser.pause(1000)
         browserA.keys('Enter')
 
         msgElemBrowserA.addValue('My name is Edgar')
+        browser.pause(1000)
         browserA.keys('Enter')
         browser.pause(200)
     })
 
     it('should read the message in browserB', () => {
-        const msgElemBrowserB = browserA.$('.inputMessage')
+        const msgElemBrowserB = browserB.$('.inputMessage')
+
         const chatLineBrowserB = browserB.$('.messageBody*=My name is')
+        chatLineBrowserB.waitForExist(5000)
+
         const message = chatLineBrowserB.getText()
-        const name = message.slice(11)
+        const name = message.split(' ').pop()
+
         msgElemBrowserB.addValue(`Hello ${name}! How are you today?`)
         browserB.keys('Enter')
         browser.pause(5000)
