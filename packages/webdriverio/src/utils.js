b25b3b283f07f095355e010a73901cde100f1a7d
Christian Bromann <github@christian-bromann.com>
2019-01-10 01:15:08 +0100

    Do mobile detection in webdriver package (#3290)
    ## Proposed changes
    
    Make mobile detection within the webdriver package since we need it to determine if the mobile protocols should get applied to the prototype or not
    
    ## Types of changes
    
    Moved some code around.
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    [//]: # (_Put an `x` in the boxes that apply. You can also fill these out after creating the PR. If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code._)
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [x] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    [//]: # (If this is a relatively large or complex change, kick off the discussion by explaining why you chose the solution you did and what alternatives you considered, etc...)
    
    ### Reviewers: @webdriverio/technical-committee

--- packages/webdriverio/src/utils.js
+++ packages/webdriverio/src/utils.js
@@ -188,36 +188,6 @@ export const getElementFromResponse = (res) => {
 }
 
 /**
- * check if current platform is mobile device
- *
- * @param  {Object}  caps  capabilities
- * @return {Boolean}       true if platform is mobile device
- */
-export function mobileDetector (caps) {
-    let isMobile = Boolean(
-        (typeof caps['appium-version'] !== 'undefined') ||
-        (typeof caps['device-type'] !== 'undefined') || (typeof caps['deviceType'] !== 'undefined') ||
-        (typeof caps['device-orientation'] !== 'undefined') || (typeof caps['deviceOrientation'] !== 'undefined') ||
-        (typeof caps.deviceName !== 'undefined') ||
-        // Check browserName for specific values
-        (caps.browserName === '' ||
-             (caps.browserName !== undefined && (caps.browserName.toLowerCase() === 'ipad' || caps.browserName.toLowerCase() === 'iphone' || caps.browserName.toLowerCase() === 'android')))
-    )
-
-    let isIOS = Boolean(
-        (caps.platformName && caps.platformName.match(/iOS/i)) ||
-        (caps.deviceName && caps.deviceName.match(/(iPad|iPhone)/i))
-    )
-
-    let isAndroid = Boolean(
-        (caps.platformName && caps.platformName.match(/Android/i)) ||
-        (caps.browserName && caps.browserName.match(/Android/i))
-    )
-
-    return { isMobile, isIOS, isAndroid }
-}
-
-/**
  * traverse up the scope chain until browser element was reached
  */
 export function getBrowserObject (elem) {
