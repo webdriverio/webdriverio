f46ced333e6d1f9bd22e614e59624f9b27b2ce8c
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-01-30 18:57:42 +0100

    wdio-allure-reporter: generate typings (#3461)
    ## Proposed changes
    
    generate typings for wdio-allure-reporter
    
    ## Types of changes
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [x] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    fix #3457 
    
    ### Reviewers: @webdriverio/technical-committee

---
+++ scripts/type-generation/generate-typings-utils.js
@@ -0,0 +1,73 @@
+
+const changeType = (text) => {
+    if (text.indexOf('Array.') > -1) {
+        const arrayText = 'Array.<'
+        text = text.substring(arrayText.length, text.length - 1) + '[]'
+    }
+
+    switch (text) {
+    case 'Buffer':
+    case 'Function':
+    case 'RegExp': {
+        break
+    }
+    case 'Element': {
+        text = 'Element<void>'
+        break
+    }
+    case 'Element[]': {
+        text = 'Element<void>[]'
+        break
+    }
+    default: {
+        text = text.toLowerCase()
+    }
+
+    }
+    return text
+}
+
+const getTypes = (types, alwaysType) => {
+    types.forEach((type, index, array) => {
+        array[index] = changeType(type)
+    })
+    types = types.join(' | ')
+    if (types === '' && !alwaysType) {
+        types = 'undefined'
+    } else if (types === '*' || (types === '' && alwaysType)) {
+        types = 'any'
+    }
+
+    return types
+}
+
+const buildCommand = (commandName, commandTags, indentation = 0) => {
+    const allParameters = []
+    let returnType = 'undefined'
+
+    for (const { type, name, optional, types, string } of commandTags) {
+        // dox parse {*} as string instead of types
+        if (types && types.length === 0 && string.includes('{*}')) {
+            types.push('*')
+        }
+        if (type === 'param') {
+            let commandTypes = getTypes(types, true)
+
+            // skipping param with dot in name that stands for Object properties description, ex: attachmentObject.name
+            if (name.indexOf('.') < 0) {
+                // get rid from default values from param name, ex: [paramName='someString'] will become paramName
+                allParameters.push(`${name.split('[').pop().split('=')[0]}${optional ? '?' : ''}: ${commandTypes}`)
+            }
+        }
+
+        if (type === 'return') {
+            returnType = getTypes(types, false)
+        }
+    }
+
+    return `${commandName}(${allParameters.length > 0 ? `\n${' '.repeat(8 + indentation)}` : ''}${allParameters.join(`,\n${' '.repeat(8 + indentation)}`)}${allParameters.length > 0 ? `\n${' '.repeat(4 + indentation)}` : ''}): ${returnType}`
+}
+
+module.exports = {
+    buildCommand
+}
