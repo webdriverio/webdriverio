c1f385bfd686a3be4e773ab377c17c4eba681252
Christian Bromann <github@christian-bromann.com>
2019-02-09 12:31:58 +0100

    Streamline typings (#3525)
    * streamline typings
    
    * Webdriver add chromium typings (#3527)
    
    ## Proposed changes
    
    Add chromium typings and fix escaping of wildcard which caused an issue in generating the typings
    
    ## Types of changes
    
    [//]: # (What types of changes does your code introduce to WebdriverIO?)
    [//]: # (_Put an `x` in the boxes that apply_)
    
    - [x ] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    [//]: # (_Put an `x` in the boxes that apply. You can also fill these out after creating the PR. If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code._)
    
    - [x ] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [ ] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    [//]: # (If this is a relatively large or complex change, kick off the discussion by explaining why you chose the solution you did and what alternatives you considered, etc...)
    
    ### Reviewers: @webdriverio/technical-committee

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -8,28 +8,11 @@ const formatter = require('./utils/formatter')
 const compiler = require('./utils/compiler')
 const ejs = require('../packages/wdio-cli/node_modules/ejs')
 const { getSubPackages } = require('./utils/helpers')
+const { PROTOCOLS, PROTOCOL_NAMES, MOBILE_PROTOCOLS, VENDOR_PROTOCOLS } = require('./constants')
 
 const config = require('../website/siteConfig')
 const sidebars = require('../website/_sidebars.json')
 
-const PROTOCOLS = {
-    webdriver: require('../packages/webdriver/protocol/webdriver.json'),
-    appium: require('../packages/webdriver/protocol/appium.json'),
-    jsonwp: require('../packages/webdriver/protocol/jsonwp.json'),
-    mjsonwp: require('../packages/webdriver/protocol/mjsonwp.json'),
-    chromium: require('../packages/webdriver/protocol/chromium.json'),
-    saucelabs: require('../packages/webdriver/protocol/saucelabs.json')
-}
-const PROTOCOL_NAMES = {
-    appium: 'Appium',
-    jsonwp: 'JSON Wire Protocol',
-    mjsonwp: 'Mobile JSON Wire Protocol',
-    webdriver: 'Webdriver Protocol',
-    chromium: 'Chromium',
-    saucelabs: 'Sauce Labs'
-}
-const MOBILE_PROTOCOLS = ['appium', 'mjsonwp']
-const VENDOR_PROTOCOLS = ['chromium']
 const TEMPLATE_PATH = path.join(__dirname, 'templates', 'api.tpl.ejs')
 const MARKDOX_OPTIONS = {
     formatter: formatter,
