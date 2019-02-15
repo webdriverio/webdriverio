e6b47ac7ddfa91fc2233cc28c9c19f54238fce1b
John Robinson <john.robinson@banno.com>
2019-02-15 09:04:49 -0500

    replaced anonymous functions with statics for easier testing

--- packages/wdio-shadow-dom-service/src/index.js
+++ packages/wdio-shadow-dom-service/src/index.js
@@ -1,26 +1,29 @@
-
-// generate a function (string) that queries for the desired selector
-const fnFactory = function(elementSelector, qsAll) {
-    const strFn = `
-      (function() {
-        // element has a shadowRoot property
-        if (this.shadowRoot) {
-          return this.shadowRoot.querySelector${qsAll ? 'All' : ''}('${elementSelector}')
-        }
-        // fall back to querying the element directly if not
-        return this.querySelector${qsAll ? 'All' : ''}('${elementSelector}')
-      })`
-    return eval(strFn)
-}
-
 export default class ShadowDomService {
+
+    static fnFactory(elementSelector, qsAll) {
+        const strFn = `
+        (function() {
+          // element has a shadowRoot property
+          if (this.shadowRoot) {
+            return this.shadowRoot.querySelector${qsAll ? 'All' : ''}('${elementSelector}')
+          }
+          // fall back to querying the element directly if not
+          return this.querySelector${qsAll ? 'All' : ''}('${elementSelector}')
+        })`
+        return eval(strFn)
+    }
+
+    static async shadow$ (selector) {
+        return await this.$(ShadowDomService.fnFactory(selector))
+    }
+
+    static async shadow$$ (selector) {
+        return await this.$$(ShadowDomService.fnFactory(selector, true))
+    }
+
     before() {
-        // create element commands
-        global.browser.addCommand('shadow$', async function shadow$ (selector) {
-            return await this.$(fnFactory(selector))
-        }, true)
-        global.browser.addCommand('shadow$$', async function shadow$ (selector) {
-            return await this.$$(fnFactory(selector, true))
-        }, true)
+        // add element commands
+        global.browser.addCommand('shadow$', ShadowDomService.shadow$, true)
+        global.browser.addCommand('shadow$$', ShadowDomService.shadow$$, true)
     }
 }
