0ed5917bb33c8049579ea007e102634efa120c68
John Robinson <jrobinsonmedia@gmail.com>
2019-02-20 12:08:59 -0500

    webdriverio: shadow$ and shadow$$ commands (#3586)
    ## Proposed changes
    See the previous PR here: https://github.com/webdriverio/webdriverio/pull/3563
    This PR moves the new shadow$ and shadow$$ commands into webdriverio instead of as a standalone service.
    
    ## Types of changes
    
    [//]: # (What types of changes does your code introduce to WebdriverIO?)
    [//]: # (_Put an `x` in the boxes that apply_)
    
    - [ ] Bugfix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    [//]: # (_Put an `x` in the boxes that apply. You can also fill these out after creating the PR. If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code._)
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [x] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    [//]: # (If this is a relatively large or complex change, kick off the discussion by explaining why you chose the solution you did and what alternatives you considered, etc...)
    I'm not super confident in the tests for these new commands. They aren't as extensive as they were in the PR where the commands were in a separate package. I wasn't sure how to port that over. Any guidance would be appreciated. 
    
    ### Reviewers: @webdriverio/technical-committee

---
+++ packages/webdriverio/src/scripts/shadowFnFactory.js
@@ -0,0 +1,13 @@
+// generate a function that can be used to query shadowRoots
+export const shadowFnFactory = function(elementSelector, qsAll) {
+    const strFn = `
+    (function() {
+      // element has a shadowRoot property
+      if (this.shadowRoot) {
+        return this.shadowRoot.querySelector${qsAll ? 'All' : ''}('${elementSelector}')
+      }
+      // fall back to querying the element directly if not
+      return this.querySelector${qsAll ? 'All' : ''}('${elementSelector}')
+    })`
+    return eval(strFn)
+}
