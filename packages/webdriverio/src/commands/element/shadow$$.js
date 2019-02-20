e120b2135e417e9d4d0f5220cecf928c8ad2277a
John Robinson <john.robinson@banno.com>
2019-02-20 09:57:02 -0500

    webdriverio: refactored shadow$ and shadow$$ commands to use shared shadowFnFactory

--- packages/webdriverio/src/commands/element/shadow$$.js
+++ packages/webdriverio/src/commands/element/shadow$$.js
@@ -1,3 +1,4 @@
+import {shadowFnFactory} from '../constant'
 /**
  *
  * Access elements inside a given element's shadowRoot
@@ -5,8 +6,7 @@
  * <example>
     :shadow$$.js
     it('should return elements inside a shadowRoot', () => {
-        const input = $('.input');
-        const innerEl = input.shadow$$('#innerEl');
+        const innerEl = $('.input').shadow$$('#innerEl');
         console.log(innerEl.getValue()); // outputs: 'test123'
     });
  * </example>
@@ -17,12 +17,6 @@
  *
  */
 
-// need to generate a function that includes the desired selector
-const fnFactory = function(elementSelector) {
-    const strFn = `(function() { return this.shadowRoot.querySelectorAll('${elementSelector}') })`
-    return eval(strFn)
-}
-
 export default async function shadowRoot (selector) {
-    return await this.$(fnFactory(selector))
+    return await this.$$(shadowFnFactory(selector, true))
 }
--- packages/webdriverio/src/commands/element/shadow$.js
+++ packages/webdriverio/src/commands/element/shadow$.js
@@ -1,3 +1,5 @@
+import {shadowFnFactory} from '../constant'
+
 /**
  *
  * Access an element inside a given element's shadowRoot
@@ -5,8 +7,7 @@
  * <example>
     :shadow$$.js
     it('should return an element inside a shadowRoot', () => {
-        const input = $('.input');
-        const innerEl = input.shadow$('#innerEl');
+        const innerEl = $('.input').shadow$('#innerEl');
         console.log(innerEl.getValue()); // outputs: 'test123'
     });
  * </example>
@@ -17,12 +18,6 @@
  *
  */
 
-// need to generate a function that includes the desired selector
-const fnFactory = function(elementSelector) {
-    const strFn = `(function() { return this.shadowRoot.querySelector('${elementSelector}') })`
-    return eval(strFn)
-}
-
 export default async function shadowRoot (selector) {
-    return await this.$(fnFactory(selector))
+    return await this.$(shadowFnFactory(selector))
 }
