50918173299df3043a0da57078b91cd4d2234e4b
John Robinson <john.robinson@banno.com>
2019-02-21 09:26:45 -0500

    webdriverio: handle the case where a function selector is used, and that function returns null instead of an element

--- packages/webdriverio/src/utils.js
+++ packages/webdriverio/src/utils.js
@@ -166,10 +166,16 @@ export const getPrototype = (scope) => {
 
 /**
  * get element id from WebDriver response
- * @param  {object} res         body object from response
- * @return {string|undefined}   element id or null if element couldn't be found
+ * @param  {?Object|undefined} res         body object from response or null
+ * @return {?string}   element id or null if element couldn't be found
  */
 export const getElementFromResponse = (res) => {
+    /**
+    * a function selector can return null
+    */
+    if (res === null || res === undefined) {
+        return null
+    }
     /**
      * deprecated JSONWireProtocol response
      */
