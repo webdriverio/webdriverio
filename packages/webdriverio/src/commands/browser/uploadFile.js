957308245c6eaa6e29a5d273496d7e0c572f99ef
christian-bromann <mail@christian-bromann.com>
2019-02-26 13:44:20 +0100

    write unit tests for command

--- packages/webdriverio/src/commands/browser/uploadFile.js
+++ packages/webdriverio/src/commands/browser/uploadFile.js
@@ -9,29 +9,33 @@
  * @uses protocol/file
  *
  */
-
 import fs from 'fs'
 import path from 'path'
 import archiver from 'archiver'
 
-
-export default function uploadFile (localPath) {
-    /*!
+export default async function uploadFile (localPath) {
+    /**
      * parameter check
      */
     if (typeof localPath !== 'string') {
         throw new Error('number or type of arguments don\'t agree with uploadFile command')
     }
 
+    /**
+     * check if command is available
+     */
+    if (typeof this.file !== 'function') {
+        throw new Error(`The uploadFile command is not available in ${this.capabilities.browserName}`)
+    }
+
     let zipData = []
     let source = fs.createReadStream(localPath)
 
-    
     return new Promise((resolve, reject) => {
         archiver('zip')
-            .on('error', (e) => { throw new Error(e) })
+            .on('error', (err) => reject(err))
             .on('data', (data) => zipData.push(data))
-            .on('end', (zipData) => Buffer.concat(zipData).toString('base64'))
+            .on('end', () => this.file(Buffer.concat(zipData).toString('base64')).then(resolve, reject))
             .append(source, { name: path.basename(localPath) })
             .finalize((err) => {
                 /* istanbul ignore next */
