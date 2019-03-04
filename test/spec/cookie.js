fff418a0264679f9fd88794c1ab783ba74c7cb2c
David Hale <dmhalejr@users.noreply.github.com>
2019-03-04 03:33:07 -0600

    #3045: Encoding Cookie Name Values (#2)
    ## Proposed changes
    Proposed solution for https://github.com/webdriverio/webdriverio/issues/3045
    
    ## Types of changes
    - [x] Bugfix (non-breaking change which fixes an issue) [removes encoding error reported]
    - [x] New feature (non-breaking change which adds functionality) [set double encoding on get/set/delete cookie actions)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [x] I have added necessary documentation (if appropriate)
    - [ ] I have proposed the same patch to the new [v5](https://github.com/webdriverio/v5) repository
    
    ### Reviewers: @christian-bromann

--- test/spec/cookie.js
+++ test/spec/cookie.js
@@ -1,12 +1,53 @@
 describe('cookie command test', () => {
+    afterEach(async function() {
+        // Clear cookies after each test
+        await this.client.deleteCookie();
+    });
+
+    it('setCookie should fail if user provides name that is not a string type', async function () {
+        try {
+            await this.client.setCookie({name: 12345, value: 'cookie saved!', domain: '127.0.0.1'})
+            await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
+
+            const cookies = await this.client.getCookie()
+            cookies.should.have.length(0) // should never happen.
+        } catch (e) {
+            (e.message).should.be.equal('Please specify a cookie object to set (see https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#cookie-json-object for documentation.');   
+        }
+    })
+
+    it('setCookie should fail if user does not provide an object type', async function () {
+        try {
+            await this.client.setCookie('not appropriate')
+            await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
+            
+            const cookies = await this.client.getCookie()
+            cookies.should.have.length(0) // should never happen.
+        } catch (e) {
+            (e.message).should.be.equal('Please specify a cookie object to set (see https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#cookie-json-object for documentation.');   
+        }
+    })
+
+    it('setCookie should fail if user does not provide name', async function () {
+        try {
+            await this.client.setCookie({value: 'cookie saved2!', domain: '127.0.0.1'})
+            await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
+            
+            const cookies = await this.client.getCookie()
+            cookies.should.have.length(0) // should never happen.
+        } catch (e) {
+            (e.message).should.be.equal('Please specify a cookie object to set (see https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#cookie-json-object for documentation.');   
+        }
+    })
+
     it('getCookie should return null / [] if no cookie(s) is/are set', async function () {
         (await this.client.getCookie('test') === null).should.be.equal(true);
         (await this.client.getCookie()).should.have.length(0)
     })
 
     it('getCookie should return multiple cookies if no name is given', async function () {
-        await this.client.setCookie({name: 'test', value: 'cookie saved!'})
-        await this.client.setCookie({name: 'test2', value: 'cookie saved2!'})
+        await this.client.setCookie({name: 'test', value: 'cookie saved!', domain: '127.0.0.1'})
+        await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
 
         const cookies = await this.client.getCookie()
         cookies.should.be.an.instanceOf(Array)
@@ -17,9 +58,30 @@ describe('cookie command test', () => {
         cookies.should.contain.a.thing.with.property('value', 'cookie saved2!')
     })
 
+    it('getCookie should return a single cookies if name is given with double URI encoding', async function () {
+        await this.client.setCookie({name: 'test', value: 'cookie saved!', domain: '127.0.0.1'})
+        await this.client.setCookie({name: '/:?test2', value: 'cookie saved2!', domain: '127.0.0.1'})
+
+        const cookie = await this.client.getCookie('/:?test2')
+        cookie.should.be.an.instanceOf(Object)
+        cookie.value.should.be.equal('cookie saved2!')
+        cookie.name.should.be.equal('%252F%253A%253Ftest2')
+    })
+
+    it('getCookie should return a single cookies full of special characters', async function () {
+        await this.client.setCookie({name: "~!@#$%^&*()_+-={}|[]\:\"'<>?,./", value: 'cookie saved!', domain: '127.0.0.1'})
+        await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
+
+        const cookie = await this.client.getCookie("~!@#$%^&*()_+-={}|[]\:\"'<>?,./")
+        cookie.should.be.an.instanceOf(Object)
+        cookie.value.should.be.equal('cookie saved!')
+        
+        cookie.name.should.be.equal("~!%2540%2523%2524%2525%255E%2526*()_%252B-%253D%257B%257D%257C%255B%255D%253A%2522'%253C%253E%253F%252C.%252F")
+    })
+
     it('getCookie should return a single cookies if name is given', async function () {
-        await this.client.setCookie({name: 'test', value: 'cookie saved!'})
-        await this.client.setCookie({name: 'test2', value: 'cookie saved2!'})
+        await this.client.setCookie({name: 'test', value: 'cookie saved!', domain: '127.0.0.1'})
+        await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
 
         const cookie = await this.client.getCookie('test2')
         cookie.should.be.an.instanceOf(Object)
@@ -32,8 +94,8 @@ describe('cookie command test', () => {
             return
         }
 
-        await this.client.setCookie({ name: 'test', value: 'cookie saved!' })
-        await this.client.setCookie({ name: 'test2', value: 'cookie2 saved!' })
+        await this.client.setCookie({ name: 'test', value: 'cookie saved!', domain: '127.0.0.1' })
+        await this.client.setCookie({ name: 'test2', value: 'cookie2 saved!', domain: '127.0.0.1' })
         await this.client.deleteCookie('test');
 
         (await this.client.getCookie('test') === null).should.be.equal(true);
@@ -45,12 +107,53 @@ describe('cookie command test', () => {
             return
         }
 
-        await this.client.setCookie({ name: 'test', value: 'cookie saved!' })
-        await this.client.setCookie({ name: 'test2', value: 'cookie2 saved!' })
+        await this.client.setCookie({ name: 'test', value: 'cookie saved!', domain: '127.0.0.1' })
+        await this.client.setCookie({ name: 'test2', value: 'cookie2 saved!', domain: '127.0.0.1' })
         await this.client.deleteCookie();
 
         (await this.client.getCookie('test') === null).should.be.equal(true);
         (await this.client.getCookie('test2') === null).should.be.equal(true);
         (await this.client.getCookie()).should.have.length(0)
     })
+
+    it('deleteCookie should delete all cookies with double URI encoding', async function () {
+        if (this.client.desiredCapabilities.browserName === 'safari' && !this.client.isMobile) {
+            return
+        }
+
+        await this.client.setCookie({ name: '/test', value: 'cookie saved!', domain: '127.0.0.1' })
+        await this.client.setCookie({ name: 'test2?:', value: 'cookie2 saved!', domain: '127.0.0.1' })
+        await this.client.deleteCookie();
+
+        (await this.client.getCookie('/test') === null).should.be.equal(true);
+        (await this.client.getCookie('test2?:') === null).should.be.equal(true);
+        (await this.client.getCookie()).should.have.length(0)
+    })
+
+    it('deleteCookie should delete a specific cookie with double URI encoding', async function () {
+        if (this.client.desiredCapabilities.browserName === 'safari' && !this.client.isMobile) {
+            return
+        }
+
+        await this.client.setCookie({ name: '/test', value: 'cookie saved!', domain: '127.0.0.1' })
+        await this.client.setCookie({ name: 'test2?:', value: 'cookie2 saved!', domain: '127.0.0.1' })
+        await this.client.deleteCookie('test2?:');
+
+        (await this.client.getCookie('test2?:') === null).should.be.equal(true);
+        (await this.client.getCookie()).should.have.length(1)
+    })
+
+    it('deleteCookie should delete a single cookies full of special characters', async function () {
+        if (this.client.desiredCapabilities.browserName === 'safari' && !this.client.isMobile) {
+            return
+        }
+
+        await this.client.setCookie({name: "~!@#$%^&*()_+-={}|[]\:\"'<>?,./", value: 'cookie saved!', domain: '127.0.0.1'})
+        await this.client.setCookie({name: 'test2', value: 'cookie saved2!', domain: '127.0.0.1'})
+        await this.client.deleteCookie("~!@#$%^&*()_+-={}|[]\:\"'<>?,./");
+
+        (await this.client.getCookie("~!@#$%^&*()_+-={}|[]\:\"'<>?,./") === null).should.be.equal(true);
+        (await this.client.getCookie()).should.have.length(1)
+    })
+
 })
