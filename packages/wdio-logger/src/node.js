0a4d11cc53b0a73f2b361c17469f43ffc87cbc53
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-03-04 22:08:21 +0100

    wdio-logger: fix webdriver log colors (#3675)
    ## Proposed changes
    
    fix coloring of webdriver's COMMAND, DATA, RESULT
    
    ![image](https://user-images.githubusercontent.com/25589559/53759960-d4202f00-3ec1-11e9-8eeb-ed462dbd41c5.png)
    
    ## Types of changes
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [ ] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    fix #3593 
    
    ### Reviewers: @webdriverio/technical-committee

--- packages/wdio-logger/src/node.js
+++ packages/wdio-logger/src/node.js
@@ -14,6 +14,12 @@ const COLORS = {
     trace: 'cyan'
 }
 
+const matches = {
+    COMMAND: 'COMMAND',
+    DATA: 'DATA',
+    RESULT: 'RESULT'
+}
+
 const SERIALIZERS = [{
     /**
      * display error stack
@@ -24,19 +30,19 @@ const SERIALIZERS = [{
     /**
      * color commands blue
      */
-    matches: (log) => log === 'COMMAND',
+    matches: (log) => log === matches.COMMAND,
     serialize: (log) => chalk.magenta(log)
 }, {
     /**
      * color data yellow
      */
-    matches: (log) => log === 'DATA',
+    matches: (log) => log === matches.DATA,
     serialize: (log) => chalk.yellow(log)
 }, {
     /**
      * color result cyan
      */
-    matches: (log) => log === 'RESULT',
+    matches: (log) => log === matches.RESULT,
     serialize: (log) => chalk.cyan(log)
 }]
 
@@ -54,6 +60,16 @@ log.methodFactory = function (methodName, logLevel, loggerName) {
          */
         if (!logFile && process.env.WDIO_LOG_PATH) {
             logFile = fs.createWriteStream(process.env.WDIO_LOG_PATH)
+        }
+
+        /**
+         * split `prefixer: value` sting to `prefixer: ` and `value`
+         * so that SERIALIZERS can match certain string
+         */
+        const match = Object.values(matches).filter(x => args[0].endsWith(`: ${x}`))[0]
+        if (match) {
+            const prefixStr = args.shift().slice(0, -match.length - 1)
+            args.unshift(prefixStr, match)
         }
 
         args = args.map((arg) => {
