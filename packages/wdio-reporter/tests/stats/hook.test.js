f560562baf0a73dd8bcf063002541df17425985b
Nicholas Bailey <nicklbailey@gmail.com>
2019-03-04 12:21:16 -0600

    Issue 3416: pass multiple errors through to test stats for reporting if available (#3672)
    ## Proposed changes
    Continuing the work started in [https://github.com/webdriverio/webdriverio/pull/3669](https://github.com/webdriverio/webdriverio/pull/3669). That PR allowed Jasmine to pass along more than one error on test run events. Here, we're handling more than one error in the base reporter, and assigning them to test stats for consumption by reporters. In packages/wdio-reporter/src/index.js we pass multiple errors if available to the test stats, and otherwise wrap the 'error' property of the event. Then in, the test/hook stats, we assign both a 'errors' property and an 'error' property (just the first element of the passed errors array). This should maintain backwards compatibility with any existing reporters/frameworks, while allowing reporters to optionally report multiple failures from a single tests.
    
    There's also a lot of added test coverage in this PR, since I wanted to be very careful not to break anything. While we executed all of the event handlers in packages/wdio-reporter/src/index.js, a lot of the event handling behavior wasn't asserted against. Open to reducing the volume of tests if anyone thinks we're over-testing there.
    
    ## Types of changes
    
    - [X] Bugfix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    - [X] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [X] I have added tests that prove my fix is effective or that my feature works
    - [X] I have added necessary documentation (if appropriate)
    
    
    ### Reviewers: @webdriverio/technical-committee

--- packages/wdio-reporter/tests/stats/hook.test.js
+++ packages/wdio-reporter/tests/stats/hook.test.js
@@ -23,13 +23,28 @@ test('should allow to be called complete', () => {
     expect(hook.end).toBeInstanceOf(Date)
 })
 
-test('should mark hook as failed if onComplete has error', () => {
+test('should mark hook as failed if onComplete has errors', () => {
     const hook = new HookStats({
         cid: '0-0',
         title: 'foobar',
         parent: 'barfoo'
     })
-    const error = new Error('boom')
-    hook.complete(error)
-    expect(hook.error).toBe(error)
+    const errors = [new Error('boom')]
+    hook.complete(errors)
+    expect(hook.errors).toBe(errors)
+    expect(hook.error.message).toBe('boom')
+    expect(hook.state).toBe('failed')
+})
+
+test('Should not mark hook as failed if onComplete has no errors', () => {
+    const hook = new HookStats({
+        cid: '0-0',
+        title: 'foobar',
+        parent: 'barfoo'
+    })
+    const errors = []
+    hook.complete(errors)
+    expect(hook.errors).toBe(errors)
+    expect(hook.error).toBeUndefined()
+    expect(hook.state).not.toBe('failed')
 })
