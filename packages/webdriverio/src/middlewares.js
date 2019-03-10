b33e3d2ef63c504c41006552c95084c842e8df13
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-03-10 16:59:17 +0100

    webdriverio: fix isDisplayed (#3701)

--- packages/webdriverio/src/middlewares.js
+++ packages/webdriverio/src/middlewares.js
@@ -15,7 +15,19 @@ export const elementErrorHandler = (fn) => (commandName, commandFn) => {
             this.elementId = element.elementId
 
             try {
-                return await fn(commandName, commandFn).apply(this, args)
+                const result = await fn(commandName, commandFn).apply(this, args)
+
+                /**
+                 * assume Safari responses like { error: 'no such element', message: '', stacktrace: '' }
+                 * as `stale element reference`
+                 */
+                if (result && result.error === 'no such element') {
+                    const err = new Error()
+                    err.name = 'stale element reference'
+                    throw err
+                }
+
+                return result
             } catch (error) {
                 if (error.name === 'stale element reference') {
                     const element = await refetchElement(this, commandName)
