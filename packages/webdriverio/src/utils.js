a6f6ec7ae605b452b54790c397ed5c32795b9481
CrispusDH <qacrispus@gmail.com>
2019-03-19 19:24:00 +0000

    webdriverio: create variable to reduce duplication

--- packages/webdriverio/src/utils.js
+++ packages/webdriverio/src/utils.js
@@ -14,6 +14,8 @@ const DIRECT_SELECTOR_REGEXP = /^(id|css selector|xpath|link text|partial link t
 const INVALID_SELECTOR_ERROR = new Error('selector needs to be typeof `string` or `function`')
 
 export const findStrategy = function (value, isW3C, isMobile) {
+    const isNameAttribute = value.search(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/) >= 0
+
     /**
      * set default selector
      */
@@ -84,8 +86,8 @@ export const findStrategy = function (value, isW3C, isMobile) {
     // or if isMobile is used even when w3c is used
     // e.g. "[name='myName']" or '[name="myName"]'
     } else if (isMobile
-        ? value.search(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/) >= 0
-        : !isW3C && value.search(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/) >= 0) {
+        ? isNameAttribute
+        : !isW3C && isNameAttribute) {
         using = 'name'
         value = value.match(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/)[2]
 
