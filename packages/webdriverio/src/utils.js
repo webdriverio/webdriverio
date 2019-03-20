4339a780cf308a473835e42cad291d086ac7a25d
CrispusDH <qacrispus@gmail.com>
2019-03-20 10:25:28 +0000

    webdriverio: refactor findStrategy method in utils

--- packages/webdriverio/src/utils.js
+++ packages/webdriverio/src/utils.js
@@ -9,135 +9,182 @@ import isObject from 'lodash.isobject'
 import { ELEMENT_KEY, W3C_SELECTOR_STRATEGIES, UNICODE_CHARACTERS } from './constants'
 
 const log = logger('webdriverio')
-const DEFAULT_SELECTOR = 'css selector'
+const DEFAULT_STRATEGY = 'css selector'
 const DIRECT_SELECTOR_REGEXP = /^(id|css selector|xpath|link text|partial link text|name|tag name|class name|-android uiautomator|-ios uiautomation|-ios predicate string|-ios class chain|accessibility id):(.+)/
 const INVALID_SELECTOR_ERROR = new Error('selector needs to be typeof `string` or `function`')
 
-export const findStrategy = function (value, isW3C, isMobile) {
-    const isNameAttribute = value.search(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/) >= 0
-
-    /**
-     * set default selector
-     */
-    let using = DEFAULT_SELECTOR
-
+const defineStrategy = function (selector) {
     /**
      * check if user has specified locator strategy directly
      */
-    const match = value.match(DIRECT_SELECTOR_REGEXP)
-    if (match) {
-        /**
-         * ensure selector strategy is supported
-         */
-        if (!isMobile && isW3C && !W3C_SELECTOR_STRATEGIES.includes(match[1])) {
-            throw new Error('InvalidSelectorStrategy') // ToDo: move error to wdio-error package
-        }
-
-        return {
-            using: match[1],
-            value: match[2]
-        }
+    if (selector.match(DIRECT_SELECTOR_REGEXP)) {
+        return 'directly'
     }
-
-    // use xPath strategy if value starts with //
-    if (value.indexOf('/') === 0 || value.indexOf('(') === 0 ||
-               value.indexOf('../') === 0 || value.indexOf('./') === 0 ||
-               value.indexOf('*/') === 0) {
-        using = 'xpath'
-
-    // use link text strategy if value starts with =
-    } else if (value.indexOf('=') === 0) {
-        using = 'link text'
-        value = value.slice(1)
-
-    // use partial link text strategy if value starts with *=
-    } else if (value.indexOf('*=') === 0) {
-        using = 'partial link text'
-        value = value.slice(2)
-
+    // use xPath strategy if selector starts with //
+    if (selector.indexOf('/') === 0 || selector.indexOf('(') === 0 ||
+        selector.indexOf('../') === 0 || selector.indexOf('./') === 0 ||
+        selector.indexOf('*/') === 0) {
+        return 'xpath'
+    }
+    // use link text strategy if selector starts with =
+    if (selector.indexOf('=') === 0) {
+        return 'link text'
+    }
+    // use partial link text strategy if selector starts with *=
+    if (selector.indexOf('*=') === 0) {
+        return 'partial link text'
+    }
     // recursive element search using the UiAutomator library (Android only)
-    } else if (value.indexOf('android=') === 0) {
-        using = '-android uiautomator'
-        value = value.slice(8)
-
+    if (selector.indexOf('android=') === 0) {
+        return '-android uiautomator'
+    }
     // recursive element search using the UIAutomation library (iOS-only)
-    } else if (value.indexOf('ios=') === 0) {
-        using = '-ios uiautomation'
-        value = value.slice(4)
-
+    if (selector.indexOf('ios=') === 0) {
+        return '-ios uiautomation'
+    }
     // recursive element search using accessibility id
-    } else if (value.indexOf('~') === 0) {
-        using = 'accessibility id'
-        value = value.slice(1)
-
+    if (selector.indexOf('~') === 0) {
+        return 'accessibility id'
+    }
     // class name mobile selector
     // for iOS = UIA...
     // for Android = android.widget
-    } else if (value.slice(0, 3) === 'UIA' || value.slice(0, 15) === 'XCUIElementType' || value.slice(0, 14).toLowerCase() === 'android.widget') {
-        using = 'class name'
-
-    // use tag name strategy if value contains a tag
+    if (selector.slice(0, 3) === 'UIA' || selector.slice(0, 15) === 'XCUIElementType' || selector.slice(0, 14).toLowerCase() === 'android.widget') {
+        return 'class name'
+    }
+    // use tag name strategy if selector contains a tag
     // e.g. "<div>" or "<div />"
-    } else if (value.search(/<[a-zA-Z-]+( \/)*>/g) >= 0) {
-        using = 'tag name'
-        value = value.replace(/<|>|\/|\s/g, '')
-
-    // use name strategy if value queries elements with name attributes for JSONWP
+    if (selector.search(/<[a-zA-Z-]+( \/)*>/g) >= 0) {
+        return 'tag name'
+    }
+    // use name strategy if selector queries elements with name attributes for JSONWP
     // or if isMobile is used even when w3c is used
     // e.g. "[name='myName']" or '[name="myName"]'
-    } else if (isMobile
-        ? isNameAttribute
-        : !isW3C && isNameAttribute) {
-        using = 'name'
-        value = value.match(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/)[2]
-
+    if (selector.search(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/) >= 0) {
+        return 'name'
+    }
     // allow to move up to the parent or select current element
-    } else if (value === '..' || value === '.') {
-        using = 'xpath'
-
+    if (selector === '..' || selector === '.') {
+        return 'xpath'
+    }
     // any element with given class, id, or attribute and content
     // e.g. h1.header=Welcome or [data-name=table-row]=Item or #content*=Intro
-    } else {
-        const match = value.match(new RegExp([
+    if (selector.match(new RegExp([
+        // HTML tag
+        /^([a-z0-9|-]*)/,
+        // optional . or # + class or id
+        /(?:(\.|#)(-?[_a-zA-Z]+[_a-zA-Z0-9-]*))?/,
+        // optional [attribute-name="attribute-selector"]
+        /(?:\[(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)(?:=(?:"|')([a-zA-z0-9\-_. ]+)(?:"|'))?\])?/,
+        // *=query or =query
+        /(\*)?=(.+)$/,
+    ].map(rx => rx.source).join('')))) {
+        return 'xpath extended'
+    }
+}
+
+export const findStrategy = function (selector, isW3C, isMobile) {
+    /**
+     * set default selector
+     */
+    let using = DEFAULT_STRATEGY
+    let value = selector
+
+    switch (defineStrategy(selector)) {
+    // user has specified locator strategy directly
+    case 'directly': {
+        const match = selector.match(DIRECT_SELECTOR_REGEXP)
+        if (!isMobile && isW3C && !W3C_SELECTOR_STRATEGIES.includes(match[1])) {
+            throw new Error('InvalidSelectorStrategy') // ToDo: move error to wdio-error package
+        }
+        using = match[1]
+        value = match[2]
+        break
+    }
+    case 'xpath': {
+        using = 'xpath'
+        break
+    }
+    case 'link text': {
+        using = 'link text'
+        value = selector.slice(1)
+        break
+    }
+    case 'partial link text': {
+        using = 'partial link text'
+        value = selector.slice(2)
+        break
+    }
+    case '-android uiautomator': {
+        using = '-android uiautomator'
+        value = selector.slice(8)
+        break
+    }
+    case '-ios uiautomation': {
+        using = '-ios uiautomation'
+        value = selector.slice(4)
+        break
+    }
+    case 'accessibility id': {
+        using = 'accessibility id'
+        value = selector.slice(1)
+        break
+    }
+    case 'class name': {
+        using = 'class name'
+        break
+    }
+    case 'tag name': {
+        using = 'tag name'
+        value = selector.replace(/<|>|\/|\s/g, '')
+        break
+    }
+    case 'name': {
+        if (isMobile || !isW3C) {
+            using = 'name'
+            value = selector.match(/^\[name=("|')([a-zA-z0-9\-_. ]+)("|')]$/)[2]
+        }
+        break
+    }
+    case 'xpath extended': {
+        using = 'xpath'
+        const match = selector.match(new RegExp([
             // HTML tag
             /^([a-z0-9|-]*)/,
             // optional . or # + class or id
             /(?:(\.|#)(-?[_a-zA-Z]+[_a-zA-Z0-9-]*))?/,
-            // optional [attribute-name="attribute-value"]
+            // optional [attribute-name="attribute-selector"]
             /(?:\[(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)(?:=(?:"|')([a-zA-z0-9\-_. ]+)(?:"|'))?\])?/,
             // *=query or =query
             /(\*)?=(.+)$/,
         ].map(rx => rx.source).join('')))
+        const PREFIX_NAME = { '.': 'class', '#': 'id' }
+        const conditions = []
+        const [
+            tag,
+            prefix, name,
+            attrName, attrValue,
+            partial, query
+        ] =  match.slice(1)
 
-        if (match) {
-            const PREFIX_NAME = { '.': 'class', '#': 'id' }
-            const conditions = []
-            const [
-                tag,
-                prefix, name,
-                attrName, attrValue,
-                partial, query
-            ] =  match.slice(1)
-
-            if (prefix) {
-                conditions.push(`contains(@${PREFIX_NAME[prefix]}, "${name}")`)
-            }
-            if (attrName) {
-                conditions.push(
-                    attrValue
-                        ? `contains(@${attrName}, "${attrValue}")`
-                        : `@${attrName}`
-                )
-            }
-            if (partial) {
-                conditions.push(`contains(., "${query}")`)
-            } else {
-                conditions.push(`normalize-space() = "${query}"`)
-            }
-
-            using = 'xpath'
-            value = `.//${tag || '*'}[${conditions.join(' and ')}]`
+        if (prefix) {
+            conditions.push(`contains(@${PREFIX_NAME[prefix]}, "${name}")`)
         }
+        if (attrName) {
+            conditions.push(
+                attrValue
+                    ? `contains(@${attrName}, "${attrValue}")`
+                    : `@${attrName}`
+            )
+        }
+        if (partial) {
+            conditions.push(`contains(., "${query}")`)
+        } else {
+            conditions.push(`normalize-space() = "${query}"`)
+        }
+        value = `.//${tag || '*'}[${conditions.join(' and ')}]`
+        break
+    }
     }
 
     /**
