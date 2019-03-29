50271c5837bead69a6f9023992ad0faec052b039
Christian Bromann <github@christian-bromann.com>
2019-03-29 15:19:22 +0100

    Update supported service and reporter list in CLI (#3789)
    ## Proposed changes
    
    The CLI wizard has shown unsupported services and reporters. This PR cleans them up and adds actual v5 supported plugins.
    
    ## Types of changes
    
    [//]: # (What types of changes does your code introduce to WebdriverIO?)
    [//]: # (_Put an `x` in the boxes that apply_)
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist
    
    [//]: # (_Put an `x` in the boxes that apply. You can also fill these out after creating the PR. If you're unsure about any of them, don't hesitate to ask. We're here to help! This is simply a reminder of what we are going to look for before merging your code._)
    
    - [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [x] I have added necessary documentation (if appropriate)
    
    ## Further comments
    
    [//]: # (If this is a relatively large or complex change, kick off the discussion by explaining why you chose the solution you did and what alternatives you considered, etc...)
    
    ### Reviewers: @webdriverio/technical-committee

--- packages/wdio-cli/src/config.js
+++ packages/wdio-cli/src/config.js
@@ -3,7 +3,6 @@ import { filterPackageName } from './utils'
 export const SUPPORTED_FRAMEWORKS = [
     'mocha', // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-mocha-framework
     'jasmine' // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-jasmine-framework
-    // 'cucumber' not yet supported (see #2872)
 ]
 
 export const SUPPORTED_REPORTER = [
@@ -13,11 +12,9 @@ export const SUPPORTED_REPORTER = [
     ' allure - https://www.npmjs.com/package/@wdio/allure-reporter',
     ' sumologic - https://www.npmjs.com/package/@wdio/sumologic-reporter',
     ' concise - https://www.npmjs.com/package/@wdio/concise-reporter',
-    ' teamcity - https://www.npmjs.com/package/wdio-teamcity-reporter',
-    ' json - https://www.npmjs.com/package/wdio-json-reporter',
-    ' testrail - https://www.npmjs.com/package/wdio-testrail-reporter',
-    ' mochawesome - https://www.npmjs.com/package/wdio-mochawesome-reporter'
-    // ' timeline - https://www.npmjs.com/package/wdio-timeline-reporter' not supported yet see https://github.com/QualityOps/wdio-timeline-reporter/issues/9
+    ' reportportal - https://www.npmjs.com/package/wdio-reportportal-reporter',
+    ' video - https://www.npmjs.com/package/wdio-video-reporter',
+    ' html - https://www.npmjs.com/package/@rpii/wdio-html-reporter'
 ]
 
 export const SUPPORTED_SERVICES = [
@@ -27,21 +24,16 @@ export const SUPPORTED_SERVICES = [
     ' selenium-standalone - https://www.npmjs.com/package/@wdio/selenium-standalone-service',
     ' devtools - https://www.npmjs.com/package/@wdio/devtools-service',
     ' applitools - https://www.npmjs.com/package/@wdio/applitools-service',
-    ' browserstack - https://www.npmjs.com/package/wdio-browserstack-service',
-    ' appium - https://www.npmjs.com/package/wdio-appium-service',
-    ' phantomjs - https://www.npmjs.com/package/wdio-phantomjs-service',
-    ' static-server - https://www.npmjs.com/package/wdio-static-server-service',
-    ' visual-regression - https://www.npmjs.com/package/wdio-visual-regression-service',
-    ' webpack - https://www.npmjs.com/package/wdio-webpack-service',
-    ' webpack-dev-server - https://www.npmjs.com/package/wdio-webpack-dev-server-service',
+    ' browserstack - https://www.npmjs.com/package/@wdio/browserstack-service',
+    ' appium - https://www.npmjs.com/package/@wdio/appium-service',
     ' chromedriver - https://www.npmjs.com/package/wdio-chromedriver-service',
-    ' iedriver - https://www.npmjs.com/package/wdio-iedriver-service',
-    ' crossbrowsertesting - https://www.npmjs.com/package/wdio-crossbrowsertesting-service'
+    ' intercept - https://www.npmjs.com/package/wdio-intercept-service',
+    ' zafira-listener - https://www.npmjs.com/package/wdio-zafira-listener-service',
+    ' reportportal - https://www.npmjs.com/package/wdio-reportportal-service'
 ]
 
 export const SUPPORTED_RUNNERS = [
     ' local - https://www.npmjs.com/package/@wdio/local-runner'
-    // ' lambda - https://www.npmjs.com/package/@wdio/lambda-runner'
 ]
 
 const LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error', 'silent']
