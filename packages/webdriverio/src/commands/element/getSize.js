50c9226bfa32e322a608599b0cd2400d32124e51
Christian Bromann <github@christian-bromann.com>
2019-04-01 22:35:00 +0200

    fix getSize code example
    closes #3799

--- packages/webdriverio/src/commands/element/getSize.js
+++ packages/webdriverio/src/commands/element/getSize.js
@@ -3,18 +3,18 @@
  * Get the width and height for an DOM-element.
  *
  * <example>
-    :getElementSize.js
-    it('should demonstrate the getElementSize command', () => {
+    :getSize.js
+    it('should demonstrate the getSize command', () => {
         browser.url('http://github.com')
         const logo = $('.octicon-mark-github')
 
-        const size = logo.getElementSize()
+        const size = logo.getSize()
         console.log(size) // outputs: { width: 32, height: 32 }
 
-        const width = logo.getElementSize('width')
+        const width = logo.getSize('width')
         console.log(width) // outputs: 32
 
-        const height = logo.getElementSize('height')
+        const height = logo.getSize('height')
         console.log(height) // outputs: 32
     })
  * </example>
@@ -22,7 +22,6 @@
  * @alias element.getElementSize
  * @param {String=} prop     size to receive [optional] ("width" or "height")
  * @return {Object|Number}    requested element size (`{ width: <Number>, height: <Number> }`) or actual width/height as number if prop param is given
- * @uses protocol/elements, protocol/getElementRect
  * @type property
  *
  */
