22b5c07b643c2f4a5595bfb32ac64b30666fc5fb
christian-bromann <mail@christian-bromann.com>
2019-04-01 11:19:47 +0200

    implement react60245 command

--- packages/webdriverio/src/scripts/resq.js
+++ packages/webdriverio/src/scripts/resq.js
@@ -2,37 +2,36 @@ export const waitToLoadReact = function waitToLoadReact () {
     window.resq.waitToLoadReact()
 }
 
-export const react$ = function react$ (selector, props) {
+export const react$ = function react$ (selector, props, state) {
     const elems = window.resq.resq$$(selector)
+        .byProps(props)
+        .byState(state)
 
-    if (Object.keys(props).length === 0) {
-        return elems[0]
+    if (elems.length === 0) {
+        return null
     }
 
-    let elem = [...elems].find((elem) => {
-        for (const [name, value] of Object.entries(props)) {
-            // logs.push(`check ${name} and ${value} for ${elem[name]}`)
-            if (elem.props[name] === value) {
-                return true
-            }
-        }
-
-        return false
-    })
-
-    if (!elem || (!elem.node && elem.children.length === 0)) {
-        return
-    }
+    let elem = elems[0]
 
     if (elem.node) {
         return elem.node
     }
 
     elem = elem.node || elem.children[0].node
-
-    if (!elem) {
-        throw new Error('NotFound')
-    }
-
     return elem
+}
+
+export const react$$ = function react$ (selector, props, state) {
+    const elems = window.resq.resq$$(selector)
+        .byProps(props)
+        .byState(state)
+
+    return elems.map((elem) => {
+        if (elem.node) {
+            return elem.node
+        }
+
+        elem = elem.node || elem.children[0].node
+        return elem
+    })
 }
