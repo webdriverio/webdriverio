d9cc9c6b640ac0b5c4ec32be591ce2f24c27772c
Baruch Velez <baruchvelez@gmail.com>
2019-04-04 22:35:35 +0200

    webdriverio: element > elem in resq script

--- packages/webdriverio/src/scripts/resq.js
+++ packages/webdriverio/src/scripts/resq.js
@@ -3,25 +3,25 @@ export const waitToLoadReact = function waitToLoadReact () {
 }
 
 export const react$ = function react$ (selector, props, state) {
-    let elem = window.resq.resq$(selector)
+    let element = window.resq.resq$(selector)
         .byProps(props)
         .byState(state)
 
-    if (!elem.name) {
+    if (!element.name) {
         return { message: `React element with selector "${selector}" wasn't found` }
     }
 
-    return elem.node || elem.children[0].node
+    return element.node || element.children[0].node
 }
 
 export const react$$ = function react$$ (selector, props, state) {
-    let elems = window.resq.resq$$(selector)
+    let elements = window.resq.resq$$(selector)
         .byProps(props)
         .byState(state)
 
-    if (!elems.length) {
+    if (!elements.length) {
         return { message: `React elements with selector "${selector}" wasn't found` }
     }
 
-    return [...elems].map(elem => elem.node || elem.children[0].node)
+    return [...elements].map(element => element.node || element.children[0].node)
 }
