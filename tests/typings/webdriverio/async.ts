ed02a2fc798b955888eeae5e708c5e28c71f6e1e
Tadashi Nemoto <tadashi0713@users.noreply.github.com>
2019-05-03 20:54:36 +0900

    Fix type definition of shadow$ and shadow$$ (#3913)
    Fix type definition of shadow$ and shadow$$

--- tests/typings/webdriverio/async.ts
+++ tests/typings/webdriverio/async.ts
@@ -26,6 +26,13 @@ async function bar() {
     const el5 = await el4.$('')
     await el4.getAttribute('class')
     await el5.scrollIntoView(false)
+
+    // shadow$ shadow$$
+    const el6 = await $('')
+    const shadowElem = await el6.shadow$('')
+    await shadowElem.click()
+    const shadowElems = await el6.shadow$$('')
+    await shadowElems[0].click()
 }
 
 // selenium-standalone-service
