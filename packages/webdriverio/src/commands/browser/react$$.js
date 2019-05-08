9b973871d73237a3b432fd3eb6f75b966a73002f
Christian Bromann <github@christian-bromann.com>
2019-05-08 11:57:14 +0400

    Update description of parameter selector
    Co-Authored-By: Raulster24 <rahulsri.pccs@gmail.com>

--- packages/webdriverio/src/commands/browser/react$$.js
+++ packages/webdriverio/src/commands/browser/react$$.js
@@ -16,7 +16,7 @@
  * </example>
  *
  * @alias browser.react$$
- * @param {String=} selector React elements or HTML elements
+ * @param {String=} selector of React component
  * @param {Object=} props  React props the element should contain
  * @param {Object=} state  React state the element should be in
  * @return {Element}
