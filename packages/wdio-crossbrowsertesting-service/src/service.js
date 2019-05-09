f1434322dbb4c510da22a26bd0257d11a95852ad
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-05-09 13:43:51 -0500

    Update service.js

--- packages/wdio-crossbrowsertesting-service/src/service.js
+++ packages/wdio-crossbrowsertesting-service/src/service.js
@@ -1,17 +1,15 @@
 import logger from '@wdio/logger'
 var request = require('request')
-var cbt = require('cbt_tunnels');
-
 
 const log = logger('@wdio/crossbrowsertesting-service')
 
-export default class CrossBrowserTestingService {
+const jobDataProperties = ['name', 'tags', 'public', 'build', 'extra']
 
+export default class CrossBrowserTestingService {
     constructor(){
         this.testCnt = 0
         this.failures = 0
     }
-
     beforeSession(config, capabilities){
         this.config = config
         this.capabilities = capabilities;
@@ -19,53 +17,42 @@ export default class CrossBrowserTestingService {
         this.config.key = config.key
         this.cbtUsername = this.config.user;
         this.cbtAuthkey = this.config.pass;
-        this.isServiceEnabled = this.cbtUsername && this.cbtAuthkey
-        
+        this.isServiceEnabled = this.cbtUsername && this.cbtAuthkey   
     }
-
     beforeSuite (suite) {
         this.suiteTitle = suite.title
     }
-
     beforeTest(test){
         if(!this.isServiceEnabled){
-            return;
+            return
         }
     }
-
     afterSuite(suite) {
         if (suite.hasOwnProperty('error')) {
             this.failures++
         }
     }
-
      afterTest(test) {
         if (!test.passed) {
             ++this.failures
         }
     }
-
     after() {
         if (!this.cbtUsername || !this.cbtAuthkey) {
-            return;
+            return
         }
-
-        return this.updateJob(this.sessionId, this.failures);
+        return this.updateJob(this.sessionId, this.failures)
     }
-
     getSessionUrl (sessionId) {
-        return `https://crossbrowsertesting.com/api/v3/selenium/${sessionId}`;
-        
+        return `https://crossbrowsertesting.com/api/v3/selenium/${sessionId}`
     }
-
     onReload(oldSessionId, newSessionId){
         if (!this.cbtUsername || !this.cbtAuthkey) {
-            return;
+            return
         }
         this.sessionId = newSessionId;
         return this.updateJob(oldSessionId, this.failures, true)
     }
-
     updateJob (sessionId, failures, calledOnReload = false) {
         return new Promise((resolve, reject) => request.put(this.getSessionUrl(sessionId), {
             json: true,
@@ -83,22 +70,18 @@ export default class CrossBrowserTestingService {
             resolve(body)
         }))
     }
-
      getBody (failures, calledOnReload = false) {
         let body = {}
         body.name = this.suiteTitle
         if (calledOnReload || this.testCnt) {
             body.name += ` (${++this.testCnt})`
         }
-
         for (let prop of jobDataProperties) {
             if (!this.capabilities[prop]) {
                 continue
             }
-
             body[prop] = this.capabilities[prop]
         }
-
         body.passed = failures === 0
         return body
     }
