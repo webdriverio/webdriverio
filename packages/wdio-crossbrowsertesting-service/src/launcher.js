dc4a24fccee325ebe61dc48c1d50af676e8a8e81
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-05-13 12:26:49 -0500

    Update launcher.js

--- packages/wdio-crossbrowsertesting-service/src/launcher.js
+++ packages/wdio-crossbrowsertesting-service/src/launcher.js
@@ -1,40 +1,38 @@
 var cbt = require('cbt_tunnels')
 
 export default class CrossBrowserTestingLauncher {
-  onPrepare (config) {
-    if (!config.cbtTunnel) {
-      return
+    onPrepare (config) {
+        if (!config.cbtTunnel) {
+            return
+        }
+
+        this.cbtOpts = Object.assign({
+            username: config.user,
+            authkey: config.key
+        }, config.cbtOpts)
+
+        this.tunnel = cbt
+
+        return new Promise(function (resolve, reject) {
+            this.tunnel.start({ 'username': config.user, 'authkey': config.key }, function (err) {
+                if (!err) {
+                    return resolve()
+                } else {
+                    return reject(err)
+                }
+            })
+        })
     }
 
-    this.cbtOpts = Object.assign({
-      username: config.user,
-      authkey: config.key
-    }, config.cbtOpts)
-
-    this.tunnel = cbt
-
-    return new Promise(function (resolve, reject) {
-      this.tunnel.start({ 'username': config.user, 'authkey': config.key }, function (err) {
-        if (!err) {
-          return resolve()
-        } else {
-          return reject(err)
-        }
-      })
-    })
-  }
-
-  onComplete () {
-    return new Promise(function (resolve, reject) {
-      this.tunnel.stop(function (err) {
-        if (!err) {
-          return resolve()
-        } else {
-          return reject(err)
-        }
-      })
-    })
-  }
+    onComplete () {
+        return new Promise(function (resolve, reject) {
+            this.tunnel.stop(function (err) {
+                if (!err) {
+                    return resolve()
+                } else {
+                    return reject(err)
+                }
+            })
+        })
+    }
 }
-
-
