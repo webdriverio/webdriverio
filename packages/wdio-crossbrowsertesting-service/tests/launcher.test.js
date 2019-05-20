2d7c4880330378eaf77b758387edcc6c5c2690d4
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-05-20 14:26:47 -0500

    Update launcher.test.js

--- packages/wdio-crossbrowsertesting-service/tests/launcher.test.js
+++ packages/wdio-crossbrowsertesting-service/tests/launcher.test.js
@@ -1,1 +1,51 @@
+import CrossBrowserTestingLauncher from '../src/launcher'
 
+describe('wdio-crossbrowsertesting-service', () => {
+    const cbtLauncher = new CrossBrowserTestingLauncher({})
+    const execute = jest.fn()
+    global.browser = {
+        execute,
+        sessionId: 'globalSessionId',
+        requestHandler: {
+            auth: {
+                user: 'test',
+                key: 'testy'
+            }
+        }
+    }
+
+    afterEach(() => execute.mockReset())
+
+    it('onPrepare: cbtTunnel is undefined', () => {
+        const config = {
+            cbtTunnel: undefined
+        }
+
+        cbtLauncher.onPrepare(config)
+        expect(cbtLauncher.cbtTunnelOpts).toBeUndefined()
+        expect(cbtLauncher.cbtTunnel).toBeUndefined()
+    })
+
+    it('onPrepare', () => {
+        const config = {
+            cbtTunnel: {},
+            cbtTunnelOpts: {
+                options: 'some options'
+            },
+            user: 'test',
+            key: 'testy'
+        }
+
+        cbtLauncher.onPrepare(config)
+        expect(cbtLauncher.cbtTunnelOpts).toEqual({ username: 'test', authkey: 'testy', options: 'some options' })
+    })
+
+    it('onComplete: no tunnel', () => {
+        cbtLauncher.tunnel = undefined
+        expect(cbtLauncher.onComplete()).toBeUndefined()
+    })
+
+    it('onComplete', async () => {
+        return expect(await cbtLauncher.onComplete())
+    })
+})
