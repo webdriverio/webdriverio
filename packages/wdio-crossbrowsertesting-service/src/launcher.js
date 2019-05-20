2b8c98f0ca85425f34a16329ae9d9902095816aa
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-05-20 14:26:33 -0500

    Update launcher.js

--- packages/wdio-crossbrowsertesting-service/src/launcher.js
+++ packages/wdio-crossbrowsertesting-service/src/launcher.js
@@ -6,33 +6,26 @@ export default class CrossBrowserTestingLauncher {
             return
         }
 
-        this.cbtOpts = Object.assign({
+        this.cbtTunnelOpts = Object.assign({
             username: config.user,
             authkey: config.key
-        }, config.cbtOpts)
+        }, config.cbtTunnelOpts)
 
-        this.tunnel = cbt
+        return new Promise((resolve, reject) => cbt.start({ 'username': config.user, 'authkey': config.key }, (err, tunnel) => {
+            if (err) {
+                return reject(err)
+            }
 
-        return new Promise(function (resolve, reject) {
-            this.tunnel.start({ 'username': config.user, 'authkey': config.key }, function (err) {
-                if (!err) {
-                    return resolve()
-                } else {
-                    return reject(err)
-                }
-            })
-        })
+            this.tunnel = tunnel
+            return resolve('connected!!!!!!')
+        }))
     }
 
     onComplete () {
-        return new Promise(function (resolve, reject) {
-            this.tunnel.stop(function (err) {
-                if (!err) {
-                    return resolve()
-                } else {
-                    return reject(err)
-                }
-            })
-        })
+        if(!this.tunnel){
+            return
+        }
+
+        return new Promise(resolve => cbt.stop(resolve))
     }
 }
