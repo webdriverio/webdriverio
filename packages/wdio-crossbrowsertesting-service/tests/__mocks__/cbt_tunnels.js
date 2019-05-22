002bec4817294d58239bee277a056d5e843e1dd1
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-05-22 14:21:04 -0500

    Create cbt_tunnels.js

---
+++ packages/wdio-crossbrowsertesting-service/tests/__mocks__/cbt_tunnels.js
@@ -0,0 +1,11 @@
+const Cbt = jest.genMockFromModule('cbt_tunnels')
+
+export const mockStart = jest.fn().mockImplementation((options, cb) => cb(null, null))
+export const mockStop = jest.fn().mockImplementation((cb) => cb(null))
+
+export const mockLocal = jest.fn().mockImplementation( function () {
+    this.start = mockStart
+    this.stop = mockStop
+})
+
+module.exports = Cbt
