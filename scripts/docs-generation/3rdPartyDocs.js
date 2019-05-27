5f9ade577417a33e24dd9bc6411c34b2a067712a
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-05-27 11:21:05 +0200

    fix sidebar and badges, add package info

--- scripts/docs-generation/3rdPartyDocs.js
+++ scripts/docs-generation/3rdPartyDocs.js
@@ -6,10 +6,9 @@ const { buildPreface } = require('../utils/helpers')
 const reporters3rdParty = require('./3rd-party/reporters.json')
 const services3rdParty = require('./3rd-party/services.json')
 
-const packages3rdParty = [...reporters3rdParty, ...services3rdParty]
 const plugins = {
-    reporter: ['Reporter', 'Reporter'],
-    service: ['Services', 'Service']
+    reporter: ['Reporter', 'Reporter', reporters3rdParty],
+    service: ['Services', 'Service', services3rdParty]
 }
 const githubHost = 'https://github.com/'
 const githubRawHost = 'https://raw.githubusercontent.com/'
@@ -17,19 +16,21 @@ const githubReadme = 'master/README.md'
 
 const readmeHeaderLines = 9
 const readmeHeaders = ['===', '# ']
-const readmeBadges = 'https://badge'
+const readmeBadges = ['https://badge', 'https://travis-ci.org/']
 
 /**
  * Generate docs for 3rd party reporters and services
  * @param {object} sidebars website/sidebars
  */
 exports.generate3rdPartyDocs = async (sidebars) => {
-    for (const [, [namePlural, nameSingular]] of Object.entries(plugins)) {
-        for (const { packageName, title, githubUrl } of packages3rdParty) {
+    for (const [, [namePlural, nameSingular, packages3rdParty]] of Object.entries(plugins)) {
+        for (const { packageName, title, githubUrl, npmUrl } of packages3rdParty) {
             const readme = await downloadReadme(githubUrl)
-            const id = `3rd-party-${packageName}`.replace(/@/g, '').replace(/\//g, '-')
+            const id = `${packageName}`.replace(/@/g, '').replace(/\//g, '-')
 
-            const doc = normalizeDoc(readme, githubUrl, buildPreface(id, title, nameSingular, `${githubUrl}/edit/${githubReadme}`))
+            const doc = normalizeDoc(readme, githubUrl,
+                buildPreface(id, title, nameSingular, `${githubUrl}/edit/${githubReadme}`),
+                buildInfo(packageName, githubUrl, npmUrl))
             fs.writeFileSync(path.join(__dirname, '..', '..', 'docs', `_${id}.md`), doc, { encoding: 'utf-8' })
 
             if (!sidebars.docs[namePlural]) {
@@ -69,15 +70,16 @@ function downloadReadme(githubUrl) {
 
 /**
  * Removes header from README.md
- * @param {string}  readme  readme content
- * @param {string}  preface readme content
- * @return {string}         readme content without header
+ * @param {string}  readme      readme content
+ * @param {string}  preface     docusaurus header
+ * @param {string}  repoInfo    repoInfo
+ * @return {string}             readme content without header
  */
-function normalizeDoc(readme, githubUrl, preface) {
+function normalizeDoc(readme, githubUrl, preface, repoInfo) {
     /**
      * remove badges
      */
-    let readmeArr = readme.split('\n').filter(row => !row.includes(readmeBadges))
+    let readmeArr = readme.split('\n').filter(row => !readmeBadges.some(b => row.includes(b)))
 
     /**
      * get index of header to remove further
@@ -95,15 +97,37 @@ function normalizeDoc(readme, githubUrl, preface) {
      * and add path to links
      */
     readmeArr.forEach((row, idx) => {
+        /**
+         * prepend # to header rows
+         */
         if (row.match(/^(#)\1* /)) {
             readmeArr[idx] = `#${row}`
         }
+
+        /**
+         * transform partial to full links
+         * match links like [foo](bar). `stringInParentheses` would be `bar`
+         * do not match [foo](http://bar), [foo](#bar)
+         */
         const urlMatcher = row.match(/\[.*\]\(((?!(#|http)).*)\)/)
         if (urlMatcher && urlMatcher.length > 1) {
-            const url = `${githubUrl}/master/${urlMatcher[1]}`.replace(githubHost, githubRawHost)
-            readmeArr[idx] = readmeArr[idx].replace(`](${urlMatcher[1]})`, `](${url})`)
+            const stringInParentheses = urlMatcher[1]
+            const url = `${githubUrl}/master/${stringInParentheses}`.replace(githubHost, githubRawHost)
+            readmeArr[idx] = readmeArr[idx].replace(`](${stringInParentheses})`, `](${url})`)
         }
     })
 
-    return [...preface, ...readmeArr].join('\n')
+    return [...preface, ...repoInfo, ...readmeArr].join('\n')
+}
+
+/**
+ * 3rd party package info
+ * @param {string} packageName package name
+ * @param {string} githubUrl GitHub url
+ * @param {string} npmUrl npm url
+ */
+function buildInfo(packageName, githubUrl, npmUrl) {
+    return [
+        `> ${packageName} is a 3rd party package, for more information please see [GitHub](${githubUrl}) | [npm](${npmUrl})`
+    ]
 }
