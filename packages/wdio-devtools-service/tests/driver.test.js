8d146cb832a7df2d6ed8f48581049d7b6cf807f4
Simon Markowski <smarkows@gmail.com>
2019-05-30 15:43:00 -0400

    @wdio/devtools-service: updating puppeteer.connect arguments

--- packages/wdio-devtools-service/tests/driver.test.js
+++ packages/wdio-devtools-service/tests/driver.test.js
@@ -1,5 +1,8 @@
 import DevToolsDriver from '../src/driver'
 
+import puppeteer from 'puppeteer-core'
+jest.mock('puppeteer-core')
+
 test('getActivePage: returns existing page if existing', async () => {
     const driver = new DevToolsDriver()
     driver.page = 'foobar'
@@ -17,4 +20,11 @@ test('send', async () => {
     driver.cdpSession = { send: jest.fn() }
     await driver.send('foo', 'bar')
     expect(driver.cdpSession.send).toBeCalledWith('foo', 'bar')
+})
+
+test('connect', async () => {
+    const url = 'https://webdriver.io'
+    const driver = await DevToolsDriver.attach(url)
+    expect(driver.browser).toBeInstanceOf(puppeteer.PuppeteerMock)
+    expect(puppeteer.connect).toBeCalledWith({ browserURL: url, defaultViewport: null })
 })
