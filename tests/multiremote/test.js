5593fd28a8225314a1765e1a26bc0b0c1dee51be
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-05-31 15:17:13 +0200

    Wrap addCommand function with Fibers in multiremote mode (#4047)
    * webdriverio: wrap addCommand function with Fibers in multiremote mode
    
    * wdio-devtools-service: fix failing test

--- tests/multiremote/test.js
+++ tests/multiremote/test.js
@@ -6,4 +6,46 @@ describe('smoke test multiremote', () => {
             JSON.stringify(browser.getTitle()),
             JSON.stringify(['Mock Page Title', 'Mock Page Other Title']))
     })
+
+    it('should respect promises', () => {
+        browser.addCommand('customFn', () => {
+            let start = Date.now()
+            browser.pause(30)
+            return Promise.all([
+                Promise.resolve(Date.now() - start),
+                Promise.resolve(Date.now() - start)
+            ])
+        })
+
+        const results = browser.customFn()
+
+        assert.strictEqual(results[0] >= 30, true, `First of [${results}] is less than 30`)
+    })
+
+    it('should throw if promise rejects', () => {
+        browser.addCommand('customFn', () => {
+            return Promise.reject('Boom!')
+        })
+
+        let err = null
+        try {
+            browser.customFn()
+        } catch (e) {
+            err = e
+        }
+        assert.equal(err.message, 'Boom!')
+    })
+
+    it('allows to create custom commands on elements that respects promises', () => {
+        browser.customCommandScenario(Object.keys(browser.instances).length)
+        browser.addCommand('myCustomPromiseCommand', function () {
+            let start = Date.now()
+            browser.pause(30)
+            return Promise.resolve(Date.now() - start)
+        }, true)
+        const elem = $('elem')
+        const results = elem.myCustomPromiseCommand()
+
+        assert.strictEqual(results[0] >= 30, true, `First of [${results}] is less than 30`)
+    })
 })
