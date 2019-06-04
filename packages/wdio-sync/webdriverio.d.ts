8d7b7761f8d52188cd553e9ac3d12e7d676cb2c6
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-06-04 11:04:48 +0200

    Fix call, execute, executeAsync, remote, multiremote (#4050)
    * typings: fix call, execute, executeAsync, remote, multiRemote, update typescript to 3.5.1
    
    * fix add command to async example

--- packages/wdio-sync/webdriverio.d.ts
+++ packages/wdio-sync/webdriverio.d.ts
@@ -1,17 +1,6 @@
 /// <reference types="@wdio/sync/webdriverio-core"/>
 
-type BrowserObject = WebDriver.ClientOptions & WebDriver.Client & WebdriverIO.Browser;
-
 declare namespace WebdriverIO {
-    function remote(
-        options?: WebDriver.Options,
-        modifier?: (...args: any[]) => any
-    ): BrowserObject;
-
-    function multiremote(
-        options: WebdriverIO.MultiRemoteOptions
-    ): WebDriver.Client;
-
     interface Browser {
         waitUntil(
             condition: () => boolean,
@@ -19,10 +8,23 @@ declare namespace WebdriverIO {
             timeoutMsg?: string,
             interval?: number
         ): boolean
+
+        // there is no way to wrap generic functions, like `<T>(arg: T) => T`
+        // have to declare explicitly for sync and async typings.
+        // https://github.com/microsoft/TypeScript/issues/5453
+        call: <T>(callback: (...args) => Promise<T>) => T;
+        execute: <T>(script: string | ((...arguments: any[]) => T), ...arguments: any[]) => T;
+
+        // also there is no way to add callback as last parameter after `...args`.
+        // https://github.com/Microsoft/TypeScript/issues/1360
+        // executeAsync: <T>(script: string | ((...arguments: any[], callback: (result: T) => void) => void), ...arguments: any[]) => T;
+        executeAsync: (script: string | ((...arguments: any[]) => void), ...arguments: any[]) => any;
     }
+
+    interface BrowserObject extends WebDriver.ClientOptions, WebDriver.Client, WebdriverIO.Browser { }
 }
 
-declare var browser: BrowserObject;
+declare var browser: WebdriverIO.BrowserObject;
 declare function $(selector: string | Function): WebdriverIO.Element;
 declare function $$(selector: string | Function): WebdriverIO.Element[];
 
