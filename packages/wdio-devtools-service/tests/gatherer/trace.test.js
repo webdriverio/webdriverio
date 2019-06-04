6937b0a5d5c20158c234f54d0abb0a37ba8d404c
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-06-04 22:33:39 +0200

    wdio-devtools-service: fix failing test

--- packages/wdio-devtools-service/tests/gatherer/trace.test.js
+++ packages/wdio-devtools-service/tests/gatherer/trace.test.js
@@ -217,7 +217,7 @@ test('onLoadEventFired: using min trace time', async () => {
 
     const start = Date.now()
     await traceGatherer.onLoadEventFired()
-    expect(Date.now() - start).toBeGreaterThanOrEqual(299)
+    expect(Date.now() - start).toBeGreaterThanOrEqual(250)
 })
 
 test('waitForMaxTimeout', async () => {
