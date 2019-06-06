6c9a35df2a5d60e6569b9e36ba0beea35ff6f5a0
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-06-06 10:30:57 +0200

    wdio-mocha-framework: add unit tests for pending tests

--- packages/wdio-mocha-framework/src/index.js
+++ packages/wdio-mocha-framework/src/index.js
@@ -277,7 +277,7 @@ class MochaAdapter {
         if (['test:start', 'test:pending'].includes(message.type)) {
             return this.getSyncEventIdStart('test')
         }
-        if (['test:pending', 'test:end', 'test:pass', 'test:fail'].includes(message.type)) {
+        if (['test:end', 'test:pass', 'test:fail'].includes(message.type)) {
             return this.getSyncEventIdEnd('test')
         }
 
