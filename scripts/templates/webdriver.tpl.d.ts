78cde98dd2d4ba4b2ce80b932072f067c90ebaaf
aha-oretama <sekine_y_529@msn.com>
2019-06-21 21:02:10 +0900

    typings: specs and exclude in capabilities is missing in type dsl.

--- scripts/templates/webdriver.tpl.d.ts
+++ scripts/templates/webdriver.tpl.d.ts
@@ -200,6 +200,10 @@ declare namespace WebDriver {
             bufferUsageReportingInterval?: boolean;
         };
 
+        // webdriverio specific
+        specs?: string[];
+        exclude?: string[];
+
         // wdio-sauce-service specific
         build?: string;
 
