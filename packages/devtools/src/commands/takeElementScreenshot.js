636745ef8950ef45bb5677e2c8b98c5a191ca868
christian-bromann <mail@christian-bromann.com>
2019-07-16 22:34:42 +0200

    add takeElementScreenshot

---
+++ packages/devtools/src/commands/takeElementScreenshot.js
@@ -0,0 +1,12 @@
+export default async function takeScreenshot ({ elementId }) {
+    const elementHandle = this.elementStore.get(elementId)
+
+    if (!elementHandle) {
+        throw new Error(`Couldn't find element with id ${elementId} in cache`)
+    }
+
+    return elementHandle.screenshot({
+        encoding: 'base64',
+        type: 'png'
+    })
+}
