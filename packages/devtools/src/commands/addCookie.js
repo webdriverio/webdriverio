b62a849a58e771b796960831695700c89cd127a6
christian-bromann <mail@christian-bromann.com>
2019-07-16 21:38:19 +0200

    add addCookie

---
+++ packages/devtools/src/commands/addCookie.js
@@ -0,0 +1,10 @@
+export default async function getElementAttribute ({ cookie }) {
+    const page = this.windows.get(this.currentWindowHandle)
+
+    if (!Object.keys(cookie).includes('name') || !Object.keys(cookie).includes('value')) {
+        throw new Error('Provided cookie object is missing either "name" or "value" property')
+    }
+
+    await page.setCookie(cookie)
+    return null
+}
