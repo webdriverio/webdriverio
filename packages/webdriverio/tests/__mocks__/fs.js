7a4163a9949b87f1b14dc3ce72b318317d68e5be
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-07-22 23:11:47 +0200

    webdriverio: discard changes in some tests

--- packages/webdriverio/tests/__mocks__/fs.js
+++ packages/webdriverio/tests/__mocks__/fs.js
@@ -1,7 +1,4 @@
-const { readdirSync } = jest.requireActual('fs')
 const fs = jest.genMockFromModule('fs')
 fs.writeFileSync = jest.fn()
 fs.existsSync = jest.fn(() => true)
-fs.readdirSync.mockImplementation(readdirSync)
-
 export default fs
