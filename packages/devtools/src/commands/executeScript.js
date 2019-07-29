2f774259db1c767a43eb7f25e9e598681ad43bb7
christian-bromann <mail@christian-bromann.com>
2019-07-29 14:09:25 +0200

    timeout executeScript execution in case an alert popped up

--- packages/devtools/src/commands/executeScript.js
+++ packages/devtools/src/commands/executeScript.js
@@ -4,7 +4,9 @@ import { SERIALIZE_PROPERTY, SERIALIZE_FLAG } from '../constants'
 
 export default async function executeScript ({ script, args }) {
     const page = this.windows.get(this.currentWindowHandle)
-    const result = await page.$eval(
+    const scriptTimeout = this.timeouts.get('script')
+
+    const executePromise = page.$eval(
         'html',
         command,
         script,
@@ -13,5 +15,23 @@ export default async function executeScript ({ script, args }) {
         ...transformExecuteArgs.call(this, args)
     )
 
+    const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(
+            () => {
+                const additionalTimeoutInfo = (
+                    Boolean(this.activeDialog) &&
+                    'a browser dialog has opened as result of a executeScript call'
+                )
+
+                return reject(new Error('script timeout' + (additionalTimeoutInfo
+                    ? `, reason: ${additionalTimeoutInfo}`
+                    : ''
+                )))
+            },
+            scriptTimeout
+        )
+    })
+
+    const result = await Promise.race([executePromise, timeoutPromise])
     return transformExecuteResult.call(this, page, result)
 }
