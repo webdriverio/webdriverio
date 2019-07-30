85eaf6737e7505d3662ca08029d8a43533fbe296
christian-bromann <mail@christian-bromann.com>
2019-07-30 13:10:54 +0200

    return width and height for getWindowRect

--- packages/devtools/src/commands/getWindowRect.js
+++ packages/devtools/src/commands/getWindowRect.js
@@ -1,5 +1,10 @@
+import { DEFAULT_WIDTH, DEFAULT_HEIGHT } from '../constants'
+
 export default async function getWindowRect () {
     const page = this.windows.get(this.currentWindowHandle)
-    const viewport = await page.viewport()
-    return Object.assign(viewport, { x: 0, y: 0 })
+    const viewport = await page.viewport() || {}
+    return Object.assign(
+        { width: DEFAULT_WIDTH, height: DEFAULT_HEIGHT, x: 0, y: 0 },
+        viewport
+    )
 }
