66d93ca4271b7fd84359d4ea3890847b49fcfe13
christian-bromann <mail@christian-bromann.com>
2019-07-30 13:04:14 +0200

    throw if cookie can't be found

--- packages/devtools/src/commands/getNamedCookie.js
+++ packages/devtools/src/commands/getNamedCookie.js
@@ -1,5 +1,11 @@
 export default async function getNamedCookie ({ name }) {
     const page = this.windows.get(this.currentWindowHandle)
     const cookies = await page.cookies()
-    return cookies.find((cookie) => cookie.name === name)
+    const cookie = cookies.find((cookie) => cookie.name === name)
+
+    if (!cookie) {
+        throw new Error(`No cookie with name ${name}`)
+    }
+
+    return cookie
 }
