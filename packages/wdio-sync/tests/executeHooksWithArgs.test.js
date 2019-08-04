240855fc4aae16aa03c2e13dac5e7fc5ac2bceec
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-08-04 17:26:28 +0900

    With promise

--- packages/wdio-sync/tests/executeHooksWithArgs.test.js
+++ packages/wdio-sync/tests/executeHooksWithArgs.test.js
@@ -6,21 +6,32 @@ describe('executeHooksWithArgs', () => {
         const hookFuga = () => {return 'fuga'}
         const argHoge = {hoge: 'hoge'}
         const argFuga = {fuga: 'fuga'}
-        const runCommand = executeHooksWithArgs([hookHoge, hookFuga], [argHoge, argFuga])
-        expect(await runCommand).toHaveLength(2)
+        const res = await executeHooksWithArgs([hookHoge, hookFuga], [argHoge, argFuga])
+        expect(res).toHaveLength(2)
     })
 
     it('one hook, one arg', async () => {
         const hook = () => {return 'hoge'}
         const arg = {hoge: 'hoge'}
-        const runCommand = executeHooksWithArgs(hook, arg)
-        expect(await runCommand).toHaveLength(1)
+        const res = await executeHooksWithArgs(hook, arg)
+        expect(res).toHaveLength(1)
     })
 
     it('with error', async () => {
-        const hook = () => {return 'hoge'}
-        hook.apply = () => {throw new Error('Fuga')}
-        const runCommand = executeHooksWithArgs(hook, [])
-        expect(await runCommand).toEqual([new Error('Fuga')])
+        const hook = () => {throw new Error('Fuga')}
+        const res = executeHooksWithArgs(hook, [])
+        expect(res).toEqual([new Error('Fuga')])
+    })
+
+    it('with er', async () => {
+        const hook = () => {
+            return new Promise((resolve) => {
+                resolve('Success!')
+            }).then((value) => {
+                return value
+            })
+        }
+        const res = executeHooksWithArgs(hook, [])
+        expect(res).toEqual([new Error('Fuga')])
     })
 })
