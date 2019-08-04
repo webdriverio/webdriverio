d62116b249c5b81d22c2741c4491d63591b83709
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-08-04 16:31:36 +0900

    Add test case

--- packages/wdio-sync/tests/executeHooksWithArgs.test.js
+++ packages/wdio-sync/tests/executeHooksWithArgs.test.js
@@ -1,9 +1,19 @@
 import executeHooksWithArgs from '../src/executeHooksWithArgs'
 
 describe('executeHooksWithArgs', () => {
-    it('should return', async () => {
+    it('multiple hooks, multiple args', async () => {
+        const hookHoge = () => {return 'hoge'}
+        const hookFuga = () => {return 'fuga'}
+        const argHoge = {hoge: 'hoge'}
+        const argFuga = {fuga: 'fuga'}
+        const runCommand = executeHooksWithArgs([hookHoge, hookFuga], [argHoge, argFuga])
+        expect(await runCommand).toHaveLength(2)
+    })
+
+    it('one hook, one arg', async () => {
         const hook = () => {return 'hoge'}
-        const runCommand = executeHooksWithArgs(hook, [])
-        expect(await runCommand).toEqual(['hoge'])
+        const arg = {hoge: 'hoge'}
+        const runCommand = executeHooksWithArgs(hook, arg)
+        expect(await runCommand).toHaveLength(1)
     })
 })
