91bc19ae25477184efbd89e72b8dd64417fbd433
Jay Chen <jay.chen@hbo.com>
2019-08-06 22:48:57 +0800

    add types for cucumberOpts, fix cucumberOpts in wdio-cli template (#4318)
    * change tags to tagExpression in cucumberOpts
    
    * add cucumberOpts types

--- packages/wdio-cucumber-framework/cucumber-framework.d.ts
+++ packages/wdio-cucumber-framework/cucumber-framework.d.ts
@@ -2,10 +2,16 @@ import { SourceLocation, ScenarioResult } from "cucumber";
 
 declare module "webdriverio" {
     interface HookFunctions extends CucumberHookFunctions {}
+    interface Options {
+        cucumberOpts?: CucumberOptions;
+    }
 }
 
 declare module "@wdio/sync" {
     interface HookFunctions extends CucumberHookFunctions {}
+    interface Options {
+        cucumberOpts?: CucumberOptions;
+    }
 }
 
 interface CucumberHookResult extends ScenarioResult {
@@ -22,4 +28,21 @@ interface CucumberHookFunctions {
     afterStep?(uri: string, feature: CucumberHookObject, scenario: CucumberHookObject, step: CucumberHookObject, result: CucumberHookResult, sourceLocation: SourceLocation): void;
     afterScenario?(uri: string, feature: CucumberHookObject, scenario: CucumberHookObject, result: CucumberHookResult, sourceLocation: SourceLocation): void;
     afterFeature?(uri: string, feature: CucumberHookObject): void;
+}
+
+interface CucumberOptions {
+    require?: string[];
+    backtrace?: boolean;
+    requireModule?: (string | object)[];
+    dryRun?: boolean;
+    failFast?: boolean;
+    format?: string[];
+    colors?: boolean;
+    snippets?: boolean;
+    source?: boolean;
+    profile?: string[];
+    strict?: boolean;
+    tagExpression?: string;
+    timeout?: number;
+    ignoreUndefinedDefinitions?: boolean;
 }
