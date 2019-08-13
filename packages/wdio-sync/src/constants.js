9cdc5c11fe7ac45cb32808448e61e5a6d0547160
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-08-13 10:15:30 +0200

    Fix stacktrace formatting (#4325)

--- packages/wdio-sync/src/constants.js
+++ packages/wdio-sync/src/constants.js
@@ -4,10 +4,14 @@ const STACKTRACE_FILTER = [
     // exclude @wdio/sync from stack traces
     'node_modules/@wdio/sync/',
 
-    // exclude webdriverio stack traces
+    // exclude webdriverio and webdriver stack traces
     'node_modules/webdriverio/build/',
+    'node_modules/webdriver/build/',
 
-    // exclude Request emit
+    // exclude request
+    'node_modules/request/request',
+
+    // exclude EventEmitter
     ' (events.js:',
     ' (domain.js:',
 
