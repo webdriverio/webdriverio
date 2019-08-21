abf4c23ca3fd547d75d20cc443fd29467ae853c3
christian-bromann <mail@christian-bromann.com>
2019-08-21 11:14:08 +0200

    make selectBy* commands work

--- packages/devtools/src/commands/elementClick.js
+++ packages/devtools/src/commands/elementClick.js
@@ -1,9 +1,28 @@
+import getElementTagName from './getElementTagName'
+import executeScript from './executeScript'
+import { ELEMENT_KEY } from '../constants'
+
+const SELECT_SCRIPT = 'return (function select (elem) { elem.selected = true }).apply(null, arguments)'
+
 export default async function elementClick ({ elementId }) {
     const page = this.windows.get(this.currentWindowHandle)
     const elementHandle = this.elementStore.get(elementId)
 
     if (!elementHandle) {
         throw new Error(`Couldn't find element with id ${elementId} in cache`)
+    }
+
+    /**
+     * in order to allow clicking on option elements (as WebDriver does)
+     * we need to check if the element is such a an element and select
+     * it instead of actually executing the click
+     */
+    const tagName = await getElementTagName.call(this, { elementId })
+    if (tagName === 'option') {
+        return executeScript.call(this, {
+            script: SELECT_SCRIPT,
+            args: [{ [ELEMENT_KEY]: elementId }]
+        })
     }
 
     /**
