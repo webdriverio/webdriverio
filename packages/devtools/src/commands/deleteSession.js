093b456c06f92bad1fd75b14e3fe4b4201a7167d
christian-bromann <mail@christian-bromann.com>
2019-09-02 09:46:02 +0200

    minor tweak in deleteSession

--- packages/devtools/src/commands/deleteSession.js
+++ packages/devtools/src/commands/deleteSession.js
@@ -1,13 +1,4 @@
 export default async function deleteSession () {
-    const pages = await this.browser.pages()
-
-    /**
-     * close all tabs
-     */
-    for (const page of pages) {
-        await page.close()
-    }
-
     await this.browser.close()
     return null
 }
