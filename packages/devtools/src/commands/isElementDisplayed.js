04318c153838d9d42c993c684c87db9361c4f697
christian-bromann <mail@christian-bromann.com>
2019-09-04 11:31:19 +0200

    scroll element into view before checking if it is displayed closes #4447

--- packages/devtools/src/commands/isElementDisplayed.js
+++ packages/devtools/src/commands/isElementDisplayed.js
@@ -1,11 +1,24 @@
-import { getStaleElementError } from '../utils'
+import executeScript from './executeScript'
 
-export default function isElementDisplayed ({ elementId }) {
+import { getStaleElementError } from '../utils'
+import { ELEMENT_KEY } from '../constants'
+
+const SCROLL_INTO_VIEW_SCRIPT = 'return (function scrollIntoView (elem) { elem.scrollIntoView() }).apply(null, arguments)'
+
+export default async function isElementDisplayed ({ elementId }) {
     const elementHandle = this.elementStore.get(elementId)
 
     if (!elementHandle) {
         throw getStaleElementError(elementId)
     }
+
+    /**
+     * scroll element into view
+     */
+    await executeScript.call(this, {
+        script: SCROLL_INTO_VIEW_SCRIPT,
+        args: [{ [ELEMENT_KEY]: elementId }]
+    })
 
     return elementHandle.isIntersectingViewport()
 }
