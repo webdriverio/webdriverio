cecf11bc944e14341f76be7243e08e3ca086088a
Christian Bromann <mail@christian-bromann.com>
2019-09-08 23:29:23 +0200

    Convert protocol elements to WebdriverIO elements (#4266)
    * allow to transform protocol element references into wdio elements
    
    * fix params
    
    * fix example

--- packages/webdriverio/src/commands/element/$.js
+++ packages/webdriverio/src/commands/element/$.js
@@ -1,7 +1,9 @@
 /**
  * The `$` command is a short way to call the [`findElement`](/docs/api/webdriver.html#findelement) command in order
  * to fetch a single element on the page similar to the `$` command from the browser scope. The difference when calling
- * it from an element scope is that the driver will look within the children of that element.
+ * it from an element scope is that the driver will look within the children of that element. You can also pass in an object as selector
+ * where the object contains a property `element-6066-11e4-a52e-4f735466cecf` with the value of a reference
+ * to an element. The command will then transform the reference to an extended WebdriverIO element.
  *
  * Note: chaining `$` and `$$` commands only make sense when you use multiple selector strategies. You will otherwise
  * make unnecessary requests that slow down the test (e.g. `$('body').$('div')` will trigger two request whereas
@@ -33,6 +35,11 @@
             return this.querySelector('a'); // Element
         }).getText()); // outputs: "API"
     });
+
+    it('should allow to convert protocol result of an element into a WebdriverIO element', () => {
+        const activeElement = browser.getActiveElement();
+        console.log($(activeElement).getTagName()); // outputs active element
+    });
  * </example>
  *
  * @alias $
@@ -43,8 +50,17 @@
  */
 import { findElement } from '../../utils'
 import { getElement } from '../../utils/getElementObject'
+import { ELEMENT_KEY } from '../../constants'
 
 export default async function $ (selector) {
+    /**
+     * convert protocol result into WebdriverIO element
+     * e.g. when element was fetched with `getActiveElement`
+     */
+    if (selector && typeof selector[ELEMENT_KEY] === 'string') {
+        return getElement.call(this, null, selector)
+    }
+
     const res = await findElement.call(this, selector)
     return getElement.call(this, selector, res)
 }
