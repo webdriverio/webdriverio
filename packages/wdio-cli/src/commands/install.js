ec66413461dbd5f29bb5e875626e7256ebe31770
Baruch Velez <baruchvelez@gmail.com>
2019-10-01 10:51:31 +0200

    Updates/wdio cli (#4402)
    * wdio-cli: change to collection of supported packages. update install cmd to use new collection
    
    * wdio-cli: fix utils tests
    
    * wdio-cli: adjust folder structure. add run command. adjust help flag
    
    * wdio-cli: add test for missingConfigurationPrompt
    
    * wdio-cli: initalize commands test suites
    
    * wdio-cli: add run command test
    
    * wdio-cli: add config command tests. change runConfigHelper default export to named export
    
    * wdio-cli: add repl command tests
    
    * wdio-cli: add install command test
    
    * wdio-cli: fix typo in supported packages constant
    
    * wdio-cli: increase cli test coverage
    
    * pkg: remove node_modules path for eslint
    
    * wdio-cli: use run handler instead of cp spawn
    
    * wdio-cli: remove try/catch block from config helper
    
    * wdio-cli: capitalize constant variable name
    
    * docs: Update CLI documentation
    
    * add unit tests for executeHooksWithArgs
    
    * fix unit tests
    
    * add unit tests for config command
    
    * PR comments
    
    * add a test
    
    * fix utils test
    
    * wdio-cli: use NPM as default package manager
    
    * wdio-cli: update docs and remove deprecation notice
    
    * wdio-cli: fix config useYarn flag
    
    * wdio-cli: fix template path in utils
    
    * wdio-cli: add default false to yarn flag in config
    
    * wdio-cli: fix config tests to correctly check yarn flag
    
    * wdio-cli: remove log in  config handler
    
    * wdio-cli: fix html reporter package name
    
    * wdio-cli: fix appium command error in ejs tpl
    
    * wdio-local-runner: add tests for worker

--- packages/wdio-cli/src/commands/install.js
+++ packages/wdio-cli/src/commands/install.js
@@ -1,113 +1,102 @@
 /* eslint-disable no-console */
 import fs from 'fs'
 import path from 'path'
-import inquirer from 'inquirer'
 import yarnInstall from 'yarn-install'
-/**
-    wdio install <type> <packageName>
- */
-import setup from '../setup'
-import { SUPPORTED_SERVICES, SUPPORTED_REPORTER, SUPPORTED_FRAMEWORKS } from '../config'
+
 import {
-    parseInstallNameAndPackage,
     replaceConfig,
     findInConfig,
-    addServiceDeps
+    addServiceDeps,
+    convertPackageHashToObject,
+    missingConfigurationPrompt
 } from '../utils'
 
+import { SUPPORTED_PACKAGES } from '../constants'
+
 const supportedInstallations = {
-    service: parseInstallNameAndPackage(SUPPORTED_SERVICES),
-    reporter: parseInstallNameAndPackage(SUPPORTED_REPORTER),
-    framework: parseInstallNameAndPackage(SUPPORTED_FRAMEWORKS)
+    service: SUPPORTED_PACKAGES.service.map(({ value }) => convertPackageHashToObject(value)),
+    reporter: SUPPORTED_PACKAGES.reporter.map(({ value }) => convertPackageHashToObject(value)),
+    framework: SUPPORTED_PACKAGES.framework.map(({ value }) => convertPackageHashToObject(value))
 }
 
 export const command = 'install <type> <name>'
 export const desc = 'Add a `reporter`, `service`, or `framework` to your WebdriverIO project'
 
-export default function builder(yargs) {
-    return yargs
-        .option('npm', {
-            desc: 'Install packages using npm',
-            type: 'boolean',
-            default: false
-        })
+export const builder = {
+    yarn: {
+        desc: 'Install packages using yarn',
+        type: 'boolean',
+        default: false
+    }
 }
 
 export async function handler(argv) {
     /**
      * type = service | reporter | framework
      * name = names for the supported service or reporter
-     * npm = optional flag to install package using npm instead of default yarn
+     * yarn = optional flag to install package using yarn instead of default yarn
      */
-    const { type, name, npm } = argv
+    const { type, name, yarn } = argv
 
-    // verify for supported types via `supportedInstallations` keys
+    /**
+     * verify for supported types via `supportedInstallations` keys
+     */
     if (!Object.keys(supportedInstallations).includes(type)) {
         console.log(`Type ${type} is not supported.`)
         process.exit(0)
         return
     }
 
-    // verify if the name of the `type` is valid
-    if (!Object.keys(supportedInstallations[type]).includes(name)) {
+    /**
+     * verify if the name of the `type` is valid
+     */
+    if (!supportedInstallations[type].find(pkg => pkg.short === name)) {
         console.log(`${name} is not a supported ${type}.`)
         process.exit(0)
         return
     }
 
     const localConfPath = path.join(process.cwd(), 'wdio.conf.js')
-
     if (!fs.existsSync(localConfPath)) {
         try {
-            const { config } = await inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'config',
-                    message: `Error: Could not install ${name} ${type} due to missing configuration. Would you like to create one?`,
-                    default: false
-                }
-            ])
+            const promptMessage = `Cannot install packages without a WebdriverIO configuration.
+You can create one by running 'wdio config'`
 
-            if (!config) {
-                console.log(`
-Cannot install packages without a WebdriverIO configuration.
-You can create one by running 'wdio config'`)
-                process.exit(0)
-            }
-
-            await setup(false)
-        } catch (error) {
-            console.error('Error installing', error)
+            await missingConfigurationPrompt('install', promptMessage, yarn)
+        } catch {
             process.exit(1)
+            return
         }
     }
+
     const configFile = fs.readFileSync(localConfPath, { encoding: 'UTF-8' })
     const match = findInConfig(configFile, type)
 
     if (match && match[0].includes(name)) {
-        console.log(`The ${type} ${name} is already part of your configuration`)
+        console.log(`The ${type} ${name} is already part of your configuration.`)
         process.exit(0)
         return
     }
 
-    const pkgNames = [supportedInstallations[type][name]]
-    addServiceDeps(pkgNames, pkgNames, true)
-    console.log(`Installing ${pkgNames}${npm ? ' using npm.' : '.'}`)
-    const install = yarnInstall({ deps: pkgNames, dev: true, respectNpm5: npm })
+    const selectedPackage = supportedInstallations[type].find(({ short }) => short === name)
+    const pkgsToInstall = [selectedPackage.package]
+
+    addServiceDeps([selectedPackage], pkgsToInstall, true)
+
+    console.log(`Installing "${selectedPackage.package}"${yarn ? ' using yarn.' : '.'}`)
+    const install = yarnInstall({ deps: pkgsToInstall, dev: true, respectNpm5: !yarn }) // use !yarn so the package forces npm install
 
     if (install.status !== 0) {
         console.error('Error installing packages', install.stderr)
         process.exit(1)
+        return
     }
 
-    console.log(`Package ${pkgNames} installed successfully.`)
-    console.log('Updating wdio.conf.js file.')
-
+    console.log(`Package "${selectedPackage.package}" installed successfully.`)
     const newConfig = replaceConfig(configFile, type, name)
-
     fs.writeFileSync(localConfPath, newConfig, { encoding: 'utf-8' })
+    console.log('Your wdio.conf.js file has been updated.')
 
-    console.log('Your wdio.conf.js file has been updated')
     process.exit(0)
 }
 /* eslint-enable no-console */
