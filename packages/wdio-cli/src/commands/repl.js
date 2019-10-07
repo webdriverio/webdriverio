6d5fc7db455bdefdbd8a0a6495f8c13cd1fbe116
Christian Bromann <mail@christian-bromann.com>
2019-10-07 13:16:26 +0200

    Fix sync support for repl command (#4581)

--- packages/wdio-cli/src/commands/repl.js
+++ packages/wdio-cli/src/commands/repl.js
@@ -1,16 +1,18 @@
 import { remote } from 'webdriverio'
+import { hasWdioSyncSupport } from '@wdio/utils'
 
 export const command = 'repl <browserName>'
 export const desc = 'Run WebDriver session in command line'
 
 export const handler = async (argv) => {
     const { browserName } = argv
+    const caps = { capabilities: { browserName } }
 
-    const client = await remote(Object.assign(argv, {
-        capabilities: {
-            browserName
-        }
-    }))
+    /**
+     * runner option required to wrap commands within Fibers context
+     */
+    const execMode = hasWdioSyncSupport ? { runner: 'repl' } : {}
+    const client = await remote({ ...argv, ...caps, ...execMode })
 
     global.$ = ::client.$
     global.$$ = ::client.$$
