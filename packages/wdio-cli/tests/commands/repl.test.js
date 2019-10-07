0164e144cea2d141916b2adbc8a513be1e11f9b6
christian-bromann <mail@christian-bromann.com>
2019-10-07 12:26:40 +0200

    fix sync support for repl

--- packages/wdio-cli/tests/commands/repl.test.js
+++ packages/wdio-cli/tests/commands/repl.test.js
@@ -1,7 +1,24 @@
-import { handler } from './../../src/commands/repl'
 import { remote } from 'webdriverio'
+import { setSyncSupport } from '@wdio/utils'
+
+import { handler } from './../../src/commands/repl'
+
+jest.mock('@wdio/utils', () => {
+    let syncSupport = false
+
+    return {
+        setSyncSupport: (val) => (syncSupport = val),
+        get hasWdioSyncSupport () {
+            return syncSupport
+        }
+    }
+})
 
 describe('Command: repl', () => {
+    beforeEach(() => {
+        remote.mockClear()
+    })
+
     it('should attach global variables', async () => {
         await handler({})
 
@@ -15,6 +32,18 @@ describe('Command: repl', () => {
 
         expect(remote).toHaveBeenCalledWith({
             browserName: 'foobar',
+            capabilities: {
+                browserName: 'foobar'
+            }
+        })
+    })
+
+    it('should set runner if @wdio/sync is installed', async () => {
+        setSyncSupport(true)
+        await handler({ browserName: 'foobar' })
+        expect(remote).toHaveBeenCalledWith({
+            browserName: 'foobar',
+            runner: 'repl',
             capabilities: {
                 browserName: 'foobar'
             }
