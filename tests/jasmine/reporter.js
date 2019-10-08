16fd41c2c7f94ba2f1a5a2b5a6b6505efc960ca7
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-10-08 09:33:43 +0200

    Report errors in beforeAll and afterAll with Jasmine (#4572)
    * wdio-jasmine-reporter: print beforeAll and afterAll errors
    
    * wdio-jasmine-framework: add tests for emitHookEvent
    
    * add jasmine reporter smoke test
    
    * avoid using sync functions in smoke runner

---
+++ tests/jasmine/reporter.js
@@ -0,0 +1,43 @@
+beforeAll(() => { })
+afterAll(() => {
+    throw new Error('afterAll failure in root scope')
+})
+
+// ignored in repoter: https://github.com/webdriverio/webdriverio/issues/4582
+it('root level test', () => {})
+
+describe('Jasmine reporter', () => {
+    beforeAll(() => {
+        throw new Error('beforeAll failure in suite scope')
+    })
+
+    // not shown in reporter
+    beforeEach(() => {})
+
+    // not shown in reporter
+    afterEach(() => {})
+
+    it('should return sync value', () => {
+        expect(browser.getTitle()).toBe('Mock Page Title')
+    })
+
+    it('should fail', () => {
+        expect(browser.getTitle()).toBe('Oh, no!')
+    })
+
+    describe('Jasmine nested suite', () => {
+        afterAll(() => { })
+
+        // not shown in reporter but fails the test
+        beforeEach(() => {
+            throw new Error('beforeEach failure in nested suite')
+        })
+
+        // not shown in reporter but fails the test
+        afterEach(() => {
+            throw new Error('afterEach failure in nested suite')
+        })
+
+        it('nested test failed by each hooks', () => {})
+    })
+})
