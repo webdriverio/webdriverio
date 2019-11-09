0fbf7a238a900d1bb58a119d52dbde12f5e6b829
Arjan Blok <arjan.blok@gmail.com>
2019-11-09 16:39:02 +0100

    Add support for async

--- packages/wdio-applitools-service/applitools-service.d.ts
+++ packages/wdio-applitools-service/applitools-service.d.ts
@@ -1,27 +1,40 @@
 /// <reference types="@applitools/visual-grid-client"/>
 
-declare namespace WebdriverIO {
-    interface Config {
-        applitoolsKey?: string;
-        applitoolsServerUrl?: string;
-        applitools?: {
-            viewport: {
-                width: number;
-                height: number;
-            };
-        };
-    }
-
-    interface Browser {
-        takeSnapshot(title: string): void;
-        takeRegionSnapshot(
-            title: string,
-            region: Eyes.Check.Region | WebdriverIO.Element | string,
-            frame?: WebdriverIO.Element | string
-        ): void;
-    }
+declare module "webdriverio" {
+    interface Config extends ApplitoolsConfig {}
+    interface Browser extends ApplitoolsBrowserAsync {}
 }
 
-declare module "@wdio/applitools-service" {
-    export default WebdriverIO;
+declare module "@wdio/sync" {
+    interface Config extends ApplitoolsConfig {}
+    interface Browser extends ApplitoolsBrowser {}
+}
+
+interface ApplitoolsConfig {
+    applitoolsKey?: string;
+    applitoolsServerUrl?: string;
+    applitools?: {
+        viewport?: {
+            width: number;
+            height: number;
+        };
+    };
+}
+
+interface ApplitoolsBrowser {
+    takeSnapshot(title: string): void;
+    takeRegionSnapshot(
+        title: string,
+        region: Eyes.Check.Region | WebdriverIO.Element | string,
+        frame?: WebdriverIO.Element | string
+    ): void;
+}
+
+interface ApplitoolsBrowserAsync {
+    takeSnapshot(title: string): Promise<void>;
+    takeRegionSnapshot(
+        title: string,
+        region: Eyes.Check.Region | WebdriverIO.Element | string,
+        frame?: WebdriverIO.Element | string
+    ): Promise<void>;
 }
