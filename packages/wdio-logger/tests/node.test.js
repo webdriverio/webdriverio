22a7e39ddd8972cee9bb55d15ff69cb17f515d5e
Jon Mohrbacher <johnnymo87@gmail.com>
2019-11-20 18:04:03 -0500

    Source 2nd copy of code instead from build dir

--- packages/wdio-logger/tests/node.test.js
+++ packages/wdio-logger/tests/node.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import nodeLogger from '../src/node'
-import nodeLogger2 from '../src/nodeCopy'
+import nodeLogger2 from '../build/node'
 
 describe('wdio-logger node', () => {
     describe('log level', () => {
