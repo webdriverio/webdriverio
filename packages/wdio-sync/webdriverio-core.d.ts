9a8315cee9998d310f5453ca79c263a1ee6d6269
John Robinson <john.robinson@banno.com>
2019-11-22 09:24:56 -0500

    fix(webdriverio): broader return types for Element.getProperty (#4797)

--- packages/wdio-sync/webdriverio-core.d.ts
+++ packages/wdio-sync/webdriverio-core.d.ts
@@ -305,7 +305,7 @@ declare namespace WebdriverIO {
          */
         getProperty(
             property: string
-        ): object | string;
+        ): object | string | boolean | number;
 
         /**
          * Get the width and height for an DOM-element.
