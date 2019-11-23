7b1472545d257edc7da51e8cea91f5e2aec0e5ba
Mykola Grybyk <25589559+mgrybyk@users.noreply.github.com>
2019-11-23 18:35:23 +0100

    isClickable shadow root support (#4800)
    * webdriverio: isClickable shadow root
    
    * add unit tests

--- packages/webdriverio/tests/scripts/isElementClickable.test.js
+++ packages/webdriverio/tests/scripts/isElementClickable.test.js
@@ -75,6 +75,33 @@ describe('isElementClickable script', () => {
         expect(isElementClickable(elemMock)).toBe(true)
     })
 
+    it('should be clickable if in viewport and elementFromPoint matches [nested shadowRoot]', () => {
+        const elemMock = {
+            getBoundingClientRect: () => ({
+                height: 55,
+                width: 22,
+                top: 33,
+                left: 455
+            }),
+            clientHeight: 55,
+            clientWidth: 22,
+            getClientRects: () => [{}],
+            scrollIntoView: () => { },
+            contains: () => false
+        }
+        global.document = {
+            elementFromPoint: () => ({
+                shadowRoot: {
+                    elementFromPoint: () => ({
+                        shadowRoot: { elementFromPoint: () => elemMock }
+                    })
+                }
+            })
+        }
+
+        expect(isElementClickable(elemMock)).toBe(true)
+    })
+
     it('should be not clickable if in viewport and elementFromPoint matches but is disabled', () => {
         const elemMock = {
             getBoundingClientRect: () => ({
@@ -110,6 +137,29 @@ describe('isElementClickable script', () => {
             contains: () => false
         }
         global.document = { elementFromPoint: () => null }
+
+        expect(isElementClickable(elemMock)).toBe(false)
+    })
+
+    it("should be not clickable if in viewport but elementFromPoint doesn't match [shadowRoot]", () => {
+        const elemMock = {
+            getBoundingClientRect: () => ({
+                height: 55,
+                width: 22,
+                top: 33,
+                left: 455
+            }),
+            clientHeight: 55,
+            clientWidth: 22,
+            getClientRects: () => [{}],
+            scrollIntoView: () => { },
+            contains: () => false
+        }
+        global.document = {
+            elementFromPoint: () => ({
+                shadowRoot: { elementFromPoint: () => null }
+            })
+        }
 
         expect(isElementClickable(elemMock)).toBe(false)
     })
