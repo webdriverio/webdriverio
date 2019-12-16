10bfc594adf81cc725adb8625ba3110ed3bd5315
christian-bromann <mail@christian-bromann.com>
2019-12-15 19:24:38 -0500

    fix types

--- packages/wdio-shared-store-service/shared-store-service.d.ts
+++ packages/wdio-shared-store-service/shared-store-service.d.ts
@@ -1,6 +1,6 @@
 type jsonPrimitive = string | number | boolean | null;
 type jsonObject = { [x: string]: jsonPrimitive | jsonObject | jsonArray };
-type jsonArray = Array<jsonPrimitive | jsonObject | jsonArrayWorkaround>;
+type jsonArray = Array<jsonPrimitive | jsonObject>;
 type jsonCompatible = jsonPrimitive | jsonObject | jsonArray;
 
 declare namespace WebdriverIO {
