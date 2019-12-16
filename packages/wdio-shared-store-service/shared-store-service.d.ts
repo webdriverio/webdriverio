675f59211f655c51871b3e9c62b690133765e40d
Christian Bromann <mail@christian-bromann.com>
2019-12-16 14:49:37 +0100

    Update minimal TypeScript version to 3.7.2 or higher (#4740)
    * update typescript to v3.7.3
    
    * fix types
    
    * add circular reference for jsonArray

--- packages/wdio-shared-store-service/shared-store-service.d.ts
+++ packages/wdio-shared-store-service/shared-store-service.d.ts
@@ -1,15 +1,7 @@
 type jsonPrimitive = string | number | boolean | null;
 type jsonObject = { [x: string]: jsonPrimitive | jsonObject | jsonArray };
-type jsonArray = Array<jsonPrimitive | jsonObject | jsonArrayWorkaround>;
+type jsonArray = Array<jsonPrimitive | jsonObject | jsonArray>;
 type jsonCompatible = jsonPrimitive | jsonObject | jsonArray;
-
-/**
- * circular reference workaround until TypeScript 3.7
- * https://github.com/microsoft/TypeScript/pull/33050
- * `jsonArray` should repplace `jsonArrayWorkaround` in `jsonArray` array types
- * and interface below can be removed
- */
-interface jsonArrayWorkaround extends Array<jsonPrimitive | jsonObject | jsonArray> { }
 
 declare namespace WebdriverIO {
     interface BrowserObject {
