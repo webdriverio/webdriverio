6aa6a793ff1e0e58c7ebc35c7722f46d51157986
Baruch Velez <baruchvelez@gmail.com>
2019-12-22 14:04:29 +0100

    wdio-cli: allow for custom path for WDIO config

--- packages/wdio-cli/tests/commands/install.test.js
+++ packages/wdio-cli/tests/commands/install.test.js
@@ -29,14 +29,14 @@ describe('Command: install', () => {
     })
 
     it('should log out when an unknown installation type is passed', async () => {
-        await installCmd.handler({ type: 'foobar' })
+        await installCmd.handler({ type: 'foobar', config: './wdio.conf.js' })
 
         expect(console.log).toHaveBeenCalled()
         expect(console.log).toHaveBeenCalledWith('Type foobar is not supported.')
     })
 
     it('should log out when unknown package name is used', async () => {
-        await installCmd.handler({ type: 'service', name: 'foobar' })
+        await installCmd.handler({ type: 'service', name: 'foobar', config: './wdio.conf.js' })
 
         expect(console.log).toHaveBeenCalled()
         expect(console.log).toHaveBeenCalledWith('foobar is not a supported service.')
@@ -46,7 +46,7 @@ describe('Command: install', () => {
         jest.spyOn(utils, 'missingConfigurationPrompt').mockImplementation(() => Promise.reject())
         fs.existsSync.mockReturnValue(false)
 
-        await installCmd.handler({ type: 'service', name: 'chromedriver' })
+        await installCmd.handler({ type: 'service', name: 'chromedriver', config: './wdio.conf.js' })
 
         expect(utils.missingConfigurationPrompt).toHaveBeenCalledWith('install', `Cannot install packages without a WebdriverIO configuration.
 You can create one by running 'wdio config'`, undefined)
@@ -56,7 +56,7 @@ You can create one by running 'wdio config'`, undefined)
         findInConfigMock.mockReturnValue(['chromedriver'])
         fs.existsSync.mockReturnValue(true)
 
-        await installCmd.handler({ type: 'service', name: 'chromedriver' })
+        await installCmd.handler({ type: 'service', name: 'chromedriver', config: './wdio.conf.js' })
 
         expect(console.log).toHaveBeenCalledWith('The service chromedriver is already part of your configuration.')
     })
@@ -67,7 +67,22 @@ You can create one by running 'wdio config'`, undefined)
         fs.readFileSync.mockReturnValue('module.config = {}')
         yarnInstall.mockImplementation(() => ({ status: 0 }))
 
-        await installCmd.handler({ type: 'service', name: 'chromedriver' })
+        await installCmd.handler({ type: 'service', name: 'chromedriver', config: './wdio.conf.js' })
+
+        expect(console.log).toHaveBeenCalledWith('Installing "wdio-chromedriver-service".')
+        expect(console.log).toHaveBeenCalledWith('Package "wdio-chromedriver-service" installed successfully.')
+        expect(utils.replaceConfig).toHaveBeenCalled()
+        expect(fs.writeFileSync).toHaveBeenCalled()
+        expect(console.log).toHaveBeenCalledWith('Your wdio.conf.js file has been updated.')
+        expect(process.exit).toHaveBeenCalledWith(0)
+    })
+    it('allows for custom config location', async () => {
+        findInConfigMock.mockReturnValue([''])
+        fs.existsSync.mockReturnValue(true)
+        fs.readFileSync.mockReturnValue('module.config = {}')
+        yarnInstall.mockImplementation(() => ({ status: 0 }))
+
+        await installCmd.handler({ type: 'service', name: 'chromedriver', config: './path/to/wdio.conf.js' })
 
         expect(console.log).toHaveBeenCalledWith('Installing "wdio-chromedriver-service".')
         expect(console.log).toHaveBeenCalledWith('Package "wdio-chromedriver-service" installed successfully.')
@@ -83,7 +98,7 @@ You can create one by running 'wdio config'`, undefined)
         yarnInstall.mockImplementation(() => ({ status: 1, stderr: 'test error' }))
         jest.spyOn(console, 'error')
 
-        await installCmd.handler({ type: 'service', name: 'chromedriver' })
+        await installCmd.handler({ type: 'service', name: 'chromedriver', config: './wdio.conf.js' })
 
         expect(console.error).toHaveBeenCalledWith('Error installing packages', 'test error')
         expect(process.exit).toHaveBeenCalledWith(1)
