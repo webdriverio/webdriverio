04eef2127e7964c46a89747ffb5357057d6fa716
Mike Salvia <msalvia@jwplayer.com>
2019-12-29 05:39:27 -0500

    💥🚀 Cucumber-style JUnit reports in @wdio/junit-reporter (#4878)
    * Creates Cucumber-styling for JUnit Reporter (@wdio/junit-reporter)
    
    * Updating current tests for original styling
    
    * Adding tests for Cucumber style Junit reporting
    
    * Accept suggested change for let to const
    
    Co-Authored-By: Christian Bromann <github@christian-bromann.com>
    
    * Use toMatchSnapshot
    
    * Additional unit test for Mocha-style reporting
    
    * Additional fixes from feedback received
    
    Co-authored-by: Christian Bromann <github@christian-bromann.com>

--- packages/wdio-junit-reporter/src/index.js
+++ packages/wdio-junit-reporter/src/index.js
@@ -17,7 +17,7 @@ class JunitReporter extends WDIOReporter {
     }
 
     onRunnerEnd (runner) {
-        const xml = this.prepareXml(runner)
+        const xml = this.buildJunitXml(runner)
         this.write(xml)
     }
 
@@ -27,11 +27,148 @@ class JunitReporter extends WDIOReporter {
         ).join('_')
     }
 
-    prepareXml (runner) {
-        const builder = junit.newBuilder()
-        const packageName = this.options.packageName
-            ? `${runner.sanitizedCapabilities}-${this.options.packageName}`
-            : runner.sanitizedCapabilities
+    addFailedHooks(suite) {
+        /**
+         * Add failed before and after all hooks to suite as tests.
+         */
+        const failedHooks = suite.hooks.filter(hook => hook.error && (hook.title === '"before all" hook' || hook.title === '"after all" hook'))
+        failedHooks.forEach(hook => {
+            const { title, _duration, error, state } = hook
+            suite.tests.push({
+                _duration,
+                title,
+                error,
+                state,
+                output: []
+            })
+        })
+        return suite
+    }
+
+    addCucumberFeatureToBuilder(builder, runner, specFileName, suite) {
+        const featureName = this.prepareName(suite.title)
+        if (suite.type === 'feature') {
+            const feature = builder.testSuite()
+                .name(featureName)
+                .timestamp(suite.start)
+                .time(suite._duration / 1000)
+                .property('specId', 0)
+                .property(this.suiteTitleLabel, suite.title)
+                .property('capabilities', runner.sanitizedCapabilities)
+                .property(this.fileNameLabel, specFileName.replace(process.cwd(), '.'))
+            this.activeFeature = feature
+            this.activeFeatureName = featureName
+        } else if (this.activeFeature) {
+            let scenario = suite
+            const testName = this.prepareName(suite.title)
+
+            const testCase = this.activeFeature.testCase()
+                .className(`${this.packageName}.${this.activeFeatureName}`)
+                .name(`${this.activeFeatureName}.${testName}`)
+                .time(scenario._duration / 1000)
+
+            scenario = this.addFailedHooks(scenario)
+
+            let stepsOutput = ''
+            let isFailing = false
+            for (let stepKey of Object.keys(scenario.tests)) { // tests are trested as steps in Cucumber
+                if (stepKey !== 'undefined') { // fix cucumber hooks crashing reporter
+                    let stepEmoji = '✅'
+                    const step = scenario.tests[stepKey]
+                    if (step.state === 'pending' || step.state === 'skipped') {
+                        if (!isFailing) {
+                            testCase.skipped()
+                        }
+                        stepEmoji = '⚠️'
+                    } else if (step.state === 'failed') {
+                        if (step.error) {
+                            if (this.options.errorOptions) {
+                                const errorOptions = this.options.errorOptions
+                                for (const key of Object.keys(errorOptions)) {
+                                    testCase[key](step.error[errorOptions[key]])
+                                }
+                            } else {
+                                // default
+                                testCase.error(step.error.message)
+                            }
+                            testCase.standardError(`\n${step.error.stack}\n`)
+                        } else {
+                            testCase.error()
+                        }
+                        isFailing = true
+                        stepEmoji = '❗'
+                    }
+                    const output = this.getStandardOutput(step)
+                    stepsOutput += output ? stepEmoji + ' ' + step.title : stepEmoji + ' ' + step.title + '\n' + output
+                }
+            }
+            testCase.standardOutput(`\n${stepsOutput}\n`)
+        }
+        return builder
+    }
+
+    addSuiteToBuilder(builder, runner, specFileName, suite) {
+        const suiteName = this.prepareName(suite.title)
+        let testSuite = builder.testSuite()
+            .name(suiteName)
+            .timestamp(suite.start)
+            .time(suite._duration / 1000)
+            .property('specId', 0)
+            .property(this.suiteTitleLabel, suite.title)
+            .property('capabilities', runner.sanitizedCapabilities)
+            .property(this.fileNameLabel, specFileName.replace(process.cwd(), '.'))
+
+        suite = this.addFailedHooks(suite)
+
+        for (let testKey of Object.keys(suite.tests)) {
+            if (testKey !== 'undefined') { // fix cucumber hooks crashing reporter (INFO: we may not need this anymore)
+                const test = suite.tests[testKey]
+                const testName = this.prepareName(test.title)
+                const testCase = testSuite.testCase()
+                    .className(`${this.packageName}.${suiteName}`)
+                    .name(testName)
+                    .time(test._duration / 1000)
+
+                if (test.state === 'pending' || test.state === 'skipped') {
+                    testCase.skipped()
+                } else if (test.state === 'failed') {
+                    if (test.error) {
+                        if (this.options.errorOptions) {
+                            const errorOptions = this.options.errorOptions
+                            for (const key of Object.keys(errorOptions)) {
+                                testCase[key](test.error[errorOptions[key]])
+                            }
+                        } else {
+                            // default
+                            testCase.error(test.error.message)
+                        }
+                        testCase.standardError(`\n${test.error.stack}\n`)
+                    } else {
+                        testCase.error()
+                    }
+                }
+
+                const output = this.getStandardOutput(test)
+                if (output) testCase.standardOutput(`\n${output}\n`)
+            }
+        }
+        return builder
+    }
+
+    buildJunitXml (runner) {
+        let builder = junit.newBuilder()
+
+        this.packageName = this.options.packageName ? `${runner.sanitizedCapabilities}-${this.options.packageName}` : runner.sanitizedCapabilities
+
+        this.isCucumberFrameworkRunner = runner.config.framework === 'cucumber'
+        if (this.isCucumberFrameworkRunner) {
+            this.packageName = `CucumberJUnitReport-${this.packageName}`
+            this.suiteTitleLabel = 'featureName'
+            this.fileNameLabel = 'featureFile'
+        } else {
+            this.suiteTitleLabel = 'suiteName'
+            this.fileNameLabel = 'file'
+        }
 
         for (let suiteKey of Object.keys(this.suites)) {
             /**
@@ -45,62 +182,11 @@ class JunitReporter extends WDIOReporter {
             // there should only be one spec file per runner so we can safely take the first element of the array
             const specFileName = runner.specs[0]
             const suite = this.suites[suiteKey]
-            const suiteName = this.prepareName(suite.title)
-            const testSuite = builder.testSuite()
-                .name(suiteName)
-                .timestamp(suite.start)
-                .time(suite._duration / 1000)
-                .property('specId', 0)
-                .property('suiteName', suite.title)
-                .property('capabilities', runner.sanitizedCapabilities)
-                .property('file', specFileName.replace(process.cwd(), '.'))
 
-            /**
-             * Add failed before and after all hooks to suite as tests.
-             */
-            const failedHooks = suite.hooks.filter(hook => hook.error && (hook.title === '"before all" hook' || hook.title === '"after all" hook'))
-            failedHooks.forEach(hook => {
-                const { title, _duration, error, state } = hook
-                suite.tests.push({
-                    _duration,
-                    title,
-                    error,
-                    state,
-                    output: []
-                })
-            })
-
-            for (let testKey of Object.keys(suite.tests)) {
-                if (testKey !== 'undefined') { // fix cucumber hooks crashing reporter
-                    const test = suite.tests[testKey]
-                    const testName = this.prepareName(test.title)
-                    const testCase = testSuite.testCase()
-                        .className(`${packageName}.${suiteName}`)
-                        .name(testName)
-                        .time(test._duration / 1000)
-
-                    if (test.state === 'pending' || test.state === 'skipped') {
-                        testCase.skipped()
-                    } else if (test.state === 'failed') {
-                        if (test.error) {
-                            if (this.options.errorOptions) {
-                                const errorOptions = this.options.errorOptions
-                                for (const key of Object.keys(errorOptions)) {
-                                    testCase[key](test.error[errorOptions[key]])
-                                }
-                            } else {
-                                // default
-                                testCase.error(test.error.message)
-                            }
-                            testCase.standardError(`\n${test.error.stack}\n`)
-                        } else {
-                            testCase.error()
-                        }
-                    }
-
-                    const output = this.getStandardOutput(test)
-                    if (output) testCase.standardOutput(`\n${output}\n`)
-                }
+            if (this.isCucumberFrameworkRunner) {
+                builder = this.addCucumberFeatureToBuilder(builder, runner, specFileName, suite)
+            } else {
+                builder = this.addSuiteToBuilder(builder, runner, specFileName, suite)
             }
         }
 
