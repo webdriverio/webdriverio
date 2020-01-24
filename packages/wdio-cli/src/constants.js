a78663b41207ec897139f5250211572541eeff78
Christian Maniewski <code@chmanie.com>
2020-01-24 17:48:27 +0100

    wdio-cli: add intercept service to wdio

--- packages/wdio-cli/src/constants.js
+++ packages/wdio-cli/src/constants.js
@@ -84,7 +84,8 @@ export const SUPPORTED_PACKAGES = {
         { name: 'docker', value: 'wdio-docker-service$--$docker' },
         { name: 'wiremock', value: 'wdio-wiremock-service$--$wiremock' },
         { name: 'slack', value: 'wdio-slack-service$--$slack' },
-    ],
+        { name: 'intercept', value: 'wdio-intercept-service$--$intercept' }
+    ]
 }
 
 export const QUESTIONNAIRE = [{
