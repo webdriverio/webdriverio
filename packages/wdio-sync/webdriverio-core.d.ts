7b4205e30cef34ea1c0fd47b87b9ef7b91a5fa83
christian-bromann <mail@christian-bromann.com>
2020-02-11 12:44:56 +0100

    generated typings

--- packages/wdio-sync/webdriverio-core.d.ts
+++ packages/wdio-sync/webdriverio-core.d.ts
@@ -212,6 +212,12 @@ declare namespace WebdriverIO {
         windowFeatures?: string
     }
 
+    type ClickOptions = {
+        button?: number | string,
+        x?: number,
+        y?: number
+    }
+
     interface Element {
         selector: string;
         elementId: string;
@@ -244,7 +250,7 @@ declare namespace WebdriverIO {
             name: string,
             func: Function
         ): void;
-
+        
         /**
          * The `$$` command is a short way to call the [`findElements`](/docs/api/webdriver.html#findelements) command in order
          * to fetch multiple elements on the page similar to the `$$` command from the browser scope. The difference when calling
@@ -287,7 +293,7 @@ declare namespace WebdriverIO {
          * Click on an element.
          */
         click(
-            options?: object
+            options?: ClickOptions
         ): void;
 
         /**
@@ -617,7 +623,7 @@ declare namespace WebdriverIO {
             name: string,
             func: (elementFetchingMethod: (selector: string) => any) => void
         ): void
-
+        
         /**
          * The `$$` command is a short way to call the [`findElements`](/docs/api/webdriver.html#findelements) command in order
          * to fetch multiple elements on the page. It returns an array with element results that will have an
