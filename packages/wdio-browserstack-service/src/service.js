8b86f2bc6b18ed447cb9520c7a3c92618016be0a
stezu <stephen.zuniga001@gmail.com>
2020-03-16 17:14:47 -0400

    fix(browserstack): update test title

--- packages/wdio-browserstack-service/src/service.js
+++ packages/wdio-browserstack-service/src/service.js
@@ -46,7 +46,7 @@ export default class BrowserstackService {
     }
 
     afterTest(test) {
-        this.fullTitle = test.parent + ' - ' + test.title
+        this.fullTitle = test.fullTitle
 
         if (!test.passed) {
             this.failures++
