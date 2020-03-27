db75e1239c4958b7593b0ff22acd75827bd596ec
christian-bromann <mail@christian-bromann.com>
2020-03-27 11:57:21 +0100

    fix got type tests - closes #5157

--- tests/typings/copy.js
+++ tests/typings/copy.js
@@ -1,6 +1,10 @@
-const rimraf = require('rimraf')
-const copyfiles = require('copyfiles')
+const fs = require('fs')
 const path = require('path')
+
+const { ln, mkdir } = require('shelljs')
+const rimraf = require('rimraf')
+
+const ROOT = path.resolve(__dirname, '..', '..')
 
 // TypeScript project root for testing particular typings
 const outDirs = [
@@ -16,31 +20,14 @@ const packages = {
     'webdriverio': 'packages/webdriverio',
     '@wdio/allure-reporter': 'packages/wdio-allure-reporter',
     '@wdio/reporter': 'packages/wdio-reporter',
-
-    '@applitools/visual-grid-client': 'packages/wdio-applitools-service/node_modules/@applitools/visual-grid-client',
     '@wdio/applitools-service': 'packages/wdio-applitools-service',
-
-    'browserstack-local': 'packages/wdio-browserstack-service/node_modules/browserstack-local',
     '@wdio/browserstack-service': 'packages/wdio-browserstack-service',
     '@wdio/sauce-service': 'packages/wdio-sauce-service',
-
-    '@types/got': 'packages/webdriver/node_modules/@types/got',
-    '@types/tough-cookie': 'packages/webdriver/node_modules/@types/tough-cookie',
-
-    '@types/mocha': 'packages/wdio-mocha-framework/node_modules/@types/mocha',
     '@wdio/mocha-framework': 'packages/wdio-mocha-framework',
-
-    '@types/cucumber': 'packages/wdio-cucumber-framework/node_modules/@types/cucumber',
     '@wdio/cucumber-framework': 'packages/wdio-cucumber-framework',
-
-    '@types/jasmine': 'packages/wdio-jasmine-framework/node_modules/@types/jasmine',
-    '@types/jasmine/ts3.1': 'packages/wdio-jasmine-framework/node_modules/@types/jasmine/ts3.1',
     '@wdio/jasmine-framework': 'packages/wdio-jasmine-framework',
-
-    '@types/selenium-standalone': 'packages/wdio-selenium-standalone-service/node_modules/@types/selenium-standalone',
     '@wdio/selenium-standalone-service': 'packages/wdio-selenium-standalone-service',
-
-    '@wdio/shared-store-service': 'packages/wdio-shared-store-service',
+    '@wdio/shared-store-service': 'packages/wdio-shared-store-service'
 }
 
 /**
@@ -49,12 +36,15 @@ const packages = {
 async function copy() {
     for (const outDir of outDirs) {
         for (const packageName of Object.keys(packages)) {
-            const packageDir = packages[packageName]
-            const packageJson = path.join(packageDir, 'package.json')
-            const typings = path.join(packageDir, '*.d.ts')
             const destination = path.join(__dirname, outDir, 'node_modules', packageName)
+            const packageDir = packages[packageName]
 
-            await new Promise(resolve => copyfiles([packageJson, typings, destination], { up: packageDir.split('/').length }, resolve))
+            const destDir = destination.split('/').slice(0, -1).join('/')
+            if (!fs.existsSync(destDir)) {
+                mkdir('-p', destDir)
+            }
+
+            ln('-s', path.join(ROOT, packageDir), destination)
         }
     }
 }
