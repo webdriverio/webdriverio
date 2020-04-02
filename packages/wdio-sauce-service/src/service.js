74cbe7593e1f9d04d8fcd42a67dab6f5854e3a98
Dylan Lacey <github@dylanlacey.com>
2020-04-02 17:45:26 +1000

    W3C sauce metadata (#5200)
    * Preference W3C user metadata when sauce service updates jobs.
    
    Current behaviour is for user-supplied metadata from desired capabilities to
    supercede derived metadata.  This change makes that work for both JSON-WP and
    W3C Compliant metadata, eg, that under the `sauce:options` namespace.
    
    * Add tests for W3C sauce:options metadata
    
    * Update README and follow Linter

--- packages/wdio-sauce-service/src/service.js
+++ packages/wdio-sauce-service/src/service.js
@@ -196,12 +196,14 @@ export default class SauceService {
             body.name += ` (${testCnt})`
         }
 
+        let caps = this.capabilities['sauce:options'] || this.capabilities
+
         for (let prop of jobDataProperties) {
-            if (!this.capabilities[prop]) {
+            if (!caps[prop]) {
                 continue
             }
 
-            body[prop] = this.capabilities[prop]
+            body[prop] = caps[prop]
         }
 
         body.passed = failures === 0
