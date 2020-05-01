08bdb537d4bd776c6fac1f65cacb7c0c3f73ec50
Ricardo Barbosa <1101461@isep.ipp.pt>
2020-05-01 22:42:48 +0200

    Enable file uploads for input elements

--- packages/devtools/src/commands/elementSendKeys.js
+++ packages/devtools/src/commands/elementSendKeys.js
@@ -1,4 +1,5 @@
 import { getStaleElementError } from '../utils'
+import path from "path"
 
 export default async function elementSendKeys ({ elementId, text }) {
     const elementHandle = this.elementStore.get(elementId)
@@ -7,9 +8,15 @@ export default async function elementSendKeys ({ elementId, text }) {
         throw getStaleElementError(elementId)
     }
 
-    await elementHandle.focus()
-    const page = this.getPageHandle()
-    await page.keyboard.type(text)
+    const tagName = await (await elementHandle.getProperty('tagName')).jsonValue()
+    const type = await (await elementHandle.getProperty('type')).jsonValue()
+
+    if(tagName === 'INPUT' && type === 'file'){
+        const paths = (text || '').split('\n').map(p => path.resolve(p))
+        await elementHandle.uploadFile(...paths)
+    } else {
+        await page.keyboard.type(text)
+    }
 
     return null
 }
