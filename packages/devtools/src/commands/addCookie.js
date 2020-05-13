3d2fe03e12986a6e440e234b6d7592a5eccd7281
thescripted <somethingscripted@gmail.com>
2020-05-13 18:27:31 -0500

    added documentations

--- packages/devtools/src/commands/addCookie.js
+++ packages/devtools/src/commands/addCookie.js
@@ -9,16 +9,16 @@ export default async function addCookie({ cookie }) {
     const page = this.windows.get(this.currentWindowHandle);
 
     const cookieProps = Object.keys(cookie);
-    if (!cookieProps.includes("name") || !cookieProps.includes("value")) {
+    if (!cookieProps.includes('name') || !cookieProps.includes('value')) {
         throw new Error(
             'Provided cookie object is missing either "name" or "value" property'
         );
     }
 
-    if (typeof cookie.value !== "string") {
-        cookie.value = cookie.value.toString();
+    if (typeof cookie.value !== 'string') {
+        cookie.value = cookie.value.toString()
     }
 
-    await page.setCookie(cookie);
-    return null;
+    await page.setCookie(cookie)
+    return null
 }
