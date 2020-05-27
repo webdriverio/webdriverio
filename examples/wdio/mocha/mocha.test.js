781764aeaff2d76d35017f40732dbf5f42f1e506
christian-bromann <mail@christian-bromann.com>
2020-05-27 15:09:26 +0200

    fix example

--- examples/wdio/mocha/mocha.test.js
+++ examples/wdio/mocha/mocha.test.js
@@ -2,14 +2,7 @@ describe('webdriver.io page', () => {
     it('should be a pending test')
 
     it('should have the right title', () => {
-        browser.url("https://ozon.ru");
-        for (let i = 0; i < 50; i++) {
-            let el = $('[data-test-id="header-search-input"]');
-            el.click();
-            el.setValue('Рыба');
-            $('[data-test-id="header-search-go"]').click();
-            console.log(`request loop: ${i}`);
-            expect(browser.getUrl()).to.contain('https://www.ozon.ru/search/?text=');
-        }
+        browser.url('https://webdriver.io')
+        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js')
     })
 })
