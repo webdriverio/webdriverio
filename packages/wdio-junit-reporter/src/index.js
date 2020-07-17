9854dea2ef436534744a8c1f6fd4653eba654bbb
Nick <i4nightmare@gmail.com>
2020-07-17 12:29:19 +0300

    Fix wdio-junit-reporter runner.config.hostname bug (#5631)

--- packages/wdio-junit-reporter/src/index.js
+++ packages/wdio-junit-reporter/src/index.js
@@ -169,7 +169,7 @@ class JunitReporter extends WDIOReporter {
 
     buildJunitXml (runner) {
         let builder = junit.newBuilder()
-        if (runner.config.hostname.indexOf('browserstack') > -1) {
+        if (runner.config.hostname !== undefined && runner.config.hostname.indexOf('browserstack') > -1) {
             // NOTE: deviceUUID is used to build sanitizedCapabilities resulting in a ever-changing package name in runner.sanitizedCapabilities when running Android tests under Browserstack. (i.e. ht79v1a03938.android.9)
             // NOTE: platformVersion is used to build sanitizedCapabilities which can be incorrect and includes a minor version for iOS which is not guaranteed to be the same under Browserstack.
             const browserstackSanitizedCapabilities = (runner.capabilities.device.toLowerCase() + '.' + runner.capabilities.os.toLowerCase() + '.' + runner.capabilities.os_version.replace(/\./g, '_')).replace(/ /g, '')
