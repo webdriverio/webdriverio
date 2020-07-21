5dd0211c69b352ca4cf2b9511d93149c2427f68f
Nick Chursin <nicktestqa@yahoo.com>
2020-07-21 10:53:13 +0300

    Code format

--- packages/wdio-junit-reporter/src/index.js
+++ packages/wdio-junit-reporter/src/index.js
@@ -172,18 +172,15 @@ class JunitReporter extends WDIOReporter {
         if (runner.config.hostname !== undefined && runner.config.hostname.indexOf('browserstack') > -1) {
             // NOTE: deviceUUID is used to build sanitizedCapabilities resulting in a ever-changing package name in runner.sanitizedCapabilities when running Android tests under Browserstack. (i.e. ht79v1a03938.android.9)
             // NOTE: platformVersion is used to build sanitizedCapabilities which can be incorrect and includes a minor version for iOS which is not guaranteed to be the same under Browserstack.
-            let browserstackSanitizedCapabilities = [
+            const browserstackSanitizedCapabilities = [
                 runner.capabilities.device,
                 runner.capabilities.os,
-                (runner.capabilities.os_version || "").replace(/\./g, "_"),
+                (runner.capabilities.os_version || '').replace(/\./g, '_'),
             ]
                 .filter(Boolean)
                 .map((capability) => capability.toLowerCase())
-                .join(".")
-                .replace(/ /g, "");
-            if (!browserstackSanitizedCapabilities) {
-                browserstackSanitizedCapabilities = runner.sanitizedCapabilities;
-            }
+                .join('.')
+                .replace(/ /g, '') || runner.sanitizedCapabilities
             this.packageName = this.options.packageName ? `${browserstackSanitizedCapabilities}-${this.options.packageName}` : browserstackSanitizedCapabilities
         } else {
             this.packageName = this.options.packageName ? `${runner.sanitizedCapabilities}-${this.options.packageName}` : runner.sanitizedCapabilities
