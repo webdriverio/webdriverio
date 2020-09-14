0571a63f6bd7fdf4758c0d9f74cf9018291644f4
christian-bromann <mail@christian-bromann.com>
2020-09-14 14:33:21 +0200

    clean up

--- scripts/build.js
+++ scripts/build.js
@@ -8,6 +8,7 @@ const { getSubPackages } = require('./utils/helpers')
 const IGNORE_COMPILING_FOR_PACKAGES = ['eslint-plugin-wdio', 'wdio-protocols']
 const args = process.argv.slice(2)
 const HAS_WATCH_FLAG = args[0] === '--watch'
+const TSCONFIG_FILE = process.env.NODE_ENV === 'production' ? 'tsconfig.prod.json' : 'tsconfig.json'
 
 if (HAS_WATCH_FLAG) {
     args.shift()
@@ -24,7 +25,7 @@ const packages = getSubPackages()
     .filter((pkg) => args.length === 0 || args.includes(pkg))
 
 shell.cd(path.join(__dirname, '..'))
-const cmd = `npx tsc -b ${packages.map((pkg) => `packages/${pkg}`).join(' ')}${HAS_WATCH_FLAG ? ' --watch' : ''}`
+const cmd = `npx tsc -b ${packages.map((pkg) => `packages/${pkg}/${TSCONFIG_FILE}`).join(' ')}${HAS_WATCH_FLAG ? ' --watch' : ''}`
 
 console.log(cmd)
 shell.exec(cmd)
