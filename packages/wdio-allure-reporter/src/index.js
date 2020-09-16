5b59b0dabb06cf0a227cda6d02c2224339b6016b
Jags <jags14385@users.noreply.github.com>
2020-09-17 07:53:43 +1000

    Fixes the issue On V6, for android tests, where the incorrect deviceName is displayed in allure reports (#5870)

--- packages/wdio-allure-reporter/src/index.js
+++ packages/wdio-allure-reporter/src/index.js
@@ -110,8 +110,12 @@ class AllureReporter extends WDIOReporter {
         const currentTest = this.allure.getCurrentTest()
 
         if (!this.isMultiremote) {
-            const { browserName, deviceName } = this.capabilities
-            const targetName = browserName || deviceName || cid
+            const { browserName, deviceName, desired } = this.capabilities
+            let targetName = browserName || deviceName || cid
+            // custom mobile grids can have device information in a `desired` cap
+            if (desired && desired.deviceName && desired.platformVersion) {
+                targetName = `${desired.deviceName} ${desired.platformVersion}`
+            }
             const browserstackVersion = this.capabilities.os_version || this.capabilities.osVersion
             const version = browserstackVersion || this.capabilities.browserVersion || this.capabilities.version || this.capabilities.platformVersion || ''
             const paramName = deviceName ? 'device' : 'browser'
