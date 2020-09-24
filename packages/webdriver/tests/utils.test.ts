0b6f207ca612d5ab729481bb434a69382a960442
christian-bromann <mail@christian-bromann.com>
2020-09-24 09:05:17 +0200

    rewrite utils test

--- packages/webdriver/tests/utils.test.ts
+++ packages/webdriver/tests/utils.test.ts
@@ -1,19 +1,18 @@
+import { Options, DesiredCapabilities } from '../src/types'
 import {
-    isSuccessfulResponse, getPrototype, setupDirectConnect, getSessionError,
+    isSuccessfulResponse, getPrototype, getSessionError,
     getErrorFromResponseBody, CustomRequestError, startWebDriverSession
 } from '../src/utils'
 
 describe('utils', () => {
     it('isSuccessfulResponse', () => {
-        expect(isSuccessfulResponse()).toBe(false)
-        expect(isSuccessfulResponse(200)).toBe(false)
         expect(isSuccessfulResponse(200, { value: { some: 'result' } })).toBe(true)
         expect(isSuccessfulResponse(404, { value: { error: new Error('foobar' ) } })).toBe(false)
         expect(isSuccessfulResponse(404, { value: { error: 'no such element' } })).toBe(true)
         expect(isSuccessfulResponse(404, { value: {
             message: 'An element could not be located on the page using the given search parameters.' }
         })).toBe(true)
-        expect(isSuccessfulResponse(200, { status: 7 })).toBe(false)
+        expect(isSuccessfulResponse(200, { status: 7, value: {} })).toBe(false)
         expect(isSuccessfulResponse(undefined, { status: 7, value: {} })).toBe(false)
         expect(isSuccessfulResponse(undefined, { status: 0, value: {} })).toBe(true)
         expect(isSuccessfulResponse(
@@ -27,34 +26,52 @@ describe('utils', () => {
     })
 
     it('getPrototype', () => {
-        const jsonWireProtocolPrototype = getPrototype({})
+        const isW3C = false
+        const isChrome = false
+        const isMobile = false
+        const isSauce = false
+        const isIOS = false
+        const isAndroid = false
+        const isSeleniumStandalone = false
+
+        const jsonWireProtocolPrototype = getPrototype({
+            isW3C, isChrome, isMobile, isSauce, isSeleniumStandalone, isIOS, isAndroid
+        })
         expect(jsonWireProtocolPrototype instanceof Object).toBe(true)
         expect(typeof jsonWireProtocolPrototype.sendKeys.value).toBe('function')
         expect(typeof jsonWireProtocolPrototype.sendCommand).toBe('undefined')
         expect(typeof jsonWireProtocolPrototype.lock).toBe('undefined')
 
-        const webdriverPrototype = getPrototype({ isW3C: true })
+        const webdriverPrototype = getPrototype({
+            isW3C: true, isChrome, isMobile, isSauce, isSeleniumStandalone, isIOS, isAndroid
+        })
         expect(webdriverPrototype instanceof Object).toBe(true)
         expect(typeof webdriverPrototype.sendKeys).toBe('undefined')
         expect(typeof webdriverPrototype.sendCommand).toBe('undefined')
         expect(typeof webdriverPrototype.performActions.value).toBe('function')
         expect(typeof webdriverPrototype.lock).toBe('undefined')
 
-        const chromiumPrototype = getPrototype({ isW3C: false, isChrome: true })
+        const chromiumPrototype = getPrototype({
+            isW3C: false, isChrome: true, isMobile, isSauce, isSeleniumStandalone, isIOS, isAndroid
+        })
         expect(chromiumPrototype instanceof Object).toBe(true)
         expect(typeof chromiumPrototype.sendCommand.value).toBe('function')
         expect(typeof chromiumPrototype.getElementValue.value).toBe('function')
         expect(typeof chromiumPrototype.elementSendKeys.value).toBe('function')
         expect(typeof chromiumPrototype.lock).toBe('undefined')
 
-        const mobilePrototype = getPrototype({ isW3C: true, isChrome: false, isMobile: true })
+        const mobilePrototype = getPrototype({
+            isW3C: true, isChrome: false, isMobile: true, isSauce, isSeleniumStandalone, isIOS, isAndroid
+        })
         expect(mobilePrototype instanceof Object).toBe(true)
         expect(typeof mobilePrototype.performActions.value).toBe('function')
         expect(typeof mobilePrototype.sendKeys.value).toBe('function')
         expect(typeof mobilePrototype.lock.value).toBe('function')
         expect(typeof mobilePrototype.getNetworkConnection.value).toBe('function')
 
-        const mobileChromePrototype = getPrototype({ isW3C: true, isChrome: true, isMobile: true })
+        const mobileChromePrototype = getPrototype({
+            isW3C: true, isChrome: true, isMobile: true, isSauce, isSeleniumStandalone, isIOS, isAndroid
+        })
         expect(mobileChromePrototype instanceof Object).toBe(true)
         expect(typeof mobileChromePrototype.sendCommand.value).toBe('function')
         expect(typeof mobileChromePrototype.performActions.value).toBe('function')
@@ -62,14 +79,15 @@ describe('utils', () => {
         expect(typeof mobileChromePrototype.lock.value).toBe('function')
         expect(typeof mobileChromePrototype.getNetworkConnection.value).toBe('function')
 
-        const saucePrototype = getPrototype({ isW3C: true, isSauce: true })
+        const saucePrototype = getPrototype({
+            isW3C: true, isChrome, isMobile, isSauce: true, isSeleniumStandalone, isIOS, isAndroid
+        })
         expect(saucePrototype instanceof Object).toBe(true)
         expect(typeof saucePrototype.getPageLogs.value).toBe('function')
     })
 
     it('getErrorFromResponseBody', () => {
         const emptyBodyError = new Error('Response has empty body')
-        expect(getErrorFromResponseBody()).toEqual(emptyBodyError)
         expect(getErrorFromResponseBody('')).toEqual(emptyBodyError)
         expect(getErrorFromResponseBody(null)).toEqual(emptyBodyError)
 
@@ -120,83 +138,22 @@ describe('utils', () => {
         expect(error.message).toBe('unknown error')
     })
 
-    describe('setupDirectConnect', () => {
-        it('should do nothing if params contain no direct connect caps', function () {
-            const params = { hostname: 'bar', capabilities: { platformName: 'baz' } }
-            const paramsCopy = JSON.parse(JSON.stringify(params))
-            setupDirectConnect(params)
-            expect(params).toEqual(paramsCopy)
-        })
-
-        it('should do nothing if params contain incomplete direct connect caps', function () {
-            const params = { hostname: 'bar', capabilities: { directConnectHost: 'baz' } }
-            const paramsCopy = JSON.parse(JSON.stringify(params))
-            setupDirectConnect(params)
-            expect(params).toEqual(paramsCopy)
-        })
-
-        it('should update connection params if caps contain all direct connect fields', function () {
-            const params = {
-                protocol: 'http',
-                hostname: 'foo',
-                port: 1234,
-                path: '',
-                capabilities: {
-                    directConnectProtocol: 'https',
-                    directConnectHost: 'bar',
-                    directConnectPort: 4321,
-                    directConnectPath: '/'
-                }
-            }
-            setupDirectConnect(params)
-            expect(params.protocol).toBe('https')
-            expect(params.hostname).toBe('bar')
-            expect(params.port).toBe(4321)
-            expect(params.path).toBe('/')
-        })
-
-        it('should update connection params even if path is empty string', function () {
-            const params = {
-                protocol: 'http',
-                hostname: 'foo',
-                port: 1234,
-                path: '/',
-                capabilities: {
-                    directConnectProtocol: 'https',
-                    directConnectHost: 'bar',
-                    directConnectPort: 4321,
-                    directConnectPath: ''
-                }
-            }
-            setupDirectConnect(params)
-            expect(params.protocol).toBe('https')
-            expect(params.hostname).toBe('bar')
-            expect(params.port).toBe(4321)
-            expect(params.path).toBe('')
-        })
-    })
-
     describe('getSessionError', () => {
         it('should return unchanged message', () => {
-            expect(getSessionError({ message: 'foobar' }, {})).toEqual('foobar')
+            expect(getSessionError({ message: 'foobar', name: 'Error' }, {})).toEqual('foobar')
         })
 
         it('should return "more info" if no message', () => {
-            expect(getSessionError({}, {})).toEqual('See wdio.* logs for more information.')
+            expect(getSessionError({ message: '', name: 'Error' }, {})).toEqual('See wdio.* logs for more information.')
         })
 
         it('should handle not properly set paths', () => {
-            expect(getSessionError({ message: 'unhandled request' }, {}))
+            expect(getSessionError({ message: 'unhandled request', name: 'Error' }, {}))
                 .toContain('Make sure you have set the "path" correctly!')
         })
 
         it('ECONNREFUSED', () => {
-            expect(getSessionError({
-                code: 'ECONNREFUSED',
-                address: '127.0.0.1',
-                port: 4444,
-                message: 'ECONNREFUSED 127.0.0.1:4444'
-            }, {
+            expect(getSessionError(new Error('ECONNREFUSED 127.0.0.1:4444'), {
                 protocol: 'https',
                 hostname: 'foobar',
                 port: 1234,
@@ -205,35 +162,34 @@ describe('utils', () => {
         })
 
         it('path: selenium-standalone path', () => {
-            expect(getSessionError({
-                message: 'Whoops! The URL specified routes to this help page.'
-            }, {})).toContain("set `path: '/wd/hub'` in")
+            expect(
+                getSessionError(
+                    new Error('Whoops! The URL specified routes to this help page.'),
+                    {}
+                )
+            ).toContain("set `path: '/wd/hub'` in")
         })
 
         it('path: chromedriver, geckodriver, etc', () => {
-            expect(getSessionError({
-                message: 'HTTP method not allowed'
-            })).toContain("set `path: '/'` in")
+            expect(getSessionError(new Error('HTTP method not allowed'))).toContain("set `path: '/'` in")
         })
 
         it('edge driver localhost issue', () => {
-            expect(getSessionError({
-                message: 'Bad Request - Invalid Hostname 400 <br> HTTP Error 400'
-            }, {})).toContain('127.0.0.1 instead of localhost')
+            expect(
+                getSessionError(new Error('Bad Request - Invalid Hostname 400 <br> HTTP Error 400'))
+            ).toContain('127.0.0.1 instead of localhost')
         })
 
         it('illegal w3c cap passed to selenium standalone', () => {
-            const message = getSessionError({
-                message: 'Illegal key values seen in w3c capabilities: [chromeOptions]'
-            }, {})
+            const message = getSessionError(
+                new Error('Illegal key values seen in w3c capabilities: [chromeOptions]')
+            )
             expect(message).toContain('[chromeOptions]')
             expect(message).toContain('add vendor prefix')
         })
 
         it('wrong host port, port in use, illegal w3c cap passed to grid', () => {
-            const message = getSessionError({
-                message: 'Response has empty body'
-            })
+            const message = getSessionError(new Error('Response has empty body'))
             expect(message).toContain('valid hostname:port or the port is not in use')
             expect(message).toContain('add vendor prefix')
         })
@@ -241,7 +197,7 @@ describe('utils', () => {
 
     describe('startWebDriverSession', () => {
         it('attaches capabilities to the params object', async () => {
-            const params = {
+            const params: Options = {
                 hostname: 'localhost',
                 port: 4444,
                 path: '/',
@@ -252,8 +208,10 @@ describe('utils', () => {
             }
             const sessionId = await startWebDriverSession(params)
             expect(sessionId).toBe('foobar-123')
-            expect(params.capabilities.browserName).toBe('mockBrowser')
-            expect(params.requestedCapabilities.browserName).toBe('chrome')
+            expect((params.capabilities as DesiredCapabilities).browserName)
+                .toBe('mockBrowser')
+            expect((params.requestedCapabilities as DesiredCapabilities).browserName)
+                .toBe('chrome')
 
         })
 
