c2dec6bf34b46cd60068d9f7c026da9a7e8428b1
Shawn Lobo <shawnlobo96@gmail.com>
2020-10-06 23:14:08 +0530

    fixed reporter to display BrowserStack device name

--- packages/wdio-allure-reporter/src/index.js
+++ packages/wdio-allure-reporter/src/index.js
@@ -114,7 +114,7 @@ class AllureReporter extends WDIOReporter {
             let targetName = browserName || deviceName || cid
             // custom mobile grids can have device information in a `desired` cap
             if (desired && desired.deviceName && desired.platformVersion) {
-                targetName = `${desired.deviceName} ${desired.platformVersion}`
+                targetName = `${desired.device} || ${desired.deviceName} ${desired.platformVersion}`
             }
             const browserstackVersion = this.capabilities.os_version || this.capabilities.osVersion
             const version = browserstackVersion || this.capabilities.browserVersion || this.capabilities.version || this.capabilities.platformVersion || ''
