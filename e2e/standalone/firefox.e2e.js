b1eff53c0adeaff6eddc17ba3bddcd63b41c6f69
christian-bromann <mail@christian-bromann.com>
2020-10-07 11:35:13 +0200

    add launch tests

---
+++ e2e/standalone/firefox.e2e.js
@@ -0,0 +1,22 @@
+import DevTools from '../packages/devtools/src/index'
+
+import { ELEMENT_KEY } from '../packages/devtools/src/constants'
+
+test('running session with Chromium Edge', async () => {
+    const browser = await DevTools.newSession({
+        outputDir: __dirname,
+        capabilities: {
+            browserName: 'firefox',
+            'moz:firefoxOptions': {
+                headless: true
+            }
+        }
+    })
+    await browser.navigateTo('https://www.whatismybrowser.com/detect/what-is-my-user-agent')
+
+    const elem = await browser.findElement('css selector', '#detected_value')
+    const userAgent = await browser.getElementText(elem[ELEMENT_KEY])
+
+    console.log('Detected user agent:', userAgent)
+    expect(userAgent).toContain('Firefox')
+})
