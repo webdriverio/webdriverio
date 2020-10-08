d4a032692106ebbf7b4d81ddee6c5b055224579a
christian-bromann <mail@christian-bromann.com>
2020-10-08 09:57:46 +0200

    fix e2e standalone tests

--- e2e/standalone/edge.e2e.js
+++ e2e/standalone/edge.e2e.js
@@ -1,7 +1,5 @@
 import DevTools from '../../packages/devtools/src/index'
 
-import { ELEMENT_KEY } from '../../packages/devtools/src/constants'
-
 let browser
 
 beforeAll(async () => {
@@ -16,14 +14,17 @@ beforeAll(async () => {
     })
 })
 
-test('running session with Chromium Edge', async () => {
-    await browser.navigateTo('https://www.whatsmyua.info/')
+describe('Chromium Edge', () => {
+    it('can browser pages', async () => {
+        await browser.navigateTo('http://json.org/')
+        expect(await browser.getTitle()).toBe('JSON')
+    })
 
-    const elem = await browser.findElement('css selector', '#custom-ua-string')
-    const userAgent = await browser.getElementProperty(elem[ELEMENT_KEY], 'value')
-
-    console.log('Detected user agent:', userAgent)
-    expect(userAgent).toContain('Edg/')
+    it('has proper UA', async () => {
+        const userAgent = await browser.executeScript('return navigator.userAgent', [])
+        console.log('Detected user agent:', userAgent)
+        expect(userAgent).toContain('Edg/')
+    })
 })
 
 afterAll(async () => {
