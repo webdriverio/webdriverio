3226c78195587aad5baf1eda2ccacdff183c4e2b
discombobulateme <paloma.oliveira@saucelabs.com>
2020-11-01 01:07:52 +0100

    Add type Araay of any to output

--- packages/wdio-concise-reporter/src/index.ts
+++ packages/wdio-concise-reporter/src/index.ts
@@ -87,7 +87,7 @@ export default class ConciseReporter extends WDIOReporter {
     * @return {Array} Stack trace output
     */
     getFailureDisplay () {
-        const output = []
+        const output: Array<any> =[]
 
         this.getOrderedSuites().map(suite => suite.tests.map(test => {
             if (test.state === 'failed') {
