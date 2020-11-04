acf57d1dd6a4e9ab58308741be9af3dcb1fdcf50
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-11-04 11:12:25 -0800

    add strictSSL as an option

--- packages/webdriver/src/constants.ts
+++ packages/webdriver/src/constants.ts
@@ -138,5 +138,12 @@ export const DEFAULTS: Record<Exclude<keyof Options, 'requestedCapabilities'>, O
     },
     directConnectPath: {
         type: 'string'
+    },
+    /**
+     * Whether it requires SSL certificates to be valid in HTTP/s requests
+     * for an environment which cannot get process environment well.
+     */
+    strictSSL: {
+        type: 'boolean'
     }
 }
