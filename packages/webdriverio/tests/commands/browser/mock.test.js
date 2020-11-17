bb3cbf6b28dd6b4d84cd9eb3fbb4194a2f3cd27f
Vahan Melikyan <vahan.melikyan@gmail.com>
2020-11-17 00:58:36 -0800

    Fixes #6057 (#6066)

--- packages/webdriverio/tests/commands/browser/mock.test.js
+++ packages/webdriverio/tests/commands/browser/mock.test.js
@@ -1,3 +1,4 @@
+import got from 'got'
 import { remote } from '../../../src'
 
 const clientMock = {
@@ -7,7 +8,8 @@ const clientMock = {
 const pageMock = {
     target: jest.fn().mockReturnValue({
         createCDPSession: jest.fn().mockReturnValue(Promise.resolve(clientMock))
-    })
+    }),
+    evaluate: jest.fn().mockReturnValue(Promise.resolve(true))
 }
 const puppeteerMock = {
     pages: jest.fn().mockReturnValue([pageMock])
@@ -21,6 +23,9 @@ jest.mock('../../../src/utils/interception/webdriver', () => class WebDriverInte
 
 describe('custom$', () => {
     let browser
+    beforeEach(async () => {
+        clientMock.send.mockClear()
+    })
 
     it('should enable the fetch domain if not already done', async () => {
         browser = await remote({
@@ -39,6 +44,28 @@ describe('custom$', () => {
 
         await browser.mock('/foobar')
         expect(clientMock.send).toBeCalledTimes(1)
+    })
+
+    it('should mock on multiple tabs', async () => {
+        browser = await remote({
+            baseUrl: 'http://foobar.com',
+            capabilities: {
+                browserName: 'devtools'
+            }
+        })
+
+        browser.puppeteer = puppeteerMock
+
+        got.setMockResponse('window-handle-2')
+        expect(clientMock.send).toBeCalledTimes(0)
+        await browser.mock('/foobar')
+        expect(clientMock.send).toBeCalledTimes(1)
+        expect(clientMock.send).toBeCalledWith('Fetch.enable', expect.any(Object))
+        expect(clientMock.on).toBeCalledWith('Fetch.requestPaused', expect.any(Function))
+        got.setMockResponse('window-handle-3')
+        await browser.mock('/foobar')
+        expect(clientMock.send).toBeCalledTimes(2)
+
     })
 
     it('should mock using WebDriver capabilities', async () => {
