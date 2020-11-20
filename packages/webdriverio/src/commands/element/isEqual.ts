9d77399b8f9d0b1a6e7e3615203c237560f74ec8
christian-bromann <mail@christian-bromann.com>
2020-11-20 11:29:29 +0100

    Rewrite  element commands

---
+++ packages/webdriverio/src/commands/element/isEqual.ts
@@ -0,0 +1,55 @@
+/**
+ *
+ * Return true if the selected element matches with the provided one.
+ *
+ * <example>
+    :isEqual.js
+    it('should detect if an element is clickable', () => {
+        const el = $('#el')
+        const sameEl = $('#el')
+        const anotherEl = $('#anotherEl')
+
+        el.isEqual(sameEl) // outputs: true
+
+        el.isEqual(anotherEl) // outputs: false
+    });
+ * </example>
+ *
+ * @alias element.isEqual
+ * @param   {Element}   el element to compare with
+ * @return  {Boolean}   true if elements are equal
+ *
+ */
+
+import { ELEMENT_KEY } from '../../constants'
+import { getBrowserObject } from '../../utils'
+
+const getWebElement = (el: WebdriverIO.Element) => ({
+    [ELEMENT_KEY]: el.elementId, // w3c compatible
+    ELEMENT: el.elementId // jsonwp compatible
+})
+
+export default async function isEqual (this: WebdriverIO.Element, el: WebdriverIO.Element) {
+    const browser: WebdriverIO.BrowserObject = getBrowserObject(this)
+
+    // mobile native
+    if (browser.isMobile) {
+        const context = await browser.getContext()
+        if (context?.toLowerCase().includes('native')) {
+            return this.elementId === el.elementId
+        }
+    }
+
+    // browser or webview
+    let result: boolean
+    try {
+        result = await browser.execute(
+            /* istanbul ignore next */
+            (el1, el2) => el1 === el2,
+            getWebElement(this), getWebElement(el))
+    } catch (err) {
+        result = false
+    }
+
+    return result
+}
---
+++ packages/webdriverio/src/commands/element/scrollIntoView.ts
@@ -0,0 +1,30 @@
+/**
+ *
+ * Scroll element into viewport ([MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView)).
+ *
+ * <example>
+    :scrollIntoView.js
+    it('should demonstrate the scrollIntoView command', () => {
+        const elem = $('#myElement');
+        // scroll to specific element
+        elem.scrollIntoView();
+    });
+ * </example>
+ *
+ * @alias element.scrollIntoView
+ * @param {object|boolean=} scrollIntoViewOptions  options for `Element.scrollIntoView()` (default: `true`)
+ * @uses protocol/execute
+ * @type utility
+ *
+ */
+
+import { ELEMENT_KEY } from '../../constants'
+
+export default function scrollIntoView (this: WebdriverIO.Element, scrollIntoViewOptions = true) {
+    return this.parent.execute(/* istanbul ignore next */function (elem: HTMLElement, options) {
+        elem.scrollIntoView(options)
+    }, {
+        [ELEMENT_KEY]: this.elementId, // w3c compatible
+        ELEMENT: this.elementId // jsonwp compatible
+    }, scrollIntoViewOptions)
+}
