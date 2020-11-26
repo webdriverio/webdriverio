35306cd485f9319905a6a6513a0de7afaad0f752
Eric Roth <ehrothusa@hotmail.com>
2020-11-25 19:55:06 -0500

    adding pdf page view

---
+++ packages/devtools/src/commands/printPage.js
@@ -0,0 +1,26 @@
+import DevToolsDriver from '../devtoolsdriver';
+
+
+export default async function printPage(options) {
+    const page = DevToolsDriver.getPageHandle();
+    if (!options) {
+      return page.pdf();
+    }
+
+    return page.pdf({
+        landscaper: options.orientation,
+        scale: options.scale,
+        printBackground: options.background,
+        width: options.width ,
+        height: options.height,
+        margin: {
+            top: options.top,
+            right: options.right,
+            bottom: options.bottom,
+            left: options.left
+        },
+        preferCSSPageSize: !options.shrinkToFit,
+        pageRanges: options.pageRanges
+    });
+
+  }
