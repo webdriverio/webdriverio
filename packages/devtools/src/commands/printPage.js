f67617bb1f623583317e87a6f089ee4f368c1023
Eric Roth <ehrothusa@hotmail.com>
2020-11-25 22:08:55 -0500

    updating print page

--- packages/devtools/src/commands/printPage.js
+++ packages/devtools/src/commands/printPage.js
@@ -1,17 +1,16 @@
-import DevToolsDriver from '../devtoolsdriver';
-
+import DevToolsDriver from '../devtoolsdriver'
 
 export default async function printPage(options) {
-    const page = DevToolsDriver.getPageHandle();
+    const page = DevToolsDriver.getPageHandle()
     if (!options) {
-      return page.pdf();
+        return page.pdf()
     }
 
     return page.pdf({
         landscaper: options.orientation,
         scale: options.scale,
         printBackground: options.background,
-        width: options.width ,
+        width: options.width,
         height: options.height,
         margin: {
             top: options.top,
@@ -21,6 +20,6 @@ export default async function printPage(options) {
         },
         preferCSSPageSize: !options.shrinkToFit,
         pageRanges: options.pageRanges
-    });
+    })
 
-  }
+}
