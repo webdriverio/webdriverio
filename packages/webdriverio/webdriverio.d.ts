5f2aa2131e69cc341fbcfd723fdd8de38ce01b1c
Dirko Odendaal <entelect@rmal1508122.oc.rabonet.com>
2020-12-07 14:52:35 +0100

    fix: improve executeAsync typing to allow callback

--- packages/webdriverio/webdriverio.d.ts
+++ packages/webdriverio/webdriverio.d.ts
@@ -43,7 +43,7 @@ declare namespace WebdriverIO {
          * the provided callback, which is always provided as the final argument to the function. The value
          * to this callback will be returned to the client.
          */
-        executeAsync: <U extends any[], V extends U>(script: string | ((...arguments: V) => void), ...arguments: U) => Promise<any>;
+        executeAsync: <T extends any[], V, W extends T>(script: string | ((...arguments: [...T, (result: V) => void]) => void), ...arguments: W) => Promise<T>;
     }
 
 
