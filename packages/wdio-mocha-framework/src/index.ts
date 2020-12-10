e10870168865b87bb35a4da3b9ac2c79912c3413
Christian Bromann <mail@christian-bromann.com>
2020-12-10 15:57:51 +0100

    Tracking doc: rewrite `@wdio/mocha-framework` package into TypeScript (#5841)

--- packages/wdio-mocha-framework/src/index.ts
+++ packages/wdio-mocha-framework/src/index.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import Mocha from 'mocha'
+import Mocha, { Runner } from 'mocha'
 import { format } from 'util'
 
 import logger from '@wdio/logger'
@@ -7,6 +7,8 @@ import { runTestInFiberContext, executeHooksWithArgs } from '@wdio/utils'
 
 import { loadModule } from './utils'
 import { INTERFACES, EVENTS, NOOP, MOCHA_TIMEOUT_MESSAGE, MOCHA_TIMEOUT_MESSAGE_REPLACEMENT } from './constants'
+import type { MochaConfig, MochaOpts, FrameworkMessage, FormattedMessage, MochaContext, MochaError } from './types'
+import type { EventEmitter } from 'events'
 
 const log = logger('@wdio/mocha-framework')
 
@@ -22,36 +24,44 @@ const log = logger('@wdio/mocha-framework')
 const MOCHA_UI_TYPE_EXTRACTOR = /^(?:.*-)?([^-.]+)(?:.js)?$/
 const DEFAULT_INTERFACE_TYPE = 'bdd'
 
+type EventTypes = 'hook' | 'test' | 'suite'
+type EventTypeProps = '_hookCnt' | '_testCnt' | '_suiteCnt'
+
 /**
  * Mocha runner
  */
 class MochaAdapter {
-    constructor(cid, config, specs, capabilities, reporter) {
-        this.cid = cid
-        this.capabilities = capabilities
-        this.reporter = reporter
-        this.specs = specs
-        this.config = Object.assign({
+    private _mocha?: Mocha
+    private _runner?: Runner
+    private _specLoadError?: Error
+
+    private _level = 0
+    private _hasTests = true
+    private _suiteIds: string[] = ['0']
+    private _suiteCnt: Map<string, number> = new Map()
+    private _hookCnt: Map<string, number> = new Map()
+    private _testCnt: Map<string, number> = new Map()
+
+    constructor(
+        private _cid: string,
+        private _config: MochaConfig,
+        private _specs: string[],
+        private _capabilities: WebDriver.Capabilities,
+        private _reporter: EventEmitter
+    ) {
+        this._config = Object.assign({
             mochaOpts: {}
-        }, config)
-        this.runner = {}
-        this.level = 0
-        this.suiteCnt = new Map()
-        this.hookCnt = new Map()
-        this.testCnt = new Map()
-        this.suiteIds = ['0']
-        this._hasTests = true
-        this.specLoadError = null
+        }, _config)
     }
 
     async init() {
-        const { mochaOpts } = this.config
-        const mocha = this.mocha = new Mocha(mochaOpts)
+        const { mochaOpts } = this._config
+        const mocha = this._mocha = new Mocha(mochaOpts)
         await mocha.loadFilesAsync()
-        mocha.reporter(NOOP)
+        mocha.reporter(NOOP as any)
         mocha.fullTrace()
 
-        this.specs.forEach((spec) => mocha.addFile(spec))
+        this._specs.forEach((spec) => mocha.addFile(spec))
         mocha.suite.on('pre-require', this.preRequire.bind(this))
         await this._loadFiles(mochaOpts)
 
@@ -61,23 +71,23 @@ class MochaAdapter {
          */
         const { setOptions } = require('expect-webdriverio')
         setOptions({
-            wait: this.config.waitforTimeout, // ms to wait for expectation to succeed
-            interval: this.config.waitforInterval, // interval between attempts
+            wait: this._config.waitforTimeout, // ms to wait for expectation to succeed
+            interval: this._config.waitforInterval, // interval between attempts
         })
 
         return this
     }
 
-    async _loadFiles(mochaOpts) {
+    async _loadFiles (mochaOpts: MochaOpts) {
         try {
-            await this.mocha.loadFilesAsync()
+            await this._mocha!.loadFilesAsync()
 
             /**
              * grep
              */
-            const mochaRunner = new Mocha.Runner(this.mocha.suite)
+            const mochaRunner = new Mocha.Runner(this._mocha!.suite, false)
             if (mochaOpts.grep) {
-                mochaRunner.grep(this.mocha.options.grep, mochaOpts.invert)
+                mochaRunner.grep(this._mocha!.options.grep as RegExp, mochaOpts.invert!)
             }
 
             this._hasTests = mochaRunner.total > 0
@@ -86,48 +96,51 @@ class MochaAdapter {
                 'Unable to load spec files quite likely because they rely on `browser` object that is not fully initialised.\n' +
                 '`browser` object has only `capabilities` and some flags like `isMobile`.\n' +
                 'Helper files that use other `browser` commands have to be moved to `before` hook.\n' +
-                `Spec file(s): ${this.specs.join(',')}\n` +
+                `Spec file(s): ${this._specs.join(',')}\n` +
                 `Error: ${err.stack}`
-            this.specLoadError = new Error(error)
+            this._specLoadError = new Error(error)
             log.warn(error)
         }
     }
 
-    hasTests() {
+    hasTests () {
         return this._hasTests
     }
 
-    async run() {
-        const mocha = this.mocha
+    async run () {
+        const mocha = this._mocha!
 
         let runtimeError
         const result = await new Promise((resolve) => {
             try {
-                this.runner = mocha.run(resolve)
+                this._runner = mocha.run(resolve)
             } catch (e) {
                 runtimeError = e
                 return resolve(1)
             }
 
-            Object.keys(EVENTS).forEach((e) =>
-                this.runner.on(e, this.emit.bind(this, EVENTS[e])))
+            Object.keys(EVENTS).forEach((e: keyof typeof EVENTS) =>
+                this._runner!.on(e, this.emit.bind(this, EVENTS[e])))
 
-            this.runner.suite.beforeAll(this.wrapHook('beforeSuite'))
-            this.runner.suite.afterAll(this.wrapHook('afterSuite'))
+            this._runner.suite.beforeAll(this.wrapHook('beforeSuite'))
+            this._runner.suite.afterAll(this.wrapHook('afterSuite'))
         })
-        await executeHooksWithArgs(this.config.after, [runtimeError || result, this.capabilities, this.specs])
+        await executeHooksWithArgs(this._config.after as Function, [runtimeError || result, this._capabilities, this._specs])
 
         /**
          * in case the spec has a runtime error throw after the wdio hook
          */
-        if (runtimeError || this.specLoadError) {
-            throw runtimeError || this.specLoadError
+        if (runtimeError || this._specLoadError) {
+            throw runtimeError || this._specLoadError
         }
 
         return result
     }
 
-    options(options, context) {
+    options (
+        options: MochaOpts,
+        context: MochaContext
+    ) {
         let { require = [], compilers = [] } = options
 
         if (typeof require === 'string') {
@@ -137,28 +150,33 @@ class MochaAdapter {
         this.requireExternalModules([...compilers, ...require], context)
     }
 
-    preRequire(context, file, mocha) {
-        const options = this.config.mochaOpts
+    preRequire (
+        context: Mocha.MochaGlobals,
+        file: string,
+        mocha: Mocha
+    ) {
+        const options = this._config.mochaOpts
 
-        const match = MOCHA_UI_TYPE_EXTRACTOR.exec(options.ui)
-        const type = (match && INTERFACES[match[1]] && match[1]) || DEFAULT_INTERFACE_TYPE
+        const match = MOCHA_UI_TYPE_EXTRACTOR.exec(options.ui!) as any as [string, keyof typeof INTERFACES]
+        const type: keyof typeof INTERFACES = (match && INTERFACES[match[1]] && match[1]) || DEFAULT_INTERFACE_TYPE
 
-        const hookArgsFn = (context) => {
-            return [{ ...context.test, parent: context.test.parent.title }, context]
+        const hookArgsFn = (context: Mocha.Context) => {
+            return [{ ...context.test, parent: context.test?.parent?.title }, context]
         }
 
-        INTERFACES[type].forEach((fnName) => {
+        INTERFACES[type].forEach((fnName: string) => {
             let testCommand = INTERFACES[type][0]
             const isTest = [testCommand, testCommand + '.only'].includes(fnName)
 
             runTestInFiberContext(
                 isTest,
-                isTest ? this.config.beforeTest : this.config.beforeHook,
+                isTest ? this._config.beforeTest! : this._config.beforeHook!,
+                // @ts-ignore
                 hookArgsFn,
-                isTest ? this.config.afterTest : this.config.afterHook,
+                isTest ? this._config.afterTest : this._config.afterHook,
                 hookArgsFn,
                 fnName,
-                this.cid
+                this._cid
             )
         })
         this.options(options, { context, file, mocha, options })
@@ -167,34 +185,34 @@ class MochaAdapter {
     /**
      * Hooks which are added as true Mocha hooks need to call done() to notify async
      */
-    wrapHook(hookName) {
+    wrapHook (hookName: keyof WebdriverIO.HookFunctions) {
         return () => executeHooksWithArgs(
-            this.config[hookName],
+            this._config[hookName] as Function,
             this.prepareMessage(hookName)
         ).catch((e) => {
             log.error(`Error in ${hookName} hook: ${e.stack.slice(7)}`)
         })
     }
 
-    prepareMessage(hookName) {
-        const params = { type: hookName }
+    prepareMessage (hookName: keyof WebdriverIO.HookFunctions) {
+        const params: FrameworkMessage = { type: hookName }
 
         switch (hookName) {
         case 'beforeSuite':
         case 'afterSuite':
-            params.payload = this.runner.suite.suites[0]
+            params.payload = this._runner?.suite.suites[0]
             break
         case 'beforeTest':
         case 'afterTest':
-            params.payload = this.runner.test
+            params.payload = this._runner?.test
             break
         }
 
         return this.formatMessage(params)
     }
 
-    formatMessage(params) {
-        let message = {
+    formatMessage (params: FrameworkMessage) {
+        let message: FormattedMessage = {
             type: params.type
         }
 
@@ -211,6 +229,7 @@ class MochaAdapter {
             }
 
             message.error = {
+                name: params.err.name,
                 message: params.err.message,
                 stack: params.err.stack,
                 type: params.err.type || params.err.name,
@@ -258,7 +277,7 @@ class MochaAdapter {
         return message
     }
 
-    requireExternalModules(modules, context) {
+    requireExternalModules (modules: string[], context: MochaContext) {
         modules.forEach(module => {
             if (!module) {
                 return
@@ -274,7 +293,7 @@ class MochaAdapter {
         })
     }
 
-    emit(event, payload, err) {
+    emit (event: string, payload: any, err?: MochaError) {
         /**
          * For some reason, Mocha fires a second 'suite:end' event for the root suite,
          * with no matching 'suite:start', so this can be ignored.
@@ -283,53 +302,53 @@ class MochaAdapter {
 
         let message = this.formatMessage({ type: event, payload, err })
 
-        message.cid = this.cid
-        message.specs = this.specs
+        message.cid = this._cid
+        message.specs = this._specs
         message.uid = this.getUID(message)
 
-        this.reporter.emit(message.type, message)
+        this._reporter.emit(message.type, message)
     }
 
-    getSyncEventIdStart(type) {
-        const prop = `${type}Cnt`
-        const suiteId = this.suiteIds[this.suiteIds.length - 1]
+    getSyncEventIdStart (type: EventTypes) {
+        const prop = `_${type}Cnt` as EventTypeProps
+        const suiteId = this._suiteIds[this._suiteIds.length - 1]
         const cnt = this[prop].has(suiteId)
-            ? this[prop].get(suiteId)
+            ? this[prop].get(suiteId) || 0
             : 0
         this[prop].set(suiteId, cnt + 1)
         return `${type}-${suiteId}-${cnt}`
     }
 
-    getSyncEventIdEnd(type) {
-        const prop = `${type}Cnt`
-        const suiteId = this.suiteIds[this.suiteIds.length - 1]
-        const cnt = this[prop].get(suiteId) - 1
+    getSyncEventIdEnd (type: EventTypes) {
+        const prop = `_${type}Cnt` as EventTypeProps
+        const suiteId = this._suiteIds[this._suiteIds.length - 1]
+        const cnt = this[prop].get(suiteId)! - 1
         return `${type}-${suiteId}-${cnt}`
     }
 
-    getUID(message) {
+    getUID (message: FrameworkMessage) {
         if (message.type === 'suite:start') {
-            const suiteCnt = this.suiteCnt.has(this.level)
-                ? this.suiteCnt.get(this.level)
+            const suiteCnt = this._suiteCnt.has(this._level.toString())
+                ? this._suiteCnt.get(this._level.toString())
                 : 0
-            const suiteId = `suite-${this.level}-${suiteCnt}`
+            const suiteId = `suite-${this._level}-${suiteCnt}`
 
-            if (this.suiteCnt.has(this.level)) {
-                this.suiteCnt.set(this.level, this.suiteCnt.get(this.level) + 1)
+            if (this._suiteCnt.has(this._level.toString())) {
+                this._suiteCnt.set(this._level.toString(), this._suiteCnt.get(this._level.toString())! + 1)
             } else {
-                this.suiteCnt.set(this.level, 1)
+                this._suiteCnt.set(this._level.toString(), 1)
             }
 
             // const suiteId = this.getSyncEventIdStart('suite')
-            this.suiteIds.push(`${this.level}${suiteCnt}`)
-            this.level++
+            this._suiteIds.push(`${this._level}${suiteCnt}`)
+            this._level++
             return suiteId
         }
         if (message.type === 'suite:end') {
-            this.level--
-            const suiteCnt = this.suiteCnt.get(this.level) - 1
-            const suiteId = `suite-${this.level}-${suiteCnt}`
-            this.suiteIds.pop()
+            this._level--
+            const suiteCnt = this._suiteCnt.get(this._level.toString())! - 1
+            const suiteId = `suite-${this._level}-${suiteCnt}`
+            this._suiteIds.pop()
             return suiteId
         }
         if (message.type === 'hook:start') {
@@ -349,9 +368,10 @@ class MochaAdapter {
     }
 }
 
-const adapterFactory = {}
+const adapterFactory: { init?: Function } = {}
 
-adapterFactory.init = async function (...args) {
+adapterFactory.init = async function (...args: any[]) {
+    // @ts-ignore just passing through args
     const adapter = new MochaAdapter(...args)
     const instance = await adapter.init()
     return instance
