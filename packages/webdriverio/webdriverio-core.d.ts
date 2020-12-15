a6e2e701d1559f9bc3c83facca9d2647520ad0c7
christian-bromann <mail@christian-bromann.com>
2020-12-15 10:29:38 +0100

    revert change

--- packages/webdriverio/webdriverio-core.d.ts
+++ packages/webdriverio/webdriverio-core.d.ts
@@ -663,7 +663,7 @@ declare namespace WebdriverIO {
             name: string,
             func: AddCommandFn<false>
         ): void;
-
+        
         /**
          * The `$$` command is a short way to call the [`findElements`](/docs/api/webdriver.html#findelements) command in order
          * to fetch multiple elements on the page similar to the `$$` command from the browser scope. The difference when calling
@@ -1044,7 +1044,7 @@ declare namespace WebdriverIO {
          */
         calls: Matches[];
 
-
+        
         /**
          * Abort the request with one of the following error codes:
          * `Failed`, `Aborted`, `TimedOut`, `AccessDenied`, `ConnectionClosed`,
@@ -1139,7 +1139,7 @@ declare namespace WebdriverIO {
             name: string,
             func: (selector: string) => HTMLElement | HTMLElement[] | NodeListOf<HTMLElement>
         ): void
-
+        
         /**
          * The `$$` command is a short way to call the [`findElements`](/docs/api/webdriver.html#findelements) command in order
          * to fetch multiple elements on the page. It returns an array with element results that will have an
@@ -1404,7 +1404,7 @@ declare namespace WebdriverIO {
         /**
          * flag to indicate multiremote browser session
          */
-        isMultiremote: true;
+        isMultiremote: boolean;
     }
 
     type MultiRemoteBrowserObject = MultiRemoteBrowser & MultiRemoteBrowserReference
