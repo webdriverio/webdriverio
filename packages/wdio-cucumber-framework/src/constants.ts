c9c21becdc31a2a25f94a353cefc59676251abc7
christian-bromann <mail@christian-bromann.com>
2020-12-23 23:30:45 +0100

    wip

--- packages/wdio-cucumber-framework/src/constants.ts
+++ packages/wdio-cucumber-framework/src/constants.ts
@@ -1,6 +1,8 @@
+import { CucumberOpts } from './types'
+
 export const DEFAULT_TIMEOUT = 60000
 
-export const DEFAULT_OPTS = {
+export const DEFAULT_OPTS: CucumberOpts = {
     backtrace: false, // <boolean> show full backtrace for errors
     requireModule: [], // <string[]> ("module") require MODULE files (repeatable)
     failAmbiguousDefinitions: false, // <boolean> treat ambiguous definitions as errors
@@ -10,13 +12,14 @@ export const DEFAULT_OPTS = {
     profile: [], // <string> (name) specify the profile to use
     require: [], // <string> (file/dir/glob) require files before executing features
     order: 'defined', // <string> switch between deterministic  and random feature execution. Either "defined", "random" or "random:42" whereas 42 is the seed for randomization
-    snippetSyntax: undefined, // <string> specify a custom snippet syntax
     snippets: true, // <boolean> hide step definition snippets for pending steps
     source: true, // <boolean> hide source uris
     strict: false, // <boolean> fail if there are any undefined or pending steps
     tagExpression: '', // <string> (expression) only execute the features or scenarios with tags matching the expression
     tagsInTitle: false, // <boolean> add cucumber tags to feature or scenario name
-    timeout: DEFAULT_TIMEOUT // <number> timeout for step definitions in milliseconds
+    timeout: DEFAULT_TIMEOUT, // <number> timeout for step definitions in milliseconds
+    scenarioLevelReporter: false,
+    featureDefaultLanguage: 'en'
 }
 
 export const NOOP = /* istanbul ignore next */ function () {}
@@ -26,4 +29,4 @@ export const CUCUMBER_HOOK_DEFINITION_TYPES = [
     'beforeTestCaseHookDefinitions',
     'afterTestCaseHookDefinitions',
     'afterTestRunHookDefinitions',
-]
+] as const
