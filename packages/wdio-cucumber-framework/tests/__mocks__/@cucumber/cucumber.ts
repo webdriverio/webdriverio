d2e9f1d20b31bba92b00b6bf0af8d770ec5aebd9
christian-bromann <mail@christian-bromann.com>
2020-12-28 21:08:51 +0100

    add adapter unit test

--- packages/wdio-cucumber-framework/tests/__mocks__/@cucumber/cucumber.ts
+++ packages/wdio-cucumber-framework/tests/__mocks__/@cucumber/cucumber.ts
@@ -11,12 +11,21 @@ export const supportCodeLibraryBuilder = {
     finalize: jest.fn()
 }
 
+export const parseGherkinMessageStream = jest.fn()
 export const setDefaultTimeout = jest.fn()
 export const setDefinitionFunctionWrapper = jest.fn()
 export const getTestCasesFromFilesystem = jest.fn().mockImplementation(() => [])
 export const PickleFilter = jest.fn()
-export const Runtime = jest.fn().mockImplementation(() => ({
-    start: jest.fn().mockImplementation(() => true)
+export const Runtime = jest.fn().mockImplementation((opts) => ({
+    start: jest.fn().mockImplementation(() => {
+        if (opts.options.shouldFail) {
+            return false
+        }
+        if (opts.options.shouldThrow) {
+            return Promise.reject(new Error(opts.options.shouldThrow))
+        }
+        return true
+    })
 }))
 export const Status = RealStatus
 
