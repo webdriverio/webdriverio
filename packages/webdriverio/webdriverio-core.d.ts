2c69cdaaa685f62697c72fb537b2c53bb351f22f
christian-bromann <mail@christian-bromann.com>
2020-12-29 12:48:56 +0100

    set typings for internal flags

--- packages/webdriverio/webdriverio-core.d.ts
+++ packages/webdriverio/webdriverio-core.d.ts
@@ -1392,6 +1392,15 @@ declare namespace WebdriverIO {
 
     interface BrowserObject {
         isMultiremote?: false;
+
+        /**
+         * @private
+         */
+        _NOT_FIBER?: boolean
+        /**
+         * @private
+         */
+        wdioRetries?: number
     }
 
     type MultiRemoteBrowserReference = Record<string, BrowserObject>
--- scripts/templates/webdriverio.tpl.d.ts
+++ scripts/templates/webdriverio.tpl.d.ts
@@ -716,6 +716,15 @@ declare namespace WebdriverIO {
 
     interface BrowserObject {
         isMultiremote?: false;
+
+        /**
+         * @private
+         */
+        _NOT_FIBER?: boolean
+        /**
+         * @private
+         */
+        wdioRetries?: number
     }
 
     type MultiRemoteBrowserReference = Record<string, BrowserObject>
