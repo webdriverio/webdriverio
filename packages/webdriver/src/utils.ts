f4407c1421bec2e690f15e7729ea28bdee10a2e2
Christian Bromann <mail@christian-bromann.com>
2021-01-05 23:21:27 +0100

    Validate W3C Capabilities (#6054)

--- packages/webdriver/src/utils.ts
+++ packages/webdriver/src/utils.ts
@@ -8,6 +8,7 @@ import Protocols from '@wdio/protocols'
 
 import WebDriverRequest, { WebDriverResponse } from './request'
 import command from './command'
+import { VALID_CAPS } from './constants'
 import { Options, JSONWPCommandError, W3CCapabilities, SessionFlags, DesiredCapabilities } from './types'
 
 const log = logger('webdriver')
@@ -23,6 +24,28 @@ const BROWSER_DRIVER_ERRORS = [
  * start browser session with WebDriver protocol
  */
 export async function startWebDriverSession (params: Options): Promise<{ sessionId: string, capabilities: DesiredCapabilities }> {
+    /**
+     * validate capabilities to check if there are no obvious mix between
+     * JSONWireProtocol and WebDriver protoocol, e.g.
+     */
+    if (params.capabilities) {
+        const extensionCaps = Object.keys(params.capabilities).filter((cap) => cap.includes(':'))
+        const invalidWebDriverCaps = Object.keys(params.capabilities)
+            .filter((cap) => !VALID_CAPS.includes(cap) && !cap.includes(':'))
+
+        /**
+         * if there are vendor extensions, e.g. sauce:options or appium:app
+         * used (only WebDriver compatible) and caps that aren't defined
+         * in the WebDriver spec
+         */
+        if (extensionCaps.length && invalidWebDriverCaps.length) {
+            throw new Error(
+                `Invalid or unsupported WebDriver capabilities found ("${invalidWebDriverCaps.join('", "')}"). ` +
+                'Ensure to only use valid W3C WebDriver capabilities (see https://w3c.github.io/webdriver/#capabilities).'
+            )
+        }
+    }
+
     /**
      * the user could have passed in either w3c style or jsonwp style caps
      * and we want to pass both styles to the server, which means we need
