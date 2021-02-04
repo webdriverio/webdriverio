dfcc7135920a3d831694bd83fc48f8431355bfe0
christian-bromann <mail@christian-bromann.com>
2021-02-04 11:59:48 +0100

    fix return type

--- packages/webdriverio/async.d.ts
+++ packages/webdriverio/async.d.ts
@@ -1,6 +1,6 @@
 type BrowserSync = import('./build/types').Browser<'async'>
 type ElementSync = import('./build/types').Element<'async'>
-type MultiRemoteBrowserSync = import('./build/types').MultiRemoteBrowser<'async'>
+type MultiRemoteBrowserAsync = import('./build/types').MultiRemoteBrowser<'async'>
 
 declare namespace WebdriverIOAsync {
     interface Browser {}
@@ -12,7 +12,7 @@ declare namespace WebdriverIO {
     interface Browser extends BrowserSync, WebdriverIOAsync.Browser { }
     interface Element extends ElementSync, WebdriverIOAsync.Element { }
     // @ts-expect-error
-    interface MultiRemoteBrowser extends MultiRemoteBrowserSync, WebdriverIOAsync.MultiRemoteBrowser { }
+    interface MultiRemoteBrowser extends MultiRemoteBrowserAsync, WebdriverIOAsync.MultiRemoteBrowser { }
 }
 
 declare function $(...args: Parameters<WebdriverIO.Browser['$']>): WebdriverIO.Element
