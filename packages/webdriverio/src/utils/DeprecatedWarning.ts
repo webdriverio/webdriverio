f79b99f8308c0edde383d55b339316e0188dc6a8
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2021-04-15 15:14:14 +0200

    webdriverio: removed DeprecatedWarning

--- packages/webdriverio/src/utils/DeprecatedWarning.ts
+++
@@ -1,9 +0,0 @@
-import logger from '@wdio/logger'
-
-const log = logger('deprecated')
-
-export class DeprecatedWarning {
-    constructor(message: string) {
-        log.warn('@DeprecatedWarning: ' + message)
-    }
-}
