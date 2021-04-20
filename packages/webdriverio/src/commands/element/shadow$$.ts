580dbd8040dc6af3ae06e8763e33b1d0367f600d
christian-bromann <mail@christian-bromann.com>
2021-04-20 10:19:36 +0200

    fix docs on shadow commands

--- packages/webdriverio/src/commands/element/shadow$$.ts
+++ packages/webdriverio/src/commands/element/shadow$$.ts
@@ -2,7 +2,9 @@ import { shadowFnFactory } from '../../scripts/shadowFnFactory'
 
 /**
  *
- * Access elements inside a given element's shadowRoot.
+ * Access elements inside a given element's shadowRoot. If you are working
+ * with lots of nested shadow roots, an alternative approach to `shadow$$`
+ * is to use the [deep selector](https://webdriver.io/docs/selectors#deep-selectors).
  *
  * <example>
     :shadow$$.js
@@ -11,9 +13,6 @@ import { shadowFnFactory } from '../../scripts/shadowFnFactory'
         console.log(innerEl.getValue()); // outputs: 'test123'
     });
  * </example>
- *
- * If you are working with lots of nested shadow roots,
- * an alternative approach to `shadow$$` is to use the [deep selector](https://webdriver.io/docs/selectors#deep-selectors).
  *
  * @alias element.shadow$$
  * @param {String|Function} selector  selector or JS Function to fetch a certain element
