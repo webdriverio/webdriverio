eff55d54516b973e14b866804d0d196a50178b26
Vitaly Iegorov <vitalyiegorov@gmail.com>
2021-04-28 08:42:31 +0300

    closes #6772

--- packages/webdriverio/src/commands/browser/$.ts
+++ packages/webdriverio/src/commands/browser/$.ts
@@ -58,7 +58,7 @@ import type { Selector } from '../../types'
  *
  */
 export default async function $ (
-    this: WebdriverIO.Browser,
+    this: WebdriverIO.Browser | WebdriverIO.Element,
     selector: Selector
 ) {
     /**
