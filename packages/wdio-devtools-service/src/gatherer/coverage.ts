d63a57efab5359a9c0e6eb508f0289dfafb8e4dd
Kai <82549164+kailin0512@users.noreply.github.com>
2021-05-25 21:41:31 -0400

    Update coverage.ts
    Include is not ready to work

--- packages/wdio-devtools-service/src/gatherer/coverage.ts
+++ packages/wdio-devtools-service/src/gatherer/coverage.ts
@@ -63,6 +63,24 @@ export default class CoverageGatherer extends EventEmitter {
                 { requestId }
             ).catch(/* istanbul ignore next */ (err: Error) => log.debug(err.message))
         }
+        
+        /**
+         * continue with requests that aren't part of include patterns
+         */
+         for(const includeFile of this._options.include){
+            if(!request.url.match(includeFile)){
+                return this._client.send('Fetch.continueRequest', { requestId }).catch(/* istanbul ignore next */ (err) => log.debug(err.message));
+            }
+        }
+
+        /**
+         * continue with requests that are part of exclude patterns
+         */
+        for(const excludeFile of this._options.exclude){
+            if(request.url.match(excludeFile)){
+                return this._client.send('Fetch.continueRequest', { requestId }).catch(/* istanbul ignore next */ (err) => log.debug(err.message));
+            }
+        }
 
         /**
          * fetch original response
