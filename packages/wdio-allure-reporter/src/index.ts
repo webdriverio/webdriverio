127d94c86c4bb424703a47898b642407c8c6b494
Christian Bromann <github@christian-bromann.com>
2021-07-12 23:09:58 +0200

    Update packages/wdio-allure-reporter/src/index.ts

--- packages/wdio-allure-reporter/src/index.ts
+++ packages/wdio-allure-reporter/src/index.ts
@@ -139,7 +139,11 @@ class AllureReporter extends WDIOReporter {
             const testObj = test as TestStats
             const argument = testObj?.argument as Argument
             const dataTable = argument?.rows?.map((a: { cells: Array<string> }) => a?.cells)
-            if (dataTable) this._allure.addAttachment('Data Table', stringify(dataTable), 'text/csv')
+            
+            if (dataTable) {
+                this._allure.addAttachment('Data Table', stringify(dataTable), 'text/csv')
+            }
+
             return step
         }
 
