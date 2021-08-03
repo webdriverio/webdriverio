5560a037c9e896f35b8ed1253977117c763f660e
Christian Bromann <mail@christian-bromann.com>
2021-08-03 10:28:08 +0200

    Improve WebDriver error handling (#7206)
    * Allow message as valid error message
    
    * update tests
    
    * fix unit tests

--- packages/webdriver/src/request/index.ts
+++ packages/webdriver/src/request/index.ts
@@ -25,6 +25,14 @@ export class RequestLibError extends Error {
 export interface WebDriverResponse {
     value: any
     /**
+     * error case
+     * https://w3c.github.io/webdriver/webdriver-spec.html#dfn-send-an-error
+     */
+    error?: string
+    message?: string
+    stacktrace?: string
+
+    /**
      * JSONWP property
      */
     status?: number
