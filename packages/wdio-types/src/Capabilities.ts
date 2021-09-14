ff33119250bfe1cd47ef68472b2f5e9058d14c32
Christian Bromann <mail@christian-bromann.com>
2021-09-14 13:30:27 +0200

    Improve mobile emulation capabilities (#7422)
    * Improve mobile emulation capabilities
    
    * add another test

--- packages/wdio-types/src/Capabilities.ts
+++ packages/wdio-types/src/Capabilities.ts
@@ -218,7 +218,14 @@ export interface ChromeOptions {
      * “userAgent.” Refer to Mobile Emulation for more information.
      */
     mobileEmulation?: {
-        [name: string]: any;
+        userAgent?: string
+        deviceName?: string
+        deviceMetrics?: {
+            width?: number
+            height?: number
+            pixelRatio?: number
+            touch?: boolean
+        }
     };
     /**
      * An optional dictionary that specifies performance logging preferences. See
