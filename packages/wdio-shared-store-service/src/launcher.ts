7b035970b1f076d4b1648f0fb4053e69aef553ec
christian-bromann <mail@christian-bromann.com>
2021-10-25 11:47:51 +0200

    fix running setValue in onPrepare hook

--- packages/wdio-shared-store-service/src/launcher.ts
+++ packages/wdio-shared-store-service/src/launcher.ts
@@ -1,6 +1,7 @@
 import logger from '@wdio/logger'
 
 import { writeFile, deleteFile, getPidPath } from './utils'
+import { setPort } from './client'
 
 const log = logger('@wdio/shared-store-service')
 
@@ -16,6 +17,7 @@ export default class SharedStoreLauncher {
     async onPrepare () {
         server = require('./server').default
         const result = await server.startServer()
+        setPort(result.port)
 
         log.info(`Started shared server on port ${result.port}`)
         await writeFile(this.pidFile, result.port.toString())
