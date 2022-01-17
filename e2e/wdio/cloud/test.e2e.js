8a82c88242252a204f115cb03cb19b9d4145457d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-01-17 01:40:16 -0800

    remove the log

--- e2e/wdio/cloud/test.e2e.js
+++ e2e/wdio/cloud/test.e2e.js
@@ -1,9 +1,5 @@
-import logger from '@wdio/logger'
-const log = logger('test.e2e')
-
 describe('this browser session', () => {
     it('should run in SL', () => {
-        log.warn(`browser.options.hostname: ${browser.options.hostname}`)
         expect(browser.options.hostname).toContain('saucelabs.com')
     })
 })
