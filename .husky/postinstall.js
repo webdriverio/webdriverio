9da4488f73e2a0252a4d62cb700e708cbdfbcd16
Sri Harsha <12621691+harsha509@users.noreply.github.com>
2022-03-11 19:40:34 +0530

    Update .husky/postinstall.js
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- .husky/postinstall.js
+++ .husky/postinstall.js
@@ -1,12 +1,12 @@
-console.log("" +
-    "##################\n" +
-    "### Attention! ###\n" +
-    "##################\n" +
-    "\n" +
-    "The WebdriverIO team is aware of the following reported vulnerabilities.\n" +
-    "These do not impact packages used by users but ones we use to generate\n" +
-    "our public website (webdriver.io). The package in question is `markdox`\n" +
-    "which depends on vulnerabile packages such as: `dox` and `ejs`.\n" +
-    "\n" +
-    "Please help us to resolve the issue, see https://github.com/webdriverio/webdriverio/issues/6640\n" +
-    "");
+console.log(`
+##################
+### Attention! ###
+##################
+
+The WebdriverIO team is aware of the following reported vulnerabilities.
+These do not impact packages used by users but ones we use to generate
+our public website (webdriver.io). The package in question is `markdox`
+which depends on vulnerabile packages such as: `dox` and `ejs`.
+
+Please help us to resolve the issue, see https://github.com/webdriverio/webdriverio/issues/6640
+`);
