ffa0accc7a30d984db5075f56d1a1f2db7572a38
Christian Bromann <git@bromann.dev>
2022-05-16 22:28:05 +0200

    add welcome bash

---
+++ .gitpod/welcome.mjs
@@ -0,0 +1,65 @@
+import chalk from 'chalk'
+import sh from 'shelljs'
+
+const orange = chalk.hex('#ea5906')
+
+const robot = `
+                   .::::::::::::::::::::::::::.
+                  .=                          =.
+                  .-                          ::
+               -+=.-    -====-.     -====-    ::-+-
+              ***=.-   +-****==    +=****-+ :-::=***.
+             .***=.-   =-****==    +=****-+ :-::=***.
+              :+*=.-    --===-      -===--    ::=*+:
+                   =.                        .=
+                    .::::::::::::::::::::::::.
+             -+************************************+-
+           .-****=.                             =****-.
+       .-=-::****                                ****-:-=-.
+     .==    .**** = =.-::-                       ****:    -=
+    .+.    .-****================================****=.    .+.
+    +.   :**********************************************:   .*
+    *   .**::****************************************::**:   *.
+    *   -*- .****=***=*=++++****:................****: -*=   +.
+    *   -*- .****-**===****+-***.                ****: -*=   +.
+    *   -*- .****-*=+-******-***..=====-.   -+==+****: -*=   +.
+    *   -*- .****-+=**-+***==***++.    .=+=+-    ****: -*=   +.
+    *   -*- .****++****++++******-:::::::--::::::****: -*=   +.
+    *   -*- .****************************************: -*=   +.
+    *   -*- .****************************************: -*=   +.
+   .*++++*= .****:::=++=:::-=++-:::::::::::::::::****: -**+++*.
+  .********=.**** :-   .=.=:  =*=    -========-  ****:-********:
+   +*******:.**** =  +- :-=  =: =    -========:  ****:.*******+
+   = -=::-:-.****=.-:+=-- :-:.:-:    ====       -****:-:--:=: =
+   + = =-=== =*****+***+++++***++++++++++++++++*****= ===-=.= +
+   =:-+-       ...-=****:+..............+:****=-...       :+-.=
+     .            :=****.+              =.****=-            ..
+                  :=****.+              =:****=-
+                  :=****:+              =-****=-
+               :::==+++*=*              *=++++==:::
+             =-        =..-            -..=        -=
+            .*+++++++++++*=            =*+*+++++++++*.
+            .***********+=:            :=+***********.
+`
+
+console.log(`
+${orange(robot)}
+${chalk.bold('     Welcome to the WebdriverIO Development Environment 👋')}
+
+Thanks to Gitpod everything is set-up and ready for you, so you can start making
+changes to the package files and test them. There is a sample standalone script
+you can run via:
+
+${chalk.bold('$ node ./examples/standalone/sample.js')}
+
+If you want to test changes for the testrunner (e.g. using Mocha), run:
+
+${chalk.bold('$ cd ./examples/wdio')}
+${chalk.bold('$ npm run test:wdio')}
+
+The following environments are available for you:
+    - Preview of the documentation page
+      ${chalk.blue.bold(await sh.exec('echo "gp url 3000"'))}
+    - Code Coverage Overview
+      ${chalk.blue.bold(await sh.exec('echo "gp url 8000"'))}
+For more information, go to: ${chalk.blue.bold('https://webdriver.io/docs/contribute')}!`)
---
+++ examples/standalone/sample.js
@@ -0,0 +1,36 @@
+const { remote } = require('../../packages/webdriverio')
+
+let browser
+
+;(async () => {
+    /**
+     * intiate browser
+     * As we use this script in our dev environments which has no
+     * browser driver nor xfvb set up we have to make sure to set
+     * the headless flag for running with devtools
+     */
+    browser = await remote({
+        capabilities: {
+            browserName: 'chrome',
+            'wdio:devtoolsOptions': {
+                headless: true
+            }
+        }
+    })
+
+    /**
+     * your test code here
+     */
+    await browser.url('https://webdriver.io')
+    console.log(await browser.getUrl())
+
+})().then(
+    /**
+     * tear down session
+     */
+    () => browser.deleteSession(),
+    (err) => {
+        console.log(`Something went wrong: ${err.stack}`)
+        return browser.deleteSession()
+    }
+)
