d0718cbfc1a25147dd86386631f016e1c7a7e6b2
Christian Bromann <git@bromann.dev>
2022-05-16 14:21:42 +0200

    rewrite eslint-plugin-wdio into ESM

--- packages/eslint-plugin-wdio/src/index.ts
+++ packages/eslint-plugin-wdio/src/index.ts
@@ -1,27 +1,27 @@
-import awaitExpect from './rules/await-expect'
-import noDebug     from './rules/no-debug'
-import noPause     from './rules/no-pause'
+import awaitExpect from './rules/await-expect.js'
+import noDebug from './rules/no-debug.js'
+import noPause from './rules/no-pause.js'
 
 const rules = {
-    'await-expect' : awaitExpect,
-    'no-debug'     : noDebug,
-    'no-pause'     : noPause
+    'await-expect': awaitExpect,
+    'no-debug': noDebug,
+    'no-pause': noPause
 }
 
 const configs = {
     recommended: {
         globals: {
-            $                  : false,
-            $$                 : false,
-            browser            : false,
-            driver             : false,
-            expect             : false,
-            multiremotebrowser : false,
+            $: false,
+            $$: false,
+            browser: false,
+            driver: false,
+            expect: false,
+            multiremotebrowser: false,
         },
-        rules : {
-            'wdio/await-expect' : 'error',
-            'wdio/no-debug'     : 'error',
-            'wdio/no-pause'     : 'error',
+        rules: {
+            'wdio/await-expect': 'error',
+            'wdio/no-debug': 'error',
+            'wdio/no-pause': 'error',
         }
     }
 }
