9d102e8074821e67003c14b1068b2ed724e2d519
praveendvd <45095911+praveendvd@users.noreply.github.com>
2022-05-17 17:18:23 +0100

    Update utils.test.ts

--- packages/wdio-cli/tests/utils.test.ts
+++ packages/wdio-cli/tests/utils.test.ts
@@ -385,10 +385,6 @@ test('validateServiceAnswers', () => {
 })
 
 describe('getCapabilities', () => {
-    afterEach(() => {
-        jest.mock('@wdio/config')
-        jest.resetModules()
-    })
 
     it('should return driver with capabilities for android', () => {
         expect(getCapabilities({ option: 'foo.apk' } as any)).toMatchSnapshot()
