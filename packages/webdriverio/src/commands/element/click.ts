a4d1500b69e3716d9576f135ed9d41673da85f9e
praveendvd <praveendvd@live.com>
2022-06-25 01:03:58 +0000

    updated documents

--- packages/webdriverio/src/commands/element/click.ts
+++ packages/webdriverio/src/commands/element/click.ts
@@ -8,8 +8,13 @@ const log = logger('webdriverio/click')
  *
  * Click on an element.
  *
- * Note: This issues a WebDriver `click` command for the selected element, which generally scrolls to and then clicks the
- * selected element. However, if you have fixed-position elements (such as a fixed header or footer) that cover up the
+ * This issues a WebDriver `click` command for the selected element , which generally scrolls to and then clicks the
+ * selected element when no options are passed. When options object is passed it uses action class instead of webdriver click which
+ * give added capabilities like passing button type, coordinates etc. By default, when using options a release action
+ * command is send after performing the click action, this could cause some alerts to be closed unexpectedly
+ * (issue: https://github.com/webdriverio/webdriverio/issues/6423) To fix this pass `option.skipRelease`.
+ *
+ * Note: If you have fixed-position elements (such as a fixed header or footer) that cover up the
  * selected element after it is scrolled within the viewport, the click will be issued at the given coordinates, but will
  * be received by your fixed (overlaying) element. In these cased the following error is thrown:
  *
@@ -64,6 +69,10 @@ const log = logger('webdriverio/click')
         const myButton = await $('#myButton')
         await myButton.click({ button: 2, x: 30, y: 40 }) // opens the contextmenu 30 horizontal and 40 vertical pixels away from location of the button (from the center of element)
     })
+    it('should skip sending releaseAction command that cause unexpected alert closure', async () => {
+        const myButton = await $('#myButton')
+        await myButton.click({ button: 2, x: 30, y: 40, skipRelease:true }) //skips sending releaseActions
+    })
  * </example>
  *
  * @alias element.click
@@ -75,7 +84,7 @@ const log = logger('webdriverio/click')
  * @param {number=}           options.y      Number (optional)
  * @param {number=}           options.skipRelease         Boolean (optional)
  */
-export default async function click (
+export default async function click(
     this: WebdriverIO.Element,
     options?: ClickOptions
 ) {
