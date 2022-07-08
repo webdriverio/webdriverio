faac31d8bce9adac976c82812e5f24c7102fbdb6
Christian Bromann <git@bromann.dev>
2022-07-08 09:10:23 +0000

    remove .only tests

--- scripts/test/globalSetup.ts
+++ scripts/test/globalSetup.ts
@@ -7,6 +7,16 @@ const nodeModulesPath = path.join(__dirname, '..', '..', 'packages', 'webdriver'
 const gotPath = path.join(nodeModulesPath, 'got')
 const tmpGotPath = path.join(nodeModulesPath, 'tmp_got')
 
+function throwBetterErrorMessage (err: Error) {
+    throw new Error(
+        'Renaming "got" dependency failed!\n' +
+        'We need to remove rename the got dependency (at "packages/webdriver/node_modules/got") ' +
+        'during the test to force Vitest to use our mocked version. You might already run unit ' +
+        'tests in a different terminal.\n\n' +
+        err.stack
+    )
+}
+
 /**
  * Rename "got" dependency so Vitest is forced to use the mock.
  * For some reasons it is not possible to mock `webdriver`s got
@@ -14,9 +24,9 @@ const tmpGotPath = path.join(nodeModulesPath, 'tmp_got')
  * the deps.inline option. This is a workaround for this.
  */
 export const setup = async () => {
-    await fs.rename(gotPath, tmpGotPath)
+    await fs.rename(gotPath, tmpGotPath).catch(throwBetterErrorMessage)
 }
 
 export const teardown = async () => {
-    await fs.rename(tmpGotPath, gotPath)
+    await fs.rename(tmpGotPath, gotPath).catch(throwBetterErrorMessage)
 }
