722a6ae42dbb44c18d209e45891c7d4b65913804
Christian Bromann <git@bromann.dev>
2022-07-19 10:49:15 +0000

    fix include path on windows

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -1,10 +1,11 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import path from 'node:path'
 
 export default defineConfig({
     test: {
         testTimeout: 1000 * 60,
-        include: [path.join(__dirname, '**', '*.test.ts')],
+        include: [path.resolve(__dirname, '**', '*.test.ts')],
         hookTimeout: 60 * 1000,
         threads: false
     }
