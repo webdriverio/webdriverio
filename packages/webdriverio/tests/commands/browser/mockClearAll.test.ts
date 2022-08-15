5ea612e029946ed7add403d2cb370000ef3be7c4
Christian Bromann <git@bromann.dev>
2022-08-15 23:41:09 +0200

    fix unit test

--- packages/webdriverio/tests/commands/browser/mockClearAll.test.ts
+++ packages/webdriverio/tests/commands/browser/mockClearAll.test.ts
@@ -14,7 +14,7 @@ vi.mock('../../../src/commands/browser/mock', () => {
     SESSION_MOCKS['foobar'].add({ clear: vi.fn(bumpCall) })
     SESSION_MOCKS['barfoo'] = new Set()
     SESSION_MOCKS['barfoo'].add({ clear: vi.fn(bumpCall) })
-    return { SESSION_MOCKS, getMockCalls: () => clearedMocks }
+    return { SESSION_MOCKS, getMockCalls: () => clearedMocks, default: vi.fn() }
 })
 vi.mock('@wdio/logger', () => import(path.join(process.cwd(), '__mocks__', '@wdio/logger')))
 
