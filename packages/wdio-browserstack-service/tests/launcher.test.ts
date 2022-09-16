959646ef9bf9821c19ba5cc07532bf53d47e8548
avinash-bharti <avinash.b@browserstack.com>
2022-09-16 16:21:50 +0530

    feat: App upload to BrowserStack for appium session
    - Upload app to browserstack if local file path is given in browserstack-service config
    
    - App can be string or object
    
    - string value of app includes filepath, app_url, custom_id, shareable_id
    
    - object can contain any one from filepath, app_url, custom_id, shareable_id
    
    - only filepath and custom_id can coexist as app object value
    
    - if custom_id given with filepath, it will be passed during app upload

--- packages/wdio-browserstack-service/tests/launcher.test.ts
+++ packages/wdio-browserstack-service/tests/launcher.test.ts
@@ -3,6 +3,8 @@ import { describe, expect, it, vi, beforeEach } from 'vitest'
 // @ts-expect-error mock feature
 import Browserstack, { mockStart } from 'browserstack-local'
 import logger from '@wdio/logger'
+import got from 'got'
+import fs from 'node:fs'
 
 import BrowserstackLauncher from '../src/launcher.js'
 import type { BrowserstackConfig } from '../src/types'
@@ -29,6 +31,13 @@ describe('onPrepare', () => {
     }
     const logInfoSpy = vi.spyOn(log, 'info').mockImplementation((string) => string)
 
+    it('should not try to upload app is app is undefined', () => {
+        const service = new BrowserstackLauncher({} as any, caps, config)
+        service.onPrepare()
+
+        expect(logInfoSpy).toHaveBeenCalledWith('app is not defined in browserstack-service config, skipping ...')
+    })
+
     it('should not call local if browserstackLocal is undefined', () => {
         const service = new BrowserstackLauncher({} as any, caps, {
             user: 'foobaruser',
@@ -53,6 +62,143 @@ describe('onPrepare', () => {
 
         expect(logInfoSpy).toHaveBeenCalledWith('browserstackLocal is not enabled - skipping...')
         expect(service.browserstackLocal).toBeUndefined()
+    })
+
+    it('should add the "app" property to a multiremote capability if no "bstack:options"', async () => {
+        const options: BrowserstackConfig = { app: 'bs://<app-id>' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = { samsungGalaxy: { capabilities: {} } }
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities.samsungGalaxy.capabilities).toEqual({ 'app': 'bs://<app-id>' })
+    })
+
+    it('should add the "appium:app" property to a multiremote capability if "bstack:options" present', async () => {
+        const options: BrowserstackConfig = { app: 'bs://<app-id>' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = { samsungGalaxy: { capabilities: { 'bstack:options': {} } } }
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities.samsungGalaxy.capabilities).toEqual({ 'bstack:options': {}, 'appium:app': 'bs://<app-id>' })
+    })
+
+    it('should add the "appium:app" property to a multiremote capability if any extension cap present', async () => {
+        const options: BrowserstackConfig = { app: 'bs://<app-id>' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = { samsungGalaxy: { capabilities: { 'appium:chromeOptions': {} } } }
+
+        await service.onPrepare(config, capabilities as any)
+        expect(capabilities.samsungGalaxy.capabilities).toEqual({ 'appium:app': 'bs://<app-id>', 'appium:chromeOptions': {} })
+    })
+
+    it('should add the "app" property to an array of capabilities if no "bstack:options"', async () => {
+        const options: BrowserstackConfig = { app: 'bs://<app-id>' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{}, {}, {}]
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities).toEqual([
+            { 'app': 'bs://<app-id>' },
+            { 'app': 'bs://<app-id>' },
+            { 'app': 'bs://<app-id>' }
+        ])
+    })
+
+    it('should add the "appium:app" property to an array of capabilities if "bstack:options" present', async () => {
+        const options: BrowserstackConfig = { app: 'bs://<app-id>' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'bstack:options': {} }, { 'bstack:options': {} }, { 'bstack:options': {} }]
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities).toEqual([
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' }
+        ])
+    })
+
+    it('should add the "appium:app" property to an array of capabilities if any extension cap present', async () => {
+        const options: BrowserstackConfig = { app: 'bs://<app-id>' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'appium:chromeOptions': {} }, { 'appium:chromeOptions': {} }]
+
+        await service.onPrepare(config, capabilities as any)
+        expect(capabilities).toEqual([
+            { 'appium:app': 'bs://<app-id>', 'appium:chromeOptions': {} },
+            { 'appium:app': 'bs://<app-id>', 'appium:chromeOptions': {} }
+        ])
+    })
+
+    it('should add the "appium:app" as custom_id of app to capability object', async () => {
+        const options: BrowserstackConfig = { app: 'custom_id' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'appium:chromeOptions': {} }, { 'appium:chromeOptions': {} }]
+
+        await service.onPrepare(config, capabilities as any)
+        expect(capabilities).toEqual([
+            { 'appium:app': 'custom_id', 'appium:chromeOptions': {} },
+            { 'appium:app': 'custom_id', 'appium:chromeOptions': {} }
+        ])
+    })
+
+    it('should add the "appium:app" as shareable_id of app to capability object', async () => {
+        const options: BrowserstackConfig = { app: 'user/custom_id' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'appium:chromeOptions': {} }, { 'appium:chromeOptions': {} }]
+
+        await service.onPrepare(config, capabilities as any)
+        expect(capabilities).toEqual([
+            { 'appium:app': 'user/custom_id', 'appium:chromeOptions': {} },
+            { 'appium:app': 'user/custom_id', 'appium:chromeOptions': {} }
+        ])
+    })
+
+    it('should add "appium:app" property with value returned from app upload to capabilities', async () => {
+        const options: BrowserstackConfig = { app: '/some/dummy/file.apk' }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'bstack:options': {} }, { 'bstack:options': {} }, { 'bstack:options': {} }]
+
+        vi.spyOn(fs, 'existsSync').mockReturnValueOnce(true)
+        vi.spyOn(service, '_uploadApp').mockImplementation(() => Promise.resolve({ app_url: 'bs://<app-id>' }))
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities).toEqual([
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' }
+        ])
+    })
+
+    it('should upload app if path property present in appConfig', async() => {
+        const options: BrowserstackConfig = { app: { path: '/path/to/app.apk' } }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'bstack:options': {} }, { 'bstack:options': {} }, { 'bstack:options': {} }]
+
+        vi.spyOn(fs, 'existsSync').mockReturnValueOnce(true)
+        vi.spyOn(service, '_uploadApp').mockImplementation(() => Promise.resolve({ app_url: 'bs://<app-id>' }))
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities).toEqual([
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' }
+        ])
+    })
+
+    it('should upload app along with custom_id if path and custom_id property present in appConfig', async() => {
+        const options: BrowserstackConfig = { app: { path: '/path/to/app.apk', custom_id: 'custom_id' } }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = [{ 'bstack:options': {} }, { 'bstack:options': {} }, { 'bstack:options': {} }]
+
+        vi.spyOn(fs, 'existsSync').mockReturnValueOnce(true)
+        vi.spyOn(service, '_uploadApp').mockImplementation(() => Promise.resolve({ app_url: 'bs://<app-id>', custom_id: 'custom_id', shareable_id: 'foobaruser/custom_id' }))
+
+        await service.onPrepare(config, capabilities)
+        expect(capabilities).toEqual([
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' },
+            { 'bstack:options': {}, 'appium:app': 'bs://<app-id>' }
+        ])
     })
 
     it('should initialize the opts object, and spawn a new Local instance', async () => {
@@ -121,14 +267,6 @@ describe('onPrepare', () => {
         ])
     })
 
-    it('should throw an error if "capabilities" is not an object/array', () => {
-        const service = new BrowserstackLauncher(options as any, caps, config)
-        const capabilities = 1
-
-        expect(() => service.onPrepare(config, capabilities as any))
-            .toThrow(TypeError('Capabilities should be an object or Array!'))
-    })
-
     it('should reject if local.start throws an error', () => {
         const service = new BrowserstackLauncher(options as any, caps, config)
         mockStart.mockImplementationOnce((_: never, cb: Function) => cb(error))
@@ -144,7 +282,7 @@ describe('onPrepare', () => {
         await service.onPrepare(config, caps)
         expect(service.browserstackLocal?.start).toHaveBeenCalled()
         await sleep(100)
-        expect(logInfoMock.mock.calls[0][0])
+        expect(logInfoMock.mock.calls[1][0])
             .toContain('Browserstack Local successfully started after')
     })
 
@@ -230,5 +368,85 @@ describe('constructor', () => {
             { 'bstack:options': { wdioService: bstackServiceVersion }, 'moz:firefoxOptions': {} },
             { 'bstack:options': { wdioService: bstackServiceVersion }, 'goog:chromeOptions': {} }
         ])
+    })
+})
+
+describe('_updateCaps', () => {
+    const options: BrowserstackConfig = { browserstackLocal: true }
+    const caps: any = [{}]
+    const config = {
+        user: 'foobaruser',
+        key: '12345',
+        capabilities: []
+    }
+
+    it('should throw an error if "capabilities" is not an object/array', () => {
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const capabilities = 1
+
+        expect(() => service._updateCaps(capabilities as any, 'local'))
+            .toThrow(TypeError('Capabilities should be an object or Array!'))
+    })
+})
+
+describe('_validateApp', () => {
+    const caps: any = [{}]
+    const config = {
+        user: 'foobaruser',
+        key: '12345',
+        capabilities: []
+    }
+
+    it('should use id as app value', async() => {
+        const options: BrowserstackConfig = { app: { id: 'bs://<app-id>' } }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+
+        const app:any = await service._validateApp(options.app as any)
+        expect(app).toEqual({ app: 'bs://<app-id>', custom_id: undefined })
+    })
+
+    it('should throw error if more than two property passed in appConfig', async() => {
+        const options: BrowserstackConfig = { app: { custom_id: 'custom_id', id: 'bs://<app-id>' } }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+
+        try {
+            await service._validateApp(options.app as any)
+        } catch (e){
+            expect(e).toEqual(`keys ${Object.keys(options.app as any)} can't co-exist as app values, use any one property from
+                                {id<string>, path<string>, custom_id<string>, shareable_id<string>}, only "path" and "custom_id" can co-exist.`)
+        }
+    })
+
+    it('should throw error if property not matches path and custom_id in appConfig', async() => {
+        const options: BrowserstackConfig = { app: { custom_id: 'custom_id', id: 'bs://<app-id>' } }
+        const service = new BrowserstackLauncher(options as any, caps, config)
+
+        try {
+            await service._validateApp(options.app as any)
+        } catch (e){
+            expect(e).toEqual(`keys ${Object.keys(options.app as any)} can't co-exist as app values, use any one property from
+                                {id<string>, path<string>, custom_id<string>, shareable_id<string>}, only "path" and "custom_id" can co-exist.`)
+        }
+    })
+})
+
+describe('_uploadApp', () => {
+    const options: BrowserstackConfig = { app: '/path/to/app.apk' }
+    const caps: any = [{}]
+    const config = {
+        user: 'foobaruser',
+        key: '12345',
+        capabilities: []
+    }
+    vi.mock('got')
+
+    got.post = vi.fn().mockReturnValue({
+        json: () => Promise.resolve({ app_url: 'bs://<app-id>' })
+    })
+
+    it('should upload the app and return app_url', async() => {
+        const service = new BrowserstackLauncher(options as any, caps, config)
+        const res = await service._uploadApp(options.app as any)
+        expect(res).toEqual({ app_url: 'bs://<app-id>' })
     })
 })
