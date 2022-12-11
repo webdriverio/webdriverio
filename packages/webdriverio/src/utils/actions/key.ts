7d3ad256904b778b679e94e4c80cad6029720395
Christian Bromann <git@bromann.dev>
2022-12-11 07:12:56 +0100

    Improve key actions (#9366)

--- packages/webdriverio/src/utils/actions/key.ts
+++ packages/webdriverio/src/utils/actions/key.ts
@@ -1,8 +1,36 @@
+import os from 'node:os'
+import type { Capabilities } from '@wdio/types'
+
 import BaseAction, { BaseActionParams } from './base.js'
+import { Key } from '../../constants.js'
 
 export default class KeyAction extends BaseAction {
     constructor (instance: WebdriverIO.Browser, params?: BaseActionParams) {
         super(instance, 'key', params)
+    }
+
+    #sanitizeKey (value: string) {
+        if (typeof value !== 'string') {
+            throw new Error(`Invalid type for key input: "${typeof value}", expected a string!`)
+        }
+
+        const platformName = (this.instance.capabilities as Capabilities.Capabilities).platformName
+        const isMac = (
+            // check capabilities first
+            platformName && platformName.match(/mac(\s)*os/i) ||
+            // if not set, expect we run locally
+            os.type().match(/darwin/i)
+        )
+
+        if (value === Key.Ctrl) {
+            return isMac ? Key.Command : Key.Control
+        }
+
+        if (value.length > 1) {
+            throw new Error(`Your key input contains more than one character: "${value}", only one is allowed though!`)
+        }
+
+        return value
     }
 
     /**
@@ -10,7 +38,7 @@ export default class KeyAction extends BaseAction {
      * @param value key value
      */
     up (value: string) {
-        this.sequence.push({ type: 'keyUp', value })
+        this.sequence.push({ type: 'keyUp', value: this.#sanitizeKey(value) })
         return this
     }
 
@@ -19,7 +47,7 @@ export default class KeyAction extends BaseAction {
      * @param value key value
      */
     down (value: string) {
-        this.sequence.push({ type: 'keyDown', value })
+        this.sequence.push({ type: 'keyDown', value: this.#sanitizeKey(value) })
         return this
     }
 }
