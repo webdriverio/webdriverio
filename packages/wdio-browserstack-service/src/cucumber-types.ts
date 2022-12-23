f47959198fffc942ab398c4b0a3f04ca130cfcc5
Karan Nagpal <nagpalkaran95@gmail.com>
2022-12-23 19:01:18 +0530

    Fix: linting issue

--- packages/wdio-browserstack-service/src/cucumber-types.ts
+++ packages/wdio-browserstack-service/src/cucumber-types.ts
@@ -1,207 +1,207 @@
 // mimic types from @cucumber/@cucumber such that users don't install that dependency when not needed
 
 export interface ITestCaseHookParameter {
-  gherkinDocument: GherkinDocument;
-  pickle: Pickle;
-  result?: TestStepResult;
-  willBeRetried?: boolean;
-  testCaseStartedId: string;
+    gherkinDocument: GherkinDocument;
+    pickle: Pickle;
+    result?: TestStepResult;
+    willBeRetried?: boolean;
+    testCaseStartedId: string;
 }
 
 export declare class Duration {
-  seconds: number
-  nanos: number
+    seconds: number
+    nanos: number
 }
 
 export declare class GherkinDocument {
-  uri?: string
-  feature?: Feature
-  comments: readonly Comment[]
+    uri?: string
+    feature?: Feature
+    comments: readonly Comment[]
 }
 
 export declare class Background {
-  location: Location
-  keyword: string
-  name: string
-  description: string
-  steps: readonly Step[]
-  id: string
+    location: Location
+    keyword: string
+    name: string
+    description: string
+    steps: readonly Step[]
+    id: string
 }
 
 export declare class DataTable {
-  location: Location
-  rows: readonly TableRow[]
+    location: Location
+    rows: readonly TableRow[]
 }
 
 export declare class DocString {
-  location: Location
-  mediaType?: string
-  content: string
-  delimiter: string
+    location: Location
+    mediaType?: string
+    content: string
+    delimiter: string
 }
 
 export declare class Examples {
-  location: Location
-  tags: readonly Tag[]
-  keyword: string
-  name: string
-  description: string
-  tableHeader?: TableRow
-  tableBody: readonly TableRow[]
-  id: string
+    location: Location
+    tags: readonly Tag[]
+    keyword: string
+    name: string
+    description: string
+    tableHeader?: TableRow
+    tableBody: readonly TableRow[]
+    id: string
 }
 
 export declare class Feature {
-  location: Location
-  tags: readonly Tag[]
-  language: string
-  keyword: string
-  name: string
-  description: string
-  children: readonly FeatureChild[]
+    location: Location
+    tags: readonly Tag[]
+    language: string
+    keyword: string
+    name: string
+    description: string
+    children: readonly FeatureChild[]
 }
 
 export declare class FeatureChild {
-  rule?: Rule
-  background?: Background
-  scenario?: Scenario
+    rule?: Rule
+    background?: Background
+    scenario?: Scenario
 }
 
 export declare class Rule {
-  location: Location
-  tags: readonly Tag[]
-  keyword: string
-  name: string
-  description: string
-  children: readonly RuleChild[]
-  id: string
+    location: Location
+    tags: readonly Tag[]
+    keyword: string
+    name: string
+    description: string
+    children: readonly RuleChild[]
+    id: string
 }
 
 export declare class RuleChild {
-  background?: Background
-  scenario?: Scenario
+    background?: Background
+    scenario?: Scenario
 }
 
 export declare class Scenario {
-  location: Location
-  tags: readonly Tag[]
-  keyword: string
-  name: string
-  description: string
-  steps: readonly Step[]
-  examples: readonly Examples[]
-  id: string
+    location: Location
+    tags: readonly Tag[]
+    keyword: string
+    name: string
+    description: string
+    steps: readonly Step[]
+    examples: readonly Examples[]
+    id: string
 }
 
 export declare class Step {
-  location: Location
-  keyword: string
-  keywordType?: StepKeywordType
-  text: string
-  docString?: DocString
-  dataTable?: DataTable
-  id: string
+    location: Location
+    keyword: string
+    keywordType?: StepKeywordType
+    text: string
+    docString?: DocString
+    dataTable?: DataTable
+    id: string
 }
 
 export declare class TableCell {
-  location: Location
-  value: string
+    location: Location
+    value: string
 }
 
 export declare class TableRow {
-  location: Location
-  cells: readonly TableCell[]
-  id: string
+    location: Location
+    cells: readonly TableCell[]
+    id: string
 }
 
 export declare class Tag {
-  location: Location
-  name: string
-  id: string
+    location: Location
+    name: string
+    id: string
 }
 
 export declare class Pickle {
-  id: string
-  uri: string
-  name: string
-  language: string
-  steps: readonly PickleStep[]
-  tags: readonly PickleTag[]
-  astNodeIds: readonly string[]
+    id: string
+    uri: string
+    name: string
+    language: string
+    steps: readonly PickleStep[]
+    tags: readonly PickleTag[]
+    astNodeIds: readonly string[]
 }
 
 export declare class PickleDocString {
-  mediaType?: string
-  content: string
+    mediaType?: string
+    content: string
 }
 
 export declare class PickleStep {
-  argument?: PickleStepArgument
-  astNodeIds: readonly string[]
-  id: string
-  type?: PickleStepType
-  text: string
+    argument?: PickleStepArgument
+    astNodeIds: readonly string[]
+    id: string
+    type?: PickleStepType
+    text: string
 }
 
 export declare class PickleStepArgument {
-  docString?: PickleDocString
-  dataTable?: PickleTable
+    docString?: PickleDocString
+    dataTable?: PickleTable
 }
 
 export declare class PickleTable {
-  rows: readonly PickleTableRow[]
+    rows: readonly PickleTableRow[]
 }
 
 export declare class PickleTableCell {
-  value: string
+    value: string
 }
 
 export declare class PickleTableRow {
-  cells: readonly PickleTableCell[]
+    cells: readonly PickleTableCell[]
 }
 
 export declare class PickleTag {
-  name: string
-  astNodeId: string
+    name: string
+    astNodeId: string
 }
 
 export declare class TestStepResult {
-  duration: Duration
-  message?: string
-  status: TestStepResultStatus
+    duration: Duration
+    message?: string
+    status: TestStepResultStatus
 }
 
 export declare enum PickleStepType {
-  UNKNOWN = 'Unknown',
-  CONTEXT = 'Context',
-  ACTION = 'Action',
-  OUTCOME = 'Outcome'
+    UNKNOWN = 'Unknown',
+    CONTEXT = 'Context',
+    ACTION = 'Action',
+    OUTCOME = 'Outcome'
 }
 
 export declare enum StepKeywordType {
-  UNKNOWN = 'Unknown',
-  CONTEXT = 'Context',
-  ACTION = 'Action',
-  OUTCOME = 'Outcome',
-  CONJUNCTION = 'Conjunction'
+    UNKNOWN = 'Unknown',
+    CONTEXT = 'Context',
+    ACTION = 'Action',
+    OUTCOME = 'Outcome',
+    CONJUNCTION = 'Conjunction'
 }
 
 export declare enum TestStepResultStatus {
-  UNKNOWN = 'UNKNOWN',
-  PASSED = 'PASSED',
-  SKIPPED = 'SKIPPED',
-  PENDING = 'PENDING',
-  UNDEFINED = 'UNDEFINED',
-  AMBIGUOUS = 'AMBIGUOUS',
-  FAILED = 'FAILED'
+    UNKNOWN = 'UNKNOWN',
+    PASSED = 'PASSED',
+    SKIPPED = 'SKIPPED',
+    PENDING = 'PENDING',
+    UNDEFINED = 'UNDEFINED',
+    AMBIGUOUS = 'AMBIGUOUS',
+    FAILED = 'FAILED'
 }
 
 export declare class Location {
-  line: number
-  column?: number
+    line: number
+    column?: number
 }
 
 export declare class Comment {
-  location: Location
-  text: string
+    location: Location
+    text: string
 }
