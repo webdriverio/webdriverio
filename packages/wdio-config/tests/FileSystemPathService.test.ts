c101cb3f2a778703043870aff7b5f4ab3cdf8fa0
Christian Bromann <git@bromann.dev>
2023-01-03 14:06:47 +0100

    update unit test

--- packages/wdio-config/tests/FileSystemPathService.test.ts
+++ packages/wdio-config/tests/FileSystemPathService.test.ts
@@ -86,11 +86,9 @@ describe('FileSystemPathService', () => {
 
         it('should throw on non present files', async function () {
             const svc = new FileSystemPathService()
-            await expect(async () => await svc.loadFile(INDEX_PATH + '.tar.gz.non-existent'))
-                .rejects
-                .toThrowError(expect.objectContaining({
-                    message: expect.stringContaining('Failed to load')
-                }))
+            const error = await svc.loadFile(INDEX_PATH + '.tar.gz.non-existent')
+                .catch((err) => err) as Error
+            expect(error.message).toContain('Failed to load')
         })
     })
 })
