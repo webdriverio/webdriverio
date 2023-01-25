97ef8330b1e70b54a6a6afb0df83f98ae05f6535
Christian Bromann <git@bromann.dev>
2023-01-25 12:26:59 -0800

    skip browser component tests for Vue and Windows

--- examples/wdio/browser-runner/wdio.conf.js
+++ examples/wdio/browser-runner/wdio.conf.js
@@ -1,6 +1,19 @@
-const path = require('node:path')
+import os from 'node:os'
+import url from 'node:url'
+import path from 'node:path'
 
-exports.config = {
+/**
+ * WebdriverIO is using this example to test its component testing features
+ * and we have experienced issues with Vue when running in Windows,
+ * see https://github.com/testing-library/vue-testing-library/issues/292
+ * Please ignore and remove this in your project!
+ */
+if (process.env.CI && process.env.WDIO_PRESET === 'vue' && os.platform() === 'win32') {
+    process.exit(0)
+}
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+export const config = {
     /**
      * specify test files
      */
