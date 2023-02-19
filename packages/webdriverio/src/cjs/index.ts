f4b55fd600b27f3b3514a3d4641233722a6ff20a
Christian Bromann <git@bromann.dev>
2023-02-18 19:38:56 -0600

    Have same exports for webdriverio cjs and esm module (#9775)
    * have same exports for webdriverio cjs and esm module
    
    * don't import objects

--- packages/webdriverio/src/cjs/index.ts
+++ packages/webdriverio/src/cjs/index.ts
@@ -2,6 +2,73 @@ import type { Options, Capabilities } from '@wdio/types'
 import type * as WebDriverTypes from 'webdriver'
 import type { AttachOptions } from '../types.js'
 
+exports.SevereServiceError = class SevereServiceError extends Error {
+    constructor(message = 'Severe Service Error occurred.') {
+        super(message)
+        this.name = 'SevereServiceError'
+    }
+}
+
+exports.Key = {
+    Ctrl: 'WDIO_CONTROL',
+    NULL: '\uE000',
+    Cancel: '\uE001',
+    Help: '\uE002',
+    Backspace: '\uE003',
+    Tab: '\uE004',
+    Clear: '\uE005',
+    Return: '\uE006',
+    Enter: '\uE007',
+    Shift: '\uE008',
+    Control: '\uE009',
+    Alt: '\uE00A',
+    Pause: '\uE00B',
+    Escape: '\uE00C',
+    Space: '\uE00D',
+    PageUp: '\uE00E',
+    PageDown: '\uE00F',
+    End: '\uE010',
+    Home: '\uE011',
+    ArrowLeft: '\uE012',
+    ArrowUp: '\uE013',
+    ArrowRight: '\uE014',
+    ArrowDown: '\uE015',
+    Insert: '\uE016',
+    Delete: '\uE017',
+    Semicolon: '\uE018',
+    Equals: '\uE019',
+    Numpad0: '\uE01A',
+    Numpad1: '\uE01B',
+    Numpad2: '\uE01C',
+    Numpad3: '\uE01D',
+    Numpad4: '\uE01E',
+    Numpad5: '\uE01F',
+    Numpad6: '\uE020',
+    Numpad7: '\uE021',
+    Numpad8: '\uE022',
+    Numpad9: '\uE023',
+    Multiply: '\uE024',
+    Add: '\uE025',
+    Separator: '\uE026',
+    Subtract: '\uE027',
+    Decimal: '\uE028',
+    Divide: '\uE029',
+    F1: '\uE031',
+    F2: '\uE032',
+    F3: '\uE033',
+    F4: '\uE034',
+    F5: '\uE035',
+    F6: '\uE036',
+    F7: '\uE037',
+    F8: '\uE038',
+    F9: '\uE039',
+    F10: '\uE03A',
+    F11: '\uE03B',
+    F12: '\uE03C',
+    Command: '\uE03D',
+    ZenkakuHankaku: '\uE040'
+}
+
 export type RemoteOptions = Options.WebdriverIO & Omit<Options.Testrunner, 'capabilities' | 'rootDir'>
 
 /**
