9d42979fb22b675d9504234a7a10fd96492d8778
Christian Bromann <git@bromann.dev>
2023-02-23 18:04:41 -0800

    add example for waitForUntil command

--- packages/webdriverio/src/commands/element/waitForDisplayed.ts
+++ packages/webdriverio/src/commands/element/waitForDisplayed.ts
@@ -19,7 +19,7 @@ import type { WaitForOptions } from '../../types.js'
  * @param {Number=}          options.interval    interval between checks (default: `waitforInterval`)
  * @return {Boolean} true     if element is displayed (or doesn't if flag is set)
  * @uses utility/waitUntil, state/isDisplayed
- * @example https://github.com/webdriverio/example-recipes/blob/9ac16b4d4cf4bc8ec87f6369439a2d0bcaae4483/waitForDisplayed/index.html
+ * @example https://github.com/webdriverio/example-recipes/blob/0bfb2b8d212b627a2659b10f4449184b657e1d59/waitForDisplayed/index.html#L3-L8
  * @example https://github.com/webdriverio/example-recipes/blob/9ac16b4d4cf4bc8ec87f6369439a2d0bcaae4483/waitForDisplayed/waitForDisplayedExample.js#L6-L14
  * @type utility
  */
