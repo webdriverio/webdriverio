beee1f642a4542b244a522769487776862efc55b
Christian Bromann <git@bromann.dev>
2023-02-28 21:35:50 -0800

    move deps to optimizeDeps section

--- packages/wdio-browser-runner/src/vite/constants.ts
+++ packages/wdio-browser-runner/src/vite/constants.ts
@@ -34,7 +34,15 @@ export const DEFAULT_VITE_CONFIG: Partial<InlineConfig> = {
         }
     },
     optimizeDeps: {
-        include: ['expect', 'jest-matcher-utils'],
+        /**
+         * the following deps are CJS packages and need to be optimized (compiled to ESM) by Vite
+         */
+        include: [
+            'expect', 'jest-matcher-utils', 'serialize-error', 'minimatch', 'css-shorthand-properties',
+            'lodash.merge', 'lodash.zip', 'lodash.clonedeep', 'lodash.pickby', 'lodash.flattendeep',
+            'aria-query', 'grapheme-splitter', 'css-value', 'rgb2hex', 'p-iteration', 'fast-safe-stringify',
+            'deepmerge-ts'
+        ],
         esbuildOptions: {
             logLevel: 'silent',
             // Node.js global to browser globalThis
