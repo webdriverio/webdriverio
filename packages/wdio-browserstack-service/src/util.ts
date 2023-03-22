277048f98521f03dfc1e563f16f4a44207db7927
sriteja777 <sritejakittu777@gmail.com>
2023-03-22 15:39:17 +0530

    fixed eslint

--- packages/wdio-browserstack-service/src/util.ts
+++ packages/wdio-browserstack-service/src/util.ts
@@ -477,7 +477,9 @@ export function isBStackSession(config: Options.Testrunner) {
 }
 
 export function shouldAddServiceVersion(config: Options.Testrunner, testObservability?: boolean): boolean {
-    if (!isBStackSession(config)) return false
+    if (!isBStackSession(config)) {
+        return false
+    }
     if (config.services && config.services.toString().includes('chromedriver') && testObservability !== false) {
         return false
     }
