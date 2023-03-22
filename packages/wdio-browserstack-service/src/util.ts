f990af0b0f8942b21256c7a0c7dbd992de513689
sriteja777 <sritejakittu777@gmail.com>
2023-03-22 15:33:55 +0530

    added observability and browserstack check

--- packages/wdio-browserstack-service/src/util.ts
+++ packages/wdio-browserstack-service/src/util.ts
@@ -469,7 +469,15 @@ export function isScreenshotCommand (args: BeforeCommandArgs & AfterCommandArgs)
     return args.endpoint && args.endpoint.includes('/screenshot')
 }
 
+export function isBStackSession(config: Options.Testrunner) {
+    if (typeof config.user === 'string' && typeof config.key === 'string' && config.key.length === 20) {
+        return true
+    }
+    return false
+}
+
 export function shouldAddServiceVersion(config: Options.Testrunner, testObservability?: boolean): boolean {
+    if (!isBStackSession(config)) return false
     if (config.services && config.services.toString().includes('chromedriver') && testObservability !== false) {
         return false
     }
