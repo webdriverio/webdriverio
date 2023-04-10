adb4ee6dbcfe993d34d8e85d2b9e4d9217e86d16
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2023-04-11 01:31:16 +0300

    feat(mock): emit 'request', 'overwrite', 'fail', 'match', 'continue' events (#10137)
    * feat(mock): emit 'request', 'overwrite', 'fail', 'match', 'continue' events
    
    * docs(mock): add event examples
    
    * Update website/docs/api/Mock.md
    
    ---------
    
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- packages/webdriverio/src/types.ts
+++ packages/webdriverio/src/types.ts
@@ -1,5 +1,6 @@
 import type { EventEmitter } from 'node:events'
 import type { AttachOptions as DevToolsAttachOptions } from 'devtools'
+import type { Protocol } from 'devtools-protocol'
 import type { SessionFlags, AttachOptions as WebDriverAttachOptions } from 'webdriver'
 import type { Options, Capabilities, FunctionProperties, ThenArg } from '@wdio/types'
 import type { ElementReference, ProtocolCommands } from '@wdio/protocols'
@@ -8,6 +9,7 @@ import type { Browser as PuppeteerBrowser } from 'puppeteer-core/lib/esm/puppete
 import type * as BrowserCommands from './commands/browser.js'
 import type * as ElementCommands from './commands/element.js'
 import type DevtoolsInterception from './utils/interception/devtools.js'
+import type { Matches } from './utils/interception/types.js'
 
 type $BrowserCommands = typeof BrowserCommands
 type $ElementCommands = typeof ElementCommands
@@ -443,7 +445,38 @@ export type DragAndDropCoordinate = {
 /**
  * WebdriverIO Mock definition
  */
-type MockFunctions = FunctionProperties<DevtoolsInterception>
+
+interface RequestEvent {
+    requestId: number
+    request: Matches
+    responseStatusCode: number
+    responseHeaders: Record<string, string>
+}
+
+interface MatchEvent extends Matches {
+    mockedResponse?: string | Buffer
+}
+
+interface OverwriteEvent {
+    requestId: number
+    responseCode: number
+    responseHeaders: Record<string, string>
+    body?: string | Record<string, any>
+}
+
+interface FailEvent {
+    requestId: number
+    errorReason: Protocol.Network.ErrorReason
+}
+
+interface MockFunctions extends Omit<FunctionProperties<DevtoolsInterception>, 'on'> {
+    on(event: 'request', callback: (request: RequestEvent) => void): Mock
+    on(event: 'match', callback: (match: MatchEvent) => void): Mock
+    on(event: 'continue', callback: (requestId: number) => void): Mock
+    on(event: 'overwrite', callback: (response: OverwriteEvent) => void): Mock
+    on(event: 'fail', callback: (error: FailEvent) => void): Mock
+}
+
 type MockProperties = Pick<DevtoolsInterception, 'calls'>
 export interface Mock extends MockFunctions, MockProperties {}
 
