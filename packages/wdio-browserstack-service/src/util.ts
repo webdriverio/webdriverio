d526ee3ca7772b4521c7e23e994dca97bcdd6dc4
amaanbs <amaan.h@browserstack.com>
2023-04-13 18:49:16 +0530

    remove buildTag filter

--- packages/wdio-browserstack-service/src/util.ts
+++ packages/wdio-browserstack-service/src/util.ts
@@ -526,16 +526,7 @@ export function getObservabilityBuildTags(options: BrowserstackConfig & Options.
         return process.env.TEST_OBSERVABILITY_BUILD_TAG.split(',')
     }
     if (options.testObservabilityOptions && options.testObservabilityOptions.buildTag) {
-        const testObservabilitybuildTags = options.testObservabilityOptions.buildTag
-        if (Array.isArray(testObservabilitybuildTags) &&
-            testObservabilitybuildTags.length > 0 &&
-            testObservabilitybuildTags.every((value) => {
-                return typeof value === 'string'
-            })
-        ) {
-            return testObservabilitybuildTags
-        }
-        log.debug('[Start_Build] Observability build tags not detected, ensure that the buildTag capability under testObservabilityOptions is passed an array of strings')
+        return options.testObservabilityOptions.buildTag
     }
     if (bstackBuildTag) {
         return [bstackBuildTag]
