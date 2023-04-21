5fa106aeecf681d9ae569a4ae15605f6a7c268d9
andrew-tompkins <andrew.tompkins@therealreal.com>
2023-04-21 14:21:32 -0600

    Update to allow for --spec cli

--- packages/wdio-config/src/lib/ConfigParser.ts
+++ packages/wdio-config/src/lib/ConfigParser.ts
@@ -65,7 +65,6 @@ export default class ConfigParser {
         if (_initialConfig.spec) {
             _initialConfig.spec = makeRelativeToCWD(_initialConfig.spec) as string[]
         }
-        this.merge(_initialConfig)
     }
 
     /**
