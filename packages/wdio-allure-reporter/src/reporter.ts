45d936c1795eaf45141f397563533e132c6d68d3
JordiSAGE <jordi.lagunas@sage.com>
2023-06-14 16:47:31 +0200

    Revert "Start step when testTitle or attachment"
    This reverts commit b290aeb88b9457d740b3882fe5263ba8dae04a45.

--- packages/wdio-allure-reporter/src/reporter.ts
+++ packages/wdio-allure-reporter/src/reporter.ts
@@ -419,14 +419,12 @@ export default class AllureReporter extends WDIOReporter {
             const argument = testObj?.argument as Argument
             const dataTable = argument?.rows?.map((a: { cells: string[] }) => a?.cells)
 
-            if (testTitle || dataTable) {
-                this._startStep(testTitle)
+            this._startStep(testTitle)
 
-                if (dataTable) {
-                    this.attachFile('Data Table', stringify(dataTable), ContentType.CSV)
-                }
-                return
+            if (dataTable) {
+                this.attachFile('Data Table', stringify(dataTable), ContentType.CSV)
             }
+            return
         }
 
         this._startTest(testTitle, test.cid)
