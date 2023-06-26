405dc3674c0b89303aa213ac9070a0391f5885f0
sriteja777 <sritejakittu777@gmail.com>
2023-06-26 19:29:45 +0530

    added tests

--- packages/wdio-browserstack-service/tests/insights-handler.test.ts
+++ packages/wdio-browserstack-service/tests/insights-handler.test.ts
@@ -349,6 +349,22 @@ describe('attachHookData', () => {
         } as any, 'hook_id')
         expect(insightsHandler['_hooks']).toEqual({ 'parent - test': ['hook_id_old', 'hook_id'] })
     })
+
+    it('add hook data in test from suite tests', () =>{
+        insightsHandler['_hooks'] = {}
+        insightsHandler['attachHookData']({
+            test: {
+                parent: {
+                    tests: [{
+                        title: 'test',
+                        parent: 'parent'
+                    }],
+                }
+            }
+        } as any, 'hook_id_from_test')
+        expect(insightsHandler['_hooks']).toEqual({ 'parent - test': ['hook_id_from_test'] })
+    })
+
 })
 
 describe('getHierarchy', () => {
@@ -375,6 +391,43 @@ describe('getHierarchy', () => {
 
     it('return empty array when no context present', () => {
         expect(insightsHandler['getHierarchy']({} as any)).toEqual([])
+    })
+})
+
+describe('getTestRunId', function () {
+    let insightsHandler: InsightsHandler
+    beforeEach(() => {
+        insightsHandler = new InsightsHandler(browser, false, 'framework')
+    })
+
+    it('should return if null context', () => {
+        expect(insightsHandler.getTestRunId(null)).toEqual(undefined)
+    })
+
+    it('return test id from current test', () => {
+        const identifier = 'parent title - some title'
+        insightsHandler['_tests'] = { [identifier]: { uuid: '1234' } }
+        expect(insightsHandler.getTestRunId({
+            currentTest: {
+                title: 'some title',
+                parent: 'parent title'
+            }
+        })).toEqual('1234')
+    })
+
+    it('return test id from test', () => {
+        const identifier = 'parent title - child title'
+        insightsHandler['_tests'] = { [identifier]: { uuid: 'some_uuid' } }
+        expect(insightsHandler.getTestRunId({
+            test: {
+                parent: {
+                    tests: [{
+                        title: 'child title',
+                        parent: 'parent title'
+                    }]
+                },
+            }
+        })).toEqual('some_uuid')
     })
 })
 
