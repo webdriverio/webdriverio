b29c1f4525b5fdf5d5b298b00cc549ed18b1778d
Christian Bromann <git@bromann.dev>
2023-07-19 11:13:35 -0700

    finish driver setup

--- packages/webdriverio/src/driver/index.ts
+++ packages/webdriverio/src/driver/index.ts
@@ -7,18 +7,17 @@ import getPort from 'get-port'
 import waitPort from 'wait-port'
 import logger from '@wdio/logger'
 import WebDriver from 'webdriver'
-// @ts-expect-error
-import { getChromeVersion } from '@testim/chrome-version'
+import { deepmerge } from 'deepmerge-ts'
 
 import { start as startSafaridriver, type SafaridriverOptions } from 'safaridriver'
 import { start as startGeckodriver, type GeckodriverParameters } from 'geckodriver'
 import { start as startEdgedriver, type EdgedriverParameters } from 'edgedriver'
-import { install, getInstalledBrowsers, Browser, type InstallOptions } from '@puppeteer/browsers'
+import { install, computeExecutablePath, Browser, type InstallOptions } from '@puppeteer/browsers'
 
 import type { Capabilities } from '@wdio/types'
 
 import detectBackend from './detectBackend.js'
-import { parseParams } from './utils.js'
+import { parseParams, setupChrome } from './utils.js'
 import { SUPPORTED_BROWSERNAMES } from '../constants.js'
 import type { RemoteOptions } from '../types'
 
@@ -94,73 +93,66 @@ export async function startWebDriver (options: RemoteOptions) {
     }
 
     const port = await getPort()
-    if (caps.browserName.toLowerCase() === 'chrome') {
-        const chromeVersion = await getChromeVersion().catch(() => undefined) as string | undefined
+    if (SUPPORTED_BROWSERNAMES.chrome.includes(caps.browserName.toLowerCase())) {
         const chromedriverOptions = caps['wdio:chromedriverOptions'] || ({} as ChromedriverOptions)
-        const cacheDir = chromedriverOptions.cacheDir || path.resolve(__dirname, '..', '..', '.chromedriver')
+        const cacheDir = chromedriverOptions.cacheDir || path.resolve(__dirname, '..', '..', '.chrome')
         const exist = await fs.access(cacheDir).then(() => true, () => false)
-
         if (!exist) {
             await fs.mkdir(cacheDir, { recursive: true })
         }
 
-        const installedBrowsers = await getInstalledBrowsers({ cacheDir })
-        const cachedChromeBrowserVersion: string | undefined = caps.browserVersion && installedBrowsers.find((browser) => (
-            browser.browser === Browser.CHROME &&
-            browser.buildId.startsWith(caps.browserVersion!)
-        ))?.buildId
-        const buildId = caps.browserVersion || chromeVersion || 'stable'
-
-        let chromedriver = installedBrowsers.find((browser) => (
-            browser.browser === Browser.CHROMEDRIVER &&
-            browser.buildId.startsWith(buildId)
-        ))
-        if (!chromedriver) {
-            chromedriver = await install({
-                ...chromedriverOptions,
-                cacheDir,
-                buildId,
-                browser: Browser.CHROMEDRIVER,
-                downloadProgressCallback: () => {}
-            })
-        }
-
-        if (!cachedChromeBrowserVersion && !chromeVersion) {
-            await install({
-                ...chromedriverOptions,
-                cacheDir,
-                buildId: caps.browserVersion || buildId,
-                browser: Browser.CHROME,
-                downloadProgressCallback: () => {}
-            })
-        }
-
-        /**
-         * this is currently necessary due to https://github.com/puppeteer/puppeteer/issues/10587
-         */
-        const chromedriverDir = (await fs.readdir(chromedriver.path)).find((entry) => entry.startsWith('chromedriver'))
-        const absoluteChromedriverPath = path.join(chromedriver.path, chromedriverDir!, 'chromedriver' + (process.platform === 'win32' ? '.exe' : ''))
-
-        driverProcess = cp.spawn(absoluteChromedriverPath, parseParams({ port, ...chromedriverOptions }))
-        driver = `ChromeDriver v${buildId}`
-    } else if (caps.browserName.toLowerCase() === 'safari') {
+        const { executablePath, buildId, platform } = await setupChrome(caps, cacheDir)
+        log.info(`Downloading Chromedriver v${buildId}`)
+        await install({
+            ...chromedriverOptions,
+            cacheDir,
+            platform,
+            buildId,
+            browser: Browser.CHROMEDRIVER,
+            downloadProgressCallback: () => {}
+        })
+        const chromedriverBinaryPath = computeExecutablePath({
+            browser: Browser.CHROMEDRIVER,
+            buildId,
+            cacheDir
+        })
+        caps['goog:chromeOptions'] = deepmerge(
+            { binary: executablePath },
+            caps['goog:chromeOptions'] || {}
+        )
+        chromedriverOptions.allowedOrigins = ['*']
+        chromedriverOptions.allowedIps = ['']
+        const driverParams = parseParams({ port, ...chromedriverOptions })
+        driverProcess = cp.spawn(chromedriverBinaryPath, driverParams)
+        driver = `ChromeDriver v${buildId} with params ${driverParams.join(' ')}`
+    } else if (SUPPORTED_BROWSERNAMES.safari.includes(caps.browserName.toLowerCase())) {
         driver = 'SafariDriver'
         driverProcess = startSafaridriver({
             ...(caps['wdio:safaridriverOptions'] || {}),
             port
         })
-    } else if (caps.browserName.toLowerCase() === 'firefox') {
+
+        /**
+         * set "Host" header as it is required by Safaridriver
+         */
+        options.headers = deepmerge({ Host: 'localhost' }, (options.headers || {}))
+    } else if (SUPPORTED_BROWSERNAMES.firefox.includes(caps.browserName.toLowerCase())) {
         driver = 'GeckoDriver'
         driverProcess = await startGeckodriver({
             ...(caps['wdio:geckodriverOptions'] || {}),
             port
         })
-    } else if (['microsoftedge', 'edge'].includes(caps.browserName.toLowerCase())) {
+    } else if (SUPPORTED_BROWSERNAMES.edge.includes(caps.browserName.toLowerCase())) {
+        const edgedriverOptions = caps['wdio:chromedriverOptions'] || ({} as ChromedriverOptions)
+        edgedriverOptions.allowedOrigins = ['*']
+        edgedriverOptions.allowedIps = ['']
         driver = 'EdgeDriver'
-        driverProcess = await startEdgedriver({
-            ...(caps['wdio:geckodriverOptions'] || {}),
-            port
-        })
+        driverProcess = await startEdgedriver({ ...edgedriverOptions, port })
+
+        /**
+         * Microsoft Edge is very particular when it comes to browser names
+         */
+        caps.browserName = 'MicrosoftEdge'
     } else {
         throw new Error(
             `Unknown browser name "${caps.browserName}". Make sure to pick from one of the following ` +
--- packages/webdriverio/src/driver/utils.ts
+++ packages/webdriverio/src/driver/utils.ts
@@ -1,7 +1,18 @@
-import decamelize from 'decamelize'
-import type { EdgedriverParameters } from 'edgedriver'
+import cp from 'node:child_process'
 
+import decamelize from 'decamelize'
+import logger from '@wdio/logger'
+import { getChromePath } from 'chrome-launcher'
+import {
+    install, canDownload, resolveBuildId, detectBrowserPlatform, Browser, ChromeReleaseChannel,
+    computeExecutablePath, type InstallOptions
+} from '@puppeteer/browsers'
+import type { EdgedriverParameters } from 'edgedriver'
+import type { Capabilities } from '@wdio/types'
+
+const log = logger('webdriver')
 const EXCLUDED_PARAMS = ['version', 'help']
+
 export function parseParams(params: EdgedriverParameters) {
     return Object.entries(params)
         .filter(([key,]) => !EXCLUDED_PARAMS.includes(key))
@@ -14,4 +25,66 @@ export function parseParams(params: EdgedriverParameters) {
         })
         .flat()
         .filter(Boolean)
+}
+
+export function getLocalChromePath() {
+    try {
+        const chromePath = getChromePath()
+        return chromePath
+    } catch (err: unknown) {
+        return
+    }
+}
+
+export function getBuildIdByPath(chromePath?: string) {
+    if (!chromePath) {
+        return
+    }
+    const versionString = cp.execSync(`"${chromePath}" --version`).toString()
+    return versionString.split(' ').pop()?.trim()
+}
+
+export async function setupChrome(caps: Capabilities.Capabilities, cacheDir: string) {
+    const platform = detectBrowserPlatform()
+    if (!platform) {
+        throw new Error('The current platform is not supported.')
+    }
+
+    if (!caps.browserVersion) {
+        const executablePath = getLocalChromePath()!
+        const tag = getBuildIdByPath(executablePath)
+
+        /**
+         * verify that we have a valid Chrome browser installed
+         */
+        if (tag) {
+            return {
+                platform,
+                executablePath,
+                buildId: await resolveBuildId(Browser.CHROME, platform, tag)
+            }
+        }
+    }
+
+    /**
+     * otherwise download provided Chrome browser version or "stable"
+     */
+    const tag = caps.browserVersion || ChromeReleaseChannel.STABLE
+    const buildId = await resolveBuildId(Browser.CHROME, platform, tag)
+    const installOptions: InstallOptions = {
+        cacheDir,
+        platform,
+        buildId,
+        browser: Browser.CHROME,
+        downloadProgressCallback: () => {}
+    }
+    const isCombinationAvailable = await canDownload(installOptions)
+    if (!isCombinationAvailable) {
+        throw new Error(`Couldn't find a matching Chrome browser for tag "${buildId}" on platform "${platform}"`)
+    }
+
+    log.info(`Downloading Chrome v${buildId}`)
+    await install(installOptions)
+    const executablePath = computeExecutablePath(installOptions)
+    return { executablePath, buildId, platform }
 }
