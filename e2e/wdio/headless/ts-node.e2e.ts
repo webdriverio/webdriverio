5dbb6b72620fea4bbef45625c5788483c2d5b2e0
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-07-29 21:42:45 +0100

    (@wdio/cli): corrected TypeScript loading

---
+++ e2e/wdio/headless/ts-node.e2e.ts
@@ -0,0 +1,16 @@
+import { describe, it } from 'mocha'
+
+describe('WebdriverIO', () => {
+
+    it('should support source maps when running TypeScript files', async () => {
+        const location = getInvocationLocation()
+
+        expect(location).toMatch(/ts-node.e2e.ts:6:26/)
+    })
+})
+
+function getInvocationLocation(): string {
+    const error = new Error('example error')
+    const lines = error.stack.split('\n')
+    return lines[2]
+}
