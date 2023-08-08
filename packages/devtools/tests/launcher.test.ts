aeb6e64c10d873b244d847ba6884b047f49adc76
mighty98 <32213921+mighty98@users.noreply.github.com>
2023-08-08 11:15:37 +0530

    Update launcher.ts to handle defaultViewport (#10883)
    * Update launcher.ts to handle defaultViewport
    
    * Update packages/devtools/src/launcher.ts
    
    Co-authored-by: Christian Bromann <git@bromann.dev>
    
    * Added unit test
    
    * Update launcher.ts
    
    * Update launcher.test.ts
    
    * Update launcher.ts
    
    remove whitespace
    
    * Update launcher.ts
    
    * Update launcher.test.ts
    
    * Update launcher.ts
    
    ---------
    
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- packages/devtools/tests/launcher.test.ts
+++ packages/devtools/tests/launcher.test.ts
@@ -740,3 +740,17 @@ test('launch Firefox while ignoring default args and not specifying W3C capabili
         })
     )
 })
+
+test('launch Chrome with defaultViewPort as null', async () => {
+    await launch({
+        browserName: 'chrome',
+        'wdio:devtoolsOptions': { defaultViewport: null }
+    })
+    expect(launchChromeBrowser).toBeCalledWith(
+        expect.objectContaining({
+            chromeFlags: expect.not.arrayContaining(
+                ['--window-size=1200,900', '--window-position=0,0']
+            )
+        })
+    )
+})
