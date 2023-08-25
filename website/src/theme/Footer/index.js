c78a5f7133e62b378eed432ee9d5fbed44239357
Nicolas <nicolascamara29@gmail.com>
2023-08-25 15:54:23 -0400

    Nick: implementation of mendable

---
+++ website/src/theme/Footer/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import { MendableFloatingButton } from '@mendable/search'
+
+export default function FooterWrapper() {
+    const {
+        siteConfig: { customFields },
+    } = useDocusaurusContext()
+
+    return (
+        <>
+            <MendableFloatingButton
+                style={{
+                    darkMode: true,
+                    accentColor: '#EA5906',
+                }}
+                icon={
+                    <img src="/img/mendable/robot-white.png" alt="Mendable Robot Icon" width={'40px'} />
+                }
+                floatingButtonStyle={{
+                    color: '#ffffff',
+                    backgroundColor: '#EA5906',
+                }}
+                dialogCustomStyle={{
+                    dialogTopMargin: '64px',
+                }}
+                cmdShortcutKey='j'
+                anon_key={customFields.mendableAnonKey}
+                dismissPopupAfter={5}
+                dialogPlaceholder='Why Webdriver.IO?'
+                welcomeMessage='Welcome! How can I help?'
+            />
+        </>
+    )
+}
