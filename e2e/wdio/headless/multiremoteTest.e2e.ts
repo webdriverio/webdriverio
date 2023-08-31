a5151c49515f3380f2fdf79db1b6d4f260470773
ccharnkij <ccharnkij@gmail.com>
2023-08-30 20:40:53 -0700

    devtools commands for multiremote

--- e2e/wdio/headless/multiremoteTest.e2e.ts
+++ e2e/wdio/headless/multiremoteTest.e2e.ts
@@ -18,4 +18,22 @@ describe('main suite 1', () => {
         await browserB.keys(Key.Enter)
         await expect(browserB.$('.inputMessage')).toHaveAttribute('placeHolder', 'Type here...')
     })
+
+    it('should return devtools result', async () => {
+        await browserA.url('https://webdriver.io')
+        await browserB.url('https://google.com')
+
+        const cookiesA = await browserA.cdp('Network', 'getCookies')
+        const cookiesB = await browserB.cdp('Network', 'getCookies')
+        const cookies = await browser.cdp('Network', 'getCookies')
+
+        expect(Object.keys(cookiesA).length).toBe(1)
+        expect(Object.keys(cookiesA)).toEqual(['cookies'])
+        expect(Object.keys(cookiesB).length).toBe(1)
+        expect(Object.keys(cookiesB)).toEqual(['cookies'])
+        expect(Object.keys(cookies).length).toBe(2)
+
+        const score = await browser.checkPWA()
+        expect(Object.keys(score).length).toBe(2)
+    })
 })
