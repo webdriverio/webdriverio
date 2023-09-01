c38a6ec97d25a202acfad795ef0369e38f6633f4
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2023-09-01 16:57:49 +0200

    fix(@wdio-utils): revert some changes, block all connections but 0.0.0.0 by default

--- packages/wdio-utils/src/driver/index.ts
+++ packages/wdio-utils/src/driver/index.ts
@@ -39,7 +39,7 @@ export async function startWebDriver (options: Options.WebDriver) {
      * in case we are running unit tests, just return
      */
     if (process.env.WDIO_SKIP_DRIVER_SETUP) {
-        options.hostname = 'localhost'
+        options.hostname = '0.0.0.0'
         options.port = 4321
         return
     }
@@ -86,6 +86,8 @@ export async function startWebDriver (options: Options.WebDriver) {
             { binary: chromeExecuteablePath },
             caps['goog:chromeOptions'] || {}
         )
+        chromedriverOptions.allowedOrigins = chromedriverOptions.allowedOrigins || ['*']
+        chromedriverOptions.allowedIps = chromedriverOptions.allowedIps || ['0.0.0.0']
         const driverParams = parseParams({ port, ...chromedriverOptions })
         driverProcess = cp.spawn(chromedriverExcecuteablePath, driverParams)
         driver = `Chromedriver v${browserVersion} with params ${driverParams.join(' ')}`
@@ -126,7 +128,7 @@ export async function startWebDriver (options: Options.WebDriver) {
         }
 
         driver = 'GeckoDriver'
-        driverProcess = await startGeckodriver({ ...geckodriverOptions, cacheDir, port, host: 'localhost' })
+        driverProcess = await startGeckodriver({ ...geckodriverOptions, cacheDir, port })
     } else if (isEdge(caps.browserName)) {
         /**
          * Microsoft Edge
@@ -175,7 +177,7 @@ export async function startWebDriver (options: Options.WebDriver) {
     await waitPort({ port, output: 'silent', timeout: DRIVER_WAIT_TIMEOUT })
         .catch((e) => { throw new Error(`Timed out to connect to ${driver}: ${e.message}`) })
 
-    options.hostname = 'localhost'
+    options.hostname = '0.0.0.0'
     options.port = port
     log.info(`Started ${driver} in ${Date.now() - start}ms on port ${port}`)
     return driverProcess
