4b7f2a30395e69e6fdc3ff4d12651a8f12792539
Tamil Selvan <tamil777selvan@gmail.com>
2023-09-15 08:21:02 -0700

    handle timeout exceptions (#11172)

--- packages/wdio-utils/tests/shim.test.ts
+++ packages/wdio-utils/tests/shim.test.ts
@@ -1,5 +1,5 @@
 import { vi, describe, it, expect } from 'vitest'
-import { wrapCommand } from '../src/shim.js'
+import { wrapCommand, executeAsync } from '../src/shim.js'
 
 describe('wrapCommand', () => {
     it('should run command with before and after hook', async () => {
@@ -41,5 +41,39 @@ describe('wrapCommand', () => {
 
         expect(afterHook).toBeCalledTimes(3)
         expect(afterHook).toBeCalledWith('someCommand', [123, 'barfoo'], undefined, error)
+    })
+})
+
+describe('executeAsync', () => {
+    it('should trigger a timeout exception if the function finishes within the specified timeframe', async () => {
+        const fn = () => new Promise((resolve) => setTimeout(resolve, 300))
+        const result = await executeAsync(fn, { attempts: 1, limit: 1 }, [], 200).catch((err) => err.message)
+        expect(result).toEqual('Timeout')
+    })
+
+    it('should not trigger a timeout exception if the function finishes within the specified timeframe', async () => {
+        const fn = () => new Promise((resolve) => setTimeout(() => resolve(true), 100))
+        const result = await executeAsync(fn, { attempts: 1, limit: 1 }, [], 300)
+        expect(result).toBe(true)
+    })
+
+    it('should retry', async () => {
+        let attempts = 0
+        const retryFunction = () => {
+            attempts++
+            if (attempts < 3) {
+                return Promise.reject(new Error('Failed'))
+            }
+            return Promise.resolve('Success')
+        }
+        const result = await executeAsync(retryFunction, { attempts: 1, limit: 3 }, [], 300)
+        expect(attempts).to.equal(3)
+        expect(result).to.equal('Success')
+    })
+
+    it('should handle errors during execution', async () => {
+        const fn = () => Promise.reject(new Error('Execution Error'))
+        const result = await executeAsync(fn, { attempts: 1, limit: 1 }, [], 200).catch((err) => err.message)
+        expect(result).toEqual('Execution Error')
     })
 })
