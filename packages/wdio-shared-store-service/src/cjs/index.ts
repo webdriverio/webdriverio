0239ec5041d2321c6e33f4f02bd861b861dd32dd
Oscar Cornejo <ocornejo@netflix.com>
2023-09-26 12:17:54 -0700

    fixed issue with jest

--- packages/wdio-shared-store-service/src/cjs/index.ts
+++ packages/wdio-shared-store-service/src/cjs/index.ts
@@ -1,26 +1,24 @@
-const esmModule = import('../index.js')
-
 exports.setValue = async function (key: string, value: never) {
-    const m = await esmModule
+    const m = await import('../index.js')
     return m.setValue(key, value)
 }
 
 exports.getValue = async function (key: string) {
-    const m = await esmModule
+    const m = await import('../index.js')
     return m.getValue(key)
 }
 
 exports.setResourcePool = async function (key: string, value: never) {
-    const m = await esmModule
+    const m = await import('../index.js')
     return m.setResourcePool(key, value)
 }
 
 exports.getValueFromPool = async function (key: string, options: never) {
-    const m = await esmModule
+    const m = await import('../index.js')
     return m.getValueFromPool(key, options)
 }
 
 exports.addValueToPool = async function (key: string, value: never) {
-    const m = await esmModule
+    const m = await import('../index.js')
     return m.addValueToPool(key, value)
 }
