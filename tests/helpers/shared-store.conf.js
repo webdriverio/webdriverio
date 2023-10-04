a72f907aadc349335bfbd53d6931e4b333eb9856
Christian Bromann <git@bromann.dev>
2023-10-04 08:27:59 -0700

    (test): add smoke tests for setting values to shared value store in onPrepare

--- tests/helpers/shared-store.conf.js
+++ tests/helpers/shared-store.conf.js
@@ -1,6 +1,12 @@
+import { setResourcePool, setValue } from '@wdio/shared-store-service'
 import { config as baseConfig } from './config.js'
 
 export const config = Object.assign({}, baseConfig, {
+    async onPrepare () {
+        await setResourcePool('availableUrls', ['url01.com', 'url02.com'])
+        await setValue('testKey', 'testValue')
+    },
+
     async beforeSuite () {
         await browser.sharedStore.setResourcePool('test-resource-pool', [1, 2, 3])
         await browser.sharedStore.set(browser.sessionId, browser.capabilities)
