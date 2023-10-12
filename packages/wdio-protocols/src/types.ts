e8d07388908cc2535d2671329fe63fdf1a523717
Tamil Selvan <tamil777selvan@gmail.com>
2023-10-12 11:00:44 +0530

    Updating appium protocols

--- packages/wdio-protocols/src/types.ts
+++ packages/wdio-protocols/src/types.ts
@@ -128,6 +128,10 @@ export interface CommandEndpoint {
      */
     ref: string
     /**
+     * description for the deprecated command
+     */
+    deprecated?: string
+    /**
      * supported command parameters
      */
     parameters: CommandParameters[]
