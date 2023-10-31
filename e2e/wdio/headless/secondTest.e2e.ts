f972312436ce2515f27ebca55637eca3e54cb0ec
Siarhei_Kliushnikau <siarhei_kliushnikau@epam.com>
2023-10-31 10:36:28 +0100

    test fix

--- e2e/wdio/headless/secondTest.e2e.ts
+++ e2e/wdio/headless/secondTest.e2e.ts
@@ -5,7 +5,7 @@ describe('main suite 1', () => {
         const caps = browser.capabilities as WebdriverIO.Capabilities
         const assertionValue = caps.browserName === 'msedge'
             ? 'headlessedg'
-            : caps.browserName && caps.browserName.includes('chrome') ? 'headlesschrome' : caps.browserName!.toLowerCase()
+            : caps.browserName && caps.browserName.includes('chrome') ? 'chrome/' : caps.browserName!.toLowerCase()
         await browser.url('http://guinea-pig.webdriver.io/')
         await expect((await $('#useragent').getText()).toLowerCase()).toContain(assertionValue)
     })
