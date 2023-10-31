786be985ab9e8dde4fbc79c72e780eea75deb691
Siarhei_Kliushnikau <siarhei_kliushnikau@epam.com>
2023-10-31 20:45:02 +0100

    reverted click test and deleted unit for moveto as it's available only for e2e currently because of scrollIntoView action

--- packages/webdriverio/tests/commands/element/moveTo.test.ts
+++ packages/webdriverio/tests/commands/element/moveTo.test.ts
@@ -9,43 +9,6 @@ vi.mock('got')
 vi.mock('@wdio/logger', () => import(path.join(process.cwd(), '__mocks__', '@wdio/logger')))
 
 describe('moveTo', () => {
-    it('should do a moveTo without params', async () => {
-        const browser = await remote({
-            baseUrl: 'http://foobar.com',
-            capabilities: {
-                browserName: 'foobar'
-            }
-        })
-
-        const elem = await browser.$('#elem')
-        // @ts-ignore mock feature
-        got.setMockResponse([undefined])
-        await elem.moveTo()
-
-        expect(vi.mocked(got).mock.calls[3][0]!.pathname).toContain('/foobar-123/actions')
-        expect(vi.mocked(got).mock.calls[3][1]!.json.actions).toHaveLength(1)
-        expect(vi.mocked(got).mock.calls[3][1]!.json.actions[0].type).toBe('pointer')
-        expect(vi.mocked(got).mock.calls[3][1]!.json.actions[0].actions).toHaveLength(1)
-        expect(vi.mocked(got).mock.calls[3][1]!.json.actions[0].actions[0])
-            .toMatchSnapshot()
-    })
-
-    it('should do a moveTo with params', async () => {
-        const browser = await remote({
-            baseUrl: 'http://foobar.com',
-            capabilities: {
-                browserName: 'foobar'
-            }
-        })
-
-        const elem = await browser.$('#elem')
-        // @ts-ignore mock feature
-        got.setMockResponse([undefined])
-        await elem.moveTo({ xOffset: 5, yOffset: 10 })
-        expect(vi.mocked(got).mock.calls[3][1]!.json.actions[0].actions[0])
-            .toMatchSnapshot()
-    })
-
     it('should do a moveTo without params (no-w3c)', async () => {
         const browser = await remote({
             baseUrl: 'http://foobar.com',
