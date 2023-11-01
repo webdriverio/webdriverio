58964bf6958468089025fdde620f476a4f3b15f7
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2023-11-01 09:49:02 +0100

    (webdriverio): resolve PR feedback

--- packages/webdriverio/src/commands/browser/throttle.ts
+++ packages/webdriverio/src/commands/browser/throttle.ts
@@ -1,5 +1,8 @@
 import type { ThrottleOptions } from '../../utils/interception/types.js'
+import logger from '@wdio/logger'
 import { getBrowserObject } from '../../utils/index.js'
+
+const log = logger('webdriverio:throttle')
 
 /**
  * Alias for throttleNetwork. The browser.throttle command will be removed in v9.
@@ -8,7 +11,7 @@ export async function throttle (
     this: WebdriverIO.Browser,
     params: ThrottleOptions
 ) {
-    console.log('Deprecated: use throttleNetwork instead')
+    log.warn('Command "throttle" is deprecated and will be removed with the next major version release! Use `throttleNetwork` instead.')
     const browser = getBrowserObject(this)
     await browser.throttleNetwork(params)
 }
