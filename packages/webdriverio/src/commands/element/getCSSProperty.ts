2b65212f78e9feac74ae2f273eb343735e6a5dd6
Pawel1894 <pawepohl@gmail.com>
2023-11-01 14:00:47 +0100

    Enable to get css value of pseudo-element

--- packages/webdriverio/src/commands/element/getCSSProperty.ts
+++ packages/webdriverio/src/commands/element/getCSSProperty.ts
@@ -1,6 +1,11 @@
 import cssShorthandProps from 'css-shorthand-properties'
 import { parseCSS } from '../../utils/index.js'
 
+type PseudoElement = {
+    target: '::before'|'::after',
+    browser: WebdriverIO.Browser | WebdriverIO.MultiRemoteBrowser
+ }
+
 /**
  *
  * Get a css property from a DOM-element selected by given selector. The return value
@@ -49,7 +54,10 @@ import { parseCSS } from '../../utils/index.js'
         //      }
         // }
 
-        var width = await elem.getCSSProperty('width')
+        var width = await elem.getCSSProperty('width', {
+            target: '::before',
+            browser
+        })
         console.log(width)
         // outputs the following:
         // {
@@ -64,41 +72,103 @@ import { parseCSS } from '../../utils/index.js'
         // }
     })
  * </example>
- *
  * @alias element.getCSSProperty
- * @param  {string}      cssProperty css property name
- * @return {CSSProperty}             The specified css of the element
+ * @param  {string}        cssProperty   css property name
+ * @param  {PseudoElement} pseudoElement css pseudo element
+ * @return {CSSProperty}                 The specified css of the element
  *
  */
 export async function getCSSProperty (
     this: WebdriverIO.Element,
-    cssProperty: string
+    cssProperty: string,
+    pseudoElement?: PseudoElement,
 ) {
+    if (cssShorthandProps.isShorthand(cssProperty)) {
+        const cssValue = await getShorthandPropertyCSSValue.call(
+            this,
+            cssProperty,
+            pseudoElement,
+        )
+
+        return parseCSS(cssValue, cssProperty)
+    }
+
+    const cssValue = await getPropertyCSSValue.call(
+        this,
+        cssProperty,
+        pseudoElement,
+    )
+
+    return parseCSS(cssValue, cssProperty)
+}
+
+async function getShorthandPropertyCSSValue(
+    this: WebdriverIO.Element,
+    cssProperty: string,
+    pseudoElement?: PseudoElement,
+) {
+    let cssValues: string[]
+    const properties = getShorthandProperties(cssProperty)
+
+    if (pseudoElement) {
+        const { browser, target } = pseudoElement
+
+        cssValues = await Promise.all(
+            properties.map((prop) => getPseudoElementCSSValue(
+                browser,
+                this,
+                prop,
+                target
+            ))
+        )
+    } else {
+        cssValues = await Promise.all(
+            properties.map((prop) => this.getElementCSSValue(this.elementId, prop))
+        )
+    }
+
+    return mergeEqualSymmetricalValue(cssValues)
+}
+
+async function getPropertyCSSValue(
+    this: WebdriverIO.Element,
+    cssProperty: string,
+    pseudoElement?: PseudoElement,
+) {
+    if (pseudoElement) {
+        const { browser, target } = pseudoElement
+
+        return await getPseudoElementCSSValue(
+            browser,
+            this,
+            cssProperty,
+            target
+        )
+    }
+    return await this.getElementCSSValue(this.elementId, cssProperty)
+}
+
+function getShorthandProperties(cssProperty: string) {
     /**
      * Getting the css value of a shorthand property results in different results
      * given that the behavior of `getComputedStyle` is not defined in this case.
      * Therefore if we don't deal with a shorthand property run `getElementCSSValue`
      * otherwise expand it and run the command for each longhand property.
      */
-    if (!cssShorthandProps.isShorthand(cssProperty)) {
-        const cssValue = await this.getElementCSSValue(this.elementId, cssProperty)
-        return parseCSS(cssValue, cssProperty)
-    }
+    return cssShorthandProps.expand(cssProperty)
+}
 
-    const properties = cssShorthandProps.expand(cssProperty)
-    let cssValues = await Promise.all(
-        properties.map((prop) => this.getElementCSSValue(this.elementId, prop))
-    )
-
+function mergeEqualSymmetricalValue(cssValues: string[]) {
     /**
      * merge equal symmetrical values
      * - e.g. `36px 10px 36px 10px` to `36px 10px`
      * - or `0px 0px 0px 0px` to `0px`
-     */
-    while ((cssValues.length % 2) === 0) {
+    */
+    let newCssValues = [...cssValues]
+    while ((newCssValues.length % 2) === 0) {
         const mergedValues = [
-            cssValues.slice(0, cssValues.length / 2).join(' '),
-            cssValues.slice(cssValues.length / 2).join(' ')
+            newCssValues.slice(0, newCssValues.length / 2).join(' '),
+            newCssValues.slice(newCssValues.length / 2).join(' ')
         ]
 
         const hasEqualProperties = mergedValues.every((v) => v === mergedValues[0])
@@ -106,8 +176,24 @@ export async function getCSSProperty (
             break
         }
 
-        cssValues = cssValues.slice(0, cssValues.length / 2)
+        newCssValues = newCssValues.slice(0, newCssValues.length / 2)
     }
 
-    return parseCSS(cssValues.join(' '), cssProperty)
+    return newCssValues.join(' ')
+}
+
+async function getPseudoElementCSSValue (
+    browser: WebdriverIO.Browser | WebdriverIO.MultiRemoteBrowser,
+    elem: WebdriverIO.Element,
+    cssProperty: string,
+    pseudoElement: string
+): Promise<string> {
+    const cssValue = await browser.execute(
+        (elem: Element, pseudoElement: string, cssProperty: string) => (window.getComputedStyle(elem, pseudoElement) as any)[cssProperty],
+        elem,
+        pseudoElement,
+        cssProperty
+    )
+
+    return cssValue
 }
