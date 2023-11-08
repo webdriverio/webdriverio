18749c20abfa09ff16c3e2c749d88762b562fc9e
Tamil Selvan <tamil777selvan@gmail.com>
2023-11-08 08:15:49 +0530

    adopting review comments

--- packages/wdio-utils/src/shim.ts
+++ packages/wdio-utils/src/shim.ts
@@ -18,6 +18,8 @@ const ELEMENT_PROPS = [
 const ACTION_COMMANDS = ['action', 'actions']
 const PROMISE_METHODS = ['then', 'catch', 'finally']
 
+const TIME_BUFFER = 3
+
 export async function executeHooksWithArgs<T> (this: any, hookName: string, hooks: Function | Function[] = [], args: any[] = []): Promise<(T | Error)[]> {
     /**
      * make sure hooks are an array of functions
@@ -290,11 +292,13 @@ export async function executeAsync(this: any, fn: Function, retries: Frameworks.
 
     try {
         /**
-         * Decrease the overall timeout by 3 milliseconds to ensure that when a test fails due to a timeout exception in Jasmine,
-         * the test objects are returned instead of being overridden by the values of the next test.
+         * To prevent test failures due to timeout exceptions in Jasmine from overwriting test objects with subsequent values,
+         * we reduce the overall timeout by a constant known as TIME_BUFFER. TIME_BUFFER acts as a safety margin, allowing a small
+         * window of time for an operation to complete before triggering a timeout. This approach ensures that test results are handled
+         * properly without affecting the overall test execution timing.
          */
         // @ts-expect-error
-        const _timeout = (this?._runnable?._timeout || globalThis.jasmine?.DEFAULT_TIMEOUT_INTERVAL || timeout) - 3
+        const _timeout = (this?._runnable?._timeout || globalThis.jasmine?.DEFAULT_TIMEOUT_INTERVAL || timeout) - TIME_BUFFER
         /**
          * Executes the function with specified timeout and returns the result, or throws an error if the timeout is exceeded.
          */
