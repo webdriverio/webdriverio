d2026d3422aa9fde8c83a58a389ade027f595d5c
Dmytro Klymenko <dklymenko.work@gmail.com>
2023-12-06 16:42:37 +0100

    add config-files

---
+++ tests/tests-cli-spec-arg/base.wdio.conf.js
@@ -0,0 +1,27 @@
+import url from 'node:url'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+export const config = {
+    /**
+     * capabilities
+     */
+    capabilities: [{
+        browserName: 'chrome'
+    }],
+
+    /**
+     * test configurations
+     */
+    logLevel: 'trace',
+    framework: 'mocha',
+    outputDir: __dirname,
+
+    reporters: ['spec'],
+    services: ['webdriver-mock'],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 15000
+    }
+}
---
+++ tests/tests-cli-spec-arg/wdio-with-all-passed.conf.js
@@ -0,0 +1,21 @@
+import { config as baseConfig } from './base.wdio.conf.js'
+
+export const config = Object.assign(
+    baseConfig,
+    {
+        /**
+         * specify test files
+         */
+        specs:
+        [
+            './mocha.test01.js',
+            [
+                './mocha.test02.js',
+                './mocha.test03.js'
+            ],
+            [
+                './mocha.test04.js'
+            ]
+        ],
+    }
+)
