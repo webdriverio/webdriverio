4746790489652bd41104a9f1c8a9ee0a5ddeff2a
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2023-12-07 13:12:04 +0100

    (@wdio/cli): do not throw if dotenv is not installed (#11826)

--- packages/wdio-cli/src/index.ts
+++ packages/wdio-cli/src/index.ts
@@ -1,4 +1,8 @@
-import 'dotenv/config'
+try {
+    await import('dotenv/config')
+} catch (error) {
+    // do not fail if dotenv is not installed
+}
 
 export { default as Launcher } from './launcher.js'
 export { default as run } from './run.js'
