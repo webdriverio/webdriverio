c98d50cede34a141069d81039ba23dc9599cae92
thiru-mohan <thirumalai.vignesh-mohan@selfridges.co.uk>
2023-12-10 23:05:20 +0000

    browserstack missing capabilities

--- packages/wdio-types/src/Capabilities.ts
+++ packages/wdio-types/src/Capabilities.ts
@@ -1128,6 +1128,8 @@ export interface BrowserStackCapabilities {
      */
     debug?: boolean
     networkLogs?: boolean
+    networkLogsExcludeHosts?: Array<string>
+    networkLogsIncludeHosts?: Array<string>
      /**
      * https://www.browserstack.com/docs/app-automate/appium/debug-failed-tests/network-logs
      * Enable viewing the response data in the Network Logs tab on your session
