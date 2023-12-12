855e25e48b8d108df61f699c1099fbd066fbb37c
Christian Bromann <git@bromann.dev>
2023-12-12 08:08:11 -0800

    (webdriverio): merge isDisplayed and isDisplayedWithinViewport (#11857)
    * (webdriverio): merge isDisplayed and isDisplayedWithinViewport
    
    * allow to specify withinViewport check in waitForDisplayed

--- packages/webdriverio/src/commands/element/waitForDisplayed.ts
+++ packages/webdriverio/src/commands/element/waitForDisplayed.ts
@@ -12,12 +12,13 @@ import type { WaitForOptions } from '../../types.js'
  * :::
  *
  * @alias element.waitForDisplayed
- * @param {WaitForOptions=}  options             waitForEnabled options (optional)
- * @param {Number=}          options.timeout     time in ms (default: 500)
- * @param {Boolean=}         options.reverse     if true it waits for the opposite (default: false)
- * @param {String=}          options.timeoutMsg  if exists it overrides the default error message
- * @param {Number=}          options.interval    interval between checks (default: `waitforInterval`)
- * @return {Boolean} true     if element is displayed (or doesn't if flag is set)
+ * @param {WaitForOptions=}  options                waitForDisplayed options (optional)
+ * @param {Number=}          options.timeout        time in ms (default: 500)
+ * @param {Boolean=}         options.reverse        if true it waits for the opposite (default: false)
+ * @param {String=}          options.timeoutMsg     if exists it overrides the default error message
+ * @param {Number=}          options.interval       interval between checks (default: `waitforInterval`)
+ * @param {Boolean=}         options.withinViewport set to `true` to wait until element is displayed within viewport (default: `false`)
+ * @return {Boolean} true    if element is displayed (or doesn't if flag is set)
  * @uses utility/waitUntil, state/isDisplayed
  * @example https://github.com/webdriverio/example-recipes/blob/0bfb2b8d212b627a2659b10f4449184b657e1d59/waitForDisplayed/index.html#L3-L8
  * @example https://github.com/webdriverio/example-recipes/blob/9ac16b4d4cf4bc8ec87f6369439a2d0bcaae4483/waitForDisplayed/waitForDisplayedExample.js#L6-L14
@@ -29,11 +30,12 @@ export async function waitForDisplayed (
         timeout = this.options.waitforTimeout,
         interval = this.options.waitforInterval,
         reverse = false,
-        timeoutMsg = `element ("${this.selector}") still ${reverse ? '' : 'not '}displayed after ${timeout}ms`
+        withinViewport = false,
+        timeoutMsg = `element ("${this.selector}") still ${reverse ? '' : 'not '}displayed${withinViewport ? ' within viewport' : ''} after ${timeout}ms`,
     }: WaitForOptions = {}
 ) {
     return this.waitUntil(
-        async () => reverse !== await this.isDisplayed(),
+        async () => reverse !== await this.isDisplayed({ withinViewport }),
         { timeout, interval, timeoutMsg }
     )
 }
