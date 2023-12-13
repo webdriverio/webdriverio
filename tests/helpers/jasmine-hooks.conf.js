6356aa298942d69e76932bfb0e9a9b3111f3c99f
DOA <hananargov@gmail.com>
2023-12-13 11:01:54 +0200

    Update tests/helpers/jasmine-hooks.conf.js
    adding comment

--- tests/helpers/jasmine-hooks.conf.js
+++ tests/helpers/jasmine-hooks.conf.js
@@ -2,6 +2,7 @@ import { config as baseConfig } from './config.js'
 
 export const config = Object.assign({}, baseConfig, {
     beforeTest() {
+        // this is a global function defined by Jasmine
         pending()
     }
 })
