9ca458099f426caf98d77b8deaed6ffa82829b44
DOA <hananargov@gmail.com>
2023-12-13 11:00:38 +0200

    Update tests/jasmine/test-skipped-hooks.ts
    updated according to comment
    
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- tests/jasmine/test-skipped-hooks.ts
+++ tests/jasmine/test-skipped-hooks.ts
@@ -2,6 +2,6 @@ import assert from 'node:assert'
 
 describe('Jasmine smoke test to be skipped via wdio hooks', () => {
     it('should be skipped', async () => {
-        assert.strictEqual(1, 1)
+        assert.strictEqual(1, 2)
     })
 })
