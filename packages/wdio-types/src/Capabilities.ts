47a00596334edde2ecd432c25c19c234bec8a78b
Christian Bromann <git@bromann.dev>
2023-12-18 17:54:14 -0800

    Update packages/wdio-types/src/Capabilities.ts

--- packages/wdio-types/src/Capabilities.ts
+++ packages/wdio-types/src/Capabilities.ts
@@ -1202,10 +1202,14 @@ export interface BrowserStackCapabilities {
      */
     autoWait?: number
     /**
-     * https://www.browserstack.com/docs/app-automate/appium/advanced-features/handle-permission-pop-ups
-     * IOS specific. For IOS 13 & above, behavior will flip if popup has more than 2 buttons
+     * iOS specific. For IOS 13 & above, behavior will flip if popup has more than 2 buttons
+     * @see https://www.browserstack.com/docs/app-automate/appium/advanced-features/handle-permission-pop-ups
      */
     autoAcceptAlerts?: boolean
+    /**
+     * iOS specific. For IOS 13 & above, behavior will flip if popup has more than 2 buttons
+     * @see https://www.browserstack.com/docs/app-automate/appium/advanced-features/handle-permission-pop-ups
+     */
     autoDismissAlerts?: boolean
     /**
      * Add a host entry (/etc/hosts) to the remote BrowserStack machine.
