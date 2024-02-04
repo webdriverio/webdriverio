203c73fc77f060809ff77cebe53661cc4b0b188e
Tamil Selvan <tamil777selvan@gmail.com>
2024-02-04 12:23:52 +0530

    fix firefox executions in node 18

--- packages/webdriver/src/request/request.ts
+++ packages/webdriver/src/request/request.ts
@@ -1,9 +1,14 @@
 import { performance } from 'node:perf_hooks'
 import type { URL } from 'node:url'
+import dns from 'node:dns'
 
 import type { Options } from '@wdio/types'
 
 import WebDriverRequest, { RequestLibError } from './index.js'
+
+// As per this https://github.com/node-fetch/node-fetch/issues/1624#issuecomment-1407717012 we are setting ipv4first as default IP resolver.
+// This can be removed when we drop Node18 support.
+dns.setDefaultResultOrder('ipv4first')
 
 export default class FetchRequest extends WebDriverRequest {
     constructor (method: string, endpoint: string, body?: Record<string, unknown>, isHubCommand: boolean = false) {
