3ce2af0f696726f224e125c580c40afe9fefbe60
Christian Bromann <git@bromann.dev>
2024-02-16 17:15:10 -0800

    fix change-case update

--- packages/wdio-appium-service/src/utils.ts
+++ packages/wdio-appium-service/src/utils.ts
@@ -1,5 +1,5 @@
 import { basename, join, resolve } from 'node:path'
-import { paramCase } from 'change-case'
+import { trainCase } from 'change-case'
 
 import type { ArgValue, KeyValueArgs } from './types.js'
 
@@ -32,7 +32,7 @@ export function formatCliArgs(args: KeyValueArgs): string[] {
             continue
         }
 
-        cliArgs.push(`--${paramCase(key)}`)
+        cliArgs.push(`--${trainCase(key)}`)
         // Only non-boolean and non-null values are added as option values
         if (typeof value !== 'boolean') {
             cliArgs.push(sanitizeCliOptionValue(value))
