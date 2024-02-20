5c11e6a6323d349402224bd7934e5797d3c2a79a
Jemish Gopani <jemishgopani2@gmail.com>
2024-02-20 22:11:17 +0530

    Fix commands (getSize, getLocation) adding new function declaration

--- packages/webdriverio/src/commands/element/getLocation.ts
+++ packages/webdriverio/src/commands/element/getLocation.ts
@@ -4,7 +4,8 @@ import { getElementRect } from '../../utils/index.js'
 export type Location = Pick<RectReturn, 'x' | 'y'>;
 
 export function getLocation (this: WebdriverIO.Element): Promise<Location>
-export function getLocation (this: WebdriverIO.Element, prop?: keyof Location): Promise<number>
+export function getLocation (this: WebdriverIO.Element, prop: keyof Location): Promise<number>
+export function getLocation (this: WebdriverIO.Element, prop?: keyof Location): Promise<Location & number>
 
 /**
  *
