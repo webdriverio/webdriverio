198a35ead6e20c5e25816646d818971a660676af
riya <riya.d@browserstack.com>
2024-02-21 14:25:43 +0530

    minor fix

--- packages/wdio-browserstack-service/src/insights-handler.ts
+++ packages/wdio-browserstack-service/src/insights-handler.ts
@@ -864,13 +864,13 @@ class _InsightsHandler {
     }
 
     private getIntegrationsObject () {
-        const caps = (this._browser as WebdriverIO.Browser).capabilities as WebdriverIO.Capabilities
-        const sessionId = (this._browser as WebdriverIO.Browser).sessionId
+        const caps = (this._browser as WebdriverIO.Browser)?.capabilities as WebdriverIO.Capabilities
+        const sessionId = (this._browser as WebdriverIO.Browser)?.sessionId
 
         return {
             capabilities: caps,
             session_id: sessionId,
-            browser: caps.browserName,
+            browser: caps?.browserName,
             browser_version: caps?.browserVersion,
             platform: caps?.platformName,
             product: this._platformMeta?.product,
