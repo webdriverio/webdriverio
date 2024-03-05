f2b0cc485299e05c3c655d5cf77f511fb5e24eea
Fabien CELLIER <33258660+lacell75@users.noreply.github.com>
2024-03-05 23:06:09 +0100

    (feat): enable chromium protocol for edge (#12432)
    * (feat): enable chromium protocol for edge
    
    * update doc
    
    * fix test
    
    * fix mistake

--- packages/webdriverio/src/commands/browser/uploadFile.ts
+++ packages/webdriverio/src/commands/browser/uploadFile.ts
@@ -4,9 +4,9 @@ import archiver from 'archiver'
 
 /**
  * Uploads a file to the Selenium Standalone server or other browser driver
- * (e.g. Chromedriver) by using the [`file`](https://webdriver.io/docs/api/selenium#file) command.
- * _Note:_ that this command is only supported if you use a Selenium Hub or
- * Chromedriver directly.
+ * (e.g. Chromedriver or EdgeDriver) by using the [`file`](https://webdriver.io/docs/api/selenium#file) command.
+ * _Note:_ that this command is only supported if you use a Selenium Hub,
+ * Chromedriver or EdgeDriver directly.
  *
  * __Note:__ this command uses an un-offical protocol feature that is currently
  * only supported in Chrome and when running a [Selenium Grid](https://www.selenium.dev/documentation/en/grid/).
