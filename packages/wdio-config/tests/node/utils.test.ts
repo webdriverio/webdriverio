bd1d04736fafe6e98791b410d74204ee597e7ef2
Johannes Pfrang <johannespfrang@gmail.com>
2024-03-09 20:13:21 +0100

    (wdio-config): check existence of ts-node/esm/transpile-only.mjs & fix a type issue (#12436)
    * (wdio-config): check existence of ts-node/esm/transpile-only.mjs (#12434)
    
    * (wdio-config): convert `--autoCompileOpts.autoCompile` type
    
    * (wdio-config) add test for missing ts-node/esm/transpile-only.mjs
    
    * (wdio-config) add test for `--autoCompileOpts.autoCompile=false`

--- packages/wdio-config/tests/node/utils.test.ts
+++ packages/wdio-config/tests/node/utils.test.ts
@@ -1,20 +1,41 @@
 import { describe, beforeEach, it, expect, vi } from 'vitest'
 import { resolve } from 'import-meta-resolve'
+import type fspType from 'node:fs/promises'
+import type { PathLike } from 'node:fs'
 
 import { loadTypeScriptCompiler } from '../../src/node/utils.js'
 
 vi.mock('import-meta-resolve', () => ({
-    resolve: vi.fn().mockResolvedValue('/some/path')
+    resolve: vi.fn().mockResolvedValue('file:///some/path')
 }))
+
+vi.mock('node:fs/promises', async () => {
+    const actualFsp = await vi.importActual<typeof fspType>('node:fs/promises')
+    return {
+        ...actualFsp,
+        access: async (path: PathLike) => {
+            if (path instanceof URL) {
+                if (path.href === 'file:///some/path') {
+                    return undefined
+                }
+                if (path.href === 'file:///some/missing/path') {
+                    throw new Error('ups')
+                }
+            }
+            return actualFsp.access(path)
+        },
+    }
+})
 
 describe('loadTypeScriptCompiler', () => {
     beforeEach(() => {
         vi.mocked(resolve).mockClear()
+        vi.unstubAllEnvs()
     })
 
     it('should return true if tsconfig exists', async () => {
         expect(await loadTypeScriptCompiler({})).toBe(true)
-        expect(resolve).toBeCalledTimes(1)
+        expect(resolve).toBeCalledTimes(2)
     })
 
     it('should return false if tsconfig exists', async () => {
@@ -24,9 +45,22 @@ describe('loadTypeScriptCompiler', () => {
     })
 
     it('should return false if WDIO_WORKER_ID is set', async () => {
-        process.env.WDIO_WORKER_ID = '1'
+        vi.stubEnv('WDIO_WORKER_ID', '1')
         vi.mocked(resolve).mockRejectedValue(new Error('ups'))
         expect(await loadTypeScriptCompiler({})).toBe(false)
         expect(resolve).toBeCalledTimes(0)
+    })
+
+    it('should return false if if ts-node/esm/transpile-only.mjs does not exist', async () => {
+        const mock = vi.mocked(resolve)
+        // resolve the first resolve call to an existing path and the second resolve call to a missing path
+        mock.mockResolvedValue('file:///some/missing/path').mockResolvedValueOnce('file:///some/path')
+        expect(await loadTypeScriptCompiler({})).toBe(false)
+        expect(resolve).toBeCalledTimes(2)
+        expect(mock).toBeCalledTimes(2)
+        expect(mock.mock.calls[0][0]).toBe('ts-node')
+        expect(mock.mock.results[0]).toStrictEqual({ type: 'return', value: 'file:///some/path' })
+        expect(mock.mock.calls[1][0]).toBe('ts-node/esm/transpile-only.mjs')
+        expect(mock.mock.results[1]).toStrictEqual({ type: 'return', value: 'file:///some/missing/path' })
     })
 })
