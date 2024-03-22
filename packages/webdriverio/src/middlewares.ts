8d53ef8f15c4e0d75d5cb579bf32ba98620488fe
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2024-03-22 10:55:29 +0100

    (webdriverio): polish intercepted errors (#12533)
    * (webdriverio): polish intercepted errors
    
    * (wdio-browser-runner): add e2e test
    
    * polish: fix test

--- packages/webdriverio/src/middlewares.ts
+++ packages/webdriverio/src/middlewares.ts
@@ -26,10 +26,7 @@ export const elementErrorHandler = (fn: Function) => (commandName: string, comma
                  * as `stale element reference`
                  */
                 const caps = getBrowserObject(this).capabilities as WebdriverIO.Capabilities
-                if (
-                    caps && caps.browserName === 'safari' &&
-                    result && result.error === 'no such element'
-                ) {
+                if (caps?.browserName === 'safari' && result?.error === 'no such element') {
                     const errorName = 'stale element reference'
                     const err = new Error(errorName)
                     err.name = errorName
@@ -38,13 +35,19 @@ export const elementErrorHandler = (fn: Function) => (commandName: string, comma
 
                 return result
             } catch (err: any) {
+                if (err.name === 'element not interactable') {
+                    const elementHTML = await element.getHTML()
+                    err.message = `Element ${elementHTML} not interactable`
+                    err.stack = err.stack ?? Error.captureStackTrace(err)
+                }
+
                 if (err.name === 'stale element reference' || isStaleElementError(err)) {
                     const element = await refetchElement(this, commandName)
                     this.elementId = element.elementId
                     this.parent = element.parent
-
                     return await fn(commandName, commandFn).apply(this, args)
                 }
+
                 throw err
             }
         }).apply(this)
