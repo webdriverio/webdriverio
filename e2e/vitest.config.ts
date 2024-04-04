be1492854805a4724c7329ebe619204a1496ff44
Christian Bromann <git@bromann.dev>
2024-04-04 10:19:56 +0200

    fix test config

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     test: {
         testTimeout: 1000 * 60 * 3,
+        exclude: ['./e2e/browser-runner', '**/node_modules/**'],
         include: ['./e2e/**/*.test.ts'],
-        hookTimeout: 60 * 1000,
-        threads: false
+        hookTimeout: 60 * 1000
     }
 })
