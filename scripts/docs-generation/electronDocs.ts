26f3b8749f46607d0c975108ae41590b3392c603
Christian Bromann <git@bromann.dev>
2024-05-02 10:31:57 -0700

    docs(fix): fix link

--- scripts/docs-generation/electronDocs.ts
+++ scripts/docs-generation/electronDocs.ts
@@ -63,7 +63,7 @@ export async function generateElectronDocs () {
                     /**
                      * transform relative links
                      */
-                    .replace('./configuration/service-configuration.md', '/electron/configuration')
+                    .replace('./configuration/service-configuration.md', '/docs/desktop-testing/electron/configuration')
             )
         ))).join('\n\n')
         await fs.writeFile(newDocsPath, `---
