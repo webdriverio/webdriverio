4beaecc503f6abaa6c7352e6ab57a867e404622c
Christian Bromann <git@bromann.dev>
2024-06-19 18:34:29 -0700

    ignore error due to issues with expect-webdriverio

--- tests/typings/webdriverio/async.ts
+++ tests/typings/webdriverio/async.ts
@@ -83,6 +83,7 @@ async function bar() {
     const multipleElems = await $$([elemA, elemB]).getElements()
     const multipleElemsChain = $$([elemA, elemB])
     expectType<WebdriverIO.ElementArray>(multipleElems)
+    // @ts-expect-error
     expectType<ChainablePromiseArray>(multipleElemsChain)
 
     ////////////////////////////////////////////////////////////////////////////////
