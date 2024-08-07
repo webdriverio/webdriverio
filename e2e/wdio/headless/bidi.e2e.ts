fe03542e295d15416a78a062ba6b175c6482bbd1
Christian Bromann <git@bromann.dev>
2024-08-07 09:42:49 -0700

    add e2e test

--- e2e/wdio/headless/bidi.e2e.ts
+++ e2e/wdio/headless/bidi.e2e.ts
@@ -37,4 +37,48 @@ describe('bidi e2e test', () => {
             }
         )
     })
+
+    const executeArgs = ['string', true, 42, -0, Infinity, -Infinity, NaN, [1, 2, 3], new Map([['foo', 'bar']]), new Set([]), /foobar/]
+
+    it('can serialize function values (sync)', async function () {
+        function validator (str: string, bool: boolean, number: number, negZero: number, infin: number, negInfin: number, notANumber: number, arr: number[], map: Map<string, unknown>, set: Set<unknown>, regex: RegExp) {
+            return [
+                'validate string:', typeof str === 'string', '\n',
+                'validate boolean', typeof bool === 'boolean', '\n',
+                'validate number', typeof number === 'number', '\n',
+                'valdiate negative zero', Object.is(negZero, -0), '\n',
+                'validate Infinity', infin === Infinity, '\n',
+                'validate negative Infinity', negInfin === -Infinity, '\n',
+                'validate NaN', Number.isNaN(notANumber), '\n',
+                'validate array', Array.isArray(arr), '\n',
+                'validate array items', arr.every(item => typeof item === 'number'), '\n',
+                'validate map', map instanceof Map, '\n',
+                'validate set', set instanceof Set, '\n',
+                'validate regular expression', regex instanceof RegExp, '\n'
+            ]
+        }
+        const result = await browser.execute(validator, ...executeArgs as any)
+        expect(result).toMatchInlineSnapshot()
+    })
+
+    it('can serialize function values (async)', async function () {
+        async function asyncValidator (str: string, bool: boolean, number: number, negZero: number, infin: number, negInfin: number, notANumber: number, arr: number[], map: Map<string, unknown>, set: Set<unknown>, regex: RegExp) {
+            return Promise.resolve([
+                'validate string:', typeof str === 'string', '\n',
+                await Promise.resolve('validate boolean async'), typeof bool === 'boolean', '\n',
+                'validate number', typeof number === 'number', '\n',
+                'valdiate negative zero', Object.is(negZero, -0), '\n',
+                'validate Infinity', infin === Infinity, '\n',
+                'validate negative Infinity', negInfin === -Infinity, '\n',
+                'validate NaN', Number.isNaN(notANumber), '\n',
+                'validate array', Array.isArray(arr), '\n',
+                'validate array items', arr.every(item => typeof item === 'number'), '\n',
+                'validate map', map instanceof Map, '\n',
+                'validate set', set instanceof Set, '\n',
+                'validate regular expression', regex instanceof RegExp, '\n'
+            ])
+        }
+        const result = await browser.executeAsync(asyncValidator as any, ...executeArgs as any)
+        expect(result).toMatchInlineSnapshot()
+    })
 })
