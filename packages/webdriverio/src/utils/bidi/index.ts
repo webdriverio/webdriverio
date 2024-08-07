fa4ffc5d50d4e2faf041aa99ffa4335bffcf96c8
Christian Bromann <git@bromann.dev>
2024-08-06 21:29:11 -0700

    feat(webdriverio): support (se/de)serialization of execute parameters

---
+++ packages/webdriverio/src/utils/bidi/index.ts
@@ -0,0 +1,76 @@
+import type { local, remote } from 'webdriver'
+
+import { EvaluateResultType, NonPrimitiveType, PrimitiveType } from './constants.js'
+
+export function parseScriptResult(result: local.ScriptEvaluateResult) {
+    const type = result.type
+
+    if (type === EvaluateResultType.Success) {
+        // @ts-expect-error
+        return deserializeValue(result.result)
+    }
+    if (type === EvaluateResultType.Exception) {
+        const columnNumber = 'columnNumber' in result.exceptionDetails ? result.exceptionDetails.columnNumber : null
+        const exception = 'exception' in result.exceptionDetails ? result.exceptionDetails.exception : null
+        const lineNumber = 'lineNumber' in result.exceptionDetails ? result.exceptionDetails.lineNumber : null
+        const stackTrace = 'stackTrace' in result.exceptionDetails ? result.exceptionDetails.stackTrace : null
+        const text = 'text' in result.exceptionDetails ? result.exceptionDetails.text : null
+        return {
+            error: {
+                message: text,
+                columnNumber,
+                exception,
+                lineNumber,
+                stackTrace,
+            }
+        }
+    }
+
+    throw new Error(`Unknown evaluate result type: ${type}`)
+}
+
+export function deserializeValue(result: remote.ScriptLocalValue) {
+    // @ts-expect-error
+    const { type, value } = result
+    if (value === NonPrimitiveType.Object) {
+        return Object.fromEntries(value.map(([key, value]: [any, any]) => {
+            return [deserializeValue(key), deserializeValue(value)]
+        }))
+    }
+    if (type === NonPrimitiveType.RegularExpression) {
+        return new RegExp(value.pattern, value.flags)
+    }
+    if (type === NonPrimitiveType.Array) {
+        return value.map((element: any) => deserializeValue(element))
+    }
+    if (type === NonPrimitiveType.Date) {
+        return new Date(value)
+    }
+    if (type === NonPrimitiveType.Map) {
+        return new Map(value.map(([key, value]: [any, any]) => (
+            [typeof key === 'string' ? key : deserializeValue(key), deserializeValue(value)]
+        )))
+    }
+    if (type === NonPrimitiveType.Set) {
+        return new Set(value.map((element: any) => deserializeValue(element)))
+    }
+    if (type === PrimitiveType.Number && value === 'NaN') {
+        return NaN
+    }
+    if (type === PrimitiveType.Number && value === 'Infinity') {
+        return Infinity
+    }
+    if (type === PrimitiveType.Number && value === '-Infinity') {
+        return -Infinity
+    }
+    if (type === PrimitiveType.Number && value === '-0') {
+        return -0
+    }
+    if (type === PrimitiveType.BigInt) {
+        return BigInt(value)
+    }
+    if (type === PrimitiveType.Null) {
+        return null
+    }
+    return value
+}
