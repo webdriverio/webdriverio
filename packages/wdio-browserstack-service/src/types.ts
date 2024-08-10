d0cfc168b30285cd02e5740f0a6339a9ad4c2638
Shubham Garg <74714209+xxshubhamxx@users.noreply.github.com>
2024-08-10 19:09:46 +0530

    V9 ai support (#13340)
    * Changes to support Healing in WDIO-v9
    
    * WDIO v9 Healing updates
    
    * lock file updates
    
    * Added try catch blocks
    
    * Updated isBrowserstackInfra
    
    * multi remote healing support
    
    * Code Quality Updates
    
    * Updated AI-SDK dependency version
    
    * Code Quality Improvements suggested in the PR review
    
    * Adding isBStackSession inside isBrowserstackInfra
    
    * Minor healing fix and added funnel instrumentation tests
    
    ---------
    
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- packages/wdio-browserstack-service/src/types.ts
+++ packages/wdio-browserstack-service/src/types.ts
@@ -29,6 +29,10 @@ export interface TestObservabilityOptions {
     key?: string
 }
 
+export interface BrowserstackOptions extends Options.Testrunner {
+    selfHeal?: boolean;
+}
+
 export interface BrowserstackConfig {
     /**
      *`buildIdentifier` is a unique id to differentiate every execution that gets appended to
@@ -152,6 +156,7 @@ export interface BrowserstackConfig {
      * @default false
     */
     turboScale?: boolean;
+    selfHeal?: boolean;
 }
 
 /**
