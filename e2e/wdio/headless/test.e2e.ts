92581a54adc48ff7923c5c79fa0d00e2aa5c4b71
Christian Bromann <git@bromann.dev>
2024-08-14 09:52:16 -0700

    fix(test): skip scroll e2e tests

--- e2e/wdio/headless/test.e2e.ts
+++ e2e/wdio/headless/test.e2e.ts
@@ -164,7 +164,7 @@ describe('main suite 1', () => {
         })
 
         inputs.forEach((input) => {
-            it(`moves to position x,y inside of iframe when passing the arguments ${JSON.stringify(input)}`, async () => {
+            it.skip(`moves to position x,y inside of iframe when passing the arguments ${JSON.stringify(input)}`, async () => {
                 await browser.execute(() => {
                     const mouse = { x: 0, y: 0 }
                     document.onmousemove = function(e){ mouse.x = e.clientX, mouse.y = e.clientY }
