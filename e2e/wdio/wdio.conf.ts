d2681ce35ed8540760c75458127bed688f8512de
Christian Bromann <git@bromann.dev>
2024-10-08 09:23:37 -0700

    fix(webdriverio): mark mock as being called even without overwrites (#13731)
    * fix(webdriverio): mark mock as being called even without overwrites
    
    * add e2e test

--- e2e/wdio/wdio.conf.ts
+++ e2e/wdio/wdio.conf.ts
@@ -12,6 +12,7 @@ export const config: WebdriverIO.Config = {
         path.join(__dirname, 'headless', 'source-maps.e2e.ts'),
         path.join(__dirname, 'headless', 'reloadSession.e2e.ts'),
         path.join(__dirname, 'headless', 'test.e2e.ts'),
+        path.join(__dirname, 'headless', 'mocking.e2e.ts'),
     ],
 
     /**
