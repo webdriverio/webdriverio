1d8be13b842a49fca677511cff4228531e596f76
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2024-10-18 20:09:11 +0300

    fix(@wdio/cli): don't try to detect TypeScript if an empty project is about to be created
    Fixes "Could not find project root directory with a package.json" at `detectCompiler()` called as a way to detect if TypeScript should be suggested by default

--- packages/wdio-cli/src/utils.ts
+++ packages/wdio-cli/src/utils.ts
@@ -358,6 +358,9 @@ export function hasBabelConfig(rootDir: string) {
  * detect if project has a compiler file
  */
 export async function detectCompiler(answers: Questionnair) {
+    // obviously there is no compiler, if there is no package.json
+    if (answers.createPackageJSON) { return false }
+
     const root = await getProjectRoot(answers)
     const rootTSConfigExist = await fs.access(path.resolve(root, 'tsconfig.json')).then(() => true, () => false)
     return (await hasBabelConfig(root) || rootTSConfigExist) ? true : false
