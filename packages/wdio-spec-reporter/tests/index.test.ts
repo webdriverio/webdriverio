c592eb0084039e3ab02341441be11a4ec54a5126
Christian Bromann <git@bromann.dev>
2024-11-12 23:19:45 -0800

    polish(@wdio/reporter): better display retried scenarios

--- packages/wdio-spec-reporter/tests/index.test.ts
+++ packages/wdio-spec-reporter/tests/index.test.ts
@@ -4,11 +4,12 @@ import {
     SUITES,
     SUITE_UIDS,
     SUITES_NO_TESTS,
-    SUITES_WITH_RETRY,
+    SUITES_WITH_TEST_RETRY,
     SUITES_WITH_DATA_TABLE,
     SUITES_MULTIPLE_ERRORS,
     SUITES_WITH_DOC_STRING,
     SUITES_NO_TESTS_WITH_HOOK_ERROR,
+    SUITES_WITH_RETRIES
 } from './__fixtures__/testdata.js'
 import { State } from '../src/types.js'
 import SpecReporter from '../src/index.js'
@@ -47,6 +48,7 @@ describe('SpecReporter', () => {
                 passed: 0,
                 skipped: 0,
                 failed: 0,
+                retried: 0
             })
         })
     })
@@ -754,11 +756,35 @@ describe('SpecReporter', () => {
             printReporter = new SpecReporter({})
             printReporter.write = vi.fn()
             printReporter['_suiteUids'] = SUITE_UIDS
-            printReporter.suites = SUITES_WITH_RETRY
+            printReporter.suites = SUITES_WITH_TEST_RETRY
         })
 
         it('should group retried test cases', () => {
             runner.failures = 0
+            printReporter.printReport(runner)
+            expect(printReporter.write.mock.calls).toMatchSnapshot()
+        })
+    })
+
+    describe('suite retry', () => {
+        let printReporter: any = null
+        const runner = getRunnerConfig({ hostname: 'localhost' })
+
+        beforeEach(() => {
+            printReporter = new SpecReporter({})
+            printReporter.write = vi.fn()
+            printReporter['_suiteUids'] = SUITE_UIDS
+            printReporter.suites = SUITES_WITH_RETRIES
+        })
+
+        it('should group retried test suites', () => {
+            runner.failures = 0
+            printReporter.onTestPass({})
+            printReporter.onTestPass({})
+            printReporter.onTestFail({})
+            printReporter.onSuiteRetry()
+            printReporter.onTestPass({})
+            printReporter.onTestPass({})
             printReporter.printReport(runner)
             expect(printReporter.write.mock.calls).toMatchSnapshot()
         })
