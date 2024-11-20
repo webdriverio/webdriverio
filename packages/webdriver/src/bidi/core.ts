13e00fc01a324f8feda91f6708aff1576af76fdc
Christian Bromann <git@bromann.dev>
2024-11-20 10:55:50 -0800

    fix(webdriver): make isBidi flag dependent on whether WebdriverIO can connect to the websocket url (#13896)
    * fix(webdriver): make isBidi flag dependent on whether WebdriverIO can connect to the websocket url
    
    * fix unit test
    
    * don't use private operator as it fails in the browser
    
    * fix command
    
    * add a comment for clarification [ci skip]

--- packages/webdriver/src/bidi/core.ts
+++ packages/webdriver/src/bidi/core.ts
@@ -14,12 +14,15 @@ const RESPONSE_TIMEOUT = 1000 * 60
 export class BidiCore {
     #id = 0
     #ws: WebSocket
-    #isConnected = false
-    #waitForConnected = Promise.resolve()
+    #waitForConnected = Promise.resolve(false)
     #webSocketUrl: string
     #pendingCommands: Map<number, (value: CommandResponse) => void> = new Map()
 
     client: Client | undefined
+    /**
+     * @private
+     */
+    private _isConnected = false
 
     constructor (webSocketUrl: string, opts?: ClientOptions) {
         this.#webSocketUrl = webSocketUrl
@@ -34,14 +37,22 @@ export class BidiCore {
          * Note: the value is defined in __mocks__/fetch.ts
          */
         if (process.env.WDIO_UNIT_TESTS) {
+            this._isConnected = true
             return
         }
 
-        this.#waitForConnected = new Promise<void>((resolve) => this.#ws.on('open', () => {
-            log.info('Connected session to Bidi protocol')
-            this.#isConnected = true
-            resolve()
-        }))
+        this.#waitForConnected = new Promise<boolean>((resolve) => {
+            this.#ws.on('open', () => {
+                log.info('Connected session to Bidi protocol')
+                this._isConnected = true
+                resolve(this._isConnected)
+            })
+            this.#ws.on('error', (err) => {
+                log.warn(`Couldn't connect to Bidi protocol: ${err.message}`)
+                this._isConnected = false
+                resolve(this._isConnected)
+            })
+        })
         return this.#waitForConnected
     }
 
@@ -58,7 +69,7 @@ export class BidiCore {
     }
 
     get isConnected () {
-        return this.#isConnected
+        return this._isConnected
     }
 
     /**
@@ -124,7 +135,7 @@ export class BidiCore {
     }
 
     public sendAsync (params: Omit<CommandData, 'id'>) {
-        if (!this.#isConnected) {
+        if (!this._isConnected) {
             throw new Error('No connection to WebDriver Bidi was established')
         }
 
