e443d1c256485b623f025c723dd1e30e7dd3d09a
wswebcreation <wim@wswebcreation.nl>
2024-11-21 21:57:14 +0100

    feat: add device scroll docs

---
+++ packages/webdriverio/src/commands/mobile/scrollIntoView.ts
@@ -0,0 +1,47 @@
+/**
+ *
+ * Scroll element into viewport for Desktop/Mobile Web <strong>AND</strong> Mobile Native Apps.
+ *
+ * :::info
+ *
+ * Scrolling for Mobile Native Apps is done based on native mobile gestures. It is only supported for the following drivers:
+ * - [appium-uiautomator2-driver](https://github.com/appium/appium-uiautomator2-driver/blob/master/docs/android-mobile-gestures.md#mobile-scrollgesture) for Android
+ * - [appium-xcuitest-driver](https://appium.github.io/appium-xcuitest-driver/latest/reference/execute-methods/#mobile-scroll) for iOS
+ *
+ * :::
+ *
+ * <example>
+    :desktop.mobile.web.scrollIntoView.js
+    it('should demonstrate the desktop/mobile web scrollIntoView command', async () => {
+        const elem = await $('#myElement');
+        // scroll to specific element
+        await elem.scrollIntoView();
+        // center element within the viewport
+        await elem.scrollIntoView({ block: 'center', inline: 'center' });
+    });
+ * </example>
+ *
+ * <example>
+    :mobile.native.app.scrollIntoView.js
+    it('should demonstrate the mobile native app scrollIntoView command', async () => {
+        const elem = await $('#myElement');
+        // scroll to a specific element in the default scrollable element for Android or iOS for a maximum of 10 scrolls
+        await elem.scrollIntoView();
+        // Scroll to the left in the scrollable element called '#scrollable' for a maximum of 5 scrolls
+        await elem.scrollIntoView({ direction: 'left', maxScrolls: 5, scrollableElement: $('#scrollable') });
+    });
+ * </example>
+ *
+ * @alias element.scrollIntoView
+ * @param {object|boolean=} options                   options for `Element.scrollIntoView()`. Default for desktop/mobile web: <br/> `{ block: 'start', inline: 'nearest' }` <br /> Default for Mobile Native App <br /> `{ maxScrolls: 10, scrollDirection: 'down' }`
+ * @param {string=}         options.behavior          See [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView). <br /><strong>WEB-ONLY</strong> (Desktop/Mobile)
+ * @param {string=}         options.block             See [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView). <br /><strong>WEB-ONLY</strong> (Desktop/Mobile)
+ * @param {string=}         options.inline            See [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView). <br /><strong>WEB-ONLY</strong> (Desktop/Mobile)
+ * @param {string=}         options.direction         Can be one of `down`, `up`, `left` or `right`, default is `down`. <br /><strong>MOBILE-NATIVE-APP-ONLY</strong>
+ * @param {number=}         options.maxScrolls        The max amount of scrolls until it will stop searching for the element, default is `10`. <br /><strong>MOBILE-NATIVE-APP-ONLY</strong>
+ * @param {Element=}        options.scrollableElement Element that is used to scroll within. If no element is provided it will use the following selector for iOS `-ios predicate string:type == "XCUIElementTypeApplication"` and the following for Android `//android.widget.ScrollView'`. If more elements match the default selector, then by default it will pick the first matching element. <br /> <strong>MOBILE-NATIVE-APP-ONLY</strong>
+ * @uses protocol/execute
+ * @type utility
+ *
+ */
+// actual implementation is located in packages/webdriverio/src/element/scrollIntoView.ts
