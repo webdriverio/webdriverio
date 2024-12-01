31a3fb329c5db0db37a5e0611ea2bfd2382003b4
David Prevost <david.prevost@goto.com>
2024-12-01 06:47:27 -0500

    Draft solution masking on the `onBeforeCommand`
    Use same default string as appium
    
    Use same default token as appium

--- packages/wdio-runner/src/index.ts
+++ packages/wdio-runner/src/index.ts
@@ -282,18 +282,32 @@ export default class Runner extends EventEmitter {
         /**
          * register command event
          */
-        browser.on('command', (command: any) => this._reporter?.emit(
-            'client:beforeCommand',
-            Object.assign(command, { sessionId: browser.sessionId })
-        ))
+        browser.on('command', (command: any) => {
+            let maskedCommand = command
+            // TODO move into a config
+            const patternWithFlags = this._config.onBeforeCommandTextPatternMasker
+
+            if (!!patternWithFlags && !!command?.body?.text && command.body.text.match(patternWithFlags)) {
+                maskedCommand = {
+                    ...command,
+                    body: {
+                        ...command.body,
+                        text: '**SECURE**'
+                    }
+                }
+            }
+            return this._reporter?.emit('client:beforeCommand', Object.assign(maskedCommand, { sessionId: browser.sessionId }))
+        })
 
         /**
          * register result event
          */
-        browser.on('result', (result: any) => this._reporter?.emit(
-            'client:afterCommand',
-            Object.assign(result, { sessionId: browser.sessionId })
-        ))
+        browser.on('result', (result: any) => {
+            // TODO dprevost to remove
+            console.log('result', result)
+            return this._reporter?.emit('client:afterCommand', Object.assign(result, { sessionId: browser.sessionId }))
+        }
+        )
 
         return browser
     }
