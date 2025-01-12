02f9f0890814fde5d6225fa61636d150f0434033
Christian Bromann <git@bromann.dev>
2025-01-12 10:25:32 -0800

    fix(webdriverio): register polyfill on all windows (#14061)

--- packages/webdriverio/src/session/polyfill.ts
+++ packages/webdriverio/src/session/polyfill.ts
@@ -20,9 +20,11 @@ export const NAME_POLYFILL = (
  */
 export class PolyfillManager extends SessionManager {
     #initialize: Promise<boolean>
+    #browser: WebdriverIO.Browser
 
     constructor(browser: WebdriverIO.Browser) {
         super(browser, PolyfillManager.name)
+        this.#browser = browser
 
         /**
          * don't run setup when Bidi is not supported or running unit tests
@@ -32,6 +34,28 @@ export class PolyfillManager extends SessionManager {
             return
         }
 
+        /**
+         * apply polyfill script for upcoming as well as current execution context
+         */
+        this.#initialize = Promise.all([
+            this.#registerScripts(),
+            this.#browser.sessionSubscribe({
+                events: ['browsingContext.contextCreated']
+            })
+        ]).then(() => {
+            log.info('polyfill script added')
+            return true
+        }, () => false)
+
+        this.#browser.on('browsingContext.contextCreated', this.#registerScripts.bind(this))
+    }
+
+    removeListeners() {
+        super.removeListeners()
+        this.#browser.off('browsingContext.contextCreated', this.#registerScripts.bind(this))
+    }
+
+    #registerScripts () {
         /**
          * A polyfill to set `__name` to the global scope which is needed for WebdriverIO to properly
          * execute custom (preload) scripts. When using `tsx` Esbuild runs some optimizations which
@@ -45,16 +69,10 @@ export class PolyfillManager extends SessionManager {
             return closure()
         }
 
-        /**
-         * apply polyfill script for upcoming as well as current execution context
-         */
-        this.#initialize = Promise.all([
-            browser.addInitScript(polyfill, NAME_POLYFILL),
-            browser.execute(polyfill, NAME_POLYFILL)
-        ]).then(() => {
-            log.info('polyfill script added')
-            return true
-        }, () => false)
+        return Promise.all([
+            this.#browser.addInitScript(polyfill, NAME_POLYFILL),
+            this.#browser.execute(polyfill, NAME_POLYFILL)
+        ])
     }
 
     async initialize () {
