329c987eb9dd1b8aae855a360134003ab5ceed70
David Prevost <david.prevost@goto.com>
2025-01-25 14:28:33 -0500

    Draft solution masking on the `onBeforeCommand`
    Use same default string as appium
    
    Use same default token as appium
    
    Full working solutions
    
    Fix compilation problems
    
    Remove it from capabilities from simplicity
    
    Fix grammar
    
    Use Regex native type
    
    Draft mask body & command args in webdriver directly
    
    Full working solution
    
    Removing old solution and complete the new one
    
    Revert "Draft solution masking on the `onBeforeCommand`"
    
    This reverts commit 5feb324af2d2a1ded4e81c86cad2670a34150b7b.
    
    Code review
    
    Update packages/webdriver/src/command.ts
    
    Co-authored-by: Christian Bromann <git@bromann.dev>
    Unit test on the regExp converter from string
    
    Add missing case of missing starting and ending slash
    
    Remove undesired changes
    
    Convert string to regExps only once
    Fix problem after rebase
    remove occurences of any
    
    Add new mask parameter to elementSendKeys to mask text input and more
    
    Added also the `/session/:sessionId/appium/settings` endpoints to generate regEx from the sensitive value so that we can mask the value int he appium log on top of WebDriver log and other touchpoint
    e2e test for masking text value with setValue command
    
    Use `updateSettings` only with Appium
    
    Review solution to be completely outside of `request.ts`
    
    Use env `WDIO_LOG_MASKING_PATTERNS` to pass regex and mask log
    
    Full working solutions to mask logs with 0 1 or 2 capturing group
    
    Working solution to mask for example BrowserStack key in logs
    
    Code review
    Fix masking not working when output directly in console
    
    mask string logging only
    
    Better code for wdio-logger masking + more unit tests
    
    - Better typing in logger to reflect reality of having only 1 arg of type string of 3 args where the third can be anything.
    - Move masking utility code into an utils file and add better unit tests
    - Add `setMaskingPatterns` following `setLogLevelsConfig` for more flexible configuration
    - Fix a couple fo edge cases
    - Ensure we mask the output of serializers too
    
    Have mask in command optional and fix ut
    
    - Have mask optional in send and add command to not interfere with expected output and be less "breaking" if masking is not used
    - fix ut affected by new logData eventhandler
    
    Better masking code for command with masking at true
    
    - Move masking code for command into an utility
    - Use args better with index to fetch mask and text
    - Apply masking only if the command has the param 'mask', 'text' and mask is true
    - Share the logger replacer with this package
    - Fix Logger mock for the `SENSITIVE_DATA_REPLACER`
    - Replace the sensitive info in both the body and args
    - Make sure we reuse existing body and args if no masking require for better backward compatibility
    - Add unit test for commands with masking
    
    Review e2e tests to had a input with masking case
    
    Remove `.only` on tests
    
    Remove fail tentative
    
    Tentative fix for failure in `react.test.tsx`
    
    - react.test.tsx: SyntaxError: Importing binding name 'SENSITIVE_DATA_REPLACER' is not found

---
+++ packages/wdio-logger/tests/utils.test.ts
@@ -0,0 +1,199 @@
+import { it, describe, expect, vi, afterEach, beforeEach, afterAll } from 'vitest'
+import { mask, parseMaskingPatterns } from '../src/utils.js'
+
+describe('wdio-logger utils', () => {
+    describe('parseMaskingPatterns', () => {
+
+        it('should works when undefined', () => {
+            const patterns = parseMaskingPatterns(undefined)
+
+            expect(patterns).toEqual(undefined)
+        })
+
+        it('should skip when pattern is empty', () => {
+            const patterns = parseMaskingPatterns('')
+
+            expect(patterns).toEqual([])
+        })
+
+        it('should works when pattern has space', () => {
+            const patterns = parseMaskingPatterns(' ')
+
+            expect(patterns).toEqual([])
+        })
+
+        it('should parse one pattern with no flag', () => {
+            const stringPatterns = '/--key=[^ ]*/'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/--key=[^ ]*/])
+        })
+
+        it('should parse one pattern no flag and no slash', () => {
+            const stringPatterns = '--key=[^ ]*'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/--key=[^ ]*/])
+        })
+
+        it('should parse one pattern having 0 group and case insensitive', () => {
+            const stringPatterns = '/--key=[^ ]*/i'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/--key=[^ ]*/i])
+        })
+
+        it('should parse one pattern having 0 group and global flag', () => {
+            const stringPatterns = '/--key=[^ ]*/g'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/--key=[^ ]*/g])
+        })
+
+        it('should parse one pattern ex having 1 group', () => {
+            const stringPatterns = '/--key=([^ ]*)/g'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/--key=([^ ]*)/g])
+        })
+
+        it('should parse one pattern having 2 group', () => {
+            const stringPatterns = '(--key=)([^ ]*)'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/(--key=)([^ ]*)/])
+        })
+
+        it('should "parse" one pattern having more than 2 group', () => {
+            const stringPatterns = '(--key)(=)([^ ]*)'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/(--key)(=)([^ ]*)/])
+        })
+
+        it('should parse two patterns having 2 groups each', () => {
+            const stringPatterns = '(--key=)([^ ]*),/(TOKEN=)([^ ]*)/i'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/(--key=)([^ ]*)/, /(TOKEN=)([^ ]*)/i])
+        })
+
+        it('skipped regex when it is invalid', () => {
+            const stringPatterns = '(--key=)([^ ]*,/(TOKEN=)([^ ]*)/i'
+
+            const patterns = parseMaskingPatterns(stringPatterns)
+
+            expect(patterns).toEqual([/(TOKEN=)([^ ]*)/i])
+        })
+    })
+
+    describe('mask', () => {
+
+        it('should return the arg as it is when not a string', () => {
+            const arg = 123
+            const patterns = [/--key=[^ ]*/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual(arg)
+        })
+
+        it('should return the arg when undefined', () => {
+            const arg = undefined
+            const patterns = [/--key=[^ ]*/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual(arg)
+        })
+
+        it('should not mask when nothing to mask', () => {
+            const arg = 'mask nothing'
+            const patterns = [/--key=[^ ]*/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual(arg)
+        })
+
+        it('should mask when there is something to mask', () => {
+            const arg = '--key=mySecretKey'
+            const patterns = [/--key=[^ ]*/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('**MASKED**')
+        })
+
+        it('should mask and keep the rest of the string', () => {
+            const arg = 'before --key=mySecretKey after'
+            const patterns = [/--key=[^ ]*/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before **MASKED** after')
+        })
+
+        it('should mask when having one capturing group', () => {
+            const arg = 'before --key=mySecretKey after'
+            const patterns = [/--key=([^ ]*)/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before **MASKED** after')
+        })
+
+        it('should mask the second capturing group only when having 2 groups', () => {
+            const arg = 'before --key=mySecretKey after'
+            const patterns = [/(--key=)([^ ]*)/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before --key=**MASKED** after')
+        })
+
+        it('should "work" ish when having more than 2 groups', () => {
+            const arg = 'before --key=mySecretKey after'
+            const patterns = [/(--key)(=)([^ ]*)/]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before --key**MASKED** after')
+        })
+
+        it('should also work with global flag', () => {
+            const arg = 'before --key=mySecretKey after'
+            const patterns = [/(--key=)([^ ]*)/g]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before --key=**MASKED** after')
+        })
+
+        it('should masked multiple occurrences with the global flag', () => {
+            const arg = 'before --key=mySecretKey1 --key=mySecretKey2 after'
+            const patterns = [/(--key=)([^ ]*)/g]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before --key=**MASKED** --key=**MASKED** after')
+        })
+
+        it('should masked properly when having case insensitive flag', () => {
+            const arg = 'before --KEY=mySecretKey1 --key=mySecretKey2 after'
+            const patterns = [/(--key=)([^ ]*)/ig]
+
+            const maskedValue = mask(arg, patterns)
+
+            expect(maskedValue).toEqual('before --KEY=**MASKED** --key=**MASKED** after')
+        })
+    })
+})
