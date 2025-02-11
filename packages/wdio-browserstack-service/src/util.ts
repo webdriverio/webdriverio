4b21aa813fea978ecceb614a449865dccb989a25
Rounak Bhatia <rounak@browserstack.com>
2025-02-11 22:27:42 +0530

    WDIO-V8 [fixes for consoleLog patch] (#14164)
    * v8_consoleLog_patch_fix
    
    * Add debug log for skipped console methods in patchConsoleLogs

--- packages/wdio-browserstack-service/src/util.ts
+++ packages/wdio-browserstack-service/src/util.ts
@@ -1296,16 +1296,26 @@ export const patchConsoleLogs = o11yErrorHandler(() => {
     const BSTestOpsPatcher = new logPatcher({})
 
     Object.keys(consoleHolder).forEach((method: keyof typeof console) => {
+        if (!(method in console) || typeof console[method] !== 'function') {
+            BStackLogger.debug(`Skipping method: ${method}, exists: ${method in console}, type: ${typeof console[method]}`)
+            return
+        }
         const origMethod = (console[method] as any).bind(console)
 
         // Make sure we don't override Constructors
         // Arrow functions are not construable
-        if (typeof console[method] === 'function'
-            && method !== 'Console'
-        ) {
+        if (typeof console[method] === 'function' && method !== 'Console') {
             (console as any)[method] = (...args: unknown[]) => {
-                origMethod(...args);
-                (BSTestOpsPatcher as any)[method](...args)
+                try {
+                    if (!Object.keys(BSTestOpsPatcher).includes(method)) {
+                        origMethod(...args)
+                    } else {
+                        origMethod(...args);
+                        (BSTestOpsPatcher as any)[method](...args)
+                    }
+                } catch (error) {
+                    origMethod(...args)
+                }
             }
         }
     })
