eff60a2ec955f9cfca295b42304d57f611874962
wswebcreation <wim@wswebcreation.nl>
2025-02-20 19:55:02 +0100

    chore: update docs

--- packages/webdriverio/src/commands/mobile/restartApp.ts
+++ packages/webdriverio/src/commands/mobile/restartApp.ts
@@ -8,10 +8,19 @@ import type { ActiveAppInfo } from '../../types.js'
  * - launching the previously active app
  *
  * :::important
- * This command will restart (terminate/close and launch/start) the app and will not reset the app state. Appium can not perform a hard reset of
+ * This command will restart (terminate/close and launch/start) the app and will NOT reset the app state. Appium can not perform a hard reset of
  * the app unless:
  * - you start a new session and the session handler removes the app state/cleans the device
  * - you have a backdoor in your app to reset the app state and Appium can call this backdoor
+ *
+ * If you want to reset the app state for Android or iOS you need to create your own reset mechanism/command in your script. Options could be:
+ *
+ * - Android: Use the `adb` command to clear the app data: `adb shell pm clear <appPackage>`
+ * - iOS: reinstall the app using the `mobile: installApp` command
+ * - ....
+ *
+ * The options you have depend on the platform, the app and the location (local with most of the times full access to the device, or in the cloud with less access) you are testing.
+ *
  * :::
  *
  * <example>
