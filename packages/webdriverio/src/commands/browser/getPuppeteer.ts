dca87ee888d99e7bbfd2a9e5c517f22e60c3e936
Ian Renauld <ianrenauld@gmail.com>
2025-03-11 08:28:27 -0400

    Add mention that Chrome DevTools protocol is not install by default and give name of required package

--- packages/webdriverio/src/commands/browser/getPuppeteer.ts
+++ packages/webdriverio/src/commands/browser/getPuppeteer.ts
@@ -18,8 +18,9 @@ const DEBUG_PIPE_FLAG = 'remote-debugging-pipe'
  * :::info
  *
  * Note that using Puppeteer requires support for Chrome DevTools protocol and e.g.
- * can not be used when running automated tests in the cloud. Find out more in the
- * [Automation Protocols](/docs/automationProtocols) section.
+ * can not be used when running automated tests in the cloud. Chrome DevTools protocol is not installed in v9 by default,
+ * use `npm install puppeteer-core` to install it.
+ * Find out more in the [Automation Protocols](/docs/automationProtocols) section.
  *
  * :::
  *
