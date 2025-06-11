c0a446482266b09211d80f23e0559b679e7d7b55
rounak bhatia <rounak@browserstack.com>
2025-06-11 23:45:54 +0530

    setting caps received from binary

--- packages/wdio-browserstack-service/src/service.ts
+++ packages/wdio-browserstack-service/src/service.ts
@@ -34,6 +34,9 @@ import { HookState } from './cli/states/hookState.js'
 import { TestFrameworkState } from './cli/states/testFrameworkState.js'
 import TestFramework from './cli/frameworks/testFramework.js'
 import { TestFrameworkConstants } from './cli/frameworks/constants/testFrameworkConstants.js'
+import AutomationFramework from './cli/frameworks/automationFramework.js'
+import WebdriverIOModule from './cli/modules/webdriverIOModule.js'
+import AutomationFrameworkInstance from './cli/instances/automationFrameworkInstance.js'
 
 export default class BrowserstackService implements Services.ServiceInstance {
     private _sessionBaseUrl = 'https://api.browserstack.com/automate/sessions'
@@ -103,7 +106,7 @@ export default class BrowserstackService implements Services.ServiceInstance {
     }
 
     @PerformanceTester.Measure(PERFORMANCE_SDK_EVENTS.EVENTS.SDK_HOOK, { hookType: 'beforeSession' })
-    async beforeSession (config: Omit<Options.Testrunner, 'capabilities'>) {
+    async beforeSession (config: Omit<Options.Testrunner, 'capabilities'>, capabilities: WebdriverIO.Capabilities) {
         // if no user and key is specified even though a browserstack service was
         // provided set user and key with values so that the session request
         // will fail
@@ -128,6 +131,12 @@ export default class BrowserstackService implements Services.ServiceInstance {
             if (hubUrl) {
                 this._config.hostname = new URL(hubUrl).hostname
             }
+            if (BrowserstackCLI.getInstance().isRunning()) {
+                await BrowserstackCLI.getInstance().getAutomationFramework()!.trackEvent(AutomationFrameworkState.CREATE, HookState.PRE, { caps: this._caps })
+            }
+            const instance = AutomationFramework.getTrackedInstance() as AutomationFrameworkInstance
+            const caps = AutomationFramework.getState(instance, WebdriverIOModule.KEY_CAPABILITIES)
+            Object.assign(capabilities, caps)
         } catch (err) {
             BStackLogger.error(`Error while connecting to Browserstack CLI: ${err}`)
         }
@@ -165,9 +174,6 @@ export default class BrowserstackService implements Services.ServiceInstance {
 
         if (this._browser) {
             try {
-                if (BrowserstackCLI.getInstance().isRunning()) {
-                    await BrowserstackCLI.getInstance().getAutomationFramework()!.trackEvent(AutomationFrameworkState.CREATE, HookState.PRE, { caps })
-                }
                 const sessionId = this._browser.sessionId
                 if (isBrowserstackSession(this._browser)) {
                     try {
