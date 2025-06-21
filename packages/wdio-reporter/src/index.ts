ca8c5cc0f733fa5eba1b6934ec0ec78e5f82af29
David Prevost <david.prevost@goto.com>
2025-06-21 08:32:27 -0400

    Fix bad import

--- packages/wdio-reporter/src/index.ts
+++ packages/wdio-reporter/src/index.ts
@@ -11,8 +11,7 @@ import type { Hook } from './stats/hook.js'
 import HookStats from './stats/hook.js'
 import TestStats, { type Test } from './stats/test.js'
 import RunnerStats from './stats/runner.js'
-import type { AfterCommandArgs, BeforeCommandArgs, CommandArgs, CustomCommand, Tag, Argument } from './types.js'
-
+import type { AfterCommandArgs, BeforeCommand, BeforeCommandArgs, CommandArgs, CustomCommand, Tag, Argument } from './types.js'
 type CustomWriteStream = { write: (content: unknown) => boolean }
 
 export default class WDIOReporter extends EventEmitter {
@@ -313,6 +312,6 @@ function getBrowserName(caps: WebdriverIO.Capabilities) {
 }
 
 export {
-    SuiteStats, Tag, HookStats, TestStats, RunnerStats, BeforeCommandArgs,
+    SuiteStats, Tag, HookStats, TestStats, RunnerStats, BeforeCommand, BeforeCommandArgs,
     AfterCommandArgs, CommandArgs, CustomCommand, Argument, Test, getBrowserName
 }
