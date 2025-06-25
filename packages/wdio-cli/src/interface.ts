96fa82dd56c242e7b4b29a0a2d4dac6c36244101
Christian Bromann <git@bromann.dev>
2025-06-25 11:08:47 -0700

    fix(@wdio/jasmine-framework): become independant from expect-webdriverio

--- packages/wdio-cli/src/interface.ts
+++ packages/wdio-cli/src/interface.ts
@@ -49,13 +49,10 @@ export default class WDIOCLInterface extends EventEmitter {
     private _skippedSpecs = 0
     private _inDebugMode = false
     private _start = new Date()
-    private _messages: {
-        reporter: Record<string, string[]>
-        debugger: Record<string, string[]>
-    } = {
-            reporter: {},
-            debugger: {}
-        }
+    private _messages = {
+        reporter: {} as Record<string, string[]>,
+        debugger: {} as Record<string, string[]>
+    }
 
     constructor(
         private _config: WebdriverIO.Config,
--- packages/wdio-runner/src/types.ts
+++ packages/wdio-runner/src/types.ts
@@ -1,4 +1,6 @@
 import type { Capabilities, Services, Workers } from '@wdio/types'
+import type { getConfig, matchers } from 'expect-webdriverio'
+
 import type BaseReporter from './reporter.js'
 
 export type BeforeArgs = Parameters<Required<Services.HookFunctions>['before']>
@@ -30,6 +32,11 @@ export interface TestFramework {
     ) => TestFramework
     run (): Promise<number>
     hasTests (): boolean
+    setupExpect?: (
+        wdioExpect: ExpectWebdriverIO.Expect,
+        wdioMatchers: typeof matchers,
+        getExpectConfig: typeof getConfig
+    ) => void
 }
 
 export interface SessionStartedMessage {
