928af61f005e7c3c47eb54630d4cbd328cccb5bb
Christian Bromann <git@bromann.dev>
2025-07-08 11:04:49 -0700

    fix component test

--- e2e/browser-runner/lit.test.js
+++ e2e/browser-runner/lit.test.js
@@ -268,7 +268,7 @@ describe('Lit Component testing', () => {
 
         const err = await $('input').click().catch((err) => err)
         expect(err.name).toBe('webdriverio(middleware): element did not become interactable')
-        expect(err.message).toBe('Element <input style="display: none;"> did not become interactable')
+        expect(err.message).toBe('Element <input style="display: none;" /> did not become interactable')
     })
 
     it('intercepts "element not interactable" errors and waits for the element to be interactable', async () => {
