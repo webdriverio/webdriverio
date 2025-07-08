e887022cc455d1b290e7a04a41d68c7b0183186a
Dmytro 🇺🇦 <113467739+DQRI@users.noreply.github.com>
2025-07-09 02:13:45 +0300

    fix(@wdio/local-runner): added graceful exit on SIGINT (#14611)
    * fix(local-runner): use asyncExitHook for graceful shutdown
    
    * fix(local-runner): changed test naming and expected timeout
    
    ---------
    
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- packages/wdio-local-runner/src/run.ts
+++ packages/wdio-local-runner/src/run.ts
@@ -1,7 +1,7 @@
-import exitHook from 'exit-hook'
+import { asyncExitHook, gracefulExit } from 'exit-hook'
 
-import Runner from '@wdio/runner'
 import logger from '@wdio/logger'
+import Runner from '@wdio/runner'
 import type { Workers } from '@wdio/types'
 
 import { SHUTDOWN_TIMEOUT } from './constants.js'
@@ -27,7 +27,7 @@ if (typeof process.send === 'function') {
 }
 
 export const runner = new Runner() as unknown as RunnerInterface
-runner.on('exit', process.exit.bind(process))
+runner.on('exit', (code = 0) => gracefulExit(code))
 runner.on('error', ({ name, message, stack }) => process.send!({
     origin: 'worker',
     name: 'error',
@@ -56,16 +56,24 @@ process.on('message', (m: Workers.WorkerCommand) => {
     )
 })
 
-/**
- * catch sigint messages as they are handled by main process
- */
-exitHook(() => {
+process.once('SIGINT', () => {
     runner.sigintWasCalled = true
-    log.info(`Received SIGINT, giving process ${SHUTDOWN_TIMEOUT}ms to shutdown gracefully`)
-
-    return new Promise<void>((resolve) => {
-        setTimeout(() => {
-            resolve()
-        }, SHUTDOWN_TIMEOUT)
-    })
+    gracefulExit(130)
 })
+
+/**
+ * registers an asynchronous exit hook to allow graceful shutdown
+ * if the process is being terminated due to SIGINT, delays exit for SHUTDOWN_TIMEOUT ms to allow cleanup
+ * otherwise, exits immediately without delay
+ */
+asyncExitHook(
+    async () => {
+        if (runner.sigintWasCalled) {
+            log.info(`Received SIGINT, giving process ${SHUTDOWN_TIMEOUT}ms to shutdown gracefully`)
+
+            return await new Promise(res => setTimeout(res, SHUTDOWN_TIMEOUT))
+        }
+    }, {
+        wait: SHUTDOWN_TIMEOUT
+    }
+)
