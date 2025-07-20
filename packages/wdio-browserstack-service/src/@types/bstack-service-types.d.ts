44b7dccfce5f34a8bb663a83ab2d23bc19dfb0fb
AdityaHirapara <adityahirapara2016@gmail.com>
2025-07-20 16:14:58 +0530

    Implement support for start and stop scan methods for manual control

--- packages/wdio-browserstack-service/src/@types/bstack-service-types.d.ts
+++ packages/wdio-browserstack-service/src/@types/bstack-service-types.d.ts
@@ -2,12 +2,16 @@ declare namespace WebdriverIO {
     interface Browser {
         getAccessibilityResultsSummary: () => Promise<{ [key: string]: any; }>,
         getAccessibilityResults: () => Promise<Array<{ [key: string]: any; }>>,
-        performScan: () => Promise<{ [key: string]: any; } | undefined>
+        performScan: () => Promise<{ [key: string]: any; } | undefined>,
+        startA11yScanning: () => void,
+        stopA11yScanning: () => void
     }
 
     interface MultiRemoteBrowser {
         getAccessibilityResultsSummary: () => Promise<{ [key: string]: any; }>,
         getAccessibilityResults: () => Promise<Array<{ [key: string]: any; }>>,
-        performScan: () => Promise<{ [key: string]: any; } | undefined>
+        performScan: () => Promise<{ [key: string]: any; } | undefined>,
+        startA11yScanning: () => void,
+        stopA11yScanning: () => void
     }
 }
