8aff2cd899235ce0d60d1b06eee2ea1fd776e9b4
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2025-07-21 12:57:40 +0200

    fix(create-wdio): resolve templates from a single path (#14653)

--- packages/create-wdio/src/utils.ts
+++ packages/create-wdio/src/utils.ts
@@ -28,7 +28,7 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 process.on('SIGINT', () => printAndExit(undefined, 'SIGINT'))
 
-const TEMPLATE_ROOT_DIR = path.join(__dirname, 'templates', 'exampleFiles')
+const TEMPLATE_ROOT_DIR = path.join(__dirname, '..', 'templates', 'exampleFiles')
 
 export function runProgram (command: string, args: string[], options: SpawnOptions) {
     const child = spawn(command, args, { stdio: 'inherit', ...options })
