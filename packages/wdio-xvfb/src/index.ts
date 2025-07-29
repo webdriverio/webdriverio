9bd5adc6e92137e0e76b8e585dbca1a71532f602
Sam Maister <goosewobbler@protonmail.com>
2025-07-29 14:29:04 +0100

    chore(wdio-xvfb): update xbps install command

--- packages/wdio-xvfb/src/index.ts
+++ packages/wdio-xvfb/src/index.ts
@@ -209,7 +209,7 @@ export class XvfbManager {
             zypper: 'sudo zypper install -y xorg-x11-server-Xvfb xvfb-run',
             pacman: 'sudo pacman -S --noconfirm xorg-server-xvfb',
             apk: 'sudo apk add --no-cache xvfb-run',
-            xbps: 'sudo xbps-install -S xvfb',
+            xbps: 'sudo xbps-install -S xvfb-run',
         }
 
         const command = installCommands[packageManager]
