73d1b2cb36cf069207d47c544e7e1ea108221b6a
Bhargavi-BS <bhargavi@browserstack.com>
2025-09-17 20:05:34 +0530

    review changes

--- packages/wdio-browserstack-service/src/cli/modules/accessibilityModule.ts
+++ packages/wdio-browserstack-service/src/cli/modules/accessibilityModule.ts
@@ -214,7 +214,7 @@ export default class AccessibilityModule extends BaseModule {
             const browser = AutomationFramework.getDriver(autoInstance) as WebdriverIO.Browser
 
             (browser as any).startA11yScanning = async () => {
-                if (!this.accessibility || !this.isAppAccessibility){
+                if (!this.accessibility && !this.isAppAccessibility){
                     return
                 }
                 this.accessibilityMap.set(sessionId, true)
@@ -227,7 +227,7 @@ export default class AccessibilityModule extends BaseModule {
             }
 
             (browser as any).stopA11yScanning = async () => {
-                if (!this.accessibility || !this.isAppAccessibility){
+                if (!this.accessibility && !this.isAppAccessibility){
                     return
                 }
                 this.accessibilityMap.set(sessionId, false)
@@ -235,7 +235,7 @@ export default class AccessibilityModule extends BaseModule {
             }
 
             (browser as any).performScan = async () => {
-                if (!this.accessibility || !this.isAppAccessibility){
+                if (!this.accessibility && !this.isAppAccessibility){
                     return
                 }
                 const results = await this.performScanCli(browser)
