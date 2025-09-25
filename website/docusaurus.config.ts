02ace2f07919a95e9f85f018ad8d9ff27233318d
wswebcreation <wim@wswebcreation.nl>
2025-09-25 08:56:26 +0200

    chore: disable farsi

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
             'ar',
             'de',
             'es',
-            'fa',
+            // 'fa', => 3 backticks are added on line 5 which breaks the markdown parser
             'fr',
             'hi',
             'it',
