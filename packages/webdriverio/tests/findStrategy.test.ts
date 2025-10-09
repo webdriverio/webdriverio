ed0bf2980782c2623f7053718dc5612f488b3e87
Filype Pereira <868423+fpereira1@users.noreply.github.com>
2025-10-09 17:29:01 +1300

    fix tests

--- packages/webdriverio/tests/findStrategy.test.ts
+++ packages/webdriverio/tests/findStrategy.test.ts
@@ -551,6 +551,12 @@ describe('selector strategies helper', () => {
 
     it('should be able to fetch element by aria label', () => {
         const element = findStrategy('aria/foobar')
+        expect(element.using).toBe('xpath')
+        expect(element.value).toMatchSnapshot()
+    })
+
+    it('should be able to fetch element by aria label in Bidi', () => {
+        const element = findStrategy('aria/foobar', null, null, true)
         expect(element.using).toBe('aria')
         expect(element.value).toBe('foobar')
     })
