eb602e454a1b2a2cf473394e938a9100ae88264e
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-10-28 00:15:53 +0300

    refactor(webdriverio): turn `async.d.ts` into `async.ts` file for strict compiler checks

---
+++ packages/webdriverio/src/@types/async.ts
@@ -0,0 +1,57 @@
+/**
+ * This file need to WebdriverIO has its independent type definitions
+ * and does not require @wdio/globals to be available. This seems to be
+ * the only feasable way to avoid cyclic dependencies between webdriverio
+ * and @wdio/globals
+ */
+
+import type { Browser as BrowserImport } from '../types.js'
+import type { Element as ElementImport } from '../types.js'
+import type { MultiRemoteBrowser as MultiRemoteBrowserImport } from '../types.js'
+
+declare global {
+    namespace WebdriverIO {
+        interface Browser extends BrowserImport { }
+        interface Element extends ElementImport { }
+        interface MultiRemoteBrowser extends MultiRemoteBrowserImport { }
+    }
+
+    namespace NodeJS {
+        interface Global {
+            multiremotebrowser: WebdriverIO.MultiRemoteBrowser
+            browser: WebdriverIO.Browser
+            driver: WebdriverIO.Browser
+        }
+    }
+
+    function $(...args: Parameters<WebdriverIO.Browser['$']>): WebdriverIO.Element
+    function $$(...args: Parameters<WebdriverIO.Browser['$$']>): ReturnType<WebdriverIO.Browser['$$']>
+    const browser: WebdriverIO.Browser
+    const driver: WebdriverIO.Browser
+    const multiremotebrowser: WebdriverIO.MultiRemoteBrowser
+
+    /**
+    * custom environment primitives for WebdriverIO when running in a browser implemented
+    * as part of the `@wdio/browser-runner` package.
+    * @see `packages/wdio-browser-runner/src/browser/driver.ts`
+    */
+    var wdio: {
+        /**
+         * This command is available when running WebdriverIO tests using `@wdio/browser-runner`.
+         * It allows you to execute a command in Node.js land if desired.
+         * @param command The command to execute
+         * @param args The arguments to pass to the command
+         * @returns The result of the command
+         */
+        execute: <CommandName>(command: CommandName, ...args: any[]) => ReturnType<(WebdriverIO.Browser & WebdriverIO.Element)[CommandName]>
+        /**
+        * This command is available when running WebdriverIO tests using `@wdio/browser-runner`.
+        * It allows you to execute a command in Node.js land if desired.
+        * @param command The command to execute
+        * @param scope The element id when the command needs to be executed from an element scope
+        * @param args The arguments to pass to the command
+        * @returns The result of the command
+        */
+        executeWithScope: <CommandName>(commandName: CommandName, scope: string, ...args: any[]) => ReturnType<(WebdriverIO.Browser & WebdriverIO.Element)[CommandName]>
+    }
+}
