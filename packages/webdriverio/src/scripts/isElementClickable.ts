8f36593957031d0e2b475a33a1a16fc159cafafe
Mrunal chaudhari <mchaudhari@tjc-group.com>
2025-11-19 22:44:19 +0530

    fix(webdriverio): correct withinViewport check logic for isDisplayed command

--- packages/webdriverio/src/scripts/isElementClickable.ts
+++ packages/webdriverio/src/scripts/isElementClickable.ts
@@ -111,8 +111,8 @@ export default function isElementClickable (elem: HTMLElement) {
         const windowHeight = (window.innerHeight || document.documentElement.clientHeight)
         const windowWidth = (window.innerWidth || document.documentElement.clientWidth)
 
-        const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) > 0)
-        const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) > 0)
+        const vertInView = (rect.top < windowHeight) && ((rect.top + rect.height) > 0)
+        const horInView = (rect.left < windowWidth) && ((rect.left + rect.width) > 0)
 
         return (vertInView && horInView)
     }
