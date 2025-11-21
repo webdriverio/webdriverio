e94186978dc9de386261c25e106e9c25dae206d8
MRUNAL CHAUDHARI <mccmrunal@gmail.com>
2025-11-21 11:36:18 +0530

    fix(webdriverio): correct withinViewport check logic for isDisplayed command (#14872)

--- packages/webdriverio/src/scripts/isElementInViewport.ts
+++ packages/webdriverio/src/scripts/isElementInViewport.ts
@@ -16,8 +16,8 @@ export default function isElementInViewport (elem: HTMLElement) {
     const windowHeight = (window.innerHeight || document.documentElement.clientHeight)
     const windowWidth = (window.innerWidth || document.documentElement.clientWidth)
 
-    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) > 0)
-    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) > 0)
+    const vertInView = (rect.top < windowHeight) && ((rect.top + rect.height) > 0)
+    const horInView = (rect.left < windowWidth) && ((rect.left + rect.width) > 0)
 
     return (vertInView && horInView)
 }
