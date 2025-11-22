d5bf0aaefd9e36299d0fe11a1c511a734bbe2f21
Mrunal chaudhari <mccmrunal@gmail.com>
2025-11-22 21:36:18 +0530

    fix(wdio-cli): respect maxInstancesPerCapability for dynamic capabilities

--- __mocks__/@wdio/config.ts
+++ __mocks__/@wdio/config.ts
@@ -4,7 +4,7 @@ import {
     validateConfig as validateConfigMock
 } from '../../packages/wdio-config/src/utils.js'
 
-export { DEFAULT_CONFIGS } from '../../packages/wdio-config/src/constants.js'
+export { DEFAULT_CONFIGS, DEFAULT_MAX_INSTANCES_PER_CAPABILITY_VALUE } from '../../packages/wdio-config/src/constants.js'
 export const isCloudCapability = vi.fn().mockImplementation(isCloudCapabilityMock)
 export const validateConfig = vi.fn().mockImplementation((defaults, config) => {
     const returnVal = validateConfigMock(defaults, config)
