cc0820345d8d9a449f8938c571d169feb2fe8698
dprevost-perso <dprevost-github@outlook.com>
2026-01-24 11:17:37 -0500

    Fix problem of overload functions with `AsyncElementProto` + tests

--- tests/typings/webdriverio/commands.ts
+++ tests/typings/webdriverio/commands.ts
@@ -1,16 +1,23 @@
 import { expectTypeOf, describe, it } from 'vitest'
+import { $ } from '@wdio/globals'
 import type { RectReturn } from '@wdio/protocols'
 
-describe('WebdriverIO commands', () => {
-    const el = {} as WebdriverIO.Element
+describe('WebdriverIO commands', async () => {
+    const chainableEl = $('foo')
+    const el = await chainableEl.getElement()
 
     it('getSize', async () => {
-        // types validations for getSize command
         expectTypeOf(await el.getSize()).toEqualTypeOf<Pick<RectReturn, 'width' | 'height'>>()
         expectTypeOf(await el.getSize()).toEqualTypeOf<{height: number, width: number}>()
         expectTypeOf(await el.getSize('height')).toEqualTypeOf<number>()
         expectTypeOf(await el.getSize('width')).toEqualTypeOf<number>()
         expectTypeOf(await el.getSize('width')).not.toEqualTypeOf<{height: number, width: number} & number>()
+
+        expectTypeOf(await chainableEl.getSize()).toEqualTypeOf<Pick<RectReturn, 'width' | 'height'>>()
+        expectTypeOf(await chainableEl.getSize()).toEqualTypeOf<{height: number, width: number}>()
+        expectTypeOf(await chainableEl.getSize('height')).toEqualTypeOf<number>()
+        expectTypeOf(await chainableEl.getSize('width')).toEqualTypeOf<number>()
+        expectTypeOf(await chainableEl.getSize('width')).not.toEqualTypeOf<{height: number, width: number} & number>()
     })
 
     it('getLocation', async () => {
@@ -20,6 +27,13 @@ describe('WebdriverIO commands', () => {
         expectTypeOf(await el.getLocation('x')).toEqualTypeOf<number>()
         expectTypeOf(await el.getLocation('y')).toEqualTypeOf<number>()
         expectTypeOf(await el.getLocation()).not.toEqualTypeOf<{x: number, y: number} & number>()
+
+        expectTypeOf(await chainableEl.getLocation()).toEqualTypeOf<Pick<RectReturn, 'x' | 'y'>>()
+        expectTypeOf((await chainableEl.getLocation()).x).toEqualTypeOf<number>()
+        expectTypeOf((await chainableEl.getLocation()).y).toEqualTypeOf<number>()
+        expectTypeOf(await chainableEl.getLocation('x')).toEqualTypeOf<number>()
+        expectTypeOf(await chainableEl.getLocation('y')).toEqualTypeOf<number>()
+        expectTypeOf(await chainableEl.getLocation()).not.toEqualTypeOf<{x: number, y: number} & number>()
     })
 
     it('getAttribute', async () => {
