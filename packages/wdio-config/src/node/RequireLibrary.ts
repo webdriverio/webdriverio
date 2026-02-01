d2b7c3783ae28c5d7dc1bd4e940f5e34436faca9
MRUNAL CHAUDHARI <mccmrunal@gmail.com>
2026-02-01 12:18:10 +0530

    fix(wdio-config): restore jiti fallback for CJS named imports (#15044)

--- packages/wdio-config/src/node/RequireLibrary.ts
+++ packages/wdio-config/src/node/RequireLibrary.ts
@@ -1,7 +1,25 @@
+
+import createJITI from 'jiti'
+
 import type { ModuleImportService } from '../types.js'
 
 export default class RequireLibrary implements ModuleImportService {
+    #jiti = createJITI(import.meta.url)
+
     import<T>(module: string): Promise<T> {
-        return import(module) as Promise<T>
+        /**
+         * we try to import the module first, if it fails we use jiti to load the module
+         */
+        return import(module).catch((err) => {
+            /**
+             * if the module is a CJS module and the import fails with a syntax error
+             * it might be because the module does not export the named export
+             * we can use jiti to load the module which will handle the named export
+             */
+            if (err instanceof Error && err.message.includes('does not provide an export named')) {
+                return this.#jiti(module)
+            }
+            throw err
+        }) as Promise<T>
     }
 }
