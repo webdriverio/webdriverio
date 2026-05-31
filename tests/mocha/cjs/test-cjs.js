6b1c042d19d35b197a2aa76572072796bfd4903e
dprevost-perso <dprevost-github@outlook.com>
2026-05-31 12:13:42 -0400

    Add cjs test with logger import

--- tests/mocha/cjs/test-cjs.js
+++ tests/mocha/cjs/test-cjs.js
@@ -1,8 +1,22 @@
 const assert = require('node:assert')
 const path = require('node:path')
 
+import logger from '@wdio/logger'
+
 describe('a cjs test file', () => {
     it('should pass', () => {
         assert.equal(path.basename(__filename), 'test-cjs.js')
     })
+
+    it('should be able to use logger', async () => {
+        const user = {
+            name: 'tomsmith',
+            password: 'SuperSecretPassword!'
+        }
+
+        // logging as per https://github.com/webdriverio/webdriverio/tree/main/packages/wdio-logger:
+        const log = logger('My Login Test')
+        log.info('logging in with user:', user)
+    })
 })
+
