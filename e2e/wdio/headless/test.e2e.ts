d98f05fb0dd6b295a60d81bacc6d8ef72c140e54
dprevost-perso <dprevost-github@outlook.com>
2026-09-15 06:39:02 -0400

    Remove lighthouse from e2e

--- e2e/wdio/headless/test.e2e.ts
+++ e2e/wdio/headless/test.e2e.ts
@@ -1,5 +1,3 @@
-/// <reference types="@wdio/lighthouse-service" />
-
 import fs from 'node:fs/promises'
 import os from 'node:os'
 import url from 'node:url'
@@ -148,33 +146,6 @@ describe('main suite 1', () => {
 
             await browser.setViewport(viewport)
         })
-    })
-
-    describe.skip('Lighthouse Service Performance Testing capabilities', () => {
-        before(() => browser.enablePerformanceAudits())
-
-        it('should allow to do performance tests', async () => {
-            await browser.url('http://json.org')
-            const metrics = await browser.getMetrics()
-            expect(typeof metrics.serverResponseTime).toBe('number')
-            expect(typeof metrics.domContentLoaded).toBe('number')
-            expect(typeof metrics.firstVisualChange).toBe('number')
-            expect(typeof metrics.firstPaint).toBe('number')
-            expect(typeof metrics.firstContentfulPaint).toBe('number')
-            expect(typeof metrics.firstMeaningfulPaint).toBe('number')
-            expect(typeof metrics.largestContentfulPaint).toBe('number')
-            expect(typeof metrics.lastVisualChange).toBe('number')
-            expect(typeof metrics.interactive).toBe('number')
-            expect(typeof metrics.load).toBe('number')
-            expect(typeof metrics.speedIndex).toBe('number')
-            expect(typeof metrics.totalBlockingTime).toBe('number')
-            expect(typeof metrics.maxPotentialFID).toBe('number')
-            expect(typeof metrics.cumulativeLayoutShift).toBe('number')
-            const score = await browser.getPerformanceScore()
-            expect(typeof score).toBe('number')
-        })
-
-        after(() => browser.disablePerformanceAudits())
     })
 
     it.skip('should be able to scroll up and down', async () => {
