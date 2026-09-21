0a7018fa05b22d7b52c150f0c37e583c2c93daf5
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-09-21 15:07:03 +0300

    docs: update MJSONWP deprecation marks

--- packages/wdio-protocols/src/protocols/mjsonwp.ts
+++ packages/wdio-protocols/src/protocols/mjsonwp.ts
@@ -3,7 +3,7 @@ export default {
         GET: {
             command: 'getPageIndex',
             ref: 'https://github.com/appium/appium-base-driver/blob/master/docs/mjsonwp/protocol-methods.md#mobile-json-wire-protocol-endpoints',
-            deprecated: 'In Appium 2.0, this method is marked as deprecated and currently has no available alternatives.',
+            deprecated: 'In Appium 2.0, this method is marked as deprecated and currently has no available alternatives. No longer supported since Appium 3.',
             parameters: [],
             returns: {
                 type: 'string',
@@ -15,7 +15,7 @@ export default {
         GET: {
             command: 'getNetworkConnection',
             ref: 'https://github.com/SeleniumHQ/mobile-spec/blob/master/spec-draft.md#device-modes',
-            deprecated: 'For Appium Android automation, use `driver.execute(\'mobile: getConnectivity\', { ... })` instead',
+            deprecated: 'For Appium Android automation, use `driver.execute(\'mobile: getConnectivity\', { ... })` instead. No longer supported since Appium 4.',
             parameters: [],
             returns: {
                 type: 'number',
@@ -27,7 +27,7 @@ export default {
         POST: {
             command: 'setNetworkConnection',
             ref: 'https://github.com/SeleniumHQ/mobile-spec/blob/master/spec-draft.md#device-modes',
-            deprecated: 'For Appium Android automation, use `driver.execute(\'mobile: setConnectivity\', { ... })` instead',
+            deprecated: 'For Appium Android automation, use `driver.execute(\'mobile: setConnectivity\', { ... })` instead. No longer supported since Appium 4.',
             parameters: [
                 {
                     name: 'type',
@@ -43,7 +43,7 @@ export default {
         GET: {
             command: 'touchPerform',
             ref: 'https://github.com/SeleniumHQ/mobile-spec/blob/master/spec-draft.md#touch-gestures',
-            deprecated: 'For Appium, use `driver.performActions(...)` instead',
+            deprecated: 'For Appium, use `driver.performActions(...)` instead. No longer supported since Appium 3.',
             parameters: [
                 {
                     name: 'actions',
@@ -59,7 +59,7 @@ export default {
         GET: {
             command: 'multiTouchPerform',
             ref: 'https://github.com/SeleniumHQ/mobile-spec/blob/master/spec-draft.md#touch-gestures',
-            deprecated: 'For Appium, use `driver.performActions(...)` instead',
+            deprecated: 'For Appium, use `driver.performActions(...)` instead. No longer supported since Appium 3.',
             parameters: [
                 {
                     name: 'actions',
@@ -81,7 +81,7 @@ export default {
         POST: {
             command: 'receiveAsyncResponse',
             ref: 'https://github.com/appium/appium-base-driver/blob/master/docs/mjsonwp/protocol-methods.md#mobile-json-wire-protocol-endpoints',
-            deprecated: 'For Appium, use `driver.executeAsyncScript(...)` instead',
+            deprecated: 'For Appium, use `driver.executeAsyncScript(...)` instead. No longer supported since Appium 4.',
             parameters: [
                 {
                     name: 'status',
